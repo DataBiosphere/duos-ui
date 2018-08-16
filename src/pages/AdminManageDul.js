6dafb65ec6161f3a6a5ce414763741935d807726
Diego Gil <diego.a.gil@gmail.com>
2018-08-16 17:09:50 -0300

    adding converted pages (#1)

--- src/pages/AdminManageDul.js
+++ src/pages/AdminManageDul.js
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import { div } from 'react-hyperscript-helpers';
+import { div, button, hr, span, i, a, input, } from 'react-hyperscript-helpers';
 import { PageHeading } from '../components/PageHeading';
 
 class AdminManageDul extends Component {
@@ -19,11 +19,79 @@ class AdminManageDul extends Component {
 
     render() {
         return (
+
             div({ className: "container" }, [
                 div({ className: "row no-margin" }, [
+                    // TBD PageHeading goes here ...
                     div({ className: "col-lg-7 col-md-7 col-sm-12 col-xs-12 no-padding" }, [
                         PageHeading({ imgSrc: "../images/icon_manage_dul.png", iconSize: "medium", color: "dul-color", title: "Manage Data Use Limitations", description: "Select and manage Data Use Limitations for DAC review" }),
                     ]),
+                    div({ className: "col-lg-5 col-md-5 col-sm-12 col-xs-12 search-reviewed no-padding" }, [
+                        div({ className: "col-lg-6 col-md-6 col-sm-7 col-xs-7" }, [
+                            div({ className: "search-text" }, [
+                                i({ className: "glyphicon glyphicon-search dul-color" }),
+                                input({ type: "search", className: "form-control users-search", placeholder: "Enter search term...", "value": "searchDUL" }),
+                            ]),
+                        ]),
+                        a({ className: "col-lg-6 col-md-6 col-sm-5 col-xs-5 admin-add-button dul-background no-margin", onClick: "AdminManage.addDul()" }, [
+                            div({ className: "all-icons add-dul_white" }, []),
+                            span({}, ["Add Data Use Limitations"]),
+                        ]),
+                    ]),
+                ]),
+
+                div({ className: "jumbotron box-vote-singleresults box-vote-no-margin" }, [
+                    div({ className: "grid-9-row pushed-2" }, [
+                        div({ className: "col-2 cell-header dul-color" }, ["Consent id"]),
+                        div({ className: "col-2 cell-header dul-color" }, ["Consent Group Name"]),
+                        div({ className: "col-1 cell-header dul-color" }, ["Election N°"]),
+                        div({ className: "col-1 cell-header dul-color" }, ["Date"]),
+                        div({ className: "col-1 cell-header f-center dul-color" }, ["Edit Record"]),
+                        div({ className: "col-1 cell-header f-center dul-color" }, ["Election status"]),
+                        div({ className: "col-1 cell-header f-center dul-color" }, ["Election actions"]),
+                    ]),
+
+                    hr({ className: "pvotes-main-separator" }),
+                    div({ "dir-paginate": "election in AdminManage.electionsList.dul | filter: searchDUL | itemsPerPage:10", "current-page": "AdminManage.currentDULPage" }, [
+                        div({ className: "grid-9-row pushed-2", "ng-class": "{'list-highlighted': election.updateStatus}" }, [
+                            div({ className: "col-2 cell-body text", "ng-class": "{flagged : election.archived}", title: "this.election.consentName " }, [
+                                span({ isRendered: "election.updateStatus", className: "glyphicon glyphicon-exclamation-sign list-highlighted-item dul-color", "tooltip": "Consent has been updated", "aria-hidden": "true", "tooltip-class": "tooltip-class", "tooltip-trigger": "true", "tooltip-placement": "right" }, []),
+                                a({ onClick: "AdminManage.open(election.consentId, 'dul_preview_results', null, true)" }, ["this.election.consentName"]),
+                            ]),
+                            div({ className: "col-2 cell-body text", "ng-class": "{empty : !election.groupName}", title: "this.election.groupName " }, ["this.election.groupName"]),
+                            div({ className: "col-1 cell-body text", "ng-class": "{empty : !election.version}" }, ["this.election.version "]),
+                            div({ className: "col-1 cell-body text" }, ["this.election.createDate | date:'yyyy-MM-dd' "]),
+                            div({ className: "col-1 cell-body f-center" }, [
+                                button({ className: "cell-button hover-color", "ng-disabled": "election.electionStatus !: 'un-reviewed' || !election.editable", onClick: "AdminManage.editDul(election.consentId)" }, ["Edit"]),
+                            ]),
+                            div({ className: "col-1 cell-body text f-center bold" }, [
+                                span({ isRendered: "election.electionStatus :: 'un-reviewed'" }, [a({ onClick: "AdminManage.open(election.consentId, 'dul_preview_results', null, false)" }, ["Un-reviewed"])]),
+                                span({ isRendered: "election.electionStatus :: 'Open'" }, [a({ onClick: "AdminManage.open(election.consentId, 'dul_review_results', null, false)" }, ["Open"]),]),
+                                span({ isRendered: "election.electionStatus :: 'Canceled'" }, [a({ onClick: "AdminManage.open(election.consentId, 'dul_preview_results', null, false)" }, ["Canceled"]),]),
+                                span({ isRendered: "election.electionStatus :: 'Closed'" }, [a({ onClick: "AdminManage.open(null, 'dul_results_record', election.electionId, false)" }, ["Reviewed"]),]),
+                            ]),
+                            div({ className: "col-1 cell-body f-center" }, [
+                                button({ isRendered: "election.electionStatus !: 'Open'", "ng-disabled": "!election.editable", className: "cell-button hover-color", onClick: "AdminManage.openCreate(election)" }, ["Create"]),
+                                button({ isRendered: "election.electionStatus :: 'Open'", className: "cell-button cancel-color", onClick: "AdminManage.openCancel(election)" }, ["Cancel"]),
+                            ]),
+                            div({ className: "icon-actions" }, [
+                                button({ "ng-disabled": "election.electionStatus :: 'un-reviewed' || election.archived", onClick: "AdminManage.openArchive(election)" }, [
+                                    span({ className: "glyphicon glyphicon-inbox caret-margin", "ng-class": "{activated : election.archived}", "aria-hidden": "true", "tooltip-class": "tooltip-class", "tooltip-trigger": "true", "tooltip-placement": "right", "tooltip": "Archive election" }, []),
+                                ]),
+                                button({ "ng-disabled": "election.electionStatus !: 'un-reviewed'", onClick: "AdminManage.openDelete(election.consentId)" }, [
+                                    span({ className: "glyphicon glyphicon-trash caret-margin", "aria-hidden": "true", "tooltip-class": "tooltip-class", "tooltip-trigger": "true", "tooltip-placement": "right", "tooltip": "Delete record" }, []),
+                                ]),
+                            ]),
+                        ]),
+                        hr({ className: "pvotes-separator" }),
+                        div({
+                            "dir-pagination-controls": "true"
+                            , "max-size": "10"
+                            , "direction-links": "true"
+                            , "boundary-links": "true"
+                            , className: "pvotes-pagination"
+                        }, []),
+                    ])
                 ])
             ])
         );
--- src/pages/DatasetCatalog.js
+++ src/pages/DatasetCatalog.js
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import { div, button, hr } from 'react-hyperscript-helpers';
+import { div, button, table, thead, tbody, th, tr, td, form, hr, input, label, i, span, a, p } from 'react-hyperscript-helpers';
 import { PageHeading } from '../components/PageHeading';
 
 class DatasetCatalog extends Component {
@@ -19,15 +19,158 @@ class DatasetCatalog extends Component {
 
     render() {
         return (
+
             div({ className: "container" }, [
                 div({ className: "row no-margin" }, [
-                    div({ className: "col-lg-7 col-md-7 col-sm-12 col-xs-12 no-padding" }, [
-                        PageHeading({ imgSrc: "../images/icon_dataset_.png", iconSize: "large", color: "dataset-color", title: "Dataset Catalog", description: "Datasets with an associated DUL to apply for secondary use" }),
-                    ]),
-                ]),
-                hr({ className: "section-separator" }),
 
-                button({}, ["Click Me!"])
+                    div({ className: "row no-margin" }, [
+                        div({ className: "col-lg-7 col-md-7 col-sm-12 col-xs-12 no-padding" }, [
+                            PageHeading({ imgSrc: "../images/icon_dataset_.png", iconSize: "large", color: "dataset-color", title: "Dataset Catalog", description: "Datasets with an associated DUL to apply for secondary use" }),
+                        ]),
+                        div({ className: "col-lg-5 col-md-5 col-sm-12 col-xs-12 search-reviewed no-padding" }, [
+                            div({ className: "col-lg-6 col-md-6 col-sm-7 col-xs-7" }, [
+                                div({ className: "search-text" }, [
+                                    i({ className: "glyphicon glyphicon-search dataset-color" }),
+                                    input({
+                                        type: "search", className: "form-control users-search", placeholder: "Enter search term..."
+                                        , "value": "searchDataset"
+                                    })
+                                ]),
+                            ]),
+                            button({
+                                download: "", "ng-disabled": "objectIdList.length === 0", onClick: "DatasetCatalog.download(objectIdList)",
+                                className: "col-lg-6 col-md-6 col-sm-5 col-xs-5 download-button dataset-background"
+                            }, [
+                                    span({}, ["Download selection"]),
+                                    span({ className: "cm-icon-button glyphicon glyphicon-download caret-margin", "aria-hidden": "true" }, []),
+                                ]),
+                        ]),
+                    ]),
+                    hr({ className: "section-separator" }),
+                ]),
+                div({ className: "table-wrap" }, [
+                    form({ className: "pos-relative" }, [
+                        div({ className: "checkbox check-all" }, [
+                            input({ type: "checkbox", "select-all": "true", className: "checkbox-inline", id: "all" }),
+                            label({ className: "regular-checkbox", for: "all" }, []),
+                        ]),
+                        div({ "ng-class": "isAdmin && !isResearcher ? 'table-scroll-admin' : 'table-scroll'" }, [
+                            table({ className: "table" }, [
+                                thead({}, [
+                                    tr({}, [
+                                        th({}, []),
+                                        th({
+                                            "ng-repeat": "dictionary in DatasetCatalog.dataSetList.dictionary",
+                                            className: "table-titles dataset-color cell-size"
+                                        }, ["this.dictionary.key "]),
+                                        th({ className: "table-titles dataset-color cell-size" }, ["ConsentId"]),
+                                        th({ className: "table-titles dataset-color cell-size" }, ["Structured Data Use Limitations"]),
+                                        th({ "ng-show": "isAdmin", className: "table-titles dataset-color cell-size" }, ["Approved Requestors"]),
+                                    ]),
+                                ]),
+                                tbody({}, [
+                                    tr({
+                                        "dir-paginate": "dataSet in DatasetCatalog.dataSetList.catalog | filter: searchDataset | itemsPerPage:10"
+                                        , "current-page": "pagination.current"
+                                    }, [
+                                            td({ "ng-repeat": "property in dataSet.properties", isRendered: "property.propertyName ==='Dataset ID'" }, [
+                                                div({ className: "checkbox" }, [
+                                                    input({
+                                                        type: "checkbox", id: "this.property.propertyValue "
+                                                        , "value": "checkMod['field_' + pagination.current + $parent.$parent.$index]"
+                                                        , className: "checkbox-inline user-checkbox", "add-object-id": "true"
+                                                    }),
+                                                    label({
+                                                        className: "regular-checkbox rp-choice-questions"
+                                                        , for: "this.property.propertyValue "
+                                                    }, []),
+                                                ]),
+                                            ]),
+                                            td({ className: "remove-datasets-col", "ng-repeat": "property in dataSet.properties", isRendered: "property.propertyName ==='Dataset ID' && isAdmin && !isResearcher" }, [
+                                                a({
+                                                    isRendered: "dataSet.deletable && isAdmin && !isResearcher", onClick: "DatasetCatalog.openDelete(property.propertyValue)"
+                                                    , tooltip: "Delete dataset", "tooltip-class": "tooltip-class", "tooltip-trigger": "true",
+                                                    "tooltip-placement": "right"
+                                                }, [
+                                                        span({
+                                                            className: "cm-icon-button glyphicon glyphicon-trash caret-margin default-color",
+                                                            "aria-hidden": "true"
+                                                        }, []),
+                                                    ]),
+                                                a({ isRendered: "!dataSet.deletable && isAdmin && !isResearcher" }, [
+                                                    span({
+                                                        className: "cm-icon-button glyphicon glyphicon-trash caret-margin dismiss-color",
+                                                        "aria-hidden": "true"
+                                                    }, []),
+                                                ]),
+                                                div({ className: "disable-datasets-col" }, [
+                                                    a({ isRendered: "dataSet.active && isAdmin && !isResearcher", onClick: "DatasetCatalog.openDisable(property.propertyValue)", "tooltip": "Disable dataset", "tooltip-class": "tooltip-class", "tooltip-trigger": "true", "tooltip-placement": "right" }, [
+                                                        span({
+                                                            className: "cm-icon-button glyphicon glyphicon-ok-circle caret-margin dataset-color",
+                                                            "aria-hidden": "true"
+                                                        }, []),
+                                                    ]),
+                                                    a({
+                                                        isRendered: "!dataSet.active && isAdmin && !isResearcher", onClick: "DatasetCatalog.openEnable(property.propertyValue)", "tooltip": "Enable dataset"
+                                                        , "tooltip-className": "tooltip-class", "tooltip-trigger": "true", "tooltip-placement": "right"
+                                                    }, [
+                                                            span({
+                                                                className: "cm-icon-button glyphicon glyphicon-ban-circle caret-margin cancel-color",
+                                                                "aria-hidden": "true"
+                                                            }, []),
+                                                        ]),
+                                                ]),
+                                                div({ className: "approval-datasets-col" }, [
+                                                    a({ isRendered: "isAdmin && !isResearcher", onClick: "DatasetCatalog.associate(property.propertyValue, dataSet.needsApproval)", "tooltip": "Connect with Data Owner", "tooltip-class": "tooltip-class", "tooltip-trigger": "true", "tooltip-placement": "right" }, [
+                                                        span({ "ng-class": "dataSet.isAssociatedToDataOwners ? 'cm-icon-button glyphicon glyphicon-link caret-margin dataset-color' : 'cm-icon-button glyphicon glyphicon-link caret-margin default-color'", "aria-hidden": "true" }, []),
+                                                    ]),
+                                                ]),
+                                            ]),
+
+                                            td({ "ng-repeat": "property in dataSet.properties", className: "table-items cell-size", "ng-class": "{'dataset-disabled': !dataSet.active}" }, [
+                                                p({ isRendered: "property.propertyName !=='dbGAP' " }, ["this.property.propertyValue"]),
+                                                a({
+                                                    href: "this.property.propertyValue ", target: "_blank"
+                                                    , isRendered: "property.propertyName ==='dbGAP' && property.propertyValue.length }), 0  "
+                                                    , className: "enabled"
+                                                }, ["Link"]),
+                                                a({
+                                                    href: "this.property.propertyValue "
+                                                    , isRendered: "property.propertyName ==='dbGAP' && property.propertyValue.length === 0   "
+                                                    , className: "disabled"
+                                                }, ["Link"]),
+                                                td({ className: "table-items cell-size", "ng-class": "{'dataset-disabled': !dataSet.active}" }, ["this.dataSet.consentId "]),
+                                                td({ className: "table-items cell-size translated-restriction hover-color", "ng-class": "{'dataset-disabled': !dataSet.active}", onClick: "DatasetCatalog.showSdul(dataSet.translatedUseRestriction)" }, [span({ style: { "cursor": "pointer" } }, ["Translated Use Restriction"]),
+                                                ]),
+
+                                                td({ "ng-show": "isAdmin", className: "table-items cell-size translated-restriction hover-color", onClick: "DatasetCatalog.downloadList(dataSet)" }, [span({ style: { "cursor": "pointer" } }, ["Download List"]),
+                                                ]),
+                                            ]),
+                                        ]),
+                                ]),
+                            ]),
+                        ]),
+
+                        div({
+                            className: "pvotes-pagination dataset-pagination", "dir-pagination-controls": "true"
+                            , "max-size": "10"
+                            , "direction-links": "true"
+                            , "boundary-links": "true"
+                        }, [
+                            ]),
+                    ]),
+                    div({ className: "f-right" }, [
+                        button({
+                            "ng-show": "isResearcher",
+                            "ng-disabled": "objectIdList.length === 0",
+                            onClick: "DatasetCatalog.exportToRequest(objectIdList)",
+                            className: "download-button dataset-background apply-dataset",
+                            "ng-class": "{'disabled': objectIdList.length === 0}",
+                            tooltip: "Request Access for selected Datasets", "tooltip-class": "tooltip-class", "tooltip-trigger": "true",
+                            "tooltip-placement": "top", "tooltip-animation": "false"
+                        }, ["Apply for Access"]),
+                    ]),
+                ])
             ])
         );
     }
