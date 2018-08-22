85a9a7d5752f0fb847251de988f69073873d7fd1
leoforconesi <lforcone@broadinstitute.org>
2018-08-22 11:04:56 -0300

    modify header on login/logout

--- src/components/DuosHeader.js
+++ src/components/DuosHeader.js
@@ -92,10 +92,11 @@ class DuosHeader extends Component {
                                 li({}, [a({ className: "navbar-duos-link", href: "/home_about" }, [div({ className: "navbar-duos-icon navbar-duos-icon-about" }, []), "About"]),]),
                                 li({}, [a({ className: "navbar-duos-link", href: "/home_help" }, [div({ className: "navbar-duos-icon navbar-duos-icon-help" }, []), "Help"]),]),
                                 li({}, [
-                                    GoogleLoginButton({isLogged:this.isLogged, loginState:this.loginState}),
+
+                                    a({onClick: this.signIn}, [GoogleLoginButton({isLogged:this.isLogged, loginState:this.loginState})]),
                                     GoogleLogoutButton({isLogged:this.isLogged, loginState:this.loginState}),
                                     // a({ className: "navbar-duos-button", href: '/login' }, ["Sign In"])
-                                    a({ className: "navbar-duos-button", onClick: this.signIn }, ["Sign In"])
+                                    // a({ className: "navbar-duos-button", onClick: this.signIn }, ["Sign In"])
                                 ]),
                                 li({}, [a({ className: "navbar-duos-link-join", href: "/home_register" }, ["Join DUOS"]),]),
                             ]),
