51de187030616bd916acd18404bcd7cae57dd714
Tadeo Riveros Koloszwa <tadeoriverosk@gmail.com>
2018-08-27 17:32:01 -0300

    merge

--- src/components/DuosHeader.js
+++ src/components/DuosHeader.js
@@ -100,16 +100,146 @@ class DuosHeader extends Component {
                 li({}, [a({ className: "navbar-duos-link", href: "/home_help" }, [div({ className: "navbar-duos-icon navbar-duos-icon-help" }, []), "Help"]),]),
                 li({}, [
 
-                  a({onClick: this.signIn}, [this.state.googleButton])
-                  // a({ className: "navbar-duos-button", href: '/login' }, ["Sign In"])
-                  // a({ className: "navbar-duos-button", onClick: this.signIn }, ["Sign In"])
-                ]),
-                li({}, [a({ className: "navbar-duos-link-join", href: "/home_register" }, ["Join DUOS"]),]),
-              ]),
+    render() {
 
-              ul({ isRendered: isLogged, className: "navbar-left no-margin" }, [
-                li({ isRendered: isChairPerson }, [
-                  a({ href: "/chair_console" }, ["DAC Console"]),
+        // let roles = ['chairperson', 'member', 'admin', 'researcher', 'dataOwner', 'alumni'];
+        let roles = ['admin', 'researcher'];
+        let isChairPerson = false;
+        let isMember = false;
+        let isAdmin = false;
+        let isResearcher = false;
+        let isDataOwner = false;
+        let isAlumni = false;
+
+        // let isLogged = false;
+        const { isLogged } = this.state;
+
+        if (isLogged) {
+            roles.forEach(role => {
+                if (role === 'chairperson') { isChairPerson = true; }
+                if (role === 'member') { isMember = true; }
+                if (role === 'admin') { isAdmin = true; }
+                if (role === 'researcher') { isResearcher = true; }
+                if (role === 'dataOwner') { isDataOwner = true; }
+                if (role === 'alumni') { isAlumni = true; }
+            });
+        }
+        let currentUser = {};
+        if (isLogged) {
+          currentUser = {
+            displayName: "Tadeo",
+            email: 'tadeo@mail.com'
+          };
+        }
+        // if (isLogged && sessionStorage.getItem("GAPI") !== null) {
+        //     currentUser = {
+        //         displayName: JSON.parse(sessionStorage.getItem("GAPI")).profileObj.name,
+        //         email: JSON.parse(sessionStorage.getItem("GAPI")).profileObj.email
+        //     };
+        // }
+        return (
+
+            nav({ className: "navbar top-navigator-bar", role: "navigation", "ng-controller": "Header as Header" }, [
+                div({ className: "row top-navigator" }, [
+                    div({ className: "navbar-header" }, [
+                        button({ type: "button", className: "navbar-toggle", onClick: this.toggleNavBar }, [
+                            span({ className: "sr-only" }, ["Toggle navigation"]),
+                            span({ className: "icon-bar" }, []),
+                            span({ className: "icon-bar" }, []),
+                            span({ className: "icon-bar" }, []),
+                        ]),
+
+                        a({ href: "/home", className: "navbar-duos-brand" }, [
+                            img({ src: "/images/duos_logo.svg", alt: "DUOS Logo" }),
+                        ]),
+                    ]),
+
+                    div({ className: "collapse navbar-collapse no-padding", collapse: "navbarCollapsed" }, [
+
+                        ul({ isRendered: isLogged, className: "navbar-right no-margin" }, [
+                            li({ className: "dropdown user-li", "is-open": "status.isopen" }, [
+                                a({ role: "button", className: "dropdown-toggle", "data-toggle": "dropdown", "ng-disabled": "disabled" }, [
+                                    div({ id: "dacUser" }, [
+                                        currentUser.displayName,
+                                        span({ className: "caret caret-margin" }, []),]),
+                                    small({ id: "dacUserMail" }, [currentUser.email]),
+                                ]),
+                                ul({ className: "dropdown-menu user-dropdown", role: "menu" }, [
+                                    li({ isRendered: isResearcher }, [a({ href: "/researcher_profile" }, ["Your Profile"]),]),
+                                    li({}, [a({ onClick: this.signOut }, ["Sign out"]),]),
+                                ]),
+                            ]),
+                        ]),
+
+                        div({ className: "collapse navbar-collapse no-padding col-lg-10 col-md-10 col-sm-9 col-xs-12 navbar-right", collapse: "navbarCollapsed" }, [
+
+                            ul({ isRendered: !isLogged, className: "no-margin" }, [
+                                li({}, [a({ className: "navbar-duos-link", href: "/home_about" }, [div({ className: "navbar-duos-icon navbar-duos-icon-about" }, []), "About"]),]),
+                                li({}, [a({ className: "navbar-duos-link", href: "/home_help" }, [div({ className: "navbar-duos-icon navbar-duos-icon-help" }, []), "Help"]),]),
+                                li({}, [
+
+                                    // a({onClick: this.signIn}, [GoogleLoginButton({isLogged:this.isLogged, loginState:this.loginState})]),
+                                    // GoogleLogoutButton({isLogged:this.isLogged, loginState:this.loginState}),
+                                    // a({ className: "navbar-duos-button", href: '/login' }, ["Sign In"]),
+                                    a({ className: "navbar-duos-button", onClick: this.signIn }, ["Sign In"])
+                                ]),
+                                li({}, [a({ className: "navbar-duos-link-join", href: "/home_register" }, ["Join DUOS"]),]),
+                            ]),
+
+                            ul({ isRendered: isLogged, className: "navbar-left no-margin" }, [
+                                li({ isRendered: isChairPerson }, [
+                                    a({ href: "/chair_console" }, ["DAC Console"]),
+                                ]),
+
+                                li({ isRendered: isMember }, [
+                                    a({ href: "/user_console" }, ["DAC Console"]),
+                                ]),
+
+                                li({ isRendered: isAdmin }, [
+                                    a({ href: "/admin_console" }, ["Admin Console"]),
+                                ]),
+
+                                li({ isRendered: isResearcher }, [
+                                    a({ href: "/researcher_console" }, ["Researcher Console"]),
+                                ]),
+
+                                li({ isRendered: isDataOwner }, [
+                                    a({ href: "/data_owner_console" }, ["Data Owner Console"]),
+                                ]),
+
+                                li({ isRendered: isResearcher }, [
+                                    a({ onClick: this.goToRP }, ["Request Application"]),
+                                ]),
+
+                                li({ className: "dropdown", onToggle: this.toggled, isRendered: isLogged }, [
+                                    a({ role: "button", className: "dropdown-toggle", "data-toggle": "dropdown" }, [
+                                        div({}, ["Statistics", span({ className: "caret caret-margin" }, []),]),
+                                    ]),
+                                    ul({ className: "dropdown-menu user-dropdown", role: "menu" }, [
+                                        li({}, [a({ href: "/summary_votes", className: "f-left" }, ["Votes Statistics"]),]),
+                                        hr({}),
+                                        li({ isRendered: true }, [
+                                            a({ "ui-sref": "reviewed_cases({menu: true})", className: "f-left" }, ["Reviewed Cases Record"]),
+                                        ]),
+                                    ]),
+                                ]),
+
+                                li({}, [a({ isRendered: isLogged, href: "/dataset_catalog" }, ["Dataset Catalog"]),]),
+                                li({ className: "dropdown", onToggle: this.toggled }, [
+                                    a({ isRendered: isLogged, role: "button", className: "dropdown-toggle", "data-toggle": "dropdown" }, [
+                                        div({}, ["Request Help", span({ className: "caret caret-margin" }, []),])
+                                    ]),
+                                    ul({ className: "dropdown-menu user-dropdown", role: "menu" }, [
+                                        li({}, [
+                                            HelpModal({linkType: "a-tag"}),
+                                        ]),
+                                        hr({}),
+                                        li({}, [a({ href: "/help_me", className: "f-left" }, ["List of Reports"])]),
+                                    ]),
+                                ]),
+                            ]),
+                        ]),
+                    ]),
                 ]),
 
                 li({ isRendered: isMember }, [
