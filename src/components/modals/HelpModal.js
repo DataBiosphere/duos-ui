9031cde04231410c9cfb6ede5138f8f2ce4ce6ee
leoforconesi <lforcone@broadinstitute.org>
2018-09-24 18:24:37 -0300

    create help report

--- src/components/modals/HelpModal.js
+++ src/components/modals/HelpModal.js
@@ -2,24 +2,30 @@ import { Component } from 'react';
 import { div, form, input, label, textarea, hh } from 'react-hyperscript-helpers';
 import { BaseModal } from '../BaseModal';
 import { Alert } from '../Alert';
+import { Help } from '../../libs/ajax';
+import { Storage } from "../../libs/storage";
 
 export const HelpModal = hh(class HelpModal extends Component {
 
   constructor(props) {
     super(props);
-
+    this.state = {
+      subject: '',
+      description: '',
+    };
     this.closeHandler = this.closeHandler.bind(this);
     this.afterOpenHandler = this.afterOpenHandler.bind(this);
     this.OKHandler = this.OKHandler.bind(this);
+    this.subjectChangeHandler = this.subjectChangeHandler.bind(this);
+    this.descriptionChangeHandler = this.descriptionChangeHandler.bind(this);
   }
 
-  OKHandler() {
-    // this is the method for handling OK click
-    // we might do something here, adding a user for instance
-    // or delegate it to the parent....
-    // DO SOMETHING HERE ...
-
-    // and call parent's OK Handler
+  async OKHandler() {
+    const report= {};
+    report.description = this.state.description;
+    report.subject = this.state.subject;
+    report.userId = Storage.getCurrentUser().dacUserId;
+    await Help.createHelpMeReport(report);
     this.props.onOKRequest('help');
   }
 
@@ -42,6 +48,23 @@ export const HelpModal = hh(class HelpModal extends Component {
 
   }
 
+  subjectChangeHandler= (e) => {
+    const subjectText = e.target.value;
+    this.setState(prev => {
+      prev.subject = subjectText;
+      return prev;
+    });
+  };
+
+  descriptionChangeHandler= (e) => {
+    const descriptionText = e.target.value;
+    this.setState(prev => {
+      prev.description = descriptionText;
+      return prev;
+    });
+  };
+
+
   render() {
 
     return (
@@ -58,18 +81,18 @@ export const HelpModal = hh(class HelpModal extends Component {
       },
 
         [
-          form({ className: "form-horizontal css-form", name: "consentForm", noValidate: "true", encType: "multipart/form-data" }, [
+          form({ className: "form-horizontal css-form", name: "consentForm", noValidate: true, encType: "multipart/form-data" }, [
             div({ className: "form-group first-form-group" }, [
               label({ id: "lbl_subject", className: "col-lg-3 col-md-3 col-sm-3 col-xs-4 control-label common-color" }, ["Subject"]),
               div({ className: "col-lg-9 col-md-9 col-sm-9 col-xs-8" }, [
-                input({ type: "text", id: "txt_subject", className: "form-control col-lg-12 vote-input", required: "true" }),
+                input({ type: "text", id: "txt_subject", className: "form-control col-lg-12 vote-input", onChange: this.subjectChangeHandler, required: true }),
               ]),
             ]),
 
             div({ className: "form-group" }, [
               label({ id: "lbl_description", className: "col-lg-3 col-md-3 col-sm-3 col-xs-4 control-label common-color" }, ["Description"]),
               div({ className: "col-lg-9 col-md-9 col-sm-9 col-xs-8" }, [
-                textarea({ name: "helpDescription", id: "txt_description", rows: "5", className: "form-control col-lg-12 vote-input", required: "true" }),
+                textarea({ name: "helpDescription", id: "txt_description", rows: "5", className: "form-control col-lg-12 vote-input", onChange: this.descriptionChangeHandler ,required: true }),
               ]),
             ]),
           ]),
