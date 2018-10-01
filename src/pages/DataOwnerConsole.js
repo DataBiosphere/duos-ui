60184978acd832b403995e362752c1b9e2561996
Nadya Lopez Zalba <nlopezzalba@users.noreply.github.com>
2018-10-01 14:01:00 -0300

    replace text in dul modal and add subheading in data owner console (#77)

--- src/pages/DataOwnerConsole.js
+++ src/pages/DataOwnerConsole.js
@@ -1,6 +1,7 @@
 import { Component, Fragment } from 'react';
 import { div, hr, h, button } from 'react-hyperscript-helpers';
 import { PageHeading } from '../components/PageHeading';
+import { PageSubHeading } from '../components/PageSubHeading';
 import { PaginatorBar } from '../components/PaginatorBar';
 import { PendingCases } from '../libs/ajax';
 import { Storage } from '../libs/storage';
@@ -12,7 +13,7 @@ class DataOwnerConsole extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      loading:true,
+      loading: true,
       dataOwnerUnreviewedCases: [],
       limit: 5,
       currentPage: 1
@@ -88,73 +89,87 @@ class DataOwnerConsole extends Component {
 
     return (
       div({ className: "container" }, [
-
         div({ className: "row no-margin" }, [
-
-          div({ className: "col-lg-8 col-md-8 col-sm-7 col-xs-12 no-padding" }, [
+          div({ className: "col-lg-12 col-md-12 col-sm-12 col-xs-12 no-padding" }, [
             PageHeading({
               id: "dataOwnerConsole",
-              color: "dataset",
+              color: "common",
               title: "Welcome " + currentUser.displayName + "!",
               description: "These are your pending cases for review"
             }),
+            hr({ className: "section-separator" })
           ]),
 
-          div({ className: "col-lg-4 col-md-4 col-sm-5 col-xs-12 search-wrapper no-padding" }, [
-            SearchBox({ id: 'dataOwnerConsole', searchHandler: this.handleSearchDul, color: 'dataset' })
-          ]),
-        ]),
-
-        div({ className: "jumbotron table-box" }, [
-          div({ className: "row no-margin" }, [
-            div({ className: "col-lg-2 col-md-2 col-sm-3 col-xs-3 cell-header dataset-color" }, ["Dataset ID"]),
-            div({ className: "col-lg-6 col-md-6 col-sm-5 col-xs-5 cell-header dataset-color" }, ["Dataset Name"]),
-            div({ className: "col-lg-2 col-md-2 col-sm-2 col-xs-2 cell-header dataset-color" }, ["Data Request ID"]),
-            div({ className: "col-lg-2 col-md-2 col-sm-2 col-xs-2 cell-header f-center dataset-color" }, ["Review/Vote"]),
-          ]),
-
-          hr({ className: "table-head-separator" }),
-
-          this.state.dataOwnerUnreviewedCases.filter(this.searchTable(searchDulText)).slice((currentPage - 1) * limit, currentPage * limit).map(pendingCase => {
-            return h(Fragment, { key: pendingCase.darCode }, [
-              div({ id: pendingCase.darCode, className: "row no-margin tableRow" }, [
-
-                div({ id: pendingCase.darCode + "_dataSetId", name: "datasetId", className: "col-lg-2 col-md-2 col-sm-3 col-xs-3 cell-body text" }, [pendingCase.dataSetId]),
-                div({ id: pendingCase.darCode + "_dataSetName", name: "datasetName", className: "col-lg-6 col-md-6 col-sm-5 col-xs-5 cell-body text" }, [pendingCase.dataSetName]),
-                div({ id: pendingCase.darCode + "_darCode", name: "darCode", className: "col-lg-2 col-md-2 col-sm-2 col-xs-2 cell-body text" }, [pendingCase.darCode]),
-
-                div({ className: "col-lg-2 col-md-2 col-sm-2 col-xs-2 cell-body f-center" }, [
-
-                  button({
-                    id: pendingCase.darCode + "_btnVote",
-                    name: "btn_vote",
-                    className: "cell-button cancel-color",
-                    isRendered: !pendingCase.alreadyVoted && (pendingCase.hasConcerns === null || !pendingCase.hasConcerns),
-                    data: pendingCase.darCode,
-                    onClick: () => this.voteReview(pendingCase)
-                  }, ["Vote"]),
-                  button({
-                    id: pendingCase.darCode + "_btnEdit",
-                    name: "btn_edit",
-                    className: "cell-button default-color",
-                    isRendered: pendingCase.alreadyVoted || pendingCase.hasConcerns,
-                    data: pendingCase.darCode,
-                    onClick: () => this.voteReview(pendingCase)
-                  }, ["Edit"]),
-                ]),
+          div({ className: "col-lg-12 col-md-12 col-sm-12 col-xs-12 no-padding" }, [
+            div({ className: "row no-margin" }, [
+              div({ className: "col-lg-8 col-md-8 col-sm-7 col-xs-12 no-padding" }, [
+                PageSubHeading({
+                  id: "dataOwnerConsole",
+                  imgSrc: "/images/icon_dataset_review.png",
+                  iconSize: "large",
+                  color: "dataset",
+                  title: "Dataset Access Request Review",
+                  description: "Should data access be granted to this applicant?"
+                })
               ]),
-              hr({ className: "table-body-separator" }),
+
+              div({ className: "col-lg-4 col-md-4 col-sm-5 col-xs-12 search-wrapper no-padding" }, [
+                SearchBox({ id: 'dataOwnerConsole', searchHandler: this.handleSearchDul, color: 'dataset' })
+              ])
+            ]),
+
+            div({ className: "jumbotron table-box" }, [
+              div({ className: "row no-margin" }, [
+                div({ className: "col-lg-2 col-md-2 col-sm-3 col-xs-3 cell-header dataset-color" }, ["Dataset ID"]),
+                div({ className: "col-lg-6 col-md-6 col-sm-5 col-xs-5 cell-header dataset-color" }, ["Dataset Name"]),
+                div({ className: "col-lg-2 col-md-2 col-sm-2 col-xs-2 cell-header dataset-color" }, ["Data Request ID"]),
+                div({ className: "col-lg-2 col-md-2 col-sm-2 col-xs-2 cell-header f-center dataset-color" }, ["Review/Vote"]),
+              ]),
+
+              hr({ className: "table-head-separator" }),
+
+              this.state.dataOwnerUnreviewedCases.filter(this.searchTable(searchDulText)).slice((currentPage - 1) * limit, currentPage * limit).map(pendingCase => {
+                return h(Fragment, { key: pendingCase.darCode }, [
+                  div({ id: pendingCase.darCode, className: "row no-margin tableRow" }, [
+
+                    div({ id: pendingCase.darCode + "_dataSetId", name: "datasetId", className: "col-lg-2 col-md-2 col-sm-3 col-xs-3 cell-body text" }, [pendingCase.dataSetId]),
+                    div({ id: pendingCase.darCode + "_dataSetName", name: "datasetName", className: "col-lg-6 col-md-6 col-sm-5 col-xs-5 cell-body text" }, [pendingCase.dataSetName]),
+                    div({ id: pendingCase.darCode + "_darCode", name: "darCode", className: "col-lg-2 col-md-2 col-sm-2 col-xs-2 cell-body text" }, [pendingCase.darCode]),
+
+                    div({ className: "col-lg-2 col-md-2 col-sm-2 col-xs-2 cell-body f-center" }, [
+
+                      button({
+                        id: pendingCase.darCode + "_btnVote",
+                        name: "btn_vote",
+                        className: "cell-button cancel-color",
+                        isRendered: !pendingCase.alreadyVoted && (pendingCase.hasConcerns === null || !pendingCase.hasConcerns),
+                        data: pendingCase.darCode,
+                        onClick: () => this.voteReview(pendingCase)
+                      }, ["Vote"]),
+                      button({
+                        id: pendingCase.darCode + "_btnEdit",
+                        name: "btn_edit",
+                        className: "cell-button default-color",
+                        isRendered: pendingCase.alreadyVoted || pendingCase.hasConcerns,
+                        data: pendingCase.darCode,
+                        onClick: () => this.voteReview(pendingCase)
+                      }, ["Edit"]),
+                    ]),
+                  ]),
+                  hr({ className: "table-body-separator" }),
+                ])
+              }),
+              PaginatorBar({
+                total: this.state.dataOwnerUnreviewedCases.length,
+                limit: limit,
+                pageCount: this.pageCount,
+                currentPage: currentPage,
+                onPageChange: this.handlePageChange,
+                changeHandler: this.handleSizeChange,
+              })
             ])
-          }),
-          PaginatorBar({
-            total: this.state.dataOwnerUnreviewedCases.length,
-            limit: limit,
-            pageCount: this.pageCount,
-            currentPage: currentPage,
-            onPageChange: this.handlePageChange,
-            changeHandler: this.handleSizeChange,
-          }),
-        ]),
+          ])
+        ])
       ])
     );
   }
