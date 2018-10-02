91f0e294c045c3b69898ab29f94262c9d29405ba
Tadeo Riveros Koloszwa <tadeoriverosk@gmail.com>
2018-10-02 17:02:45 -0300

    redirect to reports

--- src/components/DuosHeader.js
+++ src/components/DuosHeader.js
@@ -25,6 +25,7 @@ class DuosHeader extends Component {
 
   okModal = () => {
     this.setState(prev => { prev.showHelpModal = false; return prev; });
+    window.location = 'help_reports';
   };
 
   closeModal = () => {
