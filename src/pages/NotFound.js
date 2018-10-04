8fddd9ff3a9839612ab1cf497ef7d2d4a767a1d3
Nadya Lopez Zalba <nlopezzalba@users.noreply.github.com>
2018-10-04 15:21:11 -0300

    Refine all buttons (#88)
    * replace text in dul modal and add subheading in data owner console
    
    * Fix Sign In button
    
    * Minor changes on buttons
    
    * Fix buttons UI
    
    * Add missing ids from buttons
    
    * Refine all buttons

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -12,7 +12,7 @@ class NotFound extends Component {
           div({ className: "col-lg-12 col-md-12 col-sm-12 col-xs-12 no-padding" }, [
             PageHeading({ id: "notFound", color: "common", title: "Sorry, the page you were looking for was not found." }),
           ]),
-          a({ id: "btn_back", className: "btn vote-button vote-button-back vote-button-bigger f-left", style: { 'marginTop': '15px' }, href: "/home" }, ["Back to Home"]),
+          a({ id: "btn_back", className: "btn-primary btn-back f-left", style: { 'marginTop': '15px' }, href: "/home" }, ["Back to Home"]),
         ])
       ])
     );
