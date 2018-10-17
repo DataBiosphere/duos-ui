4bcb9aee5ef01455680f85ff5b0f73469c0f7822
Diego Gil <dgil@belatrixsf.com>
2018-10-17 09:09:06 -0300

    fixing month handling

--- src/libs/utils.js
+++ src/libs/utils.js
@@ -5,7 +5,7 @@ export const formatDate = (dateval) => {
   
   let dateFormat = new Date(dateval);
   let year = dateFormat.getFullYear();
-  let month = ('0' + dateFormat.getMonth()).slice(-2);
+  let month = ('0' + (dateFormat.getMonth() + 1)).slice(-2);
   let day = ('0' + dateFormat.getDate()).slice(-2);
   let datestr = year + '-' + month + '-' + day;
   return datestr;
