36f3167c897048eebc744c46779bd98a495510b7
leoforconesi <lforcone@broadinstitute.org>
2018-10-19 12:30:09 -0300

    fix button

--- src/pages/HomeRegister.js
+++ src/pages/HomeRegister.js
@@ -1,34 +1,32 @@
 import { Component } from 'react';
-import { div, h1, form, label, input, hr, span, h } from 'react-hyperscript-helpers';
+import { div, h1, form, label, input, hr, span, h, img, h3, a } from 'react-hyperscript-helpers';
 import { Config } from "../libs/config";
 import { USER_ROLES } from '../libs/utils';
 import GoogleLogin from 'react-google-login';
 import { Storage } from "../libs/storage";
 import './Login.css';
+import { User } from "../libs/ajax";
 
 class HomeRegister extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
       displayName: '',
-      loading: true,
       clientId: ''
     };
-    this.getGoogleClientId();
     this.myHandler = this.myHandler.bind(this);
+    this.getGoogleClientId();
   }
 
   async getGoogleClientId() {
     const clientKey = `${await Config.getGoogleClientId()}`;
     this.setState(prev => {
       prev.clientId = clientKey;
-      prev.loading = false;
       return prev;
     });
   }
 
-  getUser = async () => {
+  getNewUser = () => {
     let role = {};
     let user = {};
     user.roles = [];
@@ -38,38 +36,80 @@ class HomeRegister extends Component {
     return user;
   };
 
+  getUser = async () => {
+    return await User.getByEmail(Storage.getGoogleData().profileObj.email);
+  };
+
   responseGoogle = async (response) => {
     Storage.setGoogleData(response);
-    this.getUser().then(
-      user => {
-        const currentUserRoles = user.roles.map(roles => roles.name);
-        user.isChairPerson = currentUserRoles.indexOf(USER_ROLES.chairperson) > -1;
-        user.isMember = currentUserRoles.indexOf(USER_ROLES.member) > -1;
-        user.isAdmin = currentUserRoles.indexOf(USER_ROLES.admin) > -1;
-        user.isResearcher = currentUserRoles.indexOf(USER_ROLES.researcher) > -1;
-        user.isDataOwner = currentUserRoles.indexOf(USER_ROLES.dataOwner) > -1;
-        user.isAlumni = currentUserRoles.indexOf(USER_ROLES.alumni) > -1;
-        Storage.setCurrentUser(user);
-        Storage.setUserIsLogged(true);
+
+    let newUser = this.getNewUser();
+
+    User.registerUser(newUser).then(
+      data => {
+        data = Object.assign(data, this.setRoles(newUser));
+        this.setStorage(data);
         this.props.history.push('dataset_catalog?reviewProfile');
       },
       error => {
-        Storage.clearStorage();
-        alert(response.w3.U3 + ' is not a registered user.');
-      });
+        if (error.status === 400 && this.state.displayName !== '') {
+          alert("User already exists.");
+          this.getUser().then(
+            user => {
+              user = Object.assign(user, this.setRoles(user));
+              this.setStorage(user);
+              this.redirect(user)
+            },
+            error => {
+              Storage.clearStorage();
+            });
+        }
+      }
+    );
   };
+
+
+  static setRoles(user){
+    const currentUserRoles = user.roles.map(roles => roles.name);
+    let roles = {};
+    roles.isChairPerson = currentUserRoles.indexOf(USER_ROLES.chairperson) > -1;
+    roles.isMember = currentUserRoles.indexOf(USER_ROLES.member) > -1;
+    roles.isAdmin = currentUserRoles.indexOf(USER_ROLES.admin) > -1;
+    roles.isResearcher = currentUserRoles.indexOf(USER_ROLES.researcher) > -1;
+    roles.isDataOwner = currentUserRoles.indexOf(USER_ROLES.dataOwner) > -1;
+    roles.isAlumni = currentUserRoles.indexOf(USER_ROLES.alumni) > -1;
+    return roles;
+  }
+
+  static setStorage(user) {
+    Storage.setCurrentUser(user);
+    Storage.setUserIsLogged(true);
+  }
 
   forbidden = (response) => {
     Storage.clearStorage();
   };
 
   myHandler(event) {
-  let displayName = event.target.value;
+    let displayName = event.target.value;
     this.setState(prev => {
       prev.displayName = displayName;
       return prev;
     });
   }
+
+  // redirect() returns the initial page to be redirected when a user logs in
+  // for external re-directions, the method will first check if the usr has permission to access,
+  // if this is true, and user is not logged, it will be auto-logged and redirected to the url.
+  redirect = (user) => {
+     let page = user.isChairPerson ? 'chair_console' :
+        user.isMember ? 'member_console' :
+          user.isAdmin ? 'admin_console' :
+            user.isResearcher ? 'dataset_catalog?reviewProfile' :
+              user.isDataOwner ? 'data_owner_console' :
+                user.isAlumni ? 'summary_votes' : '/';
+    this.props.history.push(page);
+  };
 
   render() {
     let googleLoginButton;
@@ -79,62 +119,37 @@ class HomeRegister extends Component {
         'loading...'
       ]);
     } else {
-      console.log("clientID !!!!!!!!!!!!!!!!!!!!!!", this.state.clientId);
       googleLoginButton = h(GoogleLogin, {
-        // className: "btn_gSignInWrapper",
+        className: "btn_gSignInWrapper",
         clientId: this.state.clientId,
         onSuccess: this.responseGoogle,
         onFailure: this.forbidden,
+        disabled: this.state.displayName === ''
       }, [
-        // div({ id: "btn_gSignIn", className: "btn_gSignIn" }, [
-        //   span({ className: "icon" }),
-        //   label({}, ["Sign in with Google"])
-        // ])
+        div({id: "btn_gSignIn", className: "btn_gSignIn"}, [
+          span({className: "icon"}),
+          label({}, ["Sign in with Google"])
+        ])
       ]);
     }
 
-
-
-    // const googleLoginButton = h(GoogleLogin, {
-    //   className: "btn_gSignInWrapper",
-    //   clientId: this.state.clientId,
-    //   onSuccess: this.responseGoogle,
-    //   onFailure: this.forbidden
-    // }, [
-    //   div({ id: "btn_gSignIn", className: "btn_gSignIn" }, [
-    //     span({ className: "icon" }),
-    //     label({}, ["Sign in with Google"])
-    //   ])
-    // ]);
-
     return (
-      div({ className: "row home" }, [
+      div({ className: "container" }, [
         div({ className: "col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1 col-sm-12 col-xs-12" }, [
           h1({ className: "home-title" }, ["Join DUOS"]),
           div({ className: "home-title-description" }, ["Sign up to DUOS to find genomic datasets of interest and to submit Data Access Requests."]),
           hr({ className: "home-line" }),
           div({ className: "row" }, [
             div({ className: "col-lg-6 col-md-8 col-sm-12 col-xs-12" }, [
-              label({ className: "home-control-label col-lg-12 col-md-12 col-sm-12 col-xs-12" }, ["Full Name"]),
-              input({ className: "form-control col-lg-12 col-md-12 col-sm-12 col-xs-12", type: "text", onChange: this.myHandler }),
+            label({ className: "home-control-label col-lg-12 col-md-12 col-sm-12 col-xs-12" }, ["Full Name"]),
+            input({ className: "form-control col-lg-12 col-md-12 col-sm-12 col-xs-12", type: "text", onChange: this.myHandler }),
             ]),
-            div({ className: "landing-box-google-signin" }, [
-
-              googleLoginButton,
-            ]),
-
-            div({ className: "col-lg-6 col-md-8 col-sm-12 col-xs-12" }, [
-              div({ className: "pos-relative" }, [
-                div({ className: "custom-g-signin2 g-signin2 signInRegister", "data-onsuccess": "onSignIn", "data-theme": "dark"  }),
-                div({ isRendered: !form.name, className: "signInDisabledButton" })
-              ]),
-            ]),
-          ])
+          ]),
+          div({ className: "landing-box-google-signin" }, [googleLoginButton])
         ])
       ])
     );
   }
 }
-
 export default HomeRegister;
 
