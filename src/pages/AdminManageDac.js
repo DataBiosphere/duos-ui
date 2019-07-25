1cc1ccfd91ed47d0702c0b05a2e524ac2e1c9415
Greg Rushton <grushton@broadinstitute.org>
2019-07-25 09:00:47 -0400

    remove unused content

--- src/pages/AdminManageDac.js
+++ src/pages/AdminManageDac.js
@@ -2,8 +2,6 @@ import { Component, Fragment } from 'react';
 import { div, hr, h, span, a, button } from 'react-hyperscript-helpers';
 import { AddDacModal } from '../components/modals/AddDacModal';
 import { DAC } from '../libs/ajax';
-import { DacMembersModal } from '../components/modals/DacMembersModal';
-import { DacDatasetsModal } from '../components/modals/DacDatasetsModal';
 import { PageHeading } from '../components/PageHeading';
 import { PaginatorBar } from "../components/PaginatorBar";
 import ReactTooltip from 'react-tooltip';
@@ -35,16 +33,6 @@ class AdminManageDac extends Component {
     this.closeAddDacModal = this.closeAddDacModal.bind(this);
     this.okAddDacModal = this.okAddDacModal.bind(this);
     this.afterAddDacModalOpen = this.afterAddDacModalOpen.bind(this);
-
-    this.viewMembers = this.viewMembers.bind(this);
-    this.closeViewMembersModal = this.closeViewMembersModal.bind(this);
-    this.okViewMembersModal = this.okViewMembersModal.bind(this);
-    this.afterViewMembersModalOpen = this.afterViewMembersModalOpen.bind(this);
-
-    this.viewDatasets = this.viewDatasets.bind(this);
-    this.closeViewDatasetsModal = this.closeViewDatasetsModal.bind(this);
-    this.okViewDatasetsModal = this.okViewDatasetsModal.bind(this);
-    this.afterViewDatasetsModalOpen = this.afterViewDatasetsModalOpen.bind(this);
   }
 
   componentDidMount() {
@@ -92,36 +80,10 @@ class AdminManageDac extends Component {
     });
   }
 
-  viewMembers() {
-    this.setState({
-      showMembersModal: true,
-    });
-  }
-
-  viewDatasets() {
-    this.setState({
-      showDatasetsModal: true,
-    });
-  }
-
   closeAddDacModal() {
     this.getDacs();
     this.setState(prev => {
       prev.showModal = false;
-      return prev;
-    });
-  }
-
-  closeViewMembersModal() {
-    this.setState(prev => {
-      prev.showMembersModal = false;
-      return prev;
-    });
-  }
-
-  closeViewDatasetsModal() {
-    this.setState(prev => {
-      prev.showDatasetsModal = false;
       return prev;
     });
   }
@@ -133,27 +95,7 @@ class AdminManageDac extends Component {
     });
   }
 
-  async okViewMembersModal() {
-    this.setState(prev => {
-      prev.showMembersModal = false;
-      return prev;
-    });
-  }
-
-  async okViewDatasetsModal() {
-    this.setState(prev => {
-      prev.showDatasetsModal = false;
-      return prev;
-    });
-  }
-
   afterAddDacModalOpen() {
-  }
-
-  afterViewMembersModalOpen() {
-  }
-
-  afterViewDatasetsModalOpen() {
   }
 
   handleSearchDac = (query) => {
@@ -204,7 +146,7 @@ class AdminManageDac extends Component {
           div({ className: "grid-9-row" }, [
             div({ className: "col-2 cell-header common-color" }, ["DAC Name"]),
             div({ className: "col-3 cell-header common-color" }, ["DAC Description"]),
-            div({ className: "col-1 cell-header common-color" }, ["DAC Chair"]),
+            div({ className: "col-1 cell-header common-color" }, ["DAC Chairs"]),
             div({ className: "col-1 cell-header common-color" }, ["DAC Roster"]),
             div({ className: "col-1 cell-header common-color" }, ["DAC Datasets"]),
             div({ className: "col-1 cell-header f-center common-color" }, ["Actions"]),
@@ -283,23 +225,7 @@ class AdminManageDac extends Component {
                   onAfterOpen: this.afterAddDacModalOpen,
                   dac: this.state.selectedDac,
                 }),
-
-                DacMembersModal({
-                  isRendered: this.state.showMembersModal,
-                  showModal: this.state.showMembersModal,
-                  onOKRequest: this.okViewMembersModal,
-                  onCloseRequest: this.closeViewMembersModal,
-                  onAfterOpen: this.afterViewMembersModalOpen,
-                }),
-
-                DacDatasetsModal({
-                  isRendered: this.state.showDatasetsModal,
-                  showModal: this.state.showDatasetsModal,
-                  onOKRequest: this.okViewDatasetModal,
-                  onCloseRequest: this.closeViewDatasetsModal,
-                  onAfterOpen: this.afterViewDatasetsModalOpen,
-                })
-            ])
+              ])
             );
           }),
           PaginatorBar({
