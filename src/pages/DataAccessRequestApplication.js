dffca3595ed9144540d7c04d53c2579802d85196
Greg Rushton <rushtong@users.noreply.github.com>
2019-08-29 09:19:39 -0400

    [DUOS-438][risk=no] Fix upload button (#199)
    * fix upload button

--- src/pages/DataAccessRequestApplication.js
+++ src/pages/DataAccessRequestApplication.js
@@ -1503,7 +1503,7 @@ class DataAccessRequestApplication extends Component {
                       ]),
 
                       div({ className: "col-lg-12 col-md-12 col-sm-12 col-xs-12" }, [
-                        button({ className: "fileUpload col-lg-4 col-md-4 col-sm-6 col-xs-12 btn-secondary btn-download-pdf hover-color" }, [
+                        div({ style: { paddingTop: 7 }, className: "fileUpload col-lg-4 col-md-4 col-sm-6 col-xs-12 btn-secondary btn-download-pdf hover-color" }, [
                           span({ className: "glyphicon glyphicon-upload" }),
                           "Upload S.O. Signed Agreement",
                           input({ id: "uploadFile", type: "file", onChange: this.handleFileChange, className: "upload", required: true })
