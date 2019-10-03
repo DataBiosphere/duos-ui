2ed2bedde1aaf4e3aafb3125c2e716b6db3ab1e5
Greg Rushton <grushton@broadinstitute.org>
2019-10-03 16:02:34 -0400

    parse alias

--- src/components/DataAccessRequest.js
+++ src/components/DataAccessRequest.js
@@ -2,6 +2,26 @@ import _ from 'lodash';
 import { Component } from 'react';
 import { div, hh, span } from 'react-hyperscript-helpers';
 
+const parseAlias = (alias) => {
+  const prefix = "DUOS-";
+  const aliasInt = parseInt(alias);
+  if (aliasInt < 10) {
+    return prefix + "00000" + alias;
+  }
+  if (aliasInt < 100) {
+    return prefix + "0000" + alias;
+  }
+  if (aliasInt < 1000) {
+    return prefix + "000" + alias;
+  }
+  if (aliasInt < 10000) {
+    return prefix + "00" + alias;
+  }
+  if (aliasInt < 100000) {
+    return prefix + "0" + alias;
+  }
+  return prefix + alias;
+};
 
 const style = {
   'fontSize': 19,
@@ -17,7 +37,7 @@ const pipe = ' | ';
 export const DataAccessRequest = hh(class DataAccessRequest extends Component {
 
   render() {
-    const aliases = _.join(_.map(this.props.dar.datasets, 'alias'), ', ');
+    const aliases = _.join(_.map(this.props.dar.datasets, 'alias').map(a => parseAlias(a)), ', ');
     const names = _.join(_.map(this.props.dar.datasets, 'name'), ', ');
     return (
       div({ style: style }, [
