ac5db51d48c2ab4b8ea75b74eb6f05a3afc24669
jaime-bernales-broad <50339562+jaime-bernales-broad@users.noreply.github.com>
2019-10-08 09:06:00 -0300

    [DUOS-395][risk=no] Reference info for DAR & Dataset to DAC Review Pages (#171)
    * Reference info for DAR and  Dataset to DAC Review Pages

--- src/libs/models.js
+++ src/libs/models.js
@@ -1,4 +1,28 @@
 export const Models = {
   dac: { dac: {}, chairpersons: [], members: [] },
+  dar: {
+    researcherProperties: [],
+    researcherId: '',
+    rus: '',
+    status: '',
+    hasAdminComment: false,
+    adminComment: '',
+    hasPurposeStatements: false,
+    darCode: '',
+    projectTitle: '',
+    purposeStatements: [],
+    hasDiseases: false,
+    diseases: [],
+    researchType: [],
+    researchTypeManualReview: '',
+    pi: '',
+    havePI: false,
+    profileName: '',
+    institution: '',
+    department: '',
+    city: '',
+    country: '',
+    datasets: []
+  },
   error: { show: false, title: '', msg: [''] }
 };
