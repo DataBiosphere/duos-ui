c82242a56d6fb351f13bc07cc1b0d50258fea32a
solideoglori <52168754+solideoglori@users.noreply.github.com>
2020-02-19 17:52:55 -0500

    minor format changes to DUOS homepage (#298)
    Co-authored-by: jlawsonbroad <35933229+jlawsonbroad@users.noreply.github.com>

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -18,21 +18,18 @@ class Home extends Component {
           hr({ className: 'home-line' }),
           div({ style: { textAlign: 'center' } }, [
             div({ isRendered: !this.props.isLogged }, [
-              div({}, ['Want to explore datasets and make a data access request?']),
+              div({ style: { fontSize: 'larger', fontWeight: '500' } }, ['Want to explore datasets and make a data access request?']),
               div({ style: { margin: '1rem 0 1rem 0' } }, [
                 SignIn({ props: this.props, onSignIn: () => this.props.onSignIn(), history: this.props.history })
               ]),
               div({ style: { margin: '0 0 1rem 0' } }, [
                 span({}, [
-                  'Don\'t have a Google Account?',
-                  br(),
-                  'Create a new one ',
+                  'Don\'t have a Google Account? Create a new one ',
                   a({
                     href: 'https://accounts.google.com/SignUp?continue:https%3A%2F%2Faccounts.google.com%2Fo%2Foauth2%2Fauth%3Fopenid.realm%26scope%3Demail%2Bprofile%2Bopenid%26response_type%3Dpermission%26redirect_uri%3Dstoragerelay%3A%2F%2Fhttp%2Flocalhost%3A8000%3Fid%253Dauth721210%26ss_domain%3Dhttp%3A%2F%2Flocalhost%3A8000%26client_id%3D832251491634-smgc3b2pogqer1mmdrd3hrqic3leof3p.apps.googleusercontent.com%26fetch_basic_profile%3Dtrue%26hl%3Des-419%26from_login%3D1%26as%3D43c5de35a7316d00&ltmpl:popup',
                     target: '_blank'
-                  }, ['here. ']),
-                  br(),
-                  'Or sign up with an existing email address ',
+                  }, ['here, ']),
+                  'or sign up with an existing email address ',
                   a({
                     href: 'https://support.google.com/accounts/answer/176347',
                     target: '_blank'
@@ -40,19 +37,13 @@ class Home extends Component {
                 ])
               ])
             ]),
-            p({ style: { fontSize: 'larger', fontWeight: '500' } }, ['Are you a DAC member who wants to learn more about using DUOS?']),
-            p({ style: { fontSize: 'larger', fontWeight: '500' } }, [
-              'Email us at ',
-              h(Mailto, { email: 'DUOS-support@broadinstitute.zendesk.com' })
+            p({ style: { fontSize: 'larger', fontWeight: '500' } }, ['Are you a DAC member who wants to learn more about DUOS?']),
+            p({ style: { fontSize: 'large', fontWeight: '350' } }, [ 'Email us! ']),
+            p({ style: { fontSize: 'large', fontWeight: '500' } }, [
+               h(Mailto, { email: 'DUOS-support@broadinstitute.zendesk.com' })
             ])
           ]),
-          div({ className: 'home-content' }, [
-            p({}, [
-              'There are restrictions on researching human genomics data. For example: ',
-              span({ className: 'italic' }, ['“Data can only be used for breast cancer research with non-commercial purpose”.'])
-            ]),
-            p({}, ['The Data Use Oversight system ensures that researchers using genomics data honor these restrictions.'])
-          ]),
+
           h3({}, ['What is DUOS?']),
           div({ className: 'home-content-references' }, [
             svg({ height: '14', width: '14' }, [
