dd4a17f367cc9d2606def7e3ce039c1ca6e5756d
Greg Rushton <grushton@broadinstitute.org>
2020-02-21 17:38:17 -0500

    eslint on the rest

--- src/libs/config.js
+++ src/libs/config.js
@@ -4,57 +4,57 @@ import {Storage} from './storage';
 
 export const Config = {
 
-    getApiUrl: async () => (await getConfig()).apiUrl,
+  getApiUrl: async () => (await getConfig()).apiUrl,
 
-    getOntologyApiUrl: async () => (await getConfig()).ontologyApiUrl,
+  getOntologyApiUrl: async () => (await getConfig()).ontologyApiUrl,
 
-    getFireCloudUrl: async () => (await getConfig()).firecloudUrl,
+  getFireCloudUrl: async () => (await getConfig()).firecloudUrl,
 
-    getNihUrl: async () => (await getConfig()).nihUrl,
+  getNihUrl: async () => (await getConfig()).nihUrl,
 
-    getGoogleClientId: async () => (await getConfig()).clientId,
+  getGoogleClientId: async () => (await getConfig()).clientId,
 
-    getFeatureFlag: async (featureName) => _.get((await getConfig()).features, featureName, false),
+  getFeatureFlag: async (featureName) => _.get((await getConfig()).features, featureName, false),
 
-    authOpts: (token = Token.getToken()) => ({
-        headers: {
-            Authorization: `Bearer ${token}`,
-            Accept: 'application/json',
-            'X-App-ID': 'DUOS'
-        }
-    }),
+  authOpts: (token = Token.getToken()) => ({
+    headers: {
+      Authorization: `Bearer ${token}`,
+      Accept: 'application/json',
+      'X-App-ID': 'DUOS'
+    }
+  }),
 
-    fileOpts: (token = Token.getToken()) => ({
-        headers: {
-            Authorization: `Bearer ${token}`,
-            Accept: 'application/json'
-        }
-    }),
+  fileOpts: (token = Token.getToken()) => ({
+    headers: {
+      Authorization: `Bearer ${token}`,
+      Accept: 'application/json'
+    }
+  }),
 
-    jsonBody: body => ({
-        body: JSON.stringify(body),
-        headers: {'Content-Type': 'application/json'}
-    }),
+  jsonBody: body => ({
+    body: JSON.stringify(body),
+    headers: {'Content-Type': 'application/json'}
+  }),
 
-    fileBody: (token = Token.getToken()) => ({
-        headers: {
-            Authorization: `Bearer ${token}`,
-            Accept: '*/*'
-        }
-    }),
+  fileBody: (token = Token.getToken()) => ({
+    headers: {
+      Authorization: `Bearer ${token}`,
+      Accept: '*/*'
+    }
+  }),
 };
 
 const Token = {
-    getToken: () => {
-        return Storage.getGoogleData() !== null ? Storage.getGoogleData().accessToken : 'token';
-    }
+  getToken: () => {
+    return Storage.getGoogleData() !== null ? Storage.getGoogleData().accessToken : 'token';
+  }
 };
 
 const loadConfig = _.memoize(async () => {
-    const res = await fetch('/config.json');
-    return res.json();
+  const res = await fetch('/config.json');
+  return res.json();
 });
 
 const getConfig = async () => {
-    return await loadConfig();
+  return await loadConfig();
 };
