726b1da74d248bba10d5c1e4cf24fe06f0e97fe9
MacKenzie Kerwin <mackenziekerwin@gmail.com>
2020-02-24 17:27:11 -0500

    created a theme to apply styling

--- src/pages/MacKenzieAccessReview.jsx
+++ src/pages/MacKenzieAccessReview.jsx
@@ -1,9 +1,7 @@
 import React from "react";
-import { div, h2, h3, p, a } from "react-hyperscript-helpers";
+import { div, a } from "react-hyperscript-helpers";
 import { Storage } from "../libs/storage";
 const BORDER = "1px solid rgba(0,0,0,0.5)";
-const PADDING = "12px";
-const MARGIN = "6px";
 const LOREM_IPSUM =
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 class MacKenzieAccessReview extends React.PureComponent {
@@ -28,18 +26,16 @@ class MacKenzieAccessReview extends React.PureComponent {
   render() {
     return div(
       {
-        id: "container"
+        id: "container", className: 'container-wide centered normal-text primary'
       },
       [
         div(
           {
             id: "header",
+            className: 'section',
             style: {
               justifyContent: "space-between",
               alignItems: "center",
-              border: BORDER,
-              padding: PADDING,
-              margin: MARGIN,
               display: "flex"
             }
           },
@@ -62,22 +58,21 @@ class MacKenzieAccessReview extends React.PureComponent {
                   }
                 }),
                 div({ id: "header-text" }, [
-                  h2("Data Access Review"),
-                  p(
+                  div({ className: 'title-text' }, "Data Access Review"),
+                  div(
                     "Review the Application Summary and Data Use Limitations to determine if the researcher should be granted access to the data."
                   ),
-                  p("DAC Chairs can optionally vote as a member.")
+                  div("DAC Chairs can optionally vote as a member.")
                 ])
               ]
             ),
             div(
-              { className: "col-lg-2 col-md-3 col-sm-3 col-xs-12 no-padding" },
               [
                 a(
                   {
                     id: "btn_back",
                     onClick: this.back,
-                    className: "btn-primary btn-back"
+                    className: "button-outlined secondary"
                   },
                   "DAC Console"
                 )
@@ -85,66 +80,55 @@ class MacKenzieAccessReview extends React.PureComponent {
             )
           ]
         ),
-        div({ id: "body", style: { display: "flex" } }, [
+        div({ id: "body", className: 'normal-text', style: { display: "flex" } }, [
           div(
             {
               id: "voter-roles",
+              className: 'section',
               style: {
                 border: BORDER,
-                padding: PADDING,
-                margin: MARGIN,
                 width: "30%"
               }
             },
-            [h3("Vote as X")]
+            [div({ className: 'subheader-text' }, "Vote as X")]
           ),
           div(
             {
               id: "application",
+              className: 'section',
               style: {
-                border: BORDER,
-                padding: PADDING,
-                margin: MARGIN,
                 width: "70%"
               }
             },
             [
-              h3("Application header"),
+              div({ className: 'header-text' }, "Application header"),
               div(
                 {
                   id: "app-summary",
-                  style: {
-                    border: BORDER,
-                    padding: PADDING,
-                    margin: MARGIN
-                  }
+                  className: 'text-section',
                 },
                 [
-                  "Application summary",
+                  div({ className: 'subheader-text' }, "Application summary"),
                   div({ style: { display: "flex" } }, [
                     div(
                       {
                         id: "structured-rp",
                         style: {
-                          border: BORDER,
-                          padding: PADDING,
-                          margin: MARGIN,
+                          padding: "24px 48px 24px 0px",
                           width: "50%"
                         }
                       },
-                      [h3("Structured Research Purpose"), p(LOREM_IPSUM)]
+                      [div({ className: 'header-text' }, "Structured Research Purpose"), div(LOREM_IPSUM)]
                     ),
                     div(
                       {
                         id: "dusl",
+                        className: 'highlight-box',
                         style: {
-                          border: BORDER,
-                          padding: PADDING,
-                          margin: MARGIN,
                           width: "50%"
                         }
                       },
-                      [h3("Data Use Structured Limitations"), p(LOREM_IPSUM)]
+                      [div({ className: 'header-text highlighted' }, "Data Use Structured Limitations"), div(LOREM_IPSUM)]
                     )
                   ])
                 ]
@@ -152,26 +136,15 @@ class MacKenzieAccessReview extends React.PureComponent {
               div(
                 {
                   id: "rp",
-                  style: {
-                    border: BORDER,
-                    padding: PADDING,
-                    margin: MARGIN,
-                    width: "100%"
-                  }
+                  className: 'text-section'
                 },
-                [h3("Research Purpose"), p(LOREM_IPSUM)]
+                [div({ className: 'header-text' }, "Research Purpose"), div(LOREM_IPSUM)]
               ),
               div(
                 {
                   id: "applicant",
-                  style: {
-                    border: BORDER,
-                    padding: PADDING,
-                    margin: MARGIN,
-                    width: "100%"
-                  }
                 },
-                [h3("Applicant Information"), p(LOREM_IPSUM)]
+                [div({ className: 'header-text' }, "Applicant Information"), div(LOREM_IPSUM)]
               )
             ]
           )
