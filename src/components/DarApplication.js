1de186786c9ccfb290d751985a5f2c0002720aa9
MacKenzie Kerwin <52389456+mackenziekerwin@users.noreply.github.com>
2020-03-09 16:13:59 -0400

    added download link to full application (#337)

--- src/components/DarApplication.js
+++ src/components/DarApplication.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { div, span, hh } from "react-hyperscript-helpers";
+import { div, span, a, i, hh } from "react-hyperscript-helpers";
 import { Theme } from '../libs/theme';
+import { Files } from '../libs/ajax';
 import { AppSummary } from './AppSummary';
 
 const SECTION = {
   fontFamily: 'Montserrat',
   margin: '16px 0px 5px 0px',
   color: Theme.palette.primary,
+  display: 'flex',
 };
 
 const HEADER = {
@@ -19,13 +21,29 @@ const HEADER_BOLD = {
   fontWeight: Theme.font.weight.semibold,
 };
 
+const ICON = {
+  color: Theme.palette.link,
+  marginRight: '6px',
+};
+
 export const DarApplication = hh(class DarApplication extends React.PureComponent {
+
+  downloadDAR = () => {
+    Files.getDARFile(this.props.ids.darId);
+  };
+
   render() {
     const { voteAsChair, darInfo } = this.props;
     return div([
-      div({ style: SECTION }, [
-        span({ style: HEADER_BOLD }, darInfo.projectTitle),
-        span({ style: HEADER }, " | " + darInfo.darCode)
+      div({ id: 'header', style: SECTION }, [
+        div({ style: { minWidth: '50%' } }, [
+          span({ style: HEADER_BOLD }, darInfo.projectTitle),
+          span({ style: HEADER }, " | " + darInfo.darCode)
+        ]),
+        a({ id: 'download-dar', onClick: this.downloadDAR }, [
+          i({ className: 'glyphicon glyphicon-download-alt', style: ICON }),
+          'Full Application'
+        ])
       ]),
       div({ id: 'votes-summary', isRendered: voteAsChair }),
       AppSummary({ darInfo })
