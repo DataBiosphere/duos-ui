4e198764b76502efe9dcf49ab78d694b23ce30d4
Jonathan Lawson <jlawson@broadinstitute.org>
2020-04-10 17:48:44 -0400

    About page changes to add section, text, and image on DUO backed consent

--- src/pages/HomeAbout.js
+++ src/pages/HomeAbout.js
@@ -59,6 +59,17 @@ class HomeAbout extends Component {
                   img({ src: "/images/DUOS_LC_Process.png", className: "col-lg-12 col-md-12 col-sm-12 col-xs-12 home-content-graphic", alt: "About icon" }),
                   p( {style: { fontWeight: '450' }}, ["Combined, the DUOS Library Card Agreement and DUOS system significantly alleviate the burden of work on Signing Officials and DACs while enhancing their compliant function and expediting appropriate access for researchers. "]),
                 ]),
+
+                div({ className: "home-content" }, [
+                  h3({}, ["Machine Readable Consent Forms & DUOS"]),
+                  p( { className: "home-sections-description" }, ["Standardizing data sharing language for expedited and compliant sharing"]),
+                  p( {style: { fontWeight: '400' }}, ["To date, many consent forms contain ambiguous or region-specific data sharing language, making widespread data sharing and aggregation of datasets difficult or near impossible."]),
+                  span({}, []),
+                  a({ href: 'https://blog.primr.org/is-your-data-sharing-consent-language-transparent-and-machine-readable/', target: '_blank' }, 'GA4GH\'s Machine Readable Consent Form Initiative'),
+                  p( {style: { fontWeight: '400' }}, ["This initiative provides standard data sharing language for IRBs, funders, and investigators to use in their consent forms, based on the Data Use Ontology (DUO)."]),
+                  img({ src: "/images/machine readable consent forms.png", className: "col-lg-12 col-md-12 col-sm-12 col-xs-12 home-content-graphic", alt: "About icon" }),
+                  p( {style: { fontWeight: '400' }}, ["By using these machine readable consent form clauses, IRBs, funders, and investigators establish how their dataset(s) may be used, specifically the data use limitations, according to the international DUO standard - removing the need for latter interpretation of the consent forms, and greatly aiding DACs in reviewing DARs for the dataset(s)."]),
+                ]),
               ]),
             ]),
           ]),
