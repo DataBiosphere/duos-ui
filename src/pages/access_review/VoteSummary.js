f59bd70d88a54167894e3f116344156c900c2ac9
Greg Rushton <grushton@broadinstitute.org>
2020-04-21 12:06:29 -0400

    more wip

--- src/pages/access_review/VoteSummary.js
+++ src/pages/access_review/VoteSummary.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {div, h, hh} from 'react-hyperscript-helpers';
+import {div, h, hh, hr} from 'react-hyperscript-helpers';
 import {Theme} from '../../libs/theme';
 import {Chart} from 'react-google-charts';
 import * as fp from 'lodash/fp';
@@ -20,7 +20,7 @@ const HEADER_STYLE = {
   lineHeight: Theme.font.leading.regular,
   textTransform: 'uppercase',
   margin: '16px 0px',
-  opacity: '70%'
+  opacity: '70%',
 };
 
 const QUESTION_STYLE = {
@@ -29,7 +29,7 @@ const QUESTION_STYLE = {
   fontWeight: Theme.font.weight.regular,
   textTransform: 'none',
   margin: '16px 0px',
-  borderBottom: '1px solid'
+  borderBottom: '1px solid',
 };
 
 const baseOptions = {
@@ -79,7 +79,7 @@ export const VoteSummary = hh(
           backgroundColor: '#D3E9EC',
           height: 150,
           width: 275,
-          padding: '1rem'
+          padding: '1rem',
         },
       }, [
         div({}, [
@@ -104,7 +104,26 @@ export const VoteSummary = hh(
         ['No', negativeVotes],
         ['Pending', pendingVotes],
       ];
-    }
+    };
+
+    getMemberVoteBox = (vote) => {
+      return div({
+        style: {
+          borderRadius: 9,
+          backgroundColor: '#DFE8EE',
+          height: 150,
+          width: 275,
+          padding: 0,
+          margin: '1rem'
+        },
+      }, [
+        div({style: {padding: '1rem'}}, [vote.displayName]),
+        div({style: {padding: 0, borderTop: '#BABEC1'}}, [hr({style: {borderTop: '#BABEC1'}})]),
+        div({style: {padding: '1rem'}}, [
+          'Vote: ', vote.vote
+        ]),
+      ]);
+    };
 
     render() {
       const data = this.getVoteData(this.props.votes);
@@ -118,12 +137,25 @@ export const VoteSummary = hh(
         },
       };
 
-      return div({style: STYLE, id: 'vote-summary-question-one'}, [
-        div({style: HEADER_STYLE}, ['Question ', this.props.questionNumber, ':']),
-        div({style: QUESTION_STYLE}, [this.props.question]),
-        div({style: HEADER_STYLE}, ['How committee members voted:']),
-        this.voteResultsBox(data, options),
-      ]);
+      return div({}, [
+        div({style: STYLE, id: 'vote-summary-question-one'}, [
+          div({style: HEADER_STYLE},
+            ['Question ', this.props.questionNumber, ':']),
+          div({style: QUESTION_STYLE}, [this.props.question]),
+          div({style: HEADER_STYLE}, ['How committee members voted:']),
+          div({
+            style: {
+              display: 'flex',
+              flexWrap: 'wrap',
+            },
+          },
+          [
+            this.voteResultsBox(data, options),
+            fp.map(this.getMemberVoteBox)(this.props.votes),
+          ])
+        ]),
+      ])
+      ;
     }
 
   });
