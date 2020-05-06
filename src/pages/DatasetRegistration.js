e50bc2b66544599699e7512265a7691b32bd218b
Jonathan Lawson <jlawson@broadinstitute.org>
2020-05-06 15:40:40 -0400

    Updates to Step 3

--- src/pages/DatasetRegistration.js
+++ src/pages/DatasetRegistration.js
@@ -1424,209 +1424,23 @@ class DatasetRegistration extends Component {
               ])
             ]),
 
-            div({ isRendered: this.state.step === 3 }, [
-              div({ className: 'col-lg-10 col-lg-offset-1 col-md-12 col-sm-12 col-xs-12' }, [
-                fieldset({ disabled: this.state.formData.dar_code !== null }, [
-
-                  h3({ className: 'rp-form-title access-color' }, ['3. Research Purpose Statement']),
-
-                  div({ className: 'form-group' }, [
-                    div({ className: 'col-lg-12 col-md-12 col-sm-12 col-xs-12 rp-group' }, [
-                      label({ className: 'control-label rp-title-question' },
-                        ['3.1 In order to ensure appropriate review, please answer the questions below:'])
-                    ]),
-                    div({ className: 'row no-margin' }, [
-                      span({
-                        className: 'cancel-color required-field-error-span', isRendered: step3.inputPurposes.invalid && showValidationMessages,
-                        style: { 'marginLeft': '15px' }
-                      }, ['All fields are required'])
-                    ]),
-                    div({ className: 'col-lg-12 col-md-12 col-sm-12 col-xs-12 rp-group' }, [
-                      label({ className: 'control-label rp-choice-questions' },
-                        ['3.1.1 Will this data be used exclusively or partially for a commercial purpose?'])
-                    ]),
-                    div({ className: 'col-lg-12 col-md-12 col-sm-12 col-xs-12 rp-group' }, [
-                      YesNoRadioGroup({
-                        value: this.state.formData.forProfit, onChange: this.handleRadioChange, id: 'forProfit', name: 'forProfit',
-                        required: true
-                      })
-                    ]),
-
-                    div({ className: 'col-lg-12 col-md-12 col-sm-12 col-xs-12 rp-group' }, [
-                      label({ className: 'control-label rp-choice-questions' }, ['3.1.2 Please indicate if this study is limited to one gender?'])
-                    ]),
-
-                    div({ className: 'col-lg-12 col-md-12 col-sm-12 col-xs-12 rp-group' }, [
-                      YesNoRadioGroup({
-                        value: this.state.formData.onegender, onChange: this.handleRadioChange, id: 'onegender', name: 'onegender',
-                        required: true
-                      }),
-                      div({
-                        isRendered: this.state.formData.onegender === 'true' || this.state.formData.onegender === true,
-                        className: 'multi-step-fields', disabled: (this.state.formData.dar_code !== null)
-                      }, [
-                        span({}, ['Please specify']),
-                        div({ className: 'radio-inline' }, [
-                          genderLabels.map((option, ix) => {
-                            return (
-                              label({
-                                key: 'gender' + ix,
-                                onClick: (e) => this.handleGenderChange(e, genderValues[ix]),
-                                id: 'lbl_gender_' + ix,
-                                htmlFor: 'rad_gender_' + ix,
-                                className: 'radio-wrapper'
-                              }, [
-                                input({
-                                  type: 'radio',
-                                  id: 'rad_gender_' + ix,
-                                  name: this.state.name,
-                                  checked: this.state.formData.gender === genderValues[ix],
-                                  onChange: () => { }
-                                }),
-                                span({ className: 'radio-check' }),
-                                span({ className: 'radio-label' }, [genderLabels[ix]])
-                              ])
-                            );
-                          })
-                        ])
-                      ])
-                    ]),
-
-                    div({ className: 'col-lg-12 col-md-12 col-sm-12 col-xs-12 rp-group' }, [
-                      label({ className: 'control-label rp-choice-questions' },
-                        ['3.1.3 Please indicate if this study is restricted to a  pediatric population (under the age of 18)?'])
-                    ]),
-                    div({ className: 'col-lg-12 col-md-12 col-sm-12 col-xs-12 rp-group' }, [
-                      YesNoRadioGroup({
-                        value: this.state.formData.pediatric, onChange: this.handleRadioChange, id: 'pediatric', name: 'pediatric', required: true
-                      })
-                    ]),
-
-                    div({ className: 'col-lg-12 col-md-12 col-sm-12 col-xs-12 rp-group' }, [
-                      label({ className: 'control-label rp-choice-questions' },
-                        ['3.1.4 Does the research aim involve the study of illegal behaviors (violence, domestic abuse, prostitution, sexual victimization)?'])
-                    ]),
-                    div({ className: 'col-lg-12 col-md-12 col-sm-12 col-xs-12 rp-group' }, [
-                      YesNoRadioGroup({
-                        value: this.state.formData.illegalbehave, onChange: this.handleRadioChange, id: 'illegalbehave', name: 'illegalbehave',
-                        required: true
-                      })
-                    ]),
-
-                    div({ className: 'col-lg-12 col-md-12 col-sm-12 col-xs-12 rp-group' }, [
-                      label({ className: 'control-label rp-choice-questions' },
-                        ['3.1.5 Does the research aim involve the study of alcohol or drug abuse, or abuse of other addictive products?'])
-                    ]),
-                    div({ className: 'col-lg-12 col-md-12 col-sm-12 col-xs-12 rp-group' }, [
-                      YesNoRadioGroup({
-                        value: this.state.formData.addiction, onChange: this.handleRadioChange, id: 'addiction', name: 'addiction', required: true
-                      })
-                    ]),
-
-                    div({ className: 'col-lg-12 col-md-12 col-sm-12 col-xs-12 rp-group' }, [
-                      label({ className: 'control-label rp-choice-questions' },
-                        ['3.1.6 Does the research aim involve the study of sexual preferences or sexually transmitted diseases?'])
-                    ]),
-                    div({ className: 'col-lg-12 col-md-12 col-sm-12 col-xs-12 rp-group' }, [
-                      YesNoRadioGroup({
-                        value: this.state.formData.sexualdiseases, onChange: this.handleRadioChange, id: 'sexualdiseases', name: 'sexualdiseases',
-                        required: true
-                      })
-                    ]),
-
-                    div({ className: 'col-lg-12 col-md-12 col-sm-12 col-xs-12 rp-group' }, [
-                      label({ className: 'control-label rp-choice-questions' },
-                        ['3.1.7 Does the research aim involve the study of any stigmatizing illnesses?'])
-                    ]),
-                    div({ className: 'col-lg-12 col-md-12 col-sm-12 col-xs-12 rp-group' }, [
-                      YesNoRadioGroup({
-                        value: this.state.formData.stigmatizediseases, onChange: this.handleRadioChange, id: 'stigmatizediseases',
-                        name: 'stigmatizediseases', required: true
-                      })
-                    ]),
-
-                    div({ className: 'col-lg-12 col-md-12 col-sm-12 col-xs-12 rp-group' }, [
-                      label({ className: 'control-label rp-choice-questions' },
-                        ['3.1.8 Does the study target a vulnerable population as defined in 456 CFR (children, prisoners, pregnant women, mentally disabled persons, or ["SIGNIFICANTLY"] economically or educationally disadvantaged persons)?'])
-                    ]),
-
-                    div({ className: 'col-lg-12 col-md-12 col-sm-12 col-xs-12 rp-group' }, [
-                      YesNoRadioGroup({
-                        value: this.state.formData.vulnerablepop, onChange: this.handleRadioChange, id: 'vulnerablepop', name: 'vulnerablepop',
-                        required: true
-                      })
-                    ]),
-
-                    div({ className: 'col-lg-12 col-md-12 col-sm-12 col-xs-12 rp-group' }, [
-                      label({ className: 'control-label rp-choice-questions' },
-                        ['3.1.9 Does the research aim involve the study of Population Origins/Migration patterns?'])
-                    ]),
-                    div({ className: 'col-lg-12 col-md-12 col-sm-12 col-xs-12 rp-group' }, [
-                      YesNoRadioGroup({
-                        value: this.state.formData.popmigration, onChange: this.handleRadioChange, id: 'popmigration', name: 'popmigration',
-                        required: true
-                      })
-                    ]),
-
-                    div({ className: 'col-lg-12 col-md-12 col-sm-12 col-xs-12 rp-group' }, [
-                      label({ className: 'control-label rp-choice-questions' },
-                        ['3.1.10 Does the research aim involve the study of psychological traits, including intelligence, attention, emotion?'])
-                    ]),
-                    div({ className: 'col-lg-12 col-md-12 col-sm-12 col-xs-12 rp-group' }, [
-                      YesNoRadioGroup({
-                        value: this.state.formData.psychtraits, onChange: this.handleRadioChange, id: 'psychtraits', name: 'psychtraits',
-                        required: true
-                      })
-                    ]),
-
-                    div({ className: 'col-lg-12 col-md-12 col-sm-12 col-xs-12 rp-group' }, [
-                      label({ className: 'control-label rp-choice-questions' },
-                        ['3.1.11 Does the research correlate ethnicity, race, or gender with genotypic or other phenotypic variables, for purposes beyond biomedical or health-related research, or in ways that are not easily related to Health?'])
-                    ]),
-                    div({ className: 'col-lg-12 col-md-12 col-sm-12 col-xs-12 rp-group rp-last-group' }, [
-                      YesNoRadioGroup({
-                        value: this.state.formData.nothealth, onChange: this.handleRadioChange, id: 'nothealth', name: 'nothealth', required: true
-                      })
-                    ])
-                  ])
-                ]),
-
-                div({ className: 'row no-margin' }, [
-                  div({ className: 'col-lg-12 col-md-12 col-sm-12 col-xs-12' }, [
-                    a({ id: 'btn_prev', onClick: this.step2, className: 'btn-primary f-left access-background' }, [
-                      span({ className: 'glyphicon glyphicon-chevron-left', 'aria-hidden': 'true' }), 'Previous Step'
-                    ]),
-
-                    a({ id: 'btn_next', onClick: this.step4, className: 'btn-primary f-right access-background' }, [
-                      'Next Step', span({ className: 'glyphicon glyphicon-chevron-right', 'aria-hidden': 'true' })
-                    ]),
-
-                    a({
-                      id: 'btn_save', isRendered: this.state.formData.dar_code === null, onClick: this.partialSave,
-                      className: 'f-right btn-secondary access-color'
-                    }, ['Save'])
-                  ])
-                ])
-              ])
-            ]),
-
             div({ isRendered: this.state.step === 4 }, [
               div({ className: 'col-lg-10 col-lg-offset-1 col-md-12 col-sm-12 col-xs-12' }, [
                 fieldset({ disabled: this.state.formData.dar_code !== null }, [
 
-                  h3({ className: 'rp-form-title access-color' }, ['4. Data Use Agreements']),
+                  h3({ className: 'rp-form-title dataset-color' }, ['4. Dataset Registration Agreements']),
 
                   div({ className: 'form-group' }, [
                     div({ className: 'col-lg-12 col-md-12 col-sm-12 col-xs-12' }, [
-                      label({ className: 'control-label rp-title-question' }, [
-                        '4.1 DUOS Library Card Data Access Agreement'
+                      label({ className: 'control-label rp-title-question dataset-color' }, [
+                        '4.1 DUOS Dataset Registration Agreement'
                       ])
                     ]),
 
                     div({ className: 'row no-margin' }, [
                       div({ className: 'col-lg-12 col-md-12 col-sm-12 col-xs-12 rp-group' }, [
                         label({ className: 'control-label default-color' },
-                          ['By submitting this data access request, you agree to comply with all terms relevant to Authorized Users put forth in the agreement.'])
+                          ['By submitting this dataset registration, you agree to comply with all terms relevant to Dataset Custodians put forth in the agreement.'])
                       ]),
 
                       div({ className: 'col-lg-12 col-md-12 col-sm-12 col-xs-12 rp-group' }, [
@@ -1635,37 +1449,24 @@ class DatasetRegistration extends Component {
                           className: 'col-lg-4 col-md-4 col-sm-6 col-xs-12 btn-secondary btn-download-pdf hover-color'
                         }, [
                           span({ className: 'glyphicon glyphicon-download' }),
-                          'DUOS Library Card Agreement'
+                          'DUOS Dataset Registration Agreement'
                         ])
                       ]),
 
-                      div({ className: 'col-lg-12 col-md-12 col-sm-12 col-xs-12 rp-group' }, [
-                        p({ className: 'rp-agreement' },
-                          ['For your request to be reviewed, your Signing Official must authorize you by sending a signed Library Card data access agreement']),
-                        a({ href: '/home_about', target: '_blank' }, '(instructions here)')
-                      ])
-                    ]),
-
-                    div({ className: 'row no-margin' }, [
-                      div({ className: 'col-lg-12 col-md-12 col-sm-12 col-xs-12' }, [
-                        label({ className: 'control-label rp-title-question' }, ['4.2 Attestation Statement'])
+                      div({ className: 'col-lg-12 col-md-12 col-sm-12 col-xs-12 rp-group checkbox' }, [
+                        input({
+                          type: 'checkbox',
+                          id: 'chk_collaborator',
+                          name: 'checkCollaborator',
+                          className: 'checkbox-inline rp-checkbox',
+                          disabled: this.state.formData.dar_code !== null,
+                          checked: checkCollaborator,
+                          onChange: this.handleCheckboxChange
+                        }),
+                        label({ className: 'regular-checkbox rp-choice-questions', htmlFor: 'chk_collaborator' },
+                          ['I would like this dataset to be made publicly available for data access requests via the DUOS Dataset Catalog'])
                       ]),
 
-                      div({ className: 'col-lg-12 col-md-12 col-sm-12 col-xs-12' }, [
-                        label({ className: 'control-label default-color' }, ['I attest to the following:']),
-
-                        ol({ className: 'rp-agreement rp-last-group' }, [
-                          li({}, ['Data will only be used for approved research']),
-                          li({},
-                            ['Data confidentiality will be protected and the investigator will never make any attempt at "re-identification"']),
-                          li({},
-                            ['All applicable laws, local institutional policies, and terms and procedures specific to the study’s data access policy will be followed.']),
-                          li({}, ['No attempts will be made to identify individual study participants from whom data were obtained.']),
-                          li({}, ['Data will not be sold or shared with third parties.']),
-                          li({},
-                            ['The contributing investigator(s) who conducted the original study and the funding organizations involved in supporting the original study will be acknowledged in publications resulting from the analysis of those data.'])
-                        ])
-                      ])
                     ]),
 
                     div({ className: 'row no-margin' }, [
@@ -1685,24 +1486,24 @@ class DatasetRegistration extends Component {
 
                 div({ className: 'row no-margin' }, [
                   div({ className: 'col-lg-12 col-md-12 col-sm-12 col-xs-12' }, [
-                    a({ id: 'btn_prev', onClick: this.step3, className: 'f-left btn-primary access-background' }, [
+                    a({ id: 'btn_prev', onClick: this.step3, className: 'f-left btn-primary dataset-background' }, [
                       span({ className: 'glyphicon glyphicon-chevron-left', 'aria-hidden': 'true' }), 'Previous Step'
                     ]),
 
                     a({
                       id: 'btn_submit', isRendered: this.state.formData.dar_code === null, onClick: this.attestAndSave,
-                      className: 'f-right btn-primary access-background bold'
-                    }, ['Attest and Send']),
+                      className: 'f-right btn-primary dataset-background bold'
+                    }, ['Register in DUOS!']),
 
                     ConfirmationDialog({
                       title: 'Data Request Confirmation', disableOkBtn: this.state.disableOkBtn, disableNoBtn: this.state.disableOkBtn,
-                      color: 'access', showModal: this.state.showDialogSubmit, action: { label: 'Yes', handler: this.dialogHandlerSubmit }
+                      color: 'dataset', showModal: this.state.showDialogSubmit, action: { label: 'Yes', handler: this.dialogHandlerSubmit }
                     }, [div({ className: 'dialog-description' }, ['Are you sure you want to send this Data Access Request Application?'])]),
                     h(ReactTooltip, { id: 'tip_clearNihAccount', place: 'right', effect: 'solid', multiline: true, className: 'tooltip-wrapper' }),
 
                     a({
                       id: 'btn_save', isRendered: this.state.formData.dar_code === null, onClick: this.partialSave,
-                      className: 'f-right btn-secondary access-color'
+                      className: 'f-right btn-secondary dataset-color'
                     }, ['Save'])
                   ])
                 ])
