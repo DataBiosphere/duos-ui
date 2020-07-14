b2d832cce8fd13da8a60db0dc192fc0111793827
Marianie-Simeon <msimeon@broadinstitue.org>
2020-07-14 10:23:43 -0400

    PR Quality Review

--- src/components/modals/SupportRequestModal.js
+++ src/components/modals/SupportRequestModal.js
@@ -97,7 +97,7 @@ export const SupportRequestModal = hh(class SupportRequestModal extends Componen
       let emailText = e.target.value;
       this.setState(prev => {
         prev.email = emailText;
-        prev.valid = /.+@.+\.[A-Za-z]+$/.test(emailText) ? true: false; 
+        prev.valid = /.+@.+\.[A-Za-z]+$/.test(emailText) ? true: false;
         return prev;
       });
     };
@@ -120,38 +120,38 @@ export const SupportRequestModal = hh(class SupportRequestModal extends Componen
         action: { label: 'Submit', handler: this.OKHandler },
       },
 
-        [
-          form({ className: 'form-horizontal css-form', name: 'zendeskTicketForm', noValidate: true, encType: 'multipart/form-data' }, [
-            !this.state.isLogged && div({ className: 'form-group first-form-group' }, [
-              label({ id: 'lbl_name', className: 'common-color' }, ['Name *']),
-              input({ id: 'txt_name', placeholder:'What should we call you?', value: this.state.name, className: 'form-control col-lg-12', onChange: this.nameChangeHandler, required: true }),
-            ]),
-            div({ className: 'form-group first-form-group' }, [
-              label({ id: 'lbl_type', className: 'common-color' }, ['Type *']),
-              select({id: 'txt_question', className: 'col-lg-12 select-wrapper', value: this.state.type, onChange: this.typeChangeHandler, required: true}, [
-                option({ value: 'question'} , ['Question']),
-                option({ value: 'bug'}, ['Bug']),
-                option({ value: 'feature_request'}, ['Feature Request'])
-              ]),  
-            ]),
-            div({ className: 'form-group first-form-group' }, [
-              label({ id: 'lbl_description', className: 'common-color' }, ['How can we help you' + this.state.first_name + '? *']),
-              input({ id: 'txt_subject', placeholder:'Enter a subject', rows: '5', className: 'form-control col-lg-12 vote-input', onChange: this.subjectChangeHandler, required: true }),
-              textarea({ id: 'txt_description', placeholder:'Enter a description', rows: '5', className: 'form-control col-lg-12 vote-input', onChange: this.descriptionChangeHandler, required: true }),
-            ]),
-            div({ className: 'form-group first-form-group' }, [
-              label({ id: 'lbl_attachment', className: 'common-color' }, ['Attachment']),
-              input({ type: 'file', id: 'txt_attachment', placeholder: 'Attach a file?', className: 'form-control col-lg-12 vote-input common-color', onChange: this.attachmentChangeHandler, ref: 'fileUpload', required: false }),
-            ]),
-            !this.state.isLogged && div({ className: 'form-group first-form-group' }, [
-              label({ id: 'lbl_email', className: 'common-color' }, ['Contact email *']),
-              input({ id: 'txt_email', className: 'form-control col-lg-12 vote-input',  placeholder:'Enter a email', value: this.state.email, onChange: this.emailChangeHandler, required: true }),
-            ]),
-          ]),
-          div({ isRendered: false }, [
-            Alert({ id: 'modal', type: 'danger', title: alert.title, description: alert.msg })
-          ])
-        ])
+    [
+      form({ className: 'form-horizontal css-form', name: 'zendeskTicketForm', noValidate: true, encType: 'multipart/form-data' }, [
+        !this.state.isLogged && div({ className: 'form-group first-form-group' }, [
+          label({ id: 'lbl_name', className: 'common-color' }, ['Name *']),
+          input({ id: 'txt_name', placeholder:'What should we call you?', value: this.state.name, className: 'form-control col-lg-12', onChange: this.nameChangeHandler, required: true }),
+        ]),
+        div({ className: 'form-group first-form-group' }, [
+          label({ id: 'lbl_type', className: 'common-color' }, ['Type *']),
+          select({id: 'txt_question', className: 'col-lg-12 select-wrapper', value: this.state.type, onChange: this.typeChangeHandler, required: true}, [
+            option({ value: 'question'} , ['Question']),
+            option({ value: 'bug'}, ['Bug']),
+            option({ value: 'feature_request'}, ['Feature Request'])
+          ]),  
+        ]),
+        div({ className: 'form-group first-form-group' }, [
+          label({ id: 'lbl_description', className: 'common-color' }, ['How can we help you' + this.state.first_name + '? *']),
+          input({ id: 'txt_subject', placeholder:'Enter a subject', rows: '5', className: 'form-control col-lg-12 vote-input', onChange: this.subjectChangeHandler, required: true }),
+          textarea({ id: 'txt_description', placeholder:'Enter a description', rows: '5', className: 'form-control col-lg-12 vote-input', onChange: this.descriptionChangeHandler, required: true }),
+        ]),
+        div({ className: 'form-group first-form-group' }, [
+          label({ id: 'lbl_attachment', className: 'common-color' }, ['Attachment']),
+          input({ type: 'file', id: 'txt_attachment', placeholder: 'Attach a file?', className: 'form-control col-lg-12 vote-input common-color', onChange: this.attachmentChangeHandler, ref: 'fileUpload', required: false }),
+        ]),
+        !this.state.isLogged && div({ className: 'form-group first-form-group' }, [
+          label({ id: 'lbl_email', className: 'common-color' }, ['Contact email *']),
+          input({ id: 'txt_email', className: 'form-control col-lg-12 vote-input',  placeholder:'Enter a email', value: this.state.email, onChange: this.emailChangeHandler, required: true }),
+        ]),
+      ]),
+      div({ isRendered: false }, [
+        Alert({ id: 'modal', type: 'danger', title: alert.title, description: alert.msg })
+      ])
+    ])
     );
   }
 });
