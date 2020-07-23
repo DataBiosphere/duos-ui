7ebb403bcfc85538fab56780aae05d8df91cee4f
Justin Thomas <jthomas0209@gmail.com>
2020-07-23 09:50:55 -0400

    DUOS-591 installed react-ga package, reafactored App component for history access via useHistory (needed to listen for page changes), basic ga plumbing setup

--- src/App.js
+++ src/App.js
@@ -1,77 +1,66 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import ReactGA from 'react-ga';
 import { div, h } from 'react-hyperscript-helpers';
 import Modal from 'react-modal';
 import './App.css';
 import DuosFooter from './components/DuosFooter';
 import DuosHeader from './components/DuosHeader';
+import {useHistory} from 'react-router-dom'
 
 import { SpinnerComponent as Spinner } from './components/SpinnerComponent';
 import { Storage } from './libs/storage';
 import Routes from './Routes';
 
+function App() {
+  const [loading, setLoading] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  let history = useHistory();
 
-class App extends React.Component {
+  const initializeReactGA = () => {
+    ReactGA.initialize('UA-173273990-1', {
+      titleCase: false
+    });
+  };
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      loading: false,
-      isLoggedIn: false
+  useEffect(() => {
+    const setUserIsLogged = async () => {
+      const isLogged = await Storage.userIsLogged();
+      setIsLoggedIn(isLogged);
     };
-  }
 
-  showSpinner = () => {
-    this.setState({
-      loading: true
+    Modal.setAppElement(document.getElementById('modal-root'));
+    initializeReactGA();
+    history.listen((location) => {
+      ReactGA.pageview(location.pathname + location.search);
     });
-  };
+    setUserIsLogged();
+  }, []);
 
-  hideSpinner = () => {
-    this.setState({
-      loading: false
-    });
-  };
-
-  async componentDidMount() {
-    const isLogged = await Storage.userIsLogged();
-    this.setState({ isLoggedIn: isLogged });
-  };
-
-  signOut = () => {
+  const signOut = () => {
     Storage.setUserIsLogged(false);
     Storage.clearStorage();
-    this.setState({ isLoggedIn: false });
+    setIsLoggedIn(false);
   };
 
-  signIn = () => {
+  const signIn = () => {
     Storage.setUserIsLogged(true);
-    this.setState({ isLoggedIn: true });
+    setIsLoggedIn(true);
   };
 
-  componentWillMount() {
-    Modal.setAppElement(document.getElementById('modal-root'));
-  }
-
-  render() {
-
-    const { loading } = this.state;
-
-    return (
-      div({ className: 'body' }, [
-        div({ className: 'wrap' }, [
-          div({ className: 'main' }, [
-            h(DuosHeader, { onSignOut: this.signOut }),
-            h(Spinner, {
-              name: 'mainSpinner', group: 'duos', loadingImage: '/images/loading-indicator.svg'
-            }),
-            h(Routes, { onSignIn: this.signIn, isRendered: !loading, isLogged: this.state.isLoggedIn })
-          ])
-        ]),
-        h(DuosFooter, { isLogged: this.state.isLoggedIn })
-      ])
-
-    );
-  }
+  return (
+    div({ className: 'body' }, [
+      div({ className: 'wrap' }, [
+        div({ className: 'main' }, [
+          h(DuosHeader, { onSignOut: signOut }),
+          h(Spinner, {
+            name: 'mainSpinner', group: 'duos', loadingImage: '/images/loading-indicator.svg'
+          }),
+          h(Routes, { onSignIn: signIn, isRendered: !loading, isLogged: isLoggedIn })
+        ])
+      ]),
+      h(DuosFooter, { isLogged: isLoggedIn })
+    ])
+  );
 }
 
 export default App;
