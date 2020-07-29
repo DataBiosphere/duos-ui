c14bb7624725b06b88a96aa8ef87629d4dcd31e1
lindojong <43456581+lindojong@users.noreply.github.com>
2020-07-29 15:35:08 -0400

    add Status back in

--- src/components/DuosFooter.js
+++ src/components/DuosFooter.js
@@ -1,7 +1,10 @@
 import { a, div, footer, img, li, ul } from 'react-hyperscript-helpers';
 import { Storage } from '../libs/storage';
+import _ from 'lodash';
 
 const DuosFooter = (props) => {
+  const user = Storage.getCurrentUser();
+
   const isAdmin = props.isLogged ? Storage.getCurrentUser().isAdmin : false;
   const supportLink = isAdmin ? '/help_reports' : '/home_help';
 
@@ -13,7 +16,8 @@ const DuosFooter = (props) => {
           li({ className: "footer-links__item" }, ["\u00A9 Broad Institute"]),
           li({ className: "footer-links__item" }, [a({ target: '_blank', href: "https://www.broadinstitute.org/privacy-policy" }, ["Privacy Policy"]),]),
           li({ className: "footer-links__item" }, [a({ target: '_blank', href: "https://www.broadinstitute.org/terms-conditions" }, ["Terms of Service"]),]),
-          li({ className: "footer-links__item" }, [a({ href: supportLink }, ["Support"]),])
+          li({ className: "footer-links__item" }, [a({ href: supportLink }, ["Support"]),]),
+          li({ className: 'footer-links__item' }, [a({ href: '/status' }, ['Status'])])
         ])
       ])
     ])
