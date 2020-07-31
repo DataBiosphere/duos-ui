43e71bd89525393ba0b5049754bbf192d382683f
lindojong <43456581+lindojong@users.noreply.github.com>
2020-07-31 12:06:58 -0400

    new ReadMore component that uses preformatted hyperscript for content

---
+++ src/components/HomeReadMore.js
@@ -0,0 +1,56 @@
+import get from 'lodash/get';
+import { Component } from 'react';
+import { a, div, hh, span } from 'react-hyperscript-helpers';
+import ReactTooltip from 'react-tooltip';
+
+
+export const HomeReadMore = hh(class HomeReadMore extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      expanded: false,
+      content: get(this.props, 'content', [span({}, [''])]),
+      moreContent: get(this.props, 'moreContent', [span({}, [''])]),
+      className: get(this.props, 'className', ''),
+      style: get(this.props, 'style', {}),
+      readMoreText: get(this.props, 'readMoreText', 'Read More'),
+      readLessText: get(this.props, 'readLessText', 'Read Less')
+    };
+  }
+
+  componentDidMount() {
+    ReactTooltip.rebuild();
+  }
+
+  getContent = () => {
+    if (this.state.expanded) {
+      return [...this.state.content, ...this.state.moreContent];
+    } else {
+      return this.state.content;
+    }
+  };
+
+  readMore = () => {
+    this.setState(prev => {
+      prev.expanded = true;
+      return prev;
+    });
+  };
+
+  readLess = () => {
+    this.setState(prev => {
+      prev.expanded = false;
+      return prev;
+    });
+  };
+
+  render() {
+    const readLink = this.state.expanded ?
+      a({ onClick: () => this.readLess() }, [this.state.readLessText]) :
+      a({ onClick: () => this.readMore() }, [this.state.readMoreText]);
+    return div({}, [
+      this.getContent(),
+      readLink
+    ]);
+  }
+});
