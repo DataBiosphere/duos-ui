256b2e475c41d1ca67f436dcde003c0faecf18aa
lindojong <43456581+lindojong@users.noreply.github.com>
2020-08-03 11:29:04 -0400

    cleaned up formatting a little

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,7 @@ import { Link } from 'react-router-dom';
 class Home extends Component {
 
   render() {
-  const hometitle = {
+  const homeTitle = {
       color: '#FFFFFF',
       fontFamily: 'Montserrat',
       fontSize: '28px',
@@ -16,12 +16,13 @@ class Home extends Component {
       textAlign: 'center'
     }
 
-    const homebannerdescription = {
+    const homeBannerDescription = {
       color: '#FFFFFF',
       fontFamily: 'Montserrat',
       fontSize: '20px',
       textAlign: 'center',
-      whiteSpace: 'pre-wrap'
+      whiteSpace: 'pre-wrap',
+      padding: '0 10rem'
     }
 
     const header = {
@@ -32,7 +33,7 @@ class Home extends Component {
       textAlign: 'center'
     }
 
-    const subheader = {
+    const subHeader = {
       color: '#1F3B50',
       fontFamily: 'Montserrat',
       fontSize: '16px',
@@ -43,7 +44,7 @@ class Home extends Component {
 
     const paragraph = {
       color: '#1F3B50',
-      margin: '0 210px 2rem 210px',
+      padding: '0 10rem 2rem 10rem',
       fontFamily: 'Montserrat',
       fontSize: '14px',
       textAlign: 'center',
@@ -66,66 +67,84 @@ class Home extends Component {
     return (
 
       div({ className: 'row' }, [
-        div({ className: 'col-lg-12' }, [
+        div({ className: 'col-lg-12 col-md-12 col-sm-12 col-xs-12' }, [
           div({ className: 'row no-margin', style: { backgroundColor: 'white', height: '350px', position: 'relative' }}, [
             img({ style: { height: 'inherit', minWidth: '100%', transform: 'scale(1.1,1)', zIndex: '-99' }, src: '/images/home_header_background.png'}),
-            div({ style: {position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)'} }, [
+            div({ style: { position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)'}}, [
               img({ style: { height: '80px', width: '300px', display: 'block', margin: 'auto auto' }, src: '/images/duos_logo.svg' }),
-              h1({ style: hometitle }, ['Data Use Oversight System']),
-              div({ style: homebannerdescription }, ['Expediting data access for researchers, by facilitating and \nenhancing data access committee\'s workflows'])
+              h1({ style: homeTitle }, ['Data Use Oversight System']),
+              div({ style: homeBannerDescription }, [
+                'Expediting data access for researchers, by facilitating and enhancing data access committee\'s workflows'])
             ]),
-            div({ style: { padding: '2em 2em 0 0', display: 'flex', alignItems: 'center', position: 'absolute', top: '1rem', right: '1rem'} }, [
+            div({ style: { padding: '2em 2em 0 0', display: 'flex', alignItems: 'center', position: 'absolute', top: '1rem', right: '1rem'}}, [
               span({ style: { color: '#FFFFFF', position: 'relative', float: 'left', margin: 'auto 1rem'}}, ['Already registered?']),
-              SignIn({ props: this.props, onSignIn: () => this.props.onSignIn(), history: this.props.history, style: { position: 'relative', float: 'right'} })
-              ]),
+              SignIn({ props: this.props, onSignIn: () => this.props.onSignIn(), history: this.props.history, style: { position: 'relative', float: 'right'}})
+            ]),
           ]),
           div({ className: 'row' }, [
-            div({ style: { margin: '50px auto' } }, [
+            div({ style: { margin: '50px auto' }}, [
               h1({ style: header }, ['What is DUOS and how does it work?']),
-              h3( { style: subheader }, ['DUOS is a semi-automated data access management service which governs compliant \nsecondary use of human genomics data by leveraging:']),
+              h3({ style: subHeader },
+                ['DUOS is a semi-automated data access management service which governs compliant \nsecondary use of human genomics data by leveraging:']),
               div({}, [
                 img({
-                  className: 'col-lg-12 col-md-12 col-sm-12 col-xs-12', style: { padding: '1rem 180px', margin: 'auto auto' }, alt: 'What is DUOS graphic', src: '/images/duos_process_flow.png'
+                  className: 'col-lg-12 col-md-12 col-sm-12 col-xs-12',
+                  style: { padding: '1rem 180px', margin: 'auto auto' },
+                  alt: 'What is DUOS graphic',
+                  src: '/images/duos_process_flow.png'
                 })
               ])
             ]),
           ]),
           div({ className: 'row' }, [
             div({ className: 'row', style: { background: '#eff0f2', margin: '50px 0', padding: '60px 0 72px 0' } }, [
-            div({ className: 'col-lg-4 col-lg-offset-1' }, [
-              p({ style: header }, ['Are you a researcher?']),
-              p( { style: {...paragraph, margin: 'auto auto'} }, ['Creating a DUOS account is quick and easy.\nLorem ipsum dolor sit amet, consectetur adipiscing.\nDuis aute irure dolor in reprehenderit.']),
-                div({className:'row', style: {display: 'block', margin: 'auto auto', position: 'relative', left: '25%'}}, [
+              div({ className: 'col-lg-4 col-lg-offset-1' }, [
+                p({ style: header }, ['Are you a researcher?']),
+                p({ style: {...paragraph, padding: '0'}}, [
+                  'Creating a DUOS account is quick and easy.\nLorem ipsum dolor sit amet, consectetur adipiscing.\nDuis aute irure dolor in reprehenderit.']),
+                  div({className:'row', style: {display: 'block', margin: 'auto auto', position: 'relative', left: '25%'}}, [
+                    button({ className: 'btn-primary', style: buttonStyle }, [
+                      a({
+                        href: 'https://accounts.google.com/SignUp?continue:https%3A%2F%2Faccounts.google.com%2Fo%2Foauth2%2Fauth%3Fopenid.realm%26scope%3Demail%2Bprofile%2Bopenid%26response_type%3Dpermission%26redirect_uri%3Dstoragerelay%3A%2F%2Fhttp%2Flocalhost%3A8000%3Fid%253Dauth721210%26ss_domain%3Dhttp%3A%2F%2Flocalhost%3A8000%26client_id%3D832251491634-smgc3b2pogqer1mmdrd3hrqic3leof3p.apps.googleusercontent.com%26fetch_basic_profile%3Dtrue%26hl%3Des-419%26from_login%3D1%26as%3D43c5de35a7316d00&ltmpl:popup',
+                        style: { color: '#fff' }
+                        }, ['REGISTER'])
+                    ])
+                  ])
+              ]),
+              div({ className: 'col-lg-4 col-lg-offset-2'}, [
+                p({ style: header }, ['Are you a DAC member?']),
+                p({ style: {...paragraph, padding: '0'}}, [
+                  'Learn how managing data access and\nlorem ipsum dolor sit amet, consectetur\nadipiscing elit.']),
+                div({className:'row', style: { display: 'block', margin: 'auto auto', position: 'relative', left: '25%' }}, [
                   button({ className: 'btn-primary', style: buttonStyle }, [
-                    a({
-                      href: 'https://accounts.google.com/SignUp?continue:https%3A%2F%2Faccounts.google.com%2Fo%2Foauth2%2Fauth%3Fopenid.realm%26scope%3Demail%2Bprofile%2Bopenid%26response_type%3Dpermission%26redirect_uri%3Dstoragerelay%3A%2F%2Fhttp%2Flocalhost%3A8000%3Fid%253Dauth721210%26ss_domain%3Dhttp%3A%2F%2Flocalhost%3A8000%26client_id%3D832251491634-smgc3b2pogqer1mmdrd3hrqic3leof3p.apps.googleusercontent.com%26fetch_basic_profile%3Dtrue%26hl%3Des-419%26from_login%3D1%26as%3D43c5de35a7316d00&ltmpl:popup',
-                      style: {color: '#fff'}
-                      }, ['REGISTER'])
-                    ])])
-            ]),
-            div({ className: 'col-lg-4 col-lg-offset-2'}, [
-              p({ style: header }, ['Are you a DAC member?']),
-              p( { style: {...paragraph, margin: 'auto auto'} }, ['Learn how managing data access and\nlorem ipsum dolor sit amet, consectetur\nadipiscing elit.']),
-              div({className:'row', style: {display: 'block', margin: 'auto auto', position: 'relative', left: '25%'}}, [
-                button({ className: 'btn-primary', style: buttonStyle }, [
-                  a({href: 'home_about', style: {color: '#fff'}}, ['LEARN MORE'])
-                ])])
+                    a({href: 'home_about', style: {color: '#fff'}}, ['LEARN MORE'])
+                  ])
+                ])
+              ])
             ])
           ]),
-          ]),
           div({ className: 'row' }, [
-          div({ className: 'col-lg-10 col-lg-offset-1' }, [
-          div({}, [
-            h1({style: header}, ['About DUOS']),
-            h3({style: subheader}, ['Overview of the system and development']),
-            HomeReadMore({ props: this.props, style: { fontFamily: 'Montserrat', fontSize: '14px', fontWeight: 500, textAlign: 'center', display: 'block' }, content: [p({style:paragraph}, ['Increasingly, a major challenge to data sharing is navigating the complex web of restrictions on secondary data use. Human subjects datasets often have complex and/or ambiguous restrictions on future use deduced from the original consent form, which must be respected when utilizing data. Previously, such data use restrictions were uniquely drafted across institutions, creating vast inconsistencies and requiring the investment of significant human effort to determine if researchers should be permitted to use the data.'])], moreContent: [div({}, [p({style:paragraph}, 'As part of our efforts to enhance collaborative research, the Broad Institute developed the “Data Use Oversight System” (DUOS) to semi-automate and efficiently manage compliant sharing of human subjects data. DUOS\' objective is two-fold, to enhance data access committee\'s confidence that data use restrictions are respected while efficiently enabling appropriate data access.'),
-            p({style:paragraph}, 'To better enable the use of existing human subjects datasets in future projects, DUOS mimics, in a semi-automated fashion, the data access request review processes common to DACs globally, like those in dbGaP. To this end, the system includes interfaces to capture and structure data use restrictions and data access requests as machine-readable data use terms based on the GA4GH\'s Data Use Ontology. With these machine-readable terms for dataset\'s use limitations and data access requests established, DUOS is able to trigger a matching algorithm to reason if data access should be granted given the research purpose and the data restrictions, serving as a decision support tool for DACs using DUOS.'),
-            p({style:paragraph}, 'To evaluate the feasibility of using machine readable data use terms to interpret data use restrictions and access requests, we are piloting a trial of DUOS overseen by Partners’ Healthcare IRB. During the pilot, DACs comprised of governmental and non-governmental data custodians will pilot the use of DUOS, its ability to structure use limitations and access requests, and the accuracy of the DUOS algorithm. This aids us in improving the DUOS algorithm and providing feedback on the GA4GH Data Use Ontology based on experts’ feedback.')
-            ])]}, [p({style:paragraph}, ['child text']),
-          ])])
-        ])
+            div({ className: 'col-lg-10 col-lg-offset-1' }, [
+              div({}, [
+                h1({ style: header }, ['About DUOS']),
+                h3({ style: subHeader }, ['Overview of the system and development']),
+                HomeReadMore({
+                  props: this.props,
+                  style: { fontFamily: 'Montserrat', fontSize: '14px', fontWeight: 500, textAlign: 'center', display: 'block' },
+                  content: [
+                    p({ style: paragraph }, [
+                      'Increasingly, a major challenge to data sharing is navigating the complex web of restrictions on secondary data use. Human subjects datasets often have complex and/or ambiguous restrictions on future use deduced from the original consent form, which must be respected when utilizing data. Previously, such data use restrictions were uniquely drafted across institutions, creating vast inconsistencies and requiring the investment of significant human effort to determine if researchers should be permitted to use the data.'
+                  ])],
+                  moreContent: [
+                    div({}, [
+                      p({ style: paragraph }, 'As part of our efforts to enhance collaborative research, the Broad Institute developed the “Data Use Oversight System” (DUOS) to semi-automate and efficiently manage compliant sharing of human subjects data. DUOS\' objective is two-fold, to enhance data access committee\'s confidence that data use restrictions are respected while efficiently enabling appropriate data access.'),
+                      p({ style: paragraph }, 'To better enable the use of existing human subjects datasets in future projects, DUOS mimics, in a semi-automated fashion, the data access request review processes common to DACs globally, like those in dbGaP. To this end, the system includes interfaces to capture and structure data use restrictions and data access requests as machine-readable data use terms based on the GA4GH\'s Data Use Ontology. With these machine-readable terms for dataset\'s use limitations and data access requests established, DUOS is able to trigger a matching algorithm to reason if data access should be granted given the research purpose and the data restrictions, serving as a decision support tool for DACs using DUOS.'),
+                      p({ style: paragraph }, 'To evaluate the feasibility of using machine readable data use terms to interpret data use restrictions and access requests, we are piloting a trial of DUOS overseen by Partners’ Healthcare IRB. During the pilot, DACs comprised of governmental and non-governmental data custodians will pilot the use of DUOS, its ability to structure use limitations and access requests, and the accuracy of the DUOS algorithm. This aids us in improving the DUOS algorithm and providing feedback on the GA4GH Data Use Ontology based on experts’ feedback.')
+                ])]}),
+              ])
+            ])
           ])
-          ]),
+        ])
       ])
     );
   }
