d01cd96ee0ec1543ca73aeeb425babc1b6fefa34
lindojong <43456581+lindojong@users.noreply.github.com>
2020-08-03 12:49:49 -0400

    update home page test

--- cypress/integration/home_spec.js
+++ cypress/integration/home_spec.js
@@ -5,8 +5,8 @@ describe('Home', function() {
   it('Home page loads correctly', function() {
     cy.visit('');
     cy.contains('DUOS');
-    cy.contains('Start here!');
-    cy.contains('What is DUOS?');
+    cy.contains('Sign in with Google');
+    cy.contains('What is DUOS and how does it work?');
   });
 
 });
