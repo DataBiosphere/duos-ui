141d6824207672664b4a9be49bf8641f43e7af8b
Justin Variath Thomas <JVThomas@users.noreply.github.com>
2020-08-04 09:48:26 -0400

    DUOS-674 UI: DAR Application Step 1 Component (#536)
    * DUOS-674 initial conversion of step1 into separate component, currently testing out
    
    * DUOS-674 corrected relative path to eRACommons component in ResearcherInfo
    
    * DUOS-674 fixed incorrect hypertext syntax for step 1 render, currently debugging alert rendering errors
    
    * DUOS-674 fixed incorrect hyperscript syntax for profile alert components
    
    * DUOS-674 corrected disabled and onChange values for collaborator checkbox
    
    * DUOS-674 corrected value assignment for researcher validation check, added comments for future reference
    
    * DUOS-674 switched linkedin, orcid, and researchGate to props, associated inputs are now disabled to enforce read-only
    
    * DUOS-674 added useEffect hook to update checkCollaborator value after initial render
    
    * DUOS-674 removed trailing spaces
    
    * DUOS-674 corrected muscle memory and replaced jsx extension with js for ResearcherInfo
    
    * DUOS-674 removed comments, replaced null checks with isNil calls
    
    * DUOS-674 updated label for question 1.3

---
+++ src/pages/dar_application/ResearcherInfo.js
@@ -0,0 +1,178 @@
+import { useState, useEffect } from 'react';
+import { Alert } from '../../components/Alert';
+import { Link } from 'react-router-dom';
+import { a, div, fieldset, h, h3, input, label, span } from 'react-hyperscript-helpers';
+import { eRACommons } from '../../components/eRACommons';
+import isNil from 'lodash/fp/isNil';
+
+const profileLink = h(Link, {to:'/profile', className:'hover-color'}, ['Your Profile']);
+
+const profileUnsubmitted = span(["Please submit ", profileLink, " to be able to create a Data Access Request"]);
+
+const profileSubmitted = span(["Please make sure ", profileLink, " is updated as it will be used to pre-populate parts of the Data Access Request"]);
+
+export default function ResearcherInfo(props) {
+  const [checkCollaborator, setCheckCollaborator] = useState(props.checkCollaborator);
+
+  useEffect(() => {
+    setCheckCollaborator(props.checkCollaborator);
+  }, [props.checkCollaborator]);
+
+  //helper function to coordinate local state changes as well as updates to form data on the parent
+  const formStateChange = (stateVarSetter, attr, event) => {
+    const name = event.target.name;
+    const value = event.target[attr];
+    props.formFieldChange(name, value);
+    stateVarSetter(value);
+  };
+
+  return (
+    div({ className: 'col-lg-10 col-lg-offset-1 col-md-12 col-sm-12 col-xs-12' }, [
+      fieldset({ disabled: !isNil(props.darCode) }, [
+
+        div({ isRendered: props.completed === false, className: 'rp-alert' }, [
+          Alert({ id: 'profileUnsubmitted', type: 'danger', title: profileUnsubmitted })
+        ]),
+        div({ isRendered: props.completed === true, className: 'rp-alert' }, [
+          Alert({ id: 'profileSubmitted', type: 'info', title: profileSubmitted })
+        ]),
+
+        h3({ className: 'rp-form-title access-color' }, ['1. Researcher Information']),
+
+        div({ className: 'form-group' }, [
+          div({ className: 'col-lg-12 col-md-12 col-sm-12 col-xs-12 rp-group' }, [
+            label({ className: 'control-label rp-title-question' }, ['1.1 Researcher*'])
+          ]),
+
+          div({ className: 'col-lg-12 col-md-12 col-sm-12 col-xs-12 rp-group' }, [
+            input({
+              type: 'text',
+              name: 'researcher',
+              id: 'inputResearcher',
+              value: props.researcher,
+              disabled: true,
+              className: props.invalidResearcher && props.showValidationMessages ? 'form-control required-field-error' : 'form-control',
+              required: true
+            }),
+            span({
+              isRendered: (props.invalidResearcher) && (props.showValidationMessages), className: 'cancel-color required-field-error-span'
+            }, ['Required field'])
+          ]),
+
+          div({ className: 'col-lg-12 col-md-12 col-sm-12 col-xs-12 rp-group checkbox' }, [
+            input({
+              type: 'checkbox',
+              id: 'chk_collaborator',
+              name: 'checkCollaborator',
+              className: 'checkbox-inline rp-checkbox',
+              disabled: !isNil(props.darCode),
+              checked: checkCollaborator,
+              onChange: (e) => formStateChange(setCheckCollaborator, 'checked', e)
+            }),
+            label({ className: 'regular-checkbox rp-choice-questions', htmlFor: 'chk_collaborator' },
+              ['I am an NIH Intramural researcher (NIH email required), or internal collaborator of the PI for the selected dataset(s)'])
+          ]),
+
+          div({ className: 'col-lg-12 col-md-12 col-sm-6 col-xs-12' }, [
+            label({ className: 'control-label rp-title-question' }, [
+              '1.2 Researcher Identification',
+              div({ isRendered: checkCollaborator !== true, className: 'display-inline' }, ['*']),
+              div({ isRendered: checkCollaborator === true, className: 'display-inline italic' }, [' (optional)']),
+              span({ className: 'default-color' },
+                ['Please autenticate with ',
+                  a({ target: '_blank', href: 'https://era.nih.gov/reg-accounts/register-commons.htm' }, ['eRA Commons']), ' in order to proceed. Your ORCID iD is optional.'
+                ])
+            ])
+          ]),
+
+          span({
+            isRendered: (props.showValidationMessages && !props.nihValid), className: 'col-lg-12 col-md-12 col-sm-6 col-xs-12 cancel-color required-field-error-span'
+          }, ['NIH eRA Authentication is required']),
+
+          div({ className: 'row no-margin' }, [
+            eRACommons({
+              className: 'col-lg-6 col-md-6 col-sm-6 col-xs-12 rp-group',
+              destination: props.eRACommonsDestination,
+              onNihStatusUpdate: props.onNihStatusUpdate,
+              location: props.location,
+              validationError: props.showValidationMessages
+            }),
+            div({ className: 'col-lg-6 col-md-6 col-sm-6 col-xs-12 rp-group' }, [
+              label({ className: 'control-label' }, ['LinkedIn Profile']),
+              input({
+                type: 'text',
+                name: 'linkedIn',
+                id: 'inputLinkedIn',
+                value: props.linkedIn,
+                disabled: true,
+                className: 'form-control',
+              })
+            ])
+          ]),
+
+          div({ className: 'row no-margin' }, [
+            div({ className: 'col-lg-6 col-md-6 col-sm-6 col-xs-12' }, [
+              label({ className: 'control-label' }, ['ORCID iD']),
+              input({
+                type: 'text',
+                name: 'orcid',
+                id: 'inputOrcid',
+                value: props.orcid,
+                disabled: true,
+                className: 'form-control',
+              })
+            ]),
+
+            div({ className: 'col-lg-6 col-md-6 col-sm-6 col-xs-12' }, [
+              label({ className: 'control-label' }, ['ResearchGate ID']),
+              input({
+                type: 'text',
+                name: 'researcherGate',
+                id: 'inputResearcherGate',
+                value: props.researcherGate,
+                disabled: true,
+                className: 'form-control',
+              })
+            ])
+          ])
+        ]),
+
+        div({ className: 'form-group' }, [
+          div({ className: 'col-lg-12 col-md-12 col-sm-12 col-xs-12 rp-group' }, [
+            label({ className: 'control-label rp-title-question' }, [
+              '1.3 Principal Investigator* ',
+              span({}, ['I certify that the principal investigator listed below is aware of this study'])
+            ])
+          ]),
+          div({ className: 'col-lg-12 col-md-12 col-sm-12 col-xs-12 rp-group' }, [
+            input({
+              type: 'text',
+              name: 'investigator',
+              id: 'inputInvestigator',
+              value: props.investigator,
+              disabled: true,
+              className: props.invalidInvestigator && props.showValidationMessages ? 'form-control required-field-error' : 'form-control',
+              required: true
+            }),
+            span({
+              className: 'cancel-color required-field-error-span', isRendered: (props.invalidInvestigator) && (props.showValidationMessages)
+            }, ['Required field'])
+          ])
+        ])
+      ]),
+
+      div({ className: 'row no-margin' }, [
+        div({ className: 'col-lg-12 col-md-12 col-sm-12 col-xs-12' }, [
+          a({ id: 'btn_next', onClick: props.step2, className: 'btn-primary f-right access-background' }, [
+            'Next Step', span({ className: 'glyphicon glyphicon-chevron-right', 'aria-hidden': 'true' })
+          ]),
+
+          a({
+            id: 'btn_save', isRendered: isNil(props.darCode), onClick: props.partialSave,
+            className: 'btn-secondary f-right access-color'
+          }, ['Save'])
+        ])
+      ])
+    ])
+  );
+}
