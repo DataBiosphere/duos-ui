9809cda5bd83a8dba022a570f0f04e56fc0c807e
lindojong <43456581+lindojong@users.noreply.github.com>
2020-08-05 14:23:07 -0400

    codacy

--- src/components/ReadMore.js
+++ src/components/ReadMore.js
@@ -57,16 +57,16 @@ export const ReadMore = hh(class ReadMore extends Component {
 
   getReadLink = (fun, text, classes) => {
     const {linkElements, linkElementsStyle} = this.state.inline ?
-    {linkElements: [text], linkElementsStyle: {}} :
-    {
-      linkElements: [
-        text,
-        span({
-          className: classes,
-          style: {padding: '0 1rem'},
-          'aria-hidden': 'true',
-        })], linkElementsStyle: this.state.readStyle,
-    };
+      {linkElements: [text], linkElementsStyle: {}} :
+      {
+        linkElements: [
+          text,
+          span({
+            className: classes,
+            style: {padding: '0 1rem'},
+            'aria-hidden': 'true',
+          })], linkElementsStyle: this.state.readStyle,
+      };
     return a({ onClick: () => fun(), style: linkElementsStyle}, linkElements);
   }
 
