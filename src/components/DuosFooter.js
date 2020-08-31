9411b8067eb695ee9686d1183f6b12e915fab488
Linda Zhang <linda@broadinstitute.org>
2020-08-31 15:36:24 -0400

    clean up footer styling to improve responsive mobile look

--- src/components/DuosFooter.js
+++ src/components/DuosFooter.js
@@ -2,10 +2,31 @@ import { a, div, footer, img, li, ul } from 'react-hyperscript-helpers';
 
 function DuosFooter() {
 
+  const footerStyle = {
+    marginTop: '-100px',
+    clear: 'both',
+    backgroundColor: '#000000',
+    minHeight: '64px'
+  }
+
+  const mainFooterStyle = {
+    margin: '36px 0 0 0',
+    display: 'block',
+    width: '100%',
+    padding: '0 20px'
+  }
+
+  const footerLogoStyle = {
+    float: 'left',
+    height: '32px',
+    marginTop: '15px',
+    marginRight: '35px'
+  }
+
   return (
-    div({ className: "footer" }, [
-      footer({ className: "main-footer" }, [
-        img({ src: "/images/broad_logo_allwhite.png", className: "footer-logo", alt: "Broad Institute logo" }),
+    div({ style: footerStyle }, [
+      footer({ style: mainFooterStyle }, [
+        img({ src: "/images/broad_logo_allwhite.png", style: footerLogoStyle, alt: "Broad Institute logo" }),
         ul({ className: "footer-links" }, [
           li({ className: "footer-links__item" }, ["\u00A9 Broad Institute"]),
           li({ className: "footer-links__item" }, [a({ target: '_blank', href: "https://www.broadinstitute.org/privacy-policy" }, ["Privacy Policy"]),]),
