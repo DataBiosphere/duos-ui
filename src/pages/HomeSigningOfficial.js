d5ff9d857f45185aef9821631eee518f5beafa8d
Linda Zhang <linda@broadinstitute.org>
2020-09-10 11:06:44 -0400

    add new photos

--- src/pages/HomeSigningOfficial.js
+++ src/pages/HomeSigningOfficial.js
@@ -27,10 +27,12 @@ class HomeSigningOfficial extends Component {
           div({style: homeSection}, [
             h3({style: homeTitle}, ['Library Cards']),
             p({}, ['Currently, when a researcher makes a data access request they are not only required to obtain approval from the data access committee (DAC) which oversees the data, but also from their home instiution\'s Signing Official, who approves their request and acknowledges the acceptance of organizational liability in the case of data misuse.']),
-            img(),
+            img({src: '/images/signing_official_approval.png',
+                 alt: 'Signing Officials approve DARs',}),
             p({}, ['Estimates show Signing Officials were required to approve at least 50,000 DARs in 2019 in the US. Based on feedback from numerous Signing Officials we understand Signing Officials primary concern in the approval of data access requests (DARs) to be primarily an endorsement or authorization of the researcher, rather than the proposed research.']),
             p({}, ['To alleviate this burden on Signing Officials, DUOS is offering Signing Officials the opportunity to pre-authorize researchers to submit DARs to DACs using DUOS, via our DUOS Library Card Agreement.']),
-            img({}),
+            img({src: '/images/signing_official_preauthorize.png',
+                 alt: 'Alleviate burden on Signing Officials'}),
             p({}, ['This agreement allows a Signing Official to pre-authorize researchers from their institutions for a 1 year renewable term.']),
             a({}, []),
             p({}, ['To issue your researchers this Library Card pre-authorization, please sign and send the DUOS Library Card Agreement (above) along with a list of the first and last name, and eRA Commmons ID of each researcher you will issue this privilege, to: moc.ksednez.etutitsnidaorb@troppus-SOUD\n' +
