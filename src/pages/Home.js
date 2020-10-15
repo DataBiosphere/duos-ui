2ea5d4b5bdabcddbff35f04f3a3854509304dc6f
Jonathan Lawson <jlawson@broadinstitute.org>
2020-10-15 17:48:39 -0400

    [DUOS-801] Replace Homepage diagram with updated text version

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -121,7 +121,7 @@ class Home extends Component {
                   className: 'col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1 hidden-xs',
                   style: { padding: '1rem 5rem', margin: 'auto 10rem' },
                   alt: 'What is DUOS graphic',
-                  src: '/images/duos_process_flow.png'
+                  src: '/images/DUOS_Homepage_diagram.png'
                 })
               ])
             ])
