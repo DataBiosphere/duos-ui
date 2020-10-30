1faf3ffa47057851c039bf6c013f25d92fa5cb76
Greg Rushton <grushton@broadinstitute.org>
2020-10-30 14:28:58 -0400

    use `useState`

--- src/components/DataAccessRequestHeader.js
+++ src/components/DataAccessRequestHeader.js
@@ -1,5 +1,8 @@
 import _ from 'lodash';
+import map from 'lodash/fp';
 import {div, span} from 'react-hyperscript-helpers';
+import { DataSet } from '../libs/ajax';
+import { useState, useEffect} from 'react';
 
 const parseAlias = (alias) => {
   const n = parseInt(alias);
@@ -21,18 +24,27 @@ const pipe = ' | ';
 
 export default function DataAccessRequestHeader(props) {
 
+  const [datasets, setDatasets] = useState(props.dar.datasets || []);
+  const [consentName, setConsentName] = useState(props.consentName || '');
+  const [darCode, setDarCode] = useState(props.dar.darCode || '');
+  const [projectTitle, setProjectTitle] = useState(props.dar.projectTitle || '');
+
+  // const [fullDatasets, setFullDatasets] = useState(
+  //   map((id) => DataSet.getDataSetsByDatasetId(id))(
+  //     map('value')(props.dar.datasets)) || []);
+
   const aliases = _.join(
-    _.map(props.dar.datasets, 'alias').map(a => parseAlias(a)), ', ');
-  const names = _.join(_.map(props.dar.datasets, 'name'), ', ');
+    _.map(datasets, 'alias').map(a => parseAlias(a)), ', ');
+  const names = _.join(_.map(datasets, 'name'), ', ');
   return (
     div({style: style}, [
       div({}, [
-        span([props.dar.projectTitle, pipe]),
-        span({}, [props.dar.darCode]),
+        span([projectTitle, pipe]),
+        span({}, [darCode]),
         div({}, [
           span([aliases, pipe]),
           span([names, pipe]),
-          span({}, [props.consentName]),
+          span({}, [consentName]),
         ]),
       ]),
     ])
