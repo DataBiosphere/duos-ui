b8e84cae0c5663632efc6f9eeb44445e2064bafd
Greg Rushton <grushton@broadinstitute.org>
2020-12-03 13:34:46 -0500

    PR feedback

--- src/pages/DataAccessRequestApplication.js
+++ src/pages/DataAccessRequestApplication.js
@@ -182,8 +182,7 @@ class DataAccessRequestApplication extends Component {
       formData = await DAR.getPartialDarRequest(dataRequestId);
       const dsIds = fp.get('datasetIds')(formData);
       if (!fp.isNil(dsIds)) {
-        const datasets = await Promise.all(fp.map(async (id) =>
-          await DataSet.getDataSetsByDatasetId(id))(dsIds));
+        const datasets = await Promise.all(fp.map(async (id) => DataSet.getDataSetsByDatasetId(id))(dsIds));
         formData.datasets = fp.map(ds => this.formatDatasetForAutocomplete(ds))(datasets);
       }
     } else {
