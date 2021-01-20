5e8ff170831d6a6f47afd6116c84ed5b2c25e912
JVThomas <jthomas0209@gmail.com>
2021-01-20 14:48:36 -0500

    DUOS-760 styling adjustments on page header

--- src/pages/ChairConsole.js
+++ src/pages/ChairConsole.js
@@ -1,6 +1,6 @@
-import _ from 'lodash';
+import _, { isNil } from 'lodash';
 import { Component, Fragment } from 'react';
-import { button, div, h, hh, hr, span } from 'react-hyperscript-helpers';
+import { button, div, h, hh, hr, span, img } from 'react-hyperscript-helpers';
 import { PageHeading } from '../components/PageHeading';
 import { PageSubHeading } from '../components/PageSubHeading';
 import { PaginatorBar } from '../components/PaginatorBar';
@@ -8,7 +8,42 @@ import { SearchBox } from '../components/SearchBox';
 import { PendingCases } from '../libs/ajax';
 import { Storage } from '../libs/storage';
 import { NavigationUtils, USER_ROLES } from '../libs/utils';
+import {Notifications} from '../libs/utils';
+import { Theme } from '../libs/theme';
 
+const styles = {
+  TITLE: {
+    fontFamily: "Montserrat",
+    fontWeight: Theme.font.weight.semibold,
+    fontSize: Theme.font.size.title,
+  },
+  SMALL: {
+    fontFamily: 'Montserrat',
+    fontWeight: Theme.font.weight.regular,
+    fontSize: Theme.font.size.small
+  },
+  HEADERIMG: {
+    width: '60px',
+    height: '60px',
+  },
+  HEADER_CONTAINER: {
+    display: 'flex',
+    flexDirection: "column"
+  },
+  ICON_CONTAINER: {
+    flexBasis: '76px',
+    height: '60px',
+    paddingRight: '16px'
+  },
+  RIGHT_HEADER_SECTION: {
+    paddingTop: '4.75rem'
+  },
+  LEFT_HEADER_SECTION: {
+    display: 'flex',
+    flexDirection: 'row',
+    paddingTop: "3rem"
+  }
+};
 export const ChairConsole = hh(class ChairConsole extends Component {
 
   constructor(props) {
@@ -16,14 +51,14 @@ export const ChairConsole = hh(class ChairConsole extends Component {
     this.state = {
       showModal: false,
       currentUser: {},
-      dulLimit: 5,
+      // dulLimit: 5,
       darLimit: 5,
-      currentDulPage: 1,
+      // currentDulPage: 1,
       currentDarPage: 1,
-      totalDulPendingVotes: 0,
+      // totalDulPendingVotes: 0,
       totalAccessPendingVotes: 0,
       electionsList: {
-        dul: [],
+        // dul: [],
         access: []
       }
     };
@@ -32,12 +67,12 @@ export const ChairConsole = hh(class ChairConsole extends Component {
   }
 
 
-  handleDulPageChange = page => {
-    this.setState(prev => {
-      prev.currentDulPage = page;
-      return prev;
-    });
-  };
+  // handleDulPageChange = page => {
+  //   this.setState(prev => {
+  //     prev.currentDulPage = page;
+  //     return prev;
+  //   });
+  // };
 
   handleAccessPageChange = page => {
     this.setState(prev => {
@@ -46,13 +81,13 @@ export const ChairConsole = hh(class ChairConsole extends Component {
     });
   };
 
-  handleDulSizeChange = size => {
-    this.setState(prev => {
-      prev.dulLimit = size;
-      prev.currentDulPage = 1;
-      return prev;
-    });
-  };
+  // handleDulSizeChange = size => {
+  //   this.setState(prev => {
+  //     prev.dulLimit = size;
+  //     prev.currentDulPage = 1;
+  //     return prev;
+  //   });
+  // };
 
   handleAccessSizeChange = size => {
     this.setState(prev => {
@@ -62,56 +97,49 @@ export const ChairConsole = hh(class ChairConsole extends Component {
     });
   };
 
-  componentDidMount() {
-    let currentUser = Storage.getCurrentUser();
-    this.setState({
-      currentUser: currentUser
-    }, () => {
-      this.init(currentUser);
-    });
+  async componentDidMount() {
+    // // let currentUser = Storage.getCurrentUser();
+    // this.setState({
+    //   currentUser: currentUser
+    // }, () => {
+    //   this.init(currentUser);
+    // });
+    await this.init();
   }
 
-  init(currentUser) {
-
-    PendingCases.findConsentPendingCasesByUser(currentUser.dacUserId).then(
-      duls => {
-        this.setState(prev => {
-          prev.electionsList.dul = duls.dul;
-          prev.totalDulPendingVotes = duls.totalDulPendingVotes;
-          return prev;
-        });
-      }
-    );
-
-    PendingCases.findDataRequestPendingCasesByUser(currentUser.dacUserId).then(
-      dars => {
-        this.setState(prev => {
-          // Filter vote-able elections. See https://broadinstitute.atlassian.net/browse/DUOS-789
-          // for more work related to ensuring closed elections aren't displayed here.
-          prev.electionsList.access = _.filter(dars.access, (e) => { return e.electionStatus !== 'Closed'; });
-          prev.totalAccessPendingVotes = dars.totalAccessPendingVotes;
-          return prev;
-        });
-      }
-    );
-  }
-
-  openDULReview = (voteId, referenceId) => (e) => {
-    this.props.history.push(`dul_review/${voteId}/${referenceId}`);
+  async init() {
+    try {
+      const currentUser = Storage.getCurrentUser();
+      const pendingList = await PendingCases.findDataRequestPendingCasesByUser(currentUser.dacUserId);
+      this.setState(prev => {
+        prev.currentUser = currentUser;
+        // Filter vote-able elections. See https://broadinstitute.atlassian.net/browse/DUOS-789
+        // for more work related to ensuring closed elections aren't displayed here.
+        prev.electionsList.access = _.filter(pendingList.access, (e) => { return e.electionStatus !== 'Closed'; });
+        prev.totalAccessPendingVotes = pendingList.totalAccessPendingVotes;
+        return prev;
+      });
+    } catch(error) {
+      Notifications.showError({text: 'Error: Unable to retreive data requests from server'});
+    };
   };
 
-  isDuLCollectEnabled = (pendingCase) => {
-    const pendingCaseDulCollectStatus = (pendingCase.alreadyVoted === true);
-    const dacId = _.get(pendingCase, 'dac.dacId', 0);
-    // if the pending case doesn't have a DAC, then any chair should be able to collect votes:
-    if (dacId === 0) { return pendingCaseDulCollectStatus; }
-    const dacChairRoles = _.filter(this.state.currentUser.roles, { 'name': USER_ROLES.chairperson, 'dacId': dacId });
-    return (!_.isEmpty(dacChairRoles)) && pendingCaseDulCollectStatus;
-  };
+  // openDULReview = (voteId, referenceId) => (e) => {
+  //   this.props.history.push(`dul_review/${voteId}/${referenceId}`);
+  // };
 
-  openDulCollect = (consentId) => (e) => {
-    this.props.history.push(`dul_collect/${consentId}`);
-  };
+  // isDuLCollectEnabled = (pendingCase) => {
+  //   const pendingCaseDulCollectStatus = (pendingCase.alreadyVoted === true);
+  //   const dacId = _.get(pendingCase, 'dac.dacId', 0);
+  //   // if the pending case doesn't have a DAC, then any chair should be able to collect votes:
+  //   if (dacId === 0) { return pendingCaseDulCollectStatus; }
+  //   const dacChairRoles = _.filter(this.state.currentUser.roles, { 'name': USER_ROLES.chairperson, 'dacId': dacId });
+  //   return (!_.isEmpty(dacChairRoles)) && pendingCaseDulCollectStatus;
+  // };
+
+  // openDulCollect = (consentId) => (e) => {
+  //   this.props.history.push(`dul_collect/${consentId}`);
+  // };
 
   openFinalAccessReview = (referenceId, electionId, rpElectionId) => (e) => {
     this.props.history.push(`${'final_access_review'}/${referenceId}/${electionId}`);
@@ -161,9 +189,9 @@ export const ChairConsole = hh(class ChairConsole extends Component {
     this.setState({ showModal: false });
   }
 
-  handleSearchDul = (query) => {
-    this.setState({ searchDulText: query });
-  };
+  // handleSearchDul = (query) => {
+  //   this.setState({ searchDulText: query });
+  // };
 
   handleSearchDar = (query) => {
     this.setState({ searchDarText: query });
@@ -179,121 +207,60 @@ export const ChairConsole = hh(class ChairConsole extends Component {
 
   render() {
 
-    const { currentUser, dulLimit, darLimit, searchDulText, searchDarText } = this.state;
+    const { darLimit, searchDarText } = this.state;
     const oneColumnClass = 'col-lg-1 col-md-1 col-sm-1 col-xs-1';
     const twoColumnClass = 'col-lg-2 col-md-2 col-sm-2 col-xs-2';
     const threeColumnClass = 'col-lg-3 col-md-3 col-sm-3 col-xs-3';
 
     return (
-
-      div({ className: 'container' }, [
+      div({ className: 'row' }, [
         div({ className: 'col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1 col-sm-12 col-xs-12' }, [
-          PageHeading({
-            id: 'chairConsole', color: 'common', title: 'Welcome to your DAC Chair Console, ' + currentUser.displayName + '!',
-            description: 'These are your pending cases for review'
-          }),
 
-          hr({ className: 'section-separator' }),
-
-          div({ className: 'row no-margin' }, [
-            div({ className: 'col-lg-8 col-md-8 col-sm-8 col-xs-12 no-padding' }, [
-              PageSubHeading({
-                id: 'chairConsoleDul', imgSrc: '/images/icon_dul.png', color: 'dul', title: 'Data Use Limitations Review',
-                description: 'Were data use limitations accurately converted to a structured format?'
-              })
-            ]),
-            div({ className: 'col-lg-4 col-md-4 col-sm-4 col-xs-12 search-wrapper no-padding' }, [
-              h(SearchBox, { id: 'chairConsoleDul', searchHandler: this.handleSearchDul, pageHandler: this.handleDulPageChange, color: 'dul' })
-            ])
-          ]),
-
-          div({ className: 'jumbotron table-box' }, [
-            div({ className: 'row no-margin' }, [
-              div({ className: twoColumnClass + 'cell-header dul-color' }, ['Consent id']),
-              div({ className: threeColumnClass + ' cell-header dul-color' }, ['Consent Group Name']),
-              div({ className: twoColumnClass + ' cell-header dul-color' }, ['DAC']),
-              div({ className: twoColumnClass + ' cell-header f-center dul-color' }, [
-                'Review/Vote',
-                div({ isRendered: this.state.totalDulPendingVotes > 0, id: 'dulPendingVoteCases', className: 'pcases-small-tag' }, [
-                  this.state.totalDulPendingVotes
-                ])
+          //START: header section
+          div({ style: {display: "flex", justifyContent: "space-between"}}, [
+            div({className: "left-header-section", style: styles.LEFT_HEADER_SECTION}, [
+              div({style: styles.ICON_CONTAINER}, [
+                img({
+                  id: 'lock-icon',
+                  src: '/images/lock-icon.png',
+                  style: styles.HEADERIMG
+                })
               ]),
-              div({ className: oneColumnClass + ' cell-header f-center dul-color' }, ['Logged']),
-              div({ className: twoColumnClass + ' cell-header f-center dul-color' }, ['Actions'])
+              div({style: styles.HEADER_CONTAINER}, [
+                div({style: styles.TITLE}, ["Manage Data Access Request"]),
+                div({style: styles.SMALL}, ["Select and manage Data Access Requests for DAC review"])
+              ])
             ]),
-
-            hr({ className: 'table-head-separator' }),
-
-            this.state.electionsList.dul.filter(this.searchTable(searchDulText))
-              .slice((this.state.currentDulPage - 1) * dulLimit, this.state.currentDulPage * dulLimit)
-              .map((pendingCase, rIndex) => {
-                return h(Fragment, { key: rIndex }, [
-                  div({ className: 'row no-margin tableRowDul' }, [
-                    div({
-                      id: pendingCase.frontEndId, name: 'consentId', className: twoColumnClass + ' cell-body text',
-                      title: pendingCase.frontEndId
-                    }, [pendingCase.frontEndId]),
-                    div({
-                      id: pendingCase.frontEndId + '_consentGroup', name: 'consentGroup',
-                      className: threeColumnClass + ' cell-body text ' + (!pendingCase.consentGroupName ? 'empty' : ''),
-                      title: pendingCase.consentGroupName, dangerouslySetInnerHTML: { __html: pendingCase.consentGroupName }
-                    }, []),
-                    div({
-                      id: pendingCase.frontEndId + '_dacName', name: 'dacName', className: twoColumnClass + ' cell-body text',
-                      title: _.get(pendingCase, 'dac.name', '')
-                    }, [_.get(pendingCase, 'dac.name', '- -')]),
-                    div({ className: twoColumnClass + ' cell-body f-center' }, [
-                      button({
-                        id: pendingCase.frontEndId + '_btnVote',
-                        name: 'btn_voteDul',
-                        onClick: this.openDULReview(pendingCase.voteId, pendingCase.referenceId),
-                        className: 'cell-button ' + (pendingCase.alreadyVoted ? 'default-color' : 'cancel-color')
-                      }, [
-                        span({ isRendered: pendingCase.alreadyVoted === false }, ['Vote']),
-                        span({ isRendered: pendingCase.alreadyVoted === true }, ['Edit'])
-                      ])
-                    ]),
-
-                    div({
-                      id: pendingCase.frontEndId + '_logged', name: 'loggedDul',
-                      className: oneColumnClass + ' cell-body text f-center'
-                    }, [pendingCase.logged]),
-
-                    div({ isRendered: this.isDuLCollectEnabled(pendingCase), className: twoColumnClass + ' cell-body f-center' }, [
-                      button({
-                        id: pendingCase.frontEndId + '_btnCollect', name: 'btn_collectDul', onClick: this.openDulCollect(pendingCase.referenceId),
-                        className: 'cell-button cancel-color'
-                      }, ['Collect Votes'])
-                    ]),
-                    div({
-                      isRendered: pendingCase.alreadyVoted === false, className: twoColumnClass + ' cell-body text empty f-center'
-                    }, [])
-                  ]),
-                  hr({ className: 'table-body-separator' })
-                ]);
-              }),
-            PaginatorBar({
-              name: 'dul',
-              total: this.state.electionsList.dul.filter(this.searchTable(searchDulText)).length,
-              limit: dulLimit,
-              currentPage: this.state.currentDulPage,
-              onPageChange: this.handleDulPageChange,
-              changeHandler: this.handleDulSizeChange
-            })
-          ]),
-
-          div({ className: 'row no-margin' }, [
-            div({ className: 'col-lg-8 col-md-8 col-sm-8 col-xs-12 no-padding' }, [
-              PageSubHeading({
-                id: 'chairConsoleAccess', imgSrc: '/images/icon_access.png', color: 'access', title: 'Data Access Request Review',
-                description: 'Should data access be granted to this applicant?'
+            div({className: "right-header-section", style: styles.RIGHT_HEADER_SECTION}, [
+              h(SearchBox, {
+                id: 'chairConsoleAccess', searchHandler: this.handleSearchDar, pageHandler: this.handleAccessPageChange, color: 'access' 
               })
-            ]),
-            div({ className: 'col-lg-4 col-md-4 col-sm-4 col-xs-12 search-wrapper no-padding' }, [
-              h(SearchBox,
-                { id: 'chairConsoleAccess', searchHandler: this.handleSearchDar, pageHandler: this.handleAccessPageChange, color: 'access' })
             ])
           ]),
+          //   div({ className: 'col-lg-8 col-md-8 col-sm-8 col-xs-12 no-padding' }, [
+          //     // PageSubHeading({
+          //     //   id: 'chairConsoleAccess', imgSrc: '/images/icon_access.png', color: 'access', title: 'Data Access Request Review',
+          //     //   description: 'Select and ?'
+          //     // })
+          //     div({className: "chair-console-header row no-margin"}, [
+          //       div()
+          //       img({
+          //         id: 'lock-icon',
+          //         src: '/images/lock-icon.png',
+          //         style: styles.HEADERIMG
+          //       }),
+          //       div({style: {display: }}, [
+          //         div({style: styles.TITLE}, ["Manage Data Access Request"]),
+          //         div({style:styles.SMALL}, ['Select and manage Data Access Request for DAC Review'])
+          //       ])
+          //     ])
+          //   ]),
+          //   div({ className: 'col-lg-4 col-md-4 col-sm-4 col-xs-12 search-wrapper no-padding' }, [
+          //     h(SearchBox,
+          //       { id: 'chairConsoleAccess', searchHandler: this.handleSearchDar, pageHandler: this.handleAccessPageChange, color: 'access' })
+          //   ])
+          // ]),
+          //END: header section
 
           div({ className: 'jumbotron table-box' }, [
             div({ className: 'row no-margin' }, [
