4d695bc44ef38fa260ce9cdf53fe73bc712e7032
solideoglori <jon.nsf@gmail.com>
2021-02-01 15:17:24 -0500

    Updated LCA name and added to DAR Renewal

--- src/pages/DataAccessRequestRenewal.js
+++ src/pages/DataAccessRequestRenewal.js
@@ -987,7 +987,7 @@ class DataAccessRequestRenewal extends Component {
 
                   div({ className: 'col-lg-12 col-md-12 col-sm-12 col-xs-12 rp-group' }, [
                     a({
-                      id: 'link_downloadAgreement', href: 'BroadDataAccessAgreement.pdf', target: '_blank',
+                      id: 'link_downloadAgreement', href: 'Library_Card_Agreeement_2021.pdf', target: '_blank',
                       className: 'col-lg-4 col-md-4 col-sm-6 col-xs-12 btn-secondary btn-download-pdf hover-color'
                     }, [
                       span({ className: 'glyphicon glyphicon-download' }),
