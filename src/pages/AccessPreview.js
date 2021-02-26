56f1be6e693aac2d00aebfc4b6825c59d621999b
Greg Rushton <grushton@broadinstitute.org>
2021-02-26 09:06:28 -0500

    remove from state

--- src/pages/AccessPreview.js
+++ src/pages/AccessPreview.js
@@ -17,7 +17,6 @@ class AccessPreview extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      hasUseRestriction: true,
       consentName: '',
       isQ1Expanded: true,
       isQ2Expanded: false,
@@ -112,8 +111,7 @@ class AccessPreview extends Component {
             id: 'accessCollectVotes',
             onClick: this.toggleQ1,
             color: 'access',
-            title: this.state.hasUseRestriction ? 'Q1. Should data access be granted to this applicant?'
-              : 'Should data access be granted to this applicant?',
+            title: 'Q1. Should data access be granted to this applicant?',
             expanded: this.state.isQ1Expanded
           }, [
 
@@ -122,7 +120,6 @@ class AccessPreview extends Component {
               ApplicationSummary({
                 isRendered: !ld.isNil(this.state.darInfo) && !ld.isNil(this.state.researcherProfile),
                 mrDAR: null,
-                hasUseRestriction: this.state.hasUseRestriction,
                 darInfo: this.state.darInfo,
                 downloadDAR: this.downloadDAR,
                 researcherProfile: this.state.researcherProfile }),
@@ -139,7 +136,6 @@ class AccessPreview extends Component {
 
         div({ className: 'row no-margin' }, [
           CollapsiblePanel({
-            isRendered: this.state.hasUseRestriction,
             id: 'rpCollectVotes',
             onClick: this.toggleQ1,
             color: 'access',
