967aa58bcdfe029c127a4181828c3e841e8ecd6a
raejohanek <rjohanek@broadinstitute.org>
2021-03-05 15:16:32 -0500

    -refactoring access review code

--- src/Routes.js
+++ src/Routes.js
@@ -110,8 +110,8 @@ const Routes = (props) => (
       rolesAllowed={[USER_ROLES.chairperson, USER_ROLES.admin]} />
     <AuthenticatedRoute path="/reviewed_cases" component={ReviewedCases} props={props}
       rolesAllowed={[USER_ROLES.admin, USER_ROLES.alumni]} />
-      <AuthenticatedRoute path="/review_results/:referenceId" component={ReviewResults} props={props}
-                          rolesAllowed={[USER_ROLES.admin, USER_ROLES.alumni]} />
+    <AuthenticatedRoute path="/review_results/:referenceId" component={ReviewResults} props={props}
+      rolesAllowed={[USER_ROLES.admin, USER_ROLES.alumni]} />
     <Route path="*" component={NotFound} />
   </Switch>
 );
--- src/pages/ReviewResults.js
+++ src/pages/ReviewResults.js
@@ -1,162 +1,134 @@
 import React from 'react';
-import {div, h, span} from 'react-hyperscript-helpers';
+import {div} from 'react-hyperscript-helpers';
 import {AccessReviewHeader} from './access_review/AccessReviewHeader';
 import {DAR, DataSet, Researcher, Votes} from '../libs/ajax';
 import {Notifications} from '../libs/utils';
 import * as fp from 'lodash/fp';
-import ApplicationDownloadLink from "../components/ApplicationDownloadLink";
-import {VoteSummary} from "./access_review/VoteSummary";
-import {AppSummary} from "./access_review/AppSummary";
-import { Election } from '../../libs/ajax';
+import {DarApplication} from "./access_review/DarApplication";
+import { Election } from '../libs/ajax';
 
 const SECTION = {
-  margin: '10px auto',
+  margin: '10px 16px',
 };
 
 class ReviewResults extends React.PureComponent {
   constructor(props) {
     super(props);
     this.props = props;
-    this.state = {
-      voteAsChair: (props.location && props.location.state ? props.location.state.chairFinal : false),
-      darInfo: null,
-      datasets: null,
-      consent: null,
-      researcherProfile: null
-    };
   }
 
-  componentDidMount() {
-    this.darReviewAccess();
-  }
-
-  darReviewAccess() {
+  componentDidMount()  {
     const darId = this.props.match.params.referenceId;
-    this.getDarData(darId);
-    this.getAllVotes(darId);
+    this.getData(darId);
   }
 
-  getDarData = async (darId) => {
-    let datasets;
-    let darInfo;
-    let consent;
-    let researcherProfile;
+  async getData(darId) {
 
-    try {
-      darInfo = await DAR.getPartialDarRequest(darId);
-      // Researcher information
-      const researcherPromise = Researcher.getResearcherProfile(darInfo.userId);
-      const datasetsPromise = darInfo.datasetIds.map((id) => {
-        return DataSet.getDataSetsByDatasetId(id);
-      });
-      const consentPromise = DAR.getDarConsent(darId);
-      [consent, datasets, researcherProfile] = await Promise.all([
-        consentPromise,
-        Promise.all(datasetsPromise),
-        researcherPromise
-      ]);
-    } catch (error) {
-      Notifications.showError({text: 'Error initializing DAR Data'});
-      return Promise.reject(error);
-    }
-    this.setState((prev) => {
-      prev.datasets = datasets;
-      prev.darInfo = darInfo;
-      prev.consent = consent;
-      prev.researcherProfile = researcherProfile;
-      return prev;
-    })
-  };
+    //get information on datasets, consent, researcher, and access request
+    const getDarData = async (darId) => {
+      let datasets;
+      let darInfo;
+      let consent;
+      let researcherProfile;
 
-  getAllVotes = async (darId) => {
-    const allVotes = await Votes.getDarVotes(darId);
-    this.setState((prev) => {
-      prev.allVotes = allVotes;
-    })
-  };
+      try {
+        darInfo = await DAR.getPartialDarRequest(darId);
+        const researcherPromise = await Researcher.getResearcherProfile(darInfo.userId);
+        const datasetsPromise = darInfo.datasetIds.map((id) => {
+          return DataSet.getDataSetsByDatasetId(id);
+        });
+        const consentPromise = await DAR.getDarConsent(darId);
+        [consent, datasets, researcherProfile] = await Promise.all([
+          consentPromise,
+          Promise.all(datasetsPromise),
+          researcherPromise
+        ]);
 
-  getElectionInformation = async (darId) => {
-    let accessElection;
-    try {
-      const accessElection = await Election.findElectionByDarId(darId);
-    } catch (error) {
-      //access election is null
-    }
-    try {
-      const rpElectionReview = fp.isNil(accessElection) ? null : await Election.findRPElectionReview(accessElection.electionId, false);
-      console.log(rpElectionReview);
-      const rpElection = fp.isNil(rpElectionReview) ? null : rpElectionReview.election;
-      return {accessVote, accessElectionReview, accessElection, rpElectionReview, rpElection};
-    } catch (error) {
-      Notifications.showError({text: 'Error initializing Election Data'});
-      return Promise.reject(error);
-    }
-  };
+      } catch (error) {
+        Notifications.showError({text: 'Error initializing DAR Data'});
+        return Promise.reject(error);
+      }
+
+      return {datasets, darInfo, consent, researcherProfile};
+    };
 
 
-    //const {accessVote, accessElectionReview, accessElection, rpElectionReview, rpElection} = electionData;
+    //get information on access election and research purpose election
+    const getElectionInformation = async (darId) => {
+      let accessElection;
+      let accessElectionReview;
+      let rpElectionReview;
 
+      try {
+        accessElection = await Election.findElectionByDarId(darId);
+        console.log(accessElection);
+      } catch (error) {
+        //access election is null
+        Notifications.showError({text: 'Error initializing Data Access Election'});
+      }
+      try {
+        accessElectionReview = fp.isNil(accessElection) ? null : await Election.findDataAccessElectionReview(accessElection.electionId, true);
+        rpElectionReview = fp.isNil(accessElection) ? null : await Election.findRPElectionReview(accessElection.electionId, false);
+      } catch (error) {
+        Notifications.showError({text: 'Error initializing Election Data'});
+        return Promise.reject(error);
+      }
+      return { accessElection, accessElectionReview, rpElectionReview };
+    };
+
+
+    const [electionData, darData] = await Promise.all([
+      getElectionInformation(darId),
+      getDarData(darId)
+    ]);
+
+    const {datasets, darInfo, consent, researcherProfile} = darData;
+    const {accessElectionReview, accessElection, rpElectionReview} = electionData;
+
+    this.setState({ darId, accessElection, darInfo, consent, accessElectionReview, rpElectionReview, researcherProfile, datasets });
+    // this.setState((prev) => {
+    //   prev.datasets = datasets;
+    //   prev.darInfo = darInfo;
+    //   prev.consent = consent;
+    //   prev.researcherProfile = researcherProfile;
+    //   return prev;
+    //});
+    // this.setState((prev) => {
+    //   prev.accessElection = accessElection;
+    //   prev.accessElectionReview = accessElectionReview;
+    //   prev.rpElectionReview = rpElectionReview;
+    //   return prev;
+    // });
+
+  }
 
   render() {
-    const {history, match} = this.props;
-    const {voteAsChair, darInfo, datasets, consent, researcherProfile } = this.state;
-    const accessVotes = fp.isNil(accessElectionReview) ? null : fp.get( 'reviewVote')(accessElectionReview);
-    const rpVotes = fp.isNil(rpElectionReview) ? null : fp.get( 'reviewVote')(rpElectionReview);
+    if (!fp.isNil(this.state)) {
+      const voteAsChair = true;
+      const {history, match} = this.props;
+      const {darId, darInfo, datasets, consent, researcherProfile, accessElection, accessElectionReview, rpElectionReview } = this.state;
 
-    return div({isRendered: darInfo != null, id: 'container', paddingLeft: "16px" },
-      [
-        div(
-          {
-            id: 'header', style: SECTION
-          },
-          [AccessReviewHeader({history, match})]
-        ),
-        div({id: 'body', style: {display: 'flex'}}, [
-          div(
-            {
-              id: 'application',
-              style: {
-                ...SECTION,
-                width: '80%',
-              }
-            },
-            // [DarApplication({
-            //   voteAsChair,
-            //   darId,
-            //   darInfo,
-            //   accessElection,
-            //   consent,
-            //   accessElectionReview,
-            //   rpElectionReview,
-            //   researcherProfile,
-            //   datasets
-            // })]
-            div([
-              div({ id: 'header', style: SECTION }, [
-                div({ style: { minWidth: '50%' } }, [
-                  span({ style: '16px'}, darInfo.projectTitle),
-                  span({ style: '12px'}, ' | ' + darInfo.darCode)
-                ]),
-                h(ApplicationDownloadLink, {darInfo, researcherProfile, datasets})
-              ]),
-              VoteSummary({
-                isRendered: voteAsChair && !fp.isNil(accessVotes),
-                question: 'Should data access be granted to this application?',
-                questionNumber: '1',
-                votes: accessVotes,
-              }),
-              VoteSummary({
-                isRendered: voteAsChair && !fp.isNil(rpVotes),
-                question: 'Was the research purpose accurately converted to a structured format?',
-                questionNumber: '2',
-                votes: rpVotes,
-              }),
-              AppSummary({ darInfo, accessElection, consent, researcherProfile })
-            ])
-          )
-        ])
-      ]
-    );
+      return div({isRendered: !fp.isNil(this.state), id: 'container'}, [
+        div({id: 'header', style: SECTION}, [
+          AccessReviewHeader({history, match})
+        ]),
+        div({isRendered: !fp.isNil(darId), id: 'application', style: {...SECTION, width: '80%'}},
+          [DarApplication({
+            voteAsChair,
+            darId,
+            darInfo,
+            accessElection,
+            consent,
+            accessElectionReview,
+            rpElectionReview,
+            researcherProfile,
+            datasets
+          })
+          ],
+        )]
+      );
+    }
+    return null;
   }
 }
 
