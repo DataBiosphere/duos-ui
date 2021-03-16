6ebb68698e5d7888938fcd3182a33cdc3ca41a7b
raejohanek <rjohanek@broadinstitute.org>
2021-03-16 14:41:27 -0400

    -adjust openModal method

--- src/pages/MemberConsole.js
+++ src/pages/MemberConsole.js
@@ -1,5 +1,5 @@
 import _ from 'lodash';
-import { isNil } from 'lodash/fp'
+import { isNil } from 'lodash/fp';
 import { Component, Fragment } from 'react';
 import { button, div, h, img, span } from 'react-hyperscript-helpers';
 import { SearchBox } from '../components/SearchBox';
@@ -52,11 +52,11 @@ class MemberConsole extends Component {
 
   handleOpenModal = () =>{
     this.setState({ showModal: true });
-  }
+  };
 
   handleCloseModal= () => {
     this.setState({ showModal: false });
-  }
+  };
 
   componentDidMount() {
     this.getMembersInfo();
@@ -109,86 +109,86 @@ class MemberConsole extends Component {
     const closeSummaryModal = () => this.setState({ showModal: false });
     const openSummaryModal = async (dar) => {
       let darDetails = await DAR.getDarModalSummary(dar.dataRequestId);
+      let researcherPromise;
       if (!isNil(darDetails)) {
         darDetails.researchType = DataUseTranslation.generateResearchTypes(darDetails);
+        researcherPromise = await User.getById(darDetails.userId);
         if (!darDetails.datasetNames) {
           darDetails.datasetNames = getDatasetNames(darDetails.datasets);
         }
-        const researcher = await User.getById(darDetails.userId);
       }
-      this.setState({showModal: true, darDetails: darDetails, researcher: researcher});
+      this.setState({showModal: true, darDetails: darDetails, researcher: researcherPromise });
     };
-
     return (
 
       div({style: Styles.PAGE}, [
-            div({ style: {display: "flex", justifyContent: "space-between"}}, [
-              div({className: "left-header-section", style: Styles.LEFT_HEADER_SECTION}, [
-                div({style: Styles.ICON_CONTAINER}, [
-                  img({
-                    id: 'lock-icon',
-                    src: lockIcon,
-                    style: Styles.HEADER_IMG
-                  })
-                ]),
-                div({style: Styles.HEADER_CONTAINER}, [
-                  div({style: Styles.TITLE}, ['Data Access Request Review']),
-                  div({style: Styles.SMALL}, ['Should data access be granted to this applicant?'])
-                ])
-              ]),
-              div({className: "right-header-section", style: Styles.RIGHT_HEADER_SECTION}, [
-                h(SearchBox, { searchHandler: this.handleSearchDar, pageHandler: this.handleAccessPageChange })
-              ])
+        div({ style: {display: "flex", justifyContent: "space-between"}}, [
+          div({className: "left-header-section", style: Styles.LEFT_HEADER_SECTION}, [
+            div({style: Styles.ICON_CONTAINER}, [
+              img({
+                id: 'lock-icon',
+                src: lockIcon,
+                style: Styles.HEADER_IMG
+              })
             ]),
-          div({ style: Styles.TABLE.CONTAINER }, [
-            div({style: Styles.TABLE.HEADER_ROW}, [
-              div({style: Styles.TABLE.DATA_ID_CELL }, ["Data Request ID"]),
-              div({style: Styles.TABLE.TITLE_CELL }, ["Project Title"]),
-              div({style: Styles.TABLE.DAC_CELL }, ["DAC"]),
-              div({style: Styles.TABLE.ELECTION_STATUS_CELL }, ["Status"]),
-              div({style: Styles.TABLE.DAC_CELL }, ["Logged"]),
-              div({style: Styles.TABLE.ELECTION_STATUS_CELL }, [
-                "Review/Vote",
-                div({ isRendered: this.state.totalAccessPendingVotes > 0, className: 'pcases-small-tag' }, [this.state.totalAccessPendingVotes])
-              ])
-            ]),
-            this.state.electionsList.access
-              .filter(this.searchTable(searchDarText))
-              .slice((this.state.currentAccessPage - 1) * this.state.accessLimit, this.state.currentAccessPage * this.state.accessLimit).map((pendingCase, rIndex) => {
+            div({style: Styles.HEADER_CONTAINER}, [
+              div({style: Styles.TITLE}, ['Data Access Request Review']),
+              div({style: Styles.SMALL}, ['Should data access be granted to this applicant?'])
+            ])
+          ]),
+          div({className: "right-header-section", style: Styles.RIGHT_HEADER_SECTION}, [
+            h(SearchBox, { searchHandler: this.handleSearchDar, pageHandler: this.handleAccessPageChange })
+          ])
+        ]),
+        div({ style: Styles.TABLE.CONTAINER }, [
+          div({style: Styles.TABLE.HEADER_ROW}, [
+            div({style: Styles.TABLE.DATA_ID_CELL }, ["Data Request ID"]),
+            div({style: Styles.TABLE.TITLE_CELL }, ["Project Title"]),
+            div({style: Styles.TABLE.DAC_CELL }, ["DAC"]),
+            div({style: Styles.TABLE.ELECTION_STATUS_CELL }, ["Status"]),
+            div({style: Styles.TABLE.DAC_CELL }, ["Logged"]),
+            div({style: Styles.TABLE.ELECTION_STATUS_CELL }, [
+              "Review/Vote",
+              div({ isRendered: this.state.totalAccessPendingVotes > 0, className: 'pcases-small-tag' }, [this.state.totalAccessPendingVotes])
+            ])
+          ]),
+          this.state.electionsList.access
+            .filter(this.searchTable(searchDarText))
+            .slice((this.state.currentAccessPage - 1) * this.state.accessLimit, this.state.currentAccessPage * this.state.accessLimit).map((pendingCase, rIndex) => {
               const borderStyle = rIndex > 0 ? {borderTop: "1px solid rgba(109,110,112,0.2)"} : {};
               const dacName = !isNil(pendingCase.dac) ? pendingCase.dac.name : "--";
               return h(Fragment, { key: rIndex }, [
-                  div({style: Object.assign({}, borderStyle, Styles.TABLE.RECORD_ROW), paddingtop: '1rem' }, [
-                    div({
-                      style: Object.assign({}, Styles.TABLE.DATA_ID_CELL,  Styles.TABLE.DATA_REQUEST_TEXT),
-                      onClick: () => openSummaryModal(pendingCase),
-                      onMouseEnter: applyTextHover,
-                      onMouseLeave: (e) => removeTextHover(e, Styles.TABLE.DATA_REQUEST_TEXT.color)
-                    },  [pendingCase.frontEndId]),
-                    div({ style: Object.assign({}, Styles.TABLE.RECORD_TEXT, Styles.TABLE.TITLE_CELL) }, [pendingCase.projectTitle]),
-                    div({ style: Object.assign({}, Styles.TABLE.RECORD_TEXT, Styles.TABLE.DAC_CELL) }, [dacName]),
-                    div({ style: Object.assign({}, Styles.TABLE.RECORD_TEXT, Styles.TABLE.ELECTION_STATUS_CELL) }, [
-                      span({ isRendered: pendingCase.isReminderSent === true }, ['URGENT!']),
-                      span({ isRendered: (pendingCase.status === 'pending') && (pendingCase.isReminderSent !== true) }, ['Pending']),
-                      span({ isRendered: (pendingCase.status === 'editable') && (pendingCase.isReminderSent !== true) }, ['Editable'])
-                    ]),
-                    div({ style: Object.assign({}, Styles.TABLE.RECORD_TEXT, Styles.TABLE.DAC_CELL)}, [pendingCase.logged]),
-                    div({  style: Object.assign({}, Styles.TABLE.RECORD_TEXT, Styles.TABLE.ELECTION_STATUS_CELL, {paddingtop: "0.5rem"}) }, [
-                      button({
-                        onClick: this.openAccessReview(pendingCase.referenceId),
-                        id: pendingCase.frontEndId + '_btnVoteAccess', name: 'btn_voteAccess',
-                        className: 'cell-button hover-color'
-                      }, [
-                        span({ isRendered: pendingCase.alreadyVoted === false }, ['Vote']),
-                        span({ isRendered: pendingCase.alreadyVoted === true }, ['Edit'])
-                      ])
+                div({style: Object.assign({}, borderStyle, Styles.TABLE.RECORD_ROW), paddingtop: '1rem' }, [
+                  div({
+                    style: Object.assign({}, Styles.TABLE.DATA_ID_CELL,  Styles.TABLE.DATA_REQUEST_TEXT),
+                    onClick: () => openSummaryModal(pendingCase),
+                    onMouseEnter: applyTextHover,
+                    onMouseLeave: (e) => removeTextHover(e, Styles.TABLE.DATA_REQUEST_TEXT.color)
+                  },  [pendingCase.frontEndId]),
+                  div({ style: Object.assign({}, Styles.TABLE.RECORD_TEXT, Styles.TABLE.TITLE_CELL) }, [pendingCase.projectTitle]),
+                  div({ style: Object.assign({}, Styles.TABLE.RECORD_TEXT, Styles.TABLE.DAC_CELL) }, [dacName]),
+                  div({ style: Object.assign({}, Styles.TABLE.RECORD_TEXT, Styles.TABLE.ELECTION_STATUS_CELL) }, [
+                    span({ isRendered: pendingCase.isReminderSent === true }, ['URGENT!']),
+                    span({ isRendered: (pendingCase.status === 'pending') && (pendingCase.isReminderSent !== true) }, ['Pending']),
+                    span({ isRendered: (pendingCase.status === 'editable') && (pendingCase.isReminderSent !== true) }, ['Editable'])
+                  ]),
+                  div({ style: Object.assign({}, Styles.TABLE.RECORD_TEXT, Styles.TABLE.DAC_CELL)}, [pendingCase.logged]),
+                  div({  style: Object.assign({}, Styles.TABLE.RECORD_TEXT, Styles.TABLE.ELECTION_STATUS_CELL, {paddingtop: "0.5rem"}) }, [
+                    button({
+                      onClick: this.openAccessReview(pendingCase.referenceId),
+                      id: pendingCase.frontEndId + '_btnVoteAccess', name: 'btn_voteAccess',
+                      className: 'cell-button hover-color'
+                    }, [
+                      span({ isRendered: pendingCase.alreadyVoted === false }, ['Vote']),
+                      span({ isRendered: pendingCase.alreadyVoted === true }, ['Edit'])
                     ])
                   ])
-                ]);
-              }),
-            h(DarModal, { showModal: showModal, closeModal: closeSummaryModal, darDetails: darDetails, researcher: researcher }),
-            h(PaginationBar, {pageCount, currentPage: this.state.currentAccessPage, tableSize: this.state.accessLimit, goToPage: this.handleAccessPageChange, changeTableSize: this.handleAccessSizeChange}),
-          ])
+                ])
+              ]);
+            }),
+          h(DarModal, { showModal, closeModal: closeSummaryModal, darDetails, researcher }),
+          h(PaginationBar, {pageCount, currentPage: this.state.currentAccessPage, tableSize: this.state.accessLimit, goToPage: this.handleAccessPageChange, changeTableSize: this.handleAccessSizeChange}),
+        ])
       ])
     );
   }
