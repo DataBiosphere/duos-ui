8ec609e251ffd665cd0e86f6c3ffd8bb176b8131
JVThomas <jthomas0209@gmail.com>
2021-05-03 11:26:34 -0400

    DUOS-1098 added close button component to ConfirmationModal

--- src/components/modals/ConfirmationModal.js
+++ src/components/modals/ConfirmationModal.js
@@ -2,9 +2,11 @@ import {button, div, h} from "react-hyperscript-helpers";
 import {Styles} from "../../libs/theme";
 import Modal from "react-modal";
 import {isNil} from "lodash";
+import CloseIconComponent from '../CloseIconComponent';
 
 const ConfirmationModal = (props) => {
   const {showConfirmation, closeConfirmation, title, message, header, onConfirm, id, index} = props;
+  const closeFn = () => closeConfirmation();
 
   //id and index are optional parameters to onConfirm
   //can use an onConfirm with no parameters
@@ -23,12 +25,13 @@ const ConfirmationModal = (props) => {
     }
   }, [
     div({style: Styles.MODAL.CONFIRMATION}, [
+      h(CloseIconComponent, {closeFn}),
       div({style: Styles.MODAL.DAR_SUBHEADER}, [`${header}`]),
       div({style: Styles.MODAL.TITLE_HEADER}, [`${title}`]),
       div({style: Styles.MODAL.DAR_DETAIL}, [`${message}`]),
       div({style: {width: "40%", float: "right"}}, [
         div({style: {width: "45%", float: "left"}}, [
-          button({className: "cell-button cancel-color", onClick: () => closeConfirmation() }, ["Cancel"])
+          button({className: "cell-button cancel-color", onClick: closeFn }, ["Cancel"])
         ]),
         div({style: {width: "45%", float: "right"}}, [
           confirmButton
