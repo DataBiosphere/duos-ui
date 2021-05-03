c10b4250ebceb5528e02b8f2a5735aa0c24d5593
JVThomas <jthomas0209@gmail.com>
2021-05-03 09:08:20 -0400

    DUOS-1098 standardized close button as its own component

---
+++ src/components/CloseIconComponent.js
@@ -0,0 +1,12 @@
+import { button, span } from 'react-hyperscript-helpers';
+
+export default function CloseIconComponent(props) {
+  const { closeFn } = props;
+  return button({
+    type: 'button',
+    className: 'modal-close-btn close',
+    onClick: closeFn
+  }, [
+    span({ className: ' glyphicon glyphicon-remove default-color'})
+  ]);
+}
