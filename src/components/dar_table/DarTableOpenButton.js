cba15ebd0fe0c2da3f51e0155604018531aed231
rfricke-asymmetrik <75337761+rfricke-asymmetrik@users.noreply.github.com>
2021-05-26 09:18:31 -0400

    DUOS-1266 Change vote button styles (#1106)

--- src/components/dar_table/DarTableOpenButton.js
+++ src/components/dar_table/DarTableOpenButton.js
@@ -4,7 +4,7 @@ import { h } from 'react-hyperscript-helpers';
 
 export default function OpenButton(props) {
   const { dar, index, openConfirmation, label, disabled, addBaseStyle } = props;
-  const baseStyle = Styles.TABLE.TABLE_TEXT_BUTTON;
+  const baseStyle = Styles.TABLE.TABLE_TEXT_BUTTON_SUCCESS;
 
   return h(TableTextButton, {
     onClick: () => openConfirmation(dar, index),
@@ -12,6 +12,7 @@ export default function OpenButton(props) {
     label,
     disabled,
     dataTip: disabled ? "You do not have permission to open this election" : "Open Election",
-    style: Object.assign({}, baseStyle, addBaseStyle)
+    style: Object.assign({}, baseStyle, addBaseStyle),
+    hoverStyle: Styles.TABLE.TABLE_BUTTON_TEXT_HOVER_SUCCESS
   });
 }
