478d48d5849289794ed849428b70e2c0c7bffe40
raejohanek <rjohanek@broadinstitute.org>
2021-07-06 14:54:15 -0400

    revert ResearcherReview.js changes

--- src/pages/ResearcherReview.js
+++ src/pages/ResearcherReview.js
@@ -103,6 +103,7 @@ class ResearcherReview extends Component {
   render() {
 
     const { formData, rationale, voteStatus, user, institution } = this.state;
+
     return (
       div({ className: "container " }, [
         div({ className: "col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1 col-sm-12 col-xs-12" }, [
