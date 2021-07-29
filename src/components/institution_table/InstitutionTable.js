3caf3e7f69b7b3f1c990d5f14fce25527ee3f21f
raejohanek <rjohanek@broadinstitute.org>
2021-07-29 17:03:46 -0400

    add SO column

--- src/components/institution_table/InstitutionTable.js
+++ src/components/institution_table/InstitutionTable.js
@@ -1,6 +1,6 @@
-import { isEmpty, assign } from 'lodash/fp';
+import { isEmpty, isNil, assign } from 'lodash/fp';
 import { useState, useEffect } from 'react';
-import { div, h, a } from 'react-hyperscript-helpers';
+import { div, h, a , span } from 'react-hyperscript-helpers';
 import { Styles } from '../../libs/theme';
 import ReactTooltip from 'react-tooltip';
 import PaginationBar from '../PaginationBar';
@@ -9,6 +9,7 @@ import AddInstitutionModal from '../modals/AddInstitutionModal';
 export const tableHeaderTemplate = [
   div({style: Styles.TABLE.ID_CELL}, ["ID"]),
   div({style: Styles.TABLE.INSTITUTION_CELL}, ["Institution"]),
+  div({style: Styles.TABLE.INSTITUTION_CELL}, ["Signing Officials"]),
   div({style: Styles.TABLE.DATA_ID_CELL}, ["Create User"]),
   div({style: Styles.TABLE.SUBMISSION_DATE_CELL}, ["Create Date"]),
   div({style: Styles.TABLE.DATA_ID_CELL}, ["Update User"]),
@@ -19,6 +20,7 @@ const loadingMarginOverwrite = {margin: '1rem 2%'};
 
 export const tableRowLoadingTemplate = [
   div({style: assign(Styles.TABLE.ID_CELL, loadingMarginOverwrite), className: 'text-placeholder'}),
+  div({style: assign(Styles.TABLE.INSTITUTION_CELL, loadingMarginOverwrite), className: 'text-placeholder'}),
   div({style: assign(Styles.TABLE.INSTITUTION_CELL, loadingMarginOverwrite), className: 'text-placeholder'}),
   div({style: assign(Styles.TABLE.DATA_ID_CELL, loadingMarginOverwrite), className: 'text-placeholder'}),
   div({style: assign(Styles.TABLE.SUBMISSION_DATE_CELL, loadingMarginOverwrite), className: 'text-placeholder'}),
@@ -72,6 +74,14 @@ export default function InstitutionTable(props) {
       div({style: Styles.TABLE.CONTAINER}, [
         div({style: Styles.TABLE.HEADER_ROW}, [tableHeaderTemplate]),
         filteredList.slice((currentPage - 1) * tableSize, (currentPage * tableSize)).map((inst, index) => {
+          let signingOfficials = '';
+          isNil(inst.signingOfficials) ? signingOfficials = '' : inst.signingOfficials.forEach((user, i) => {
+            signingOfficials = signingOfficials.concat(user.displayName, ' (', user.email, ')');
+            //if there are multiple SOs in the list and this is not the last one, add line break
+            if (inst.signingOfficials.length > 1 && i < (inst.signingOfficials.length - 1)) {
+              signingOfficials = signingOfficials.concat('\n');
+            }
+          });
           const borderStyle = index > 0 ? {borderTop: "1px solid rgba(109,110,112,0.2)"} : {};
           return div({style: Object.assign({}, borderStyle, Styles.TABLE.RECORD_ROW), key: `${inst.id}-${index}`}, [
             div({
@@ -81,12 +91,14 @@ export default function InstitutionTable(props) {
               style: Object.assign({}, Styles.TABLE.INSTITUTION_CELL)
             }, [
               a({ style: {
-                overflow: "hidden",
                 whiteSpace: "nowrap",
                 textOverflow: "ellipsis"},
               onClick: () => { openUpdateModal(inst.id); }
               }, [inst.name])
             ]),
+            div({
+              style: Object.assign({}, Styles.TABLE.INSTITUTION_CELL)
+            }, [span(signingOfficials)]),
             div({
               style: Object.assign({}, Styles.TABLE.DATA_ID_CELL)
             }, [inst.createUser ? inst.createUser.displayName : '']),
