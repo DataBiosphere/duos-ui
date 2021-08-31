8b6a001e1e75baf74056500456592f655f96a89f
Greg Rushton <grushton@broadinstitute.org>
2021-08-31 13:44:42 -0400

    cleanup

--- cypress/integration/auth_spec.js
+++ cypress/integration/auth_spec.js
@@ -10,10 +10,9 @@ const roles = {
 
 describe('Authentication', function() {
 
-  it('Background Sign-in authenticates', function() {
+  it('Background Sign-in Admin', function() {
     cy.viewport(2000, 2000);
     cy.auth(roles.ADMIN).then(credentials => {
-      // console.log(JSON.stringify(credentials, null, 2));
       cy.visit('/backgroundsignin');
       cy.get('textarea').type(credentials.access_token, {delay: 0});
       cy.get('form').submit();
