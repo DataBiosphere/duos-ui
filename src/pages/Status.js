a187ce7a43222987d94608299fce07b1296b6cf8
Greg Rushton <grushton@broadinstitute.org>
2021-09-14 16:42:33 -0400

    rm fc status

--- src/pages/Status.js
+++ src/pages/Status.js
@@ -22,23 +22,15 @@ export const Status = hh(class Status extends Component {
     return bools.length === 1 && bools[0];
   };
 
-  isFCHealthy = (fcStatus) => {
-    return _.get(fcStatus, 'ok', false);
-  };
-
   async componentDidMount() {
     const consentStatusUrl = `${ await Config.getApiUrl() }/status`;
     const ontologyStatusUrl = `${ await Config.getOntologyApiUrl() }/status`;
-    const fireCloudStatusUrl = `${ await Config.getFireCloudUrl() }/status`;
     fetch(consentStatusUrl, { method: 'GET' })
       .then(response => response.json())
       .then(data => this.setState({ consentStatus: data }));
     fetch(ontologyStatusUrl, { method: 'GET' })
       .then(response => response.json())
       .then(data => this.setState({ ontologyStatus: data }));
-    fetch(fireCloudStatusUrl, { method: 'GET' })
-      .then(response => response.json())
-      .then(data => this.setState({ fireCloudStatus: data }));
   }
 
   render() {
@@ -46,22 +38,18 @@ export const Status = hh(class Status extends Component {
     const unhealthyState = <DiameterVariant fill={ 'red' } style={ { marginLeft: '2rem', verticalAlign: 'middle' } }/>;
     const consentHealthy = this.isHealthy(this.state.consentStatus) ? healthyState : unhealthyState;
     const ontologyHealthy = this.isHealthy(this.state.ontologyStatus) ? healthyState : unhealthyState;
-    const fireCloudHealthy = this.isFCHealthy(this.state.fireCloudStatus) ? healthyState : unhealthyState;
 
     return (
       div({ style: { margin: '2rem' } }, [
         ul({ style: { marginTop: '2rem', listStyle: 'none', fontSize: 'x-large' } }, [
           li({}, [a({ href: '#consent' }, 'Consent'), ' ', consentHealthy]),
-          li({}, [a({ href: '#ontology' }, 'Ontology'), ' ', ontologyHealthy]),
-          li({}, [a({ href: '#firecloud' }, 'FireCloud'), ' ', fireCloudHealthy])
+          li({}, [a({ href: '#ontology' }, 'Ontology'), ' ', ontologyHealthy])
         ]),
         hr(),
         h2({}, [a({ id: 'consent' }, ['Consent Status'])]),
         pre({}, [JSON.stringify(this.state.consentStatus, null, 4)]),
         h2({}, [a({ id: 'ontology' }, ['Ontology Status'])]),
-        pre({}, [JSON.stringify(this.state.ontologyStatus, null, 4)]),
-        h2({}, [a({ id: 'firecloud' }, ['FireCloud Status'])]),
-        pre({}, [JSON.stringify(this.state.fireCloudStatus, null, 4)])
+        pre({}, [JSON.stringify(this.state.ontologyStatus, null, 4)])
       ]));
   }
 
