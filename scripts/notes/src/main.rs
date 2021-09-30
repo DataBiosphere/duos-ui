4b68ef8608f5ef5b74ad08d52ac9f08e8d4316c7
Greg Rushton <grushton@broadinstitute.org>
2021-09-30 09:30:56 -0400

    parallelize

--- scripts/notes/src/main.rs
+++ scripts/notes/src/main.rs
@@ -1,5 +1,6 @@
 use std::path::Path;
 use std::string::String;
+use rayon::prelude::*;
 
 use config::Config;
 use serde_json::Result;
@@ -11,15 +12,14 @@ mod jira;
 
 fn main() {
     let config: Result<Config> = config::parse_config("./config.json");
-    // TODO: Parallelize
-    for repo in config.unwrap().repos {
+    config.unwrap().repos.into_par_iter().for_each(|repo| {
         let title: String = repo.title.clone().unwrap_or(titlecase(repo.name.clone().as_str()));
         let header: Vec<String> = vec![format!("\n\n{}:", title)];
         let messages: Vec<String> = checkout_and_generate_log_messages(repo.name.clone(), repo.tag_pattern.clone(), repo.from_commit.clone(), repo.to_commit.clone());
         for m in [header, messages].concat() {
             println!("{}", m);
         }
-    }
+    });
 }
 
 /// Generates a list of commit messages from the provided DataBiosphere repo
