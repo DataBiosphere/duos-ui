833b8464bbd3e041cfb50bed87a67a2f427860ea
JVThomas <jthomas0209@gmail.com>
2021-11-10 09:15:09 -0500

    DUOS-1493 added helper function to better evaluate completed status

--- src/libs/utils.js
+++ src/libs/utils.js
@@ -4,7 +4,7 @@ import 'noty/lib/themes/bootstrap-v3.css';
 import { forEach } from 'lodash';
 import { DAR, DataSet } from "./ajax";
 import {Theme, Styles } from "./theme";
-import { find, first, map, isEmpty, filter, cloneDeep, isNil, toLower, includes, sortedUniq, every} from "lodash/fp";
+import { find, first, map, isEmpty, filter, cloneDeep, isNil, toLower, includes, sortedUniq, every, pick} from "lodash/fp";
 import _ from 'lodash';
 import {User} from "./ajax";
 
@@ -97,6 +97,7 @@ export const getPropertyValuesFromUser = (user) => {
     zipcode: findPropertyValue(UserProperties.ZIPCODE, user)
   };
 
+  researcherProps.institutionId = user.institutionId;
   return researcherProps;
 };
 
@@ -655,3 +656,33 @@ export const getBooleanFromEventHtmlDataValue = (e) => {
   }
   return false;
 };
+
+export const evaluateTrueString = (boolString) => {
+  return !isEmpty(boolString) && toLower(boolString) === "true";
+}
+
+//helper method for ResearcherInfo component in DAR application page
+export const completedResearcherInfoCheck = (properties) => {
+  const {
+    piName,
+    isThePI,
+    havePI,
+    piEmail,
+    institutionId,
+  } = properties;
+
+  const piCheck = ({isThePI, havePI, piEmail, piName}) => {
+    //conditions listed are invalid checks
+    //if all are true, value returned MUST be false, since pi portion of the form is incomplete
+    const isThePIFalse = !evaluateTrueString(isThePI);
+    const havePITrue = evaluateTrueString(havePI);
+    const piAttrEmpty = isEmpty(piName) || isEmpty(piEmail)  
+    return !(isThePIFalse && havePITrue && piAttrEmpty)
+  }
+
+  const stringAttrs = pick(['displayName', 'address1', 'city', 'state', 'zipCode', 'country'])(properties);
+  const stringAttrsCompleted = every((string) => !isEmpty(string))(stringAttrs);
+  const institutionPresent = !isNil(institutionId);
+  const piValid = piCheck({isThePI, havePI, piEmail, piName});
+  return piValid && stringAttrsCompleted && institutionPresent;
+}
