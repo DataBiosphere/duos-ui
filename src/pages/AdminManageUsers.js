49c51ad7119365fd84b51014cae1dcd2103fec6f
Shae Marks <smarks.dev1@gmail.com>
2022-01-20 13:42:56 -0500

    Enable Researcher Review button for all users

--- src/pages/AdminManageUsers.js
+++ src/pages/AdminManageUsers.js
@@ -200,13 +200,9 @@ class AdminManageUsers extends Component {
 
                     a({
                       id: user.dacUserId + "_btnResearcherReview", name: "btn_researcherReview", onClick: () => this.openResearcherReview(user.dacUserId),
-                      isRendered: user.researcher !== false && user.completed === true, className: "admin-manage-buttons col-lg-10 col-md-10 col-sm-10 col-xs-9"
+                      isRendered: true, className: "admin-manage-buttons col-lg-10 col-md-10 col-sm-10 col-xs-9"
                     }, [
                       div({ className: 'enabled' }, ["Review"]),
-                    ]),
-
-                    a({ isRendered: user.researcher === "false" || !user.completed, className: "admin-manage-buttons col-lg-10 col-md-10 col-sm-10 col-xs-9" }, [
-                      div({ className: "disabled" }, ["Review"]),
                     ]),
 
                   ]),
