949ec09da82a4019e5e2ce12f7ab219c9d1b9332
Greg Rushton <grushton@broadinstitute.org>
2022-01-25 10:19:21 -0500

    second test wip

--- cypress/component/SigningOfficialTable/lca_text.spec.js
+++ cypress/component/SigningOfficialTable/lca_text.spec.js
@@ -5,6 +5,8 @@ import {mount} from '@cypress/react';
 import SigningOfficialTable
   from '../../../src/components/signing_official_table/SigningOfficialTable';
 
+const lcaHeaderText = 'Broad Data Use Oversight System (DUOS) – LIBRARY CARD AGREEMENT';
+
 describe('SigningOfficialTable - Tests', function() {
   it('SigningOfficialTable - Add New Researcher modal displays the LCA Text', function() {
     cy.viewport(600, 300);
@@ -17,8 +19,30 @@ describe('SigningOfficialTable - Tests', function() {
     const button = cy.contains('Add New Researcher');
     expect(button).to.exist;
     button.click();
-    const lcaHeader = cy.contains(
-      'Broad Data Use Oversight System (DUOS) – LIBRARY CARD AGREEMENT');
+    const lcaHeader = cy.contains(lcaHeaderText);
     expect(lcaHeader).to.exist;
+  });
+
+  it('SigningOfficialTable - Issue modal displays the LCA Text', function() {
+    cy.viewport(600, 300);
+    mount(<SigningOfficialTable
+      isLoading={false}
+      signingOfficial={{institutionId: 1}}
+      researchers={[
+        {
+          email: 'email',
+          dacUserId: 1,
+          displayName: 'researcher',
+          roles: [{name: 'Researcher'}],
+          libraryCards: []
+        }
+      ]}
+      unregisteredResearchers={[]}
+    />);
+    // const button = cy.contains('Add New Researcher');
+    // expect(button).to.exist;
+    // button.click();
+    // const lcaHeader = cy.contains(lcaHeaderText);
+    // expect(lcaHeader).to.exist;
   });
 });
