81be892332905f9b13920e26d64ee63905275951
Shae Marks <smarks.dev1@gmail.com>
2022-02-11 14:25:26 -0500

    Fix functionality to display vote information

--- src/components/vote_summary_table/VoteSummaryTable.js
+++ src/components/vote_summary_table/VoteSummaryTable.js
@@ -1,16 +1,17 @@
 import SimpleTable from "../SimpleTable";
 import {h} from "react-hyperscript-helpers";
 import {Styles} from "../../libs/theme";
-import {isEmpty} from "lodash/fp";
+import {isEmpty, isNil} from "lodash/fp";
 import {useState} from "react";
-import {User} from "../../libs/ajax";
+import {formatDate} from "../../libs/utils";
 
 
 const styles = {
   baseStyle: {
-    fontFamily: 'Montserrat Regular',
-    fontSize: '14px',
+    fontFamily: 'Montserrat',
+    fontSize: '1.6rem',
     fontWeight: 400,
+    color: '#333F52',
     display: 'flex',
     padding: '1rem 2%',
     justifyContent: 'space-between',
@@ -20,52 +21,12 @@ const styles = {
     justifyContent: 'space-between',
   }),
   cellWidths: {
-    vote: '25%',
-    name: '20%',
-    date: '25%',
-    rationale: '20%',
+    vote: '10%',
+    name: '15%',
+    date: '10%',
+    rationale: '50%',
   },
 };
-
-function voteCellData({vote = '- -', voteId, label = 'vote'}) {
-  return {
-    data: vote,
-    id: voteId,
-    cellStyle: { width: styles.cellWidth.date },
-    sortable: true,
-    label
-  };
-}
-
-function nameCellData({name = '- -', voteId, label = 'name'}) {
-  return {
-    data: name,
-    id: voteId,
-    cellStyle: { width: styles.cellWidth.name },
-    sortable: true,
-    label
-  };
-}
-
-function dateCellData({date = '- -', voteId, label = 'date'}) {
-  return {
-    data: date,
-    id: voteId,
-    cellStyle: { width: styles.cellWidth.date },
-    sortable: true,
-    label
-  };
-}
-
-function rationaleCellData({rationale = '- -', voteId, label = 'rationale'}) {
-  return {
-    data: rationale,
-    id: voteId,
-    cellStyle: { width: styles.cellWidth.rationale },
-    sortable: true,
-    label
-  };
-}
 
 const columnHeaderFormat = {
   vote: {label: 'Vote', cellStyle: {width: styles.cellWidths.vote}},
@@ -80,13 +41,12 @@ const columnHeaderData = () => {
 };
 
 const processVoteSummaryRowData = ({ dacVotes }) => {
-  if(!isEmpty(dacVotes)) {
+  if(!isNil(dacVotes)) {
     return dacVotes.map((dacVote) => {
-      const { voteId, vote, dacUserId, createDate, updateDate, rationale } = dacVote;
-      const user = User.getById(dacUserId);
-      const { displayName } = user;  //TODO: check case of null info
+      const { vote, displayName } = dacVote;
+      const { voteId, createDate, updateDate, rationale, requestRevision } = vote;
       return [
-        voteCellData({vote, voteId}),
+        voteCellData({vote, requestRevision, voteId}),
         nameCellData({name: displayName, voteId}),
         dateCellData({date: updateDate || createDate, voteId}),
         rationaleCellData({rationale, voteId})
@@ -96,6 +56,51 @@ const processVoteSummaryRowData = ({ dacVotes }) => {
 };
 
 
+function voteCellData({vote, requestRevision, voteId, label = 'vote'}) {
+  const voteResult = requestRevision ? "Request Revision" :
+    isNil(vote.vote) ? "- -" :
+      vote.vote ? "Yes" : "No";
+
+  return {
+    data: voteResult,  //TODO need request revision case + format better
+    id: voteId,
+    cellStyle: { width: styles.cellWidths.vote },
+    sortable: true,
+    label
+  };
+}
+
+function nameCellData({name = '- -', voteId, label = 'name'}) {
+  return {
+    data: name,
+    id: voteId,
+    cellStyle: { width: styles.cellWidths.name },
+    sortable: true,
+    label
+  };
+}
+
+function dateCellData({date, voteId, label = 'date'}) {
+  return {
+    data: isNil(date) ? '- - ' : formatDate(date),
+    id: voteId,
+    cellStyle: { width: styles.cellWidths.date },
+    sortable: true,
+    label
+  };
+}
+
+function rationaleCellData({rationale = '- -', voteId, label = 'rationale'}) {
+  return {
+    data: rationale,
+    id: voteId,
+    cellStyle: { width: styles.cellWidths.rationale },
+    sortable: true,
+    label
+  };
+}
+
+
 export default function VoteSummaryTable(props) {
   const [sort, setSort] = useState({ colIndex: 0, dir: 1 });
   const [tableSize, setTableSize] = useState(10);
@@ -103,10 +108,10 @@ export default function VoteSummaryTable(props) {
 
   return h(SimpleTable, {
     isLoading,
-    "rowData": processVoteSummaryRowData(dacVotes),
+    "rowData": processVoteSummaryRowData({ dacVotes }),
     "columnHeaders": columnHeaderData(),
     styles,
-    tableSize: tableSize,
+    tableSize,
     sort,
     onSort: setSort
   });
