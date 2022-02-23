7088edd9893b89ec6899f8a75151c629ce08d2f4
Greg Rushton <grushton@broadinstitute.org>
2022-02-23 13:36:53 -0500

    formatting

--- src/components/LibraryCardAgreementTermsDownload.js
+++ src/components/LibraryCardAgreementTermsDownload.js
@@ -1,12 +1,12 @@
 import LibraryCardAgreementLink from '../assets/Library_Card_Agreement_2021.pdf';
 import DownloadIcon from 'react-material-icon-svg/dist/Download';
-import {a, div} from "react-hyperscript-helpers";
-import React from "react";
+import {a, div} from 'react-hyperscript-helpers';
+import React from 'react';
 
 const downloadIcon = <DownloadIcon fill={'black'} style={{verticalAlign: 'middle'}}/>;
 export const LibraryCardAgreementTermsDownload =
   div({}, [
-    "I agree to the terms of the Library Card Agreement",
+    'I agree to the terms of the Library Card Agreement',
     a({
       id: 'link_downloadAgreement',
       href: LibraryCardAgreementLink,
