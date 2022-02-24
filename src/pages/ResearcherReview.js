0c5831a4840f36584c82118e00a7c2cfe3c7988f
Shae Marks <smarks.dev1@gmail.com>
2022-02-24 15:13:57 -0500

    Remove console.log from debugging in ResearcherReview

--- src/pages/ResearcherReview.js
+++ src/pages/ResearcherReview.js
@@ -44,7 +44,6 @@ export const ResearcherReview = hh(class ResearcherReview extends Component {
   }
 
   calculateResearcherInfo = () => {
-    console.log("Processing props.user: " + JSON.stringify(this.props.user));
     const user = this.props.user;
     let researcherProps = getPropertyValuesFromUser(user);
     this.setState(prev => {
