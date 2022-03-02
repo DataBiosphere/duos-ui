db254376d692631b9c5ca6c0d28bec475961582c
Shae Marks <smarks.dev1@gmail.com>
2022-03-02 10:48:52 -0500

    Add styles to data use pill and description

--- src/components/collection_voting_slab/DataUsePill.js
+++ src/components/collection_voting_slab/DataUsePill.js
@@ -1,5 +1,23 @@
 import {div} from "react-hyperscript-helpers";
 
+const styles = {
+  code: {
+    height: '32px',
+    width: '53px',
+    borderRadius: '16px',
+    backgroundColor: '#0948B7',
+    color: '#FFFFFF',
+    fontFamily: 'Arial',
+    fontSize: '14px',
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+  description: {
+    color: '#333F52',
+    fontFamily: 'Arial',
+    fontSize: '14px'
+  }
+};
 
 
 export default function DataUsePill(props) {
