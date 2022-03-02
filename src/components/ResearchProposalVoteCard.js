5ddba2c2154a6728de40a0e69e0bdf06357643c5
Shae Marks <smarks.dev1@gmail.com>
2022-03-02 08:54:12 -0500

    Add shadow to collapsed view of component

--- src/components/ResearchProposalVoteCard.js
+++ src/components/ResearchProposalVoteCard.js
@@ -1,24 +1,41 @@
 import {useState} from "react";
-import {a, div} from "react-hyperscript-helpers";
-import {StructuredDarRp} from "../StructuredDarRp";
+import {a, div, h} from "react-hyperscript-helpers";
+import {StructuredDarRp} from "./StructuredDarRp";
+import DataUseVoteSummary from "./common/DataUseVoteSummary/DataUseVoteSummary";
 
 const styles = {
-  headerStyle: {
+  srpHeader: {
     color: '#000000',
     fontFamily: 'Montserrat',
     fontSize: '1.6rem',
     fontWeight: 'bold'
   },
-  textStyle: {
+  srpText: {
     color: '#333F52',
     fontFamily: 'Montserrat',
     fontSize: '1.4rem'
   },
-  tabStyle: {
-    color: '#F1EDE8'
+  collapsedTab: {
+    backgroundColor: '#F1EDE8',
+    borderRadius: '5px 5px 0px 0px',
+    //TODO: reassess shadow code once not overridden by other stylings
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0,
+    shadowRadius: 4.65,
+
+    elevation: 8,
   }
 };
 
+const researchPurposeTab = () => {
+  return div({}, [
+    "Structured Research Purpose"
+  ]);
+};
 
 export default function ResearchProposalVoteCard(props) {
   const [expanded, setExpanded] = useState(false);
@@ -36,15 +53,16 @@ export default function ResearchProposalVoteCard(props) {
   };
 
   return div({className: 'col-lg-6 col-md-6 col-sm-12 col-xs-12 jumbotron box-vote-stats'}, [
-    div([
+    div({className: 'srp_collapsed', style: styles.collapsedTab}, [
+      researchPurposeTab(),
       StructuredDarRp({
         darInfo,
-        headerStyle: styles.headerStyle,
-        textStyle: styles.textStyle
+        headerStyle: styles.srpHeader,
+        textStyle: styles.srpText
       }),
       linkToExpand(),
     ]),
-    div({isRendered: expanded }, [
+    div({className: 'srp_expanded', isRendered: expanded}, [
       "filler text"
     ])
   ]);
