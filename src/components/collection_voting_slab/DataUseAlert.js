751ab9e48812ae929da280c4faecdbc9dbe0695d
Shae Marks <smarks.dev1@gmail.com>
2022-03-02 23:46:14 -0500

    Edit styling of DataUseAlert box

--- src/components/collection_voting_slab/DataUseAlert.js
+++ src/components/collection_voting_slab/DataUseAlert.js
@@ -1,13 +1,40 @@
 import {div, span} from "react-hyperscript-helpers";
+import * as ld from "lodash";
 
 
 const styles = {
-box: {
-  border: '2px solid #DB3214',
-  borderRadius: '8px',
-  backgroundColor: '#FFFFFF'
-}
+  box: {
+    backgroundColor: '#FFFFFF',
+    color: '#DB3214',
+    fontFamily: 'Montserrat',
+    padding: '1rem',
+    border: '2px solid #DB3214',
+    borderRadius: '8px',
+    display: 'flex',
+    alignItems: 'center',
+    columnGap: '1rem',
+    width: 'fit-content'
+  },
+  text: {
+    fontSize: '1.6rem',
+    fontWeight: '500',
+  },
+  exclamationPoint: {
+    fontSize: '5rem'
+  }
 
+};
+
+const dataUseDescriptions = (translatedDataUse) => {
+  const descriptions = ld.flatMap(ld.keys(translatedDataUse), key => {
+    const dataUses = translatedDataUse[key];
+    if (!ld.isEmpty(dataUses)) {
+      return ld.map(dataUses, dataUse => {
+        return div([dataUse.description, '\n']);
+      });
+    }
+  });
+  return div({style: styles.text}, [descriptions]);
 };
 
 
@@ -15,6 +42,7 @@ export default function DataUseAlert(props) {
   const {translatedDataUse} = props;
 
   return div({className: 'data_use_alert', style: styles.box}, [
-    "ExclamationIcon"
+    span({style: styles.exclamationPoint}, ['!']),
+    dataUseDescriptions(translatedDataUse)
   ]);
 }
