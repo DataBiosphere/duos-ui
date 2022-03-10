7d61cc7d91e852448f95d2471fde7f81913c2088
Shae Marks <smarks.dev1@gmail.com>
2022-03-10 01:14:50 -0500

    Add base style to component

--- src/components/CollectionSubmitVoteBox.js
+++ src/components/CollectionSubmitVoteBox.js
@@ -1,17 +1,25 @@
 import {div, span} from "react-hyperscript-helpers";
 
+const styles = {
+  baseStyle: {
+    fontFamily: 'Montserrat',
+    display: 'flex',
+    flexDirection: 'column'
+  }
+};
+
 export default function CollectionSubmitVoteBox(props) {
   const {question} = props;
 
   return (
-    div([
+    div({style: styles.baseStyle}, [
       div([question]),
-      div({dataCy: 'content'}[
-        div({dataCy: 'vote-container'},[
+      div([
+        div([
           span(["Your Vote*"]),
           div(["Buttons"])
         ]),
-        div({dataCy: 'rationale-container'},[
+        div([
           span(["Rationale (optional):"]),
           div(["Rationale Box"])
         ])
