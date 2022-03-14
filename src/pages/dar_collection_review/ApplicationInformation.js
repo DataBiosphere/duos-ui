554ac41ebf5bfa1c68b1d2090a7b4d7e046c816b
Quazi Hoque <qhoque@broadinstitute.org>
2022-03-14 17:07:33 -0400

    Passing cloudComputing through to the row generation function to filter out cloud-provider details when unneeded. Refactored the lodash filter logic to be more readable. Corrected boolean init values for anvilStorage, localComputing, and cloudComputing. A few styling changes to get the output looking closer to the mockup.

--- src/pages/dar_collection_review/ApplicationInformation.js
+++ src/pages/dar_collection_review/ApplicationInformation.js
@@ -8,7 +8,7 @@ const styles = {
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'flex-start',
-    width: '48rem',
+    width: '49rem',
   },
   label: {
     fontWeight: 600,
@@ -24,15 +24,17 @@ const styles = {
     width: '80%',
     display: 'flex',
     justifyContent: 'flex-start',
-    marginBottom: '5rem',
+    marginBottom: '3rem',
   },
   title: {
     fontWeight: 800,
     fontSize: '2.7rem',
+    margin: '1.5rem 0',
   },
   subheader: {
     fontWeight: 800,
     fontSize: '2.4rem',
+    margin: '1.5rem 0',
   },
   textBox: {
     marginTop: '1.5rem',
@@ -44,7 +46,7 @@ const styles = {
     width: '80%',
     display: 'flex',
     justifyContent: 'space-between',
-    marginBottom: '5rem',
+    marginBottom: '3rem',
   },
 };
 
@@ -62,13 +64,19 @@ const generateLabelSpanContents = (labelValue, key,  spanValue, isLoading) => {
 };
 
 // function to generate application details content
-const dynamicRowGeneration = (rowElementMaxCount, appDetailLabels, loading) => {
+const dynamicRowGeneration = (rowElementMaxCount, appDetailLabels, loading, cloudComputing) => {
   // lodash/fp filter (non-empty string, non-empty object, non-empty array, booleans)
+  // also filter out the cloud-provider element if cloudComputing is false
   const labels = filter(label => {
-    return (typeof label.value === 'boolean' || !isEmpty(label.value));
+    return (
+      (typeof label.value === 'boolean')
+      || (!isEmpty(label.value) && label.key !== 'cloud-provider')
+      || (label.key === 'cloud-provider' && cloudComputing === true)
+    );
   }) (appDetailLabels);
 
   const labelArray = labels.map(label => {
+    debugger; // eslint-disable-line
     if (typeof label.value === 'boolean') {
       // Inject "Yes" / "No" for booleans
       return generateLabelSpanContents(label.title, label.key, (label.value ? 'Yes' : 'No'), loading);
@@ -110,9 +118,9 @@ export default function ApplicationInformation(props) {
     signingOfficialEmail = '- -',
     itDirectorEmail = '- -',
     internalLabStaff = [],
-    anvilStorage = '',
-    localComputing = '',
-    cloudComputing = '',
+    anvilStorage = false,
+    localComputing = false,
+    cloudComputing = false,
     cloudProvider = '- -',
     cloudProviderDescription
   } = props;
@@ -155,7 +163,7 @@ export default function ApplicationInformation(props) {
           generateLabelSpanContents('', 'row-three-blank', '', false) //blank span to keep row elements in line with those above
         ])
       ]),
-      !isLoading ? div({className: 'non-technical-summary-subheader', style: styles.title}, ["Non-Technical Summary"])
+      !isLoading ? div({className: 'non-technical-summary-subheader', style: styles.subheader}, ["Non-Technical Summary"])
         : div({className: 'text-placeholder', key: 'non-technical-summary-title-placeholder', style: {height: '4rem', width: '20%', marginBottom: '2rem'}}),
       div({className: 'non-technical-summary-container'}, [
         !isLoading ? div({className: 'non-technical-summary-textbox', style: styles.textBox}, [nonTechSummary])
@@ -164,14 +172,15 @@ export default function ApplicationInformation(props) {
           }})
       ]),
       div({className: 'application-details-container', style: { margin: '2.5rem 0'}}, [
-        div({className: 'applicant-details-subheader', style: styles.title}, ["Application Details"]),
-        dynamicRowGeneration(2, appDetailLabels, isLoading),
-        (cloudComputing) ? div({className: 'cloud-provider-description-container'}, [
-          !isLoading ? div({className: 'cloud-provider-description-textbox', style: styles.textBox}, [cloudProviderDescription])
-            : div({className: 'text-placeholder', key: 'cloud-provider-description-placeholder', style: { height: '18rem',
-              width: '100%',
-            }})
-        ])  : ''
+        div({className: 'applicant-details-subheader', style: styles.subheader}, ["Application Details"]),
+        dynamicRowGeneration(2, appDetailLabels, isLoading, cloudComputing),
+        (cloudComputing) ?
+          div({className: 'cloud-provider-description-container'}, [
+            !isLoading ? div({className: 'cloud-provider-description-textbox', style: styles.textBox}, [cloudProviderDescription])
+              : div({className: 'text-placeholder', key: 'cloud-provider-description-placeholder', style: { height: '18rem',
+                width: '100%',
+              }})
+          ])  : ''
       ]),
     ])
   );
