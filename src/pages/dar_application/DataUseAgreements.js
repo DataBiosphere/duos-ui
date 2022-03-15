54960c00279f9b807bc93b1c73320c44c119efd3
Shae Marks <smarks.dev1@gmail.com>
2022-03-15 02:38:52 -0400

    Change section 4.1 title to Attestation and Agreement(s)

--- src/pages/dar_application/DataUseAgreements.js
+++ src/pages/dar_application/DataUseAgreements.js
@@ -54,7 +54,7 @@ export default function DataUseAgreements(props) {
     div({ className: 'col-lg-10 col-lg-offset-1 col-md-12 col-sm-12 col-xs-12' }, [
       fieldset({ disabled: !isNil(darCode) }, [
 
-        h3({ className: 'rp-form-title access-color' }, ['4.1 Data Use Agreements']),
+        h3({ className: 'rp-form-title access-color' }, ['4.1 Attestation and Agreement(s)']),
 
         div({ className: 'form-group' }, [
           div({ className: 'col-lg-12 col-md-12 col-sm-12 col-xs-12' }, [
