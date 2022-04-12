7db06e3a69f97f731751bf9072707c8dc80e04f3
Shae Marks <smarks.dev1@gmail.com>
2022-04-12 15:30:24 -0400

    Add MultiDatasetVotingSlabs for each bucket

--- src/pages/dar_collection_review/MultiDatasetVotingTab.js
+++ src/pages/dar_collection_review/MultiDatasetVotingTab.js
@@ -2,7 +2,9 @@ import MultiDatasetVoteSlab from "../../components/collection_voting_slab/MultiD
 import {div, h} from "react-hyperscript-helpers";
 import ResearchProposalVoteSlab from "../../components/collection_voting_slab/ResearchProposalVoteSlab";
 import {useEffect, useState} from "react";
-import {find, get, filter, flow, sortBy} from 'lodash/fp';
+import {find, get, filter, flow, sortBy, map, isNil} from 'lodash/fp';
+import {Storage} from "../../libs/storage";
+import {User} from "../../libs/ajax";
 
 const styles = {
   baseStyle: {
@@ -19,99 +21,47 @@ const styles = {
 export default function MultiDatasetVotingTab(props) {
   const [rpBucket, setRpBucket] = useState({});
   const [dataBuckets, setDataBuckets] = useState([]);
-  const {darInfo, buckets, isChair, isLoading} = props;
+  const [collectionDatasets, setCollectionDatasets] = useState([]);
+  const [dacDatasetIds, setDacDatasetIds] = useState([]);
+  const {darInfo, buckets, collection, isChair, isLoading} = props;
 
-  const [bucket, setBucket] = useState({
-    elections: [
-      [
-        {dataSetId: 385, electionId: 102, status: 'Open', electionType: 'DataAccess'},
-        {electionType: 'RP Vote'}
-      ],
-      [
-        {dataSetId: 415, electionId: 104, status: 'Open', electionType: 'DataAccess'},
-        {electionType: 'RP Vote'}
-      ],
-      [
-        {dataSetId: 103, electionId: 103, status: 'Closed', electionType: 'DataAccess'},
-        {electionType: 'RP Vote'}
-      ]
-    ],
-    votes: [
-      {
-        rp: {
-          finalVotes: [{dacUserId: 5060, displayName: 'Shae', vote: true, rationale: 'test', electionId: 102, voteId: 1, createDate: 14124124414}],
-          chairpersonVotes: [{dacUserId: 5060, displayName: 'Shae', vote: true, rationale: 'test', electionId: 102, voteId: 1, createDate: 14124124414}],
-          memberVotes: [
-            {dacUserId: 5060, displayName: 'Shae', rationale: 'test1', electionId: 102, voteId: 1, createDate: 14124124414},
-            {dacUserId: 5007, displayName: 'Grace', rationale: 'test1', vote: false, electionId: 102, voteId: 2, createDate: 40815342},
-            {dacUserId: 3365, displayName: 'Julian', vote: true, electionId: 102, voteId: 3, createDate: 9999999}
-          ]
-        },
-        dataAccess: {
-          finalVotes: [{dacUserId: 5060, displayName: 'Shae', vote: true, rationale: 'test1', electionId: 102, voteId: 1, createDate: 14124124414}],
-          chairpersonVotes: [{dacUserId: 5060, displayName: 'Shae', vote: true, rationale: 'test1', electionId: 102, voteId: 1, createDate: 14124124414}],
-          memberVotes: [
-            {dacUserId: 5060, displayName: 'Shae', rationale: 'test1', electionId: 102, voteId: 1, createDate: 14124124414},
-            {dacUserId: 5007, displayName: 'Grace', rationale: 'test1', vote: false, electionId: 102, voteId: 2, createDate: 40815342},
-            {dacUserId: 3365, displayName: 'Julian', vote: true, electionId: 102, voteId: 3, createDate: 9999999}
-          ]
-        }
-      },
-      {
-        rp: {
-          finalVotes: [{dacUserId: 5060, displayName: 'Shae', vote: true, rationale: 'test', electionId: 104, voteId: 5, createDate: 22122412512}],
-          chairpersonVotes: [{dacUserId: 5060, displayName: 'Shae', vote: true, rationale: 'test', electionId: 104, voteId: 5, createDate: 22122412512}],
-          memberVotes: [
-            {dacUserId: 5060, displayName: 'Shae', rationale: 'test2', electionId: 104, voteId: 5, createDate: 22122412512},
-            {dacUserId: 5007, displayName: 'Grace', rationale: 'test1', vote: false, electionId: 104, voteId: 4, createDate: 40815342},
-            {dacUserId: 3365, displayName: 'Julian', vote: false, electionId: 104, voteId: 6}
-          ]
-        },
-        dataAccess: {
-          finalVotes: [{dacUserId: 5060, displayName: 'Shae', vote: false, rationale: 'test2', electionId: 104, voteId: 5, createDate: 22122412512}],
-          chairpersonVotes: [{dacUserId: 5060, displayName: 'Shae', vote: false, rationale: 'test2', electionId: 104, voteId: 5, createDate: 22122412512}],
-          memberVotes: [
-            {dacUserId: 5060, displayName: 'Shae', rationale: 'test2', electionId: 104, voteId: 5, createDate: 22122412512},
-            {dacUserId: 5007, displayName: 'Grace', rationale: 'test1', vote: false, electionId: 104, voteId: 4, createDate: 40815342},
-            {dacUserId: 3365, displayName: 'Julian', vote: false, electionId: 104, voteId: 6}
-          ]
-        }
-      },
-      {
-        rp: {
-          finalVotes: [],
-          chairpersonVotes: [],
-          memberVotes: [
-            {dacUserId: 5007, displayName: 'Grace', vote: false, electionId: 103, voteId: 7},
-            {dacUserId: 3365, displayName: 'Julian', vote: true, electionId: 103, voteId: 8}
-          ]
-        },
-        dataAccess: {
-          finalVotes: [],
-          chairpersonVotes: [],
-          memberVotes: [
-            {dacUserId: 5007, displayName: 'Grace', vote: false, electionId: 103, voteId: 7},
-            {dacUserId: 3365, displayName: 'Julian', vote: true, electionId: 103, voteId: 8}
-          ]
-        }
-      },
-    ],
-    dataUses: [
-      {code: 'GRU', description: 'Use is permitted for any research purpose'},
-      {code: 'HMB', description: 'Use is permitted for a health, medical, or biomedical research purpose'}
-    ]
-  });
-
-  useEffect(() => {
+  useEffect( () => {
+    setCollectionDatasets(get('datasets')(collection));
     setRpBucket(find(bucket => get('key')(bucket) === 'RP Vote')(buckets));
     setDataBuckets(flow(
       filter(bucket => get('key')(bucket) !== 'RP Vote'),
       sortBy(bucket => get('key')(bucket))
     )(buckets));
-  }, [buckets]);
+  }, [buckets, collection]);
+
+  useEffect(() => {
+    const init = async () => {
+      const dacDatasets = await User.getUserRelevantDatasets();
+      const datasetIds = flow(
+        map(dataset => dataset.dataSetId),
+        filter(datasetId => !isNil(datasetId))
+      )(dacDatasets);
+      setDacDatasetIds(datasetIds);
+    };
+
+    try {
+      init();
+    } catch(error) {
+      setDacDatasetIds([]);
+    }
+  }, []);
+
 
   const DatasetVoteSlabs = () => {
-
+    return map((bucket, i) => {
+      return h(MultiDatasetVoteSlab,{
+        title: `GROUP ${i}`,
+        bucket,
+        dacDatasetIds,
+        collectionDatasets,
+        isChair
+      });
+    })(dataBuckets);
   };
 
   return div({style: styles.baseStyle}, [
@@ -121,6 +71,7 @@ export default function MultiDatasetVotingTab(props) {
       bucket: rpBucket,
       isChair,
       isLoading
-    })
+    }),
+    DatasetVoteSlabs()
   ]);
 }
