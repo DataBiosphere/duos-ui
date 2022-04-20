6d3bf2183ca515a06bf89205656d620ef614b0ed
Shae Marks <smarks.dev1@gmail.com>
2022-04-20 13:58:29 -0400

    Set colors for specific pie slice values

--- src/components/common/VotesPieChart.js
+++ src/components/common/VotesPieChart.js
@@ -1,20 +1,26 @@
 import { Chart } from 'react-google-charts';
 import { h, div } from 'react-hyperscript-helpers';
-import { isNil, startCase, isEmpty} from 'lodash/fp';
+import { isNil, isEmpty} from 'lodash/fp';
 import { map } from 'lodash';
 import { useMemo } from 'react';
 
+const pieSliceColors = {
+  0: {color: 'green'},
+  1: {color: 'red'},
+  2: {color: 'gray'}
+};
+
 const processVotes = (votes) => {
   const headerData = ["Vote", "Total Votes"];
-  const decisionMap = {};
+  const decisionMap = {
+    "Yes" : 0,
+    "No": 0,
+    "Not submitted" : 0
+  };
 
   votes.forEach((v) => {
     const value = v.vote ? "Yes" : !isNil(v.vote) ? "No" : "Not submitted";
-    if(isNil(decisionMap[value])) {
-      decisionMap[value] = 1;
-    } else {
-      decisionMap[value]++;
-    }
+    decisionMap[value]++;
   });
   const decisionDataArray = map(decisionMap, (count, key) => [key, count]);
   return [headerData, ...decisionDataArray];
@@ -32,7 +38,14 @@ export default function VotesPieChart(props) {
   } = props;
 
   const processedVotes = useMemo(() => processVotes(votes), [votes]);
-  const options = { title, pieHole, is3d: false, fontName: 'Montserrat', pieSliceText: 'none' };
+  const options = {
+    title,
+    pieHole,
+    is3d: false,
+    fontName: 'Montserrat',
+    pieSliceText: 'none',
+    slices: pieSliceColors
+  };
 
   if(isEmpty(votes)) {
     return div({style, className: `${keyString}-pie-chart-no-data`}, [`No data for ${keyString}`]);
