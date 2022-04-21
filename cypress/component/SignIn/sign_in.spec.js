7f6cade3cec3ee671b0c63d76c91f5b16dbf47df
Greg Rushton <grushton@broadinstitute.org>
2022-04-21 09:14:57 -0400

    add spinner test

--- cypress/component/SignIn/sign_in.spec.js
+++ cypress/component/SignIn/sign_in.spec.js
@@ -5,18 +5,26 @@ import {mount} from '@cypress/react';
 import SignIn from '../../../src/components/SignIn';
 import { Config } from '../../../src/libs/config';
 
+const signInText = "Sign-in";
+
+// Note that we do not want to click the signin button
+// in tests as that would trigger an auth-flow we cannot
+// replicate in a test environment.
 describe('Sign In Component', function() {
-  it('Sign In Button Loads', function () {
+  it('Sign In Button Loads when client id is valid', function () {
     cy.viewport(600, 300);
     // Load the client id from perf so we can have a valid button
     cy.readFile('config/perf.json').then((config) => {
       const clientId = config.clientId;
       cy.stub(Config, 'getGoogleClientId').returns(clientId);
       mount(<SignIn />);
-      cy.contains("Sign-in").should('exist');
+      cy.contains(signInText).should('exist');
     });
   });
-  // Note that we do not want to click the signin button
-  // in tests as that would trigger an auth-flow we cannot
-  // replicate in a test environment.
+  it('Spinner loads when client id is empty', function () {
+    cy.viewport(600, 300);
+    cy.stub(Config, 'getGoogleClientId').returns('');
+    mount(<SignIn />);
+    cy.contains(signInText).should('not.exist');
+  });
 });
