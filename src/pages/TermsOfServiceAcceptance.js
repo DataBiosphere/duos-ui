44a9fac4ef1e4af46f9ac63cd868a80537f18f78
Greg Rushton <grushton@broadinstitute.org>
2022-04-27 16:57:13 -0400

    add buttons and accept callback

--- src/pages/TermsOfServiceAcceptance.js
+++ src/pages/TermsOfServiceAcceptance.js
@@ -1,11 +1,33 @@
-import {div, h1} from 'react-hyperscript-helpers';
-import {useEffect, useState} from 'react';
+import {button, div, h1} from 'react-hyperscript-helpers';
+import {useCallback, useEffect, useState} from 'react';
 import {TosService} from "../libs/tosService";
+import {Storage} from '../libs/storage';
 import {Navigation} from '../libs/utils';
 
-export default function TermsOfServiceAcceptance() {
+export default function TermsOfServiceAcceptance(props) {
 
   const [tosText, setTosText] = useState('');
+  const {history} = props;
+
+  const acceptButtonStyle = {
+    backgroundColor: 'rgb(0, 96, 159)',
+    color: 'white',
+    border: '1px solid rgb(0, 96, 159)',
+    borderRadius: '4px',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize: '1.45rem',
+    padding: '5px 10px',
+    cursor: 'pointer',
+    fontWeight: '600',
+    marginLeft: '1rem',
+  };
+
+  const rejectButtonStyle = Object.assign({}, acceptButtonStyle, {
+    color: 'black',
+    backgroundColor: 'rgb(255, 255, 255)'
+  });
 
   useEffect(() => {
     const init = async () => {
@@ -15,12 +37,27 @@ export default function TermsOfServiceAcceptance() {
     init();
   }, []);
 
+  const acceptToS = useCallback(async () => {
+    const user = await Storage.getCurrentUser();
+    await TosService.acceptTos();
+    Navigation.back(user, history);
+  }, [history]);
+
+
   return div({style: TosService.getBackgroundStyle()}, [
     div({style: TosService.getContainerStyle()}, [
       h1({style: {color: '#00609f', marginLeft: '25px'}}, ['DUOS Terms of Service']),
       div({style: TosService.getScrollableStyle()}, [tosText]),
-      div({}, ["Accept TOS"]),
-      div({}, ["Reject TOS"])
+      div({
+        style: {
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'right',
+        }
+      }, [
+        button({style: rejectButtonStyle}, ["Reject TOS"]),
+        button({style: acceptButtonStyle, onClick: acceptToS}, ["Accept TOS"])
+      ])
     ])
   ]);
 }
