c1df7da4aad293bc254432279cb80fefd0fe0d45
shaemarks <81024249+shaemarks@users.noreply.github.com>
2022-05-02 16:16:40 -0400

    [DUOS-1710] library card revocation while dar vote open (#1520)
    * Move error message above voting boxes for Chair tab on single DARs
    
    * Check if researcher is missing a library card and the console is for chairs
    
    * Add logic to disable data access approval when library card is missing
    
    * Add static alert message on chair tab when library card is missing
    
    * Add tests for new warning text and disabling yes button
    
    * Remove extra comma
    
    * Remove extra whitespace
    
    * Change disabled to isVotingDisabled
    
    Co-authored-by: Shae Marks <smarks.dev1@gmail.com>

--- src/pages/dar_collection_review/MultiDatasetVotingTab.js
+++ src/pages/dar_collection_review/MultiDatasetVotingTab.js
@@ -6,11 +6,15 @@ import {find, get, filter, flow, sortBy, map, isNil, isEmpty} from 'lodash/fp';
 import {Storage} from "../../libs/storage";
 import {User} from "../../libs/ajax";
 import {extractUserDataAccessVotesFromBucket} from "../../utils/DarCollectionUtils";
+import {Alert} from "../../components/Alert";
 
 const styles = {
   baseStyle: {
     backgroundColor: '#FFFFFF',
     padding: '35px',
+    whiteSpace: 'pre-line'
+  },
+  slabs: {
     display: 'flex',
     flexDirection: 'column',
     rowGap: '35px'
@@ -19,7 +23,8 @@ const styles = {
     color: '#333F52',
     fontFamily: 'Montserrat',
     fontSize: '2.4rem',
-    fontWeight: 'bold'
+    fontWeight: 'bold',
+    marginBottom: '10px'
   }
 };
 
@@ -34,6 +39,8 @@ export default function MultiDatasetVotingTab(props) {
   const [collectionDatasets, setCollectionDatasets] = useState([]);
   const [dacDatasetIds, setDacDatasetIds] = useState([]);
   const {darInfo, buckets, collection, isChair, isLoading} = props;
+  const missingLibraryCardMessage = "The Researcher must have a Library Card before data access can be granted.\n" +
+    "You can still deny this request and/or vote on the Structured Research Purpose.";
 
   useEffect( () => {
     setCollectionDatasets(get('datasets')(collection));
@@ -57,8 +64,8 @@ export default function MultiDatasetVotingTab(props) {
     init();
   }, []);
 
-
   const DatasetVoteSlabs = () => {
+    const isApprovalDisabled = dataAccessApprovalDisabled();
     let index = 0;
     return map(bucket => {
       index++;
@@ -68,19 +75,37 @@ export default function MultiDatasetVotingTab(props) {
         dacDatasetIds,
         collectionDatasets,
         isChair,
+        isApprovalDisabled,
         key: bucket.key
       });
     })(dataBuckets);
   };
 
+  const dataAccessApprovalDisabled = () => {
+    const researcherLibraryCards = flow(
+      get('createUser'),
+      get('libraryCards')
+    )(collection);
+    const researcherMissingLibraryCards = isNil(researcherLibraryCards) || isEmpty(researcherLibraryCards);
+    return isChair && researcherMissingLibraryCards;
+  };
+
   return div({style: styles.baseStyle}, [
     div({style: styles.title}, ["Research Proposal"]),
-    h(ResearchProposalVoteSlab, {
-      darInfo,
-      bucket: rpBucket,
-      isChair,
-      isLoading
+    Alert({
+      type: 'danger',
+      title: missingLibraryCardMessage,
+      id: 'missing_lc',
+      isRendered: dataAccessApprovalDisabled()
     }),
-    DatasetVoteSlabs()
+    div({style: styles.slabs}, [
+      h(ResearchProposalVoteSlab, {
+        darInfo,
+        bucket: rpBucket,
+        isChair,
+        isLoading
+      }),
+      DatasetVoteSlabs()
+    ])
   ]);
 }
