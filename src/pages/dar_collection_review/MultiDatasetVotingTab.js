401749bb9021a957e007a5f076ce3f2a44ce0607
Shae Marks <smarks.dev1@gmail.com>
2022-05-03 10:17:54 -0400

    cleanup

--- src/pages/dar_collection_review/MultiDatasetVotingTab.js
+++ src/pages/dar_collection_review/MultiDatasetVotingTab.js
@@ -2,9 +2,8 @@ import MultiDatasetVoteSlab from "../../components/collection_voting_slab/MultiD
 import {div, h} from "react-hyperscript-helpers";
 import ResearchProposalVoteSlab from "../../components/collection_voting_slab/ResearchProposalVoteSlab";
 import {useEffect, useState} from "react";
-import {find, get, filter, flow, sortBy, map, isNil} from 'lodash/fp';
+import {find, get, filter, flow, sortBy, map, isNil, isEmpty} from 'lodash/fp';
 import {User} from "../../libs/ajax";
-import {extractUserDataAccessVotesFromBucket} from "../../utils/DarCollectionUtils";
 import {Alert} from "../../components/Alert";
 
 const styles = {
