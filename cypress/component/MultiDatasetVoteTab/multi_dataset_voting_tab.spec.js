1f346977a479d1034db154c9ce659693d21a331a
Shae Marks <smarks.dev1@gmail.com>
2022-05-03 08:35:57 -0400

    Add tests for filtering logic

--- cypress/component/MultiDatasetVoteTab/multi_dataset_voting_tab.spec.js
+++ cypress/component/MultiDatasetVoteTab/multi_dataset_voting_tab.spec.js
@@ -4,6 +4,8 @@ import {mount} from "@cypress/react";
 import {Storage} from "../../../src/libs/storage";
 import {User} from "../../../src/libs/ajax";
 import MultiDatasetVotingTab from "../../../src/pages/dar_collection_review/MultiDatasetVotingTab";
+import {collapseVotesByUser} from "../../../src/utils/DarCollectionUtils";
+import {filterBucketsForUser} from "../../../src/pages/dar_collection_review/DarCollectionReview";
 
 const darInfo = {
   "rus": "test",
@@ -189,3 +191,28 @@ describe('MultiDatasetVoteTab - Tests', function() {
     cy.get('.table-data').should('not.exist');
   });
 });
+
+describe('filterBucketsForUser - Tests', function() {
+  it('Filters out buckets if current user has no votes in it', function () {
+    const currentUser = {dacUserId: 100};
+    const prefilteredBuckets = [bucket1, bucket2];
+
+    const filteredBuckets = filterBucketsForUser(currentUser, prefilteredBuckets);
+    expect(filteredBuckets).to.have.lengthOf(1);
+    expect(filteredBuckets).to.deep.include(bucket1);
+    expect(filteredBuckets).to.not.deep.include(bucket2);
+  });
+
+  it('Does not filter out rp buckets or buckets with votes by the current user', function () {
+    const currentUser = {dacUserId: 200};
+    const rpBucket = {isRp: true, key: 'RP Vote'};
+    const prefilteredBuckets = [rpBucket, bucket1, bucket2];
+
+    const filteredBuckets = filterBucketsForUser(currentUser, prefilteredBuckets);
+    expect(filteredBuckets).to.have.lengthOf(3);
+    expect(filteredBuckets).to.deep.include(rpBucket);
+    expect(filteredBuckets).to.deep.include(bucket1);
+    expect(filteredBuckets).to.deep.include(bucket2);
+  });
+});
+
