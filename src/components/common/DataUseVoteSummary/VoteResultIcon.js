1afa3400f848e5e0b8f7d9f8305170f57bbb0e84
Shae Marks <smarks.dev1@gmail.com>
2022-05-05 11:43:30 -0400

    Replace icons for different vote summary states

--- src/components/common/DataUseVoteSummary/VoteResultIcon.js
+++ src/components/common/DataUseVoteSummary/VoteResultIcon.js
@@ -1,9 +1,8 @@
-import { CheckCircleOutlined, CancelOutlined, AutorenewOutlined, CompareArrowsOutlined } from '@material-ui/icons';
-import { blue, green, red, yellow } from '@material-ui/core/colors';
+import { CheckCircle, Cancel, Autorenew, RemoveCircle } from '@material-ui/icons';
 import { h, div } from 'react-hyperscript-helpers';
 
 const iconFontStyle = {
-  fontFamily: 'Arial',
+  fontFamily: 'M',
   fontSize: '1.6rem',
   fontWeight: 600,
   flex: 1,
@@ -25,81 +24,37 @@ export default function VoteResultIcon({ result, propKey }) {
   const templates = {
     true: {
       output: [
-        h(CheckCircleOutlined, {
+        h(CheckCircle, {
           className: `vote-result-yes-icon-${propKey}`,
-          style: { fontSize: iconSize, margin, flex: 1 },
-        }),
-        div(
-          {
-            className: `vote-result-yes-text-${propKey}`,
-            style: modifiedTextStyle,
-          },
-          ['Yes']
-        ),
-      ],
-      colorStyle: {
-        backgroundColor: green[500],
-        flex: 1,
-      },
+          style: { fontSize: iconSize, margin, flex: 1, color: '#1FA371' },
+        })
+      ]
     },
     false: {
       output: [
-        h(CancelOutlined, {
+        h(Cancel, {
           className: `vote-result-no-icon-${propKey}`,
-          style: { fontSize: iconSize, margin, flex: 1 },
-        }),
-        div(
-          {
-            className: `vote-result-no-text-${propKey}`,
-            style: modifiedTextStyle,
-          },
-          ['No']
-        ),
-      ],
-      colorStyle: {
-        backgroundColor: red[500],
-        flex: 1,
-      },
+          style: { fontSize: iconSize, margin, flex: 1, color: '#DA0003' },
+        })
+      ]
     },
     underReview: {
       output: [
-        h(AutorenewOutlined, {
+        h(Autorenew, {
           className: `vote-result-under-review-icon-${propKey}`,
-          style: { fontSize: iconSize, margin, flex: 1 },
-        }),
-        div(
-          {
-            className: `vote-result-under-review-text-${propKey}`,
-            style: Object.assign({}, modifiedTextStyle, { fontSize: '1.5rem' }),
-          },
-          ['Under Review']
-        ),
-      ],
-      colorStyle: {
-        backgroundColor: blue[500],
-        flex: 1,
-      },
+          style: { fontSize: iconSize, margin, flex: 1, color: '#A3ADBF' },
+        })
+      ]
     },
     mixed: {
       output: [
-        h(CompareArrowsOutlined, {
+        h(RemoveCircle, {
           className: `vote-result-mixed-icon-${propKey}`,
-          style: { fontSize: iconSize, margin, flex: 1 },
-        }),
-        div({
-          className: `vote-result-mixed-text-${propKey}`,
-          style: modifiedTextStyle,
+          style: { fontSize: iconSize, margin, flex: 1, color: '#A3ADBF' },
           'data-tip':
             'Mixed outcome stems from varying election results within this Data Use categorization.',
-          'data-for': 'tip_mixed_result'
-        }, [
-          'Mixed',
-        ]),
-      ],
-      colorStyle: {
-        backgroundColor: yellow[500],
-        color: 'white',
-      },
+        })
+      ]
     }
   };
   const { output, colorStyle } = templates[result];
