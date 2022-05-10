5cc11e621a076935bff8f4f113dc5087bd94edf6
Justin Variath Thomas <JVThomas@users.noreply.github.com>
2022-05-10 15:49:22 -0400

    Update src/utils/VoteUtils.js
    Added missing semi-colon
    
    Co-authored-by: Gregory Rushton <rushtong@users.noreply.github.com>

--- src/utils/VoteUtils.js
+++ src/utils/VoteUtils.js
@@ -1,7 +1,7 @@
 import {isEmpty} from 'lodash/fp';
 //helper method -> process match data and converts it to a Yes/No algorithm vote result
 export const processMatchData = (matchData) => {
-  if(isEmpty(matchData)){ return 'N/A'};
+  if(isEmpty(matchData)){ return 'N/A';};
   const {match, failed} = matchData;
   return failed ? 'Unable to determine a system match' : match ? 'Yes' : 'No';
 };
