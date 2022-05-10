5231e4b73192aaa1f5ad8d7e565189b2f70d08c5
Shae Marks <smarks.dev1@gmail.com>
2022-05-10 07:58:41 -0400

    Adjust review-page-body placement

--- src/pages/dar_collection_review/DarCollectionReview.js
+++ src/pages/dar_collection_review/DarCollectionReview.js
@@ -171,7 +171,7 @@ export default function DarCollectionReview(props) {
       }),
       h(DataUseVoteSummary, { dataUseBuckets, isLoading }),
     ]),
-    div({ className: 'review-page-body', style: {padding: '1% 0% 0% 5.2%', backgroundColor: tabContainerColor} }, [ //TODO: take the margin measurements and apply as padding here
+    div({ className: 'review-page-body', style: {padding: '1% 0% 0% 5.1%', backgroundColor: tabContainerColor} }, [ //TODO: take the margin measurements and apply as padding here
       h(TabControl, {
         labels: Object.values(tabs),
         selectedTab,
