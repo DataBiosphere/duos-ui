3330745f85cba8449d9cf3ea21a07a0237b58c94
Shae Marks <smarks.dev1@gmail.com>
2022-05-24 15:21:24 -0400

    Move merging of props to Routes

--- src/Routes.js
+++ src/Routes.js
@@ -51,7 +51,6 @@ import NewChairConsole from './pages/NewChairConsole';
 import NewMemberConsole from './pages/NewMemberConsole';
 import TermsOfService from './pages/TermsOfService';
 import TermsOfServiceAcceptance from './pages/TermsOfServiceAcceptance';
-import DarVoteReview from './pages/dar_collection_review/DarVoteReview';
 
 const Routes = (props) => (
   <Switch>
@@ -100,7 +99,8 @@ const Routes = (props) => (
       props.env === 'dev' ? <AuthenticatedRoute path="/new_member_console" component={NewMemberConsole} props={props} rolesAllowed={[USER_ROLES.member]}/>
         : ''
     }
-    <AuthenticatedRoute path="/dar_vote_review/:collectionId" component={DarVoteReview} props={props} rolesAllowed={[USER_ROLES.chairperson, USER_ROLES.member]}/>
+    <AuthenticatedRoute path="/dar_vote_review/:collectionId" component={DarCollectionReview} props={Object.assign({}, props, {readOnly: true})}
+      rolesAllowed={[USER_ROLES.chairperson, USER_ROLES.member]}/>
     <AuthenticatedRoute path="/chair_console" component={ChairConsole} props={props} rolesAllowed={[USER_ROLES.chairperson]} />
     <AuthenticatedRoute path="/member_console" component={MemberConsole} props={props} rolesAllowed={[USER_ROLES.member]} />
     <AuthenticatedRoute path="/data_owner_console" component={DataOwnerConsole} props={props} rolesAllowed={[USER_ROLES.dataOwner]} />
--- src/pages/dar_collection_review/DarVoteReview.js
+++
@@ -1,6 +0,0 @@
-import DarCollectionReview from "./DarCollectionReview";
-
-export default function DarVoteReview(props) {
-  const updatedProps = Object.assign({}, props, {readOnly: true});
-  return DarCollectionReview(updatedProps);
-}
