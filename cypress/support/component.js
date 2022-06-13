0bd9b9f76f03b471979d5878eeda72ed915faa83
JVThomas <jthomas0209@gmail.com>
2022-06-13 16:04:53 -0400

    DUOS-cypress-upgrade removed old cypress mount command

--- cypress/support/component.js
+++ cypress/support/component.js
@@ -18,10 +18,5 @@ import './commands';
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
-
-import { mount } from 'cypress/react';
-
-Cypress.Commands.add('mount', mount);
-
 // Example use:
 // cy.mount(<MyComponent />)
