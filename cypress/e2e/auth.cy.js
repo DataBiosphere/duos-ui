fe00902a2e5501777762a4afab6ae43325686461
clu@asymmetrik.com <clu@asymmetrik.com>
2022-06-29 11:28:44 -0400

    [DUOS-1886] Hm. testing unit test update

--- cypress/e2e/auth.cy.js
+++ cypress/e2e/auth.cy.js
@@ -13,10 +13,12 @@ describe('Authentication', function() {
   it('Background Sign-in Admin', function() {
     cy.viewport(1000, 1000);
     cy.auth(roles.ADMIN).then(credentials => {
+      cy.viewport(1300, 650);
       cy.visit('/backgroundsignin');
       cy.get('textarea').type(credentials.access_token, {delay: 0});
       cy.get('form').submit();
       cy.contains('Admin Console');
+      cy.contains('DAR Requests');
       cy.get('[id="collapsed-navigation-icon-button"]').click();
       cy.contains('Sign Out').click();
     });
