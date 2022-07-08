41537dddbaddc9297b9ca3326118b6debc949c30
Connor Barker <connorlbark@gmail.com>
2022-07-08 09:08:23 -0400

    [DUOS-1692] Add reject ToS button (#1651)
    * add reject ToS button
    
    * fixes

--- src/pages/TermsOfService.js
+++ src/pages/TermsOfService.js
@@ -1,11 +1,15 @@
-import {div, h1} from 'react-hyperscript-helpers';
+import {div, h1, h} from 'react-hyperscript-helpers';
 import {useEffect, useState} from 'react';
+import { Storage } from '../libs/storage';
 import {TosService} from '../libs/tosService';
+import SimpleButton from '../components/SimpleButton';
 
 
-export default function TermsOfService() {
+export default function TermsOfService(props) {
 
   const [tosText, setTosText] = useState('');
+  const {history} = props;
+  const isLogged = Storage.userIsLogged();
 
   useEffect(() => {
     const init = async () => {
@@ -15,10 +19,38 @@ export default function TermsOfService() {
     init();
   }, []);
 
+  const rejectAndSignOut = async () => {
+    // update Sam that ToS was rejected
+    await TosService.rejectTos();
+
+    // log user out and send them back home.
+    await Storage.setUserIsLogged(false);
+    await Storage.clearStorage();
+    history.push('/');
+  };
+
   return div({style: TosService.getBackgroundStyle()}, [
     div({style: TosService.getContainerStyle()}, [
       h1({style: {color: '#00609f', marginLeft: '25px'}}, ['DUOS Terms of Service']),
-      div({style: TosService.getScrollableStyle(), className: 'markdown-body'}, [tosText])
-    ])
+      div({style: TosService.getScrollableStyle(), className: 'markdown-body'}, [tosText]),
+      div({isRendered: isLogged, style: {display: 'flex', justifyContent: 'right', paddingRight: '5rem'}}, [
+        h(SimpleButton, {
+          keyProp: 'tos-accept',
+          label: 'Reject Terms of Service',
+          isRendered: true,
+          onClick: rejectAndSignOut,
+          baseColor: '#d13b07',
+          hoverStyle: {
+            backgroundColor: '#b83206',
+            color: 'white'
+          },
+          additionalStyle: {
+            textTransform: 'none',
+            padding: '5px 10px',
+            fontSize: '1.45rem',
+          },
+        })
+      ]),
+    ]),
   ]);
 }
