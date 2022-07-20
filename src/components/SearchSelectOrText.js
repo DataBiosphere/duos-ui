a155c989f1186e748c71c9127670bbbd7152c57a
lu-c <clu@asymmetrik.com>
2022-07-20 16:46:07 -0400

    [DUOS-1893][risk=no] Added submit help ticket for feature request (#1678)
    * remove old code + select or text + signing official
    
    * finished researcher page, conform to style
    
    * fix eraCommons, signing official logic, eslint bugs
    
    * use new update self endpoint
    
    * use /api/user instead of /api/user/me
    
    * update styling, cleanup payload
    
    * remove unnecessary filtering in payload
    
    Co-authored-by: Connor Barker <connorlbark@gmail.com>

--- src/components/SearchSelectOrText.js
+++ src/components/SearchSelectOrText.js
@@ -1,11 +1,9 @@
 import { useState, useEffect, useRef } from 'react';
 import {div, input, span, a, ul, li } from 'react-hyperscript-helpers';
-import {find} from 'lodash/fp';
 import {isEqual} from 'lodash';
 
 import './SearchSelect.css';
 
-// Similar to SearchSelect, but allows the user to input free text
 export const SearchSelectOrText = (props) => {
   const { onPresetSelection, onManualSelection, placeholder, options, searchPlaceholder, id, label } = props;
   const [currentDisplay, setCurrentDisplay] = useState(placeholder || '');
@@ -101,7 +99,6 @@ export const SearchSelectOrText = (props) => {
               selectManual(searchTerms.current.value);
             }
           },
-
           ref: searchTerms
         }),
         div({
