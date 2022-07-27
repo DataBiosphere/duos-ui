608715a5362e5867d3876f30e68b3e8129c1432c
Connor Barker <connorlbark@gmail.com>
2022-07-27 12:56:43 -0400

    fix tests

--- cypress/component/DarCollectionTable/admin_actions.spec.js
+++ cypress/component/DarCollectionTable/admin_actions.spec.js
@@ -1,49 +1,71 @@
 /* eslint-disable no-undef */
 import { React } from 'react';
 import { mount } from 'cypress/react';
+import { cloneDeep } from 'lodash/fp';
 import AdminActions from '../../../src/components/dar_collection_table/AdminActions';
 
-const collectionId = 1;
-const props = {
-  collection: {
-    darCollectionId: collectionId,
-    dars: {
-      1: {
-        elections: {
-          1: {status: "Open"}
-        }
-      },
-      2: {
-        elections: {
-          2: {status: "Closed"}
-        }
-      }
-    }
-  },
-  showCancelModal: () => {},
-  updateCollections: () => {},
+const darColl = {
+  'darCollectionId': 2345,
+  "referenceIds": [
+    '4a3fd-g77fd-2f345-4h2g31',
+    '0a4jn-g838d-bsdg8-6s7fs7',
+  ],
+  'darCode': "DAR-9583",
+  'name': "Example DAR 1",
+  "submissionDate": "2022-07-26",
+  'researcherName': "John Doe",
+  'institutionName': "Broad Institute",
+  "status": "Draft",
+  "actions": [
+    'Review',
+    'Cancel',
+  ],
+  "hasVoted": false,
+  'datasetCount': 4
+}
+
+let props;
+
+const initialProps = {
+  collection: darColl,
+  showConfirmationModal: () => {},
+  history: {}
 };
+
+beforeEach(() => {
+  props = cloneDeep(initialProps);
+});
+
 
 describe('Admin Actions - Container', () => {
   it('renders the actions container div', () => {
+    mount(<AdminActions {...props}/>);
+    cy.get('.admin-actions').should('exist');
+  });
+});
+
+describe('Admin Actions - Cancel Button', () => {
+  it('renders the cancel button if the collection is cancelable', () => {
+    props.collection.actions = ['Cancel'];
     mount(<AdminActions {...props} />);
-    const containerDiv = cy.get(`.admin-actions`);
-    containerDiv.should('exist');
+    cy.get(`#admin-cancel-${props.collection.darCollectionId}`).should('exist');
+  });
+  it('does not render if the election is already canceled', () => {
+    props.collection.actions = [];
+    mount(<AdminActions {...props} />);
+    cy.get(`#admin-cancel-${props.collection.darCollectionId}`).should('not.exist');
   });
 });
 
 describe('Admin Actions - Open Button', () => {
-  it('renders the open button if some of the elections are not open', () => {
+  it('renders the open button if the collection is openable', () => {
+    props.collection.actions = ['Open'];
     mount(<AdminActions {...props} />);
-    const button = cy.get(`#admin-open-${collectionId}`);
-    button.should('exist');
+    cy.get(`#admin-open-${props.collection.darCollectionId}`).should('exist');
   });
-});
-
-describe('Admin Actions - Closed button', () => {
-  it('renders the cancel button if cancelable elections exists', () => {
+  it('does not render if the election is already opened', () => {
+    props.collection.actions = [];
     mount(<AdminActions {...props} />);
-    const button = cy.get(`#admin-cancel-${collectionId}`);
-    button.should('exist');
+    cy.get(`#admin-open-${props.collection.darCollectionId}`).should('not.exist');
   });
 });
