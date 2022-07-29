93b32c478a1f8fadcb17c157bc0d9dd45e6dc0e3
Connor Barker <connorlbark@gmail.com>
2022-07-29 15:21:45 -0400

    update styling of manage user table

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -7,15 +7,21 @@ import { defaultTo } from 'lodash/fp';
 export default function SearchBar(props) {
   const { handleSearchChange } = props;
   const searchTerms = useRef('');
+
+  const width = props.width ? props.width : '100%';
+  const margin = props.margin ? props.margin : '';
+  const button = props.button ? props.button : div();
+
   const style = props.style ? props.style : {
-    width: '100%',
+    width: width,
     border: '1px solid #cecece',
     backgroundColor: '#f3f6f7',
     borderRadius: '5px',
     height: '4rem',
     paddingLeft: '2%',
     fontFamily: 'Montserrat',
-    fontSize: '1.5rem'
+    fontSize: '1.5rem',
+    margin: margin,
   };
 
   return div({className: 'right-header-section', style: Styles.RIGHT_HEADER_SECTION}, [
@@ -27,6 +33,7 @@ export default function SearchBar(props) {
       style,
       onChange:() => handleSearchChange(props.searchRef ? defaultTo('')(props.searchRef.current.value) : searchTerms),
       ref: props.searchRef || searchTerms
-    })
+    }),
+    button
   ]);
 }
---
+++ src/components/manage_users_table/ManageUsersTableCellData.js
@@ -0,0 +1,70 @@
+import {map, sortedUniq} from 'lodash';
+import {h, button} from 'react-hyperscript-helpers';
+import {styles} from './ManageUsersTable';
+import {Link} from 'react-router-dom';
+
+export function usernameCellData({displayName, userId, label= 'user-name'}) {
+  return {
+    data: h(Link, {to: `/admin_edit_user/${userId}`}, [displayName]),
+    value: displayName,
+    id: userId,
+    style : {
+      color: '#354052',
+      fontSize: styles.fontSize.username,
+      paddingRight: '2%'
+    },
+    label
+  };
+}
+
+export function googleIdCellData({userId, email, label = 'google-id'}) {
+
+
+  return {
+    data: email,
+    value: email,
+    id: userId,
+    style: {
+      color: styles.color.googleId,
+      fontSize: styles.fontSize.googleId,
+      fontWeight: '500'
+    },
+    label
+  };
+}
+
+//Redirect for admin review page, only used in admin manage dar collections table
+export function roleCellData({userId, roles, label = 'user-role'}) {
+  return {
+    isComponent: true,
+    data: sortedUniq(map(roles, 'name')).join('   '),
+    label,
+    id: userId,
+  };
+}
+
+export function actionsCellData({userId, user, editUser}) {
+
+  return {
+    isComponent: true,
+    id: userId,
+    style: {
+      color: styles.color.actions,
+      fontSize: styles.fontSize.actions
+    },
+    label: 'table-actions',
+    data: button({
+      id: userId + '_btnEditUser',
+      name: 'btn_editUser',
+      className: 'cell-button hover-color',
+      onClick: editUser(user)
+    }, ['Edit']),
+  };
+}
+
+export default {
+  usernameCellData,
+  googleIdCellData,
+  roleCellData,
+  actionsCellData
+};
