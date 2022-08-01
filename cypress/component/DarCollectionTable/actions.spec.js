d22221327093ae3940834dd975579d5480a04c58
clu@asymmetrik.com <clu@asymmetrik.com>
2022-08-01 15:44:40 -0400

    testing cypress tests on github

--- cypress/component/DarCollectionTable/actions.spec.js
+++ cypress/component/DarCollectionTable/actions.spec.js
@@ -196,14 +196,18 @@ describe('Researcher Actions - Delete Button', () => {
 
 describe('Researcher Actions - Draft', () => {
   it('uses the referenceId in id if draft', () => {
-    propCopy.consoleType = 'researcher';
+    // propCopy.collection = draftDarColl;
+    // mount(<Actions {...propCopy} />);
+    // cy.get(`#researcher-resume-${refId1}`).should('exist');
+    // cy.get(`#researcher-review-${refId1}`).should('exist');
+    // cy.get(`#researcher-cancel-${refId1}`).should('exist');
+    // cy.get(`#researcher-delete-${refId1}`).should('exist');
+    // cy.get(`#revise-collection-${refId1}`).should('exist');
+
     propCopy.collection = draftDarColl;
     mount(<Actions {...propCopy} />);
-    cy.get(`#researcher-resume-${refId1}`).should('exist');
+    cy.get(`#researcher-delete-${collectionId}`).should('not.exist'); // just checking this one next.
     cy.get(`#researcher-review-${refId1}`).should('exist');
-    cy.get(`#researcher-cancel-${refId1}`).should('exist');
-    cy.get(`#researcher-delete-${refId1}`).should('exist');
-    cy.get(`#revise-collection-${refId1}`).should('exist');
   });
 });
 
