6a6b20044e7d0ada6f66ae1f1b28b8f9b2fe2a3d
Shae Marks <smarks.dev1@gmail.com>
2022-08-29 09:17:07 -0400

    Remove DataCustodianTable from SigningOfficialResearchers page

--- src/pages/SigningOfficialResearchers.js
+++ src/pages/SigningOfficialResearchers.js
@@ -5,7 +5,6 @@ import {Styles} from '../libs/theme';
 import SigningOfficialTable from '../components/signing_official_table/SigningOfficialTable';
 import {User} from '../libs/ajax';
 import { USER_ROLES } from '../libs/utils';
-import DataCustodianTable from '../components/data_custodian_table/DataCustodianTable';
 
 
 export default function SigningOfficialResearchers() {
@@ -42,8 +41,7 @@ export default function SigningOfficialResearchers() {
   return (
     div({style: Styles.PAGE}, [
       div({style: {}, className: 'signing-official-tabs'}, [
-        h(SigningOfficialTable, {researchers, signingOfficial, unregisteredResearchers, isLoading}, []),
-        h(DataCustodianTable, {researchers, signingOfficial, unregisteredResearchers, isLoading}, []),
+        h(SigningOfficialTable, {researchers, signingOfficial, unregisteredResearchers, isLoading}, [])
       ])
     ])
   );
