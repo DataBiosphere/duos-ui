14c13b09c04a7b6b0cb6180d5862423a8746986d
Greg Rushton <grushton@broadinstitute.org>
2022-09-13 06:59:53 -0400

    more rm jquery

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-// import $ from 'jquery';
-// import Popper from 'popper.js';
-import 'jquery/src/jquery';
-
 import 'bootstrap/dist/js/bootstrap.min';
 
 import React from 'react';
