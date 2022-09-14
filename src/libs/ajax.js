2025450b861e6b45b83eb79aa19d6b9022565415
clu@asymmetrik.com <clu@asymmetrik.com>
2022-09-14 12:14:42 -0400

    added slash

--- src/libs/ajax.js
+++ src/libs/ajax.js
@@ -880,7 +880,7 @@ export const ToS = {
 
 export const Translate = {
   translate: async (body) => {
-    const url = `${await getOntologyUrl()}translate/paragraph`;
+    const url = `${await getOntologyUrl()}/translate/paragraph`;
     const res = await axios.post(url, body, Config.authOpts());
     return res.data;
   },
