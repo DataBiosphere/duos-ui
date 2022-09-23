2eaab1fb9db8bd73024040988d05bbcc9cc49be9
Connor Barker <connorlbark@gmail.com>
2022-09-23 11:17:26 -0400

    feedback: add back LC page code

---
+++ src/pages/AdminManageLC.js
@@ -0,0 +1,44 @@
+import { useState, useEffect } from 'react';
+import { h } from 'react-hyperscript-helpers';
+import { Institution, User } from '../libs/ajax';
+import { LibraryCard } from '../libs/ajax';
+import { Notifications, USER_ROLES } from '../libs/utils';
+import LibraryCardTable from '../components/library_card_table/LibraryCardTable';
+
+export default function AdminManageLC() {
+  const [libraryCards, setLibraryCards] = useState();
+  const [institutions, setInstitutions] = useState();
+  const [users, setUsers] = useState();
+
+  //init hook to get users, institutions, and cards to be passed down as props
+  useEffect(() => {
+    const initData = async() => {
+      const dataPromiseArray = await Promise.all([
+        LibraryCard.getAllLibraryCards(),
+        Institution.list(),
+        User.list(USER_ROLES.admin)
+      ]);
+      const cards = dataPromiseArray[0];
+      const institutions = dataPromiseArray[1].map((institution) => {
+        return {
+          ...institution,
+          key: institution.id,
+          displayText: institution.name
+        };
+      });
+      const users = dataPromiseArray[2];
+      setLibraryCards(cards);
+      setInstitutions(institutions);
+      setUsers(users);
+    };
+    try{
+      initData();
+    } catch(error) {
+      Notifications.showError({text:'Error: Failed to initialize component'});
+    }
+  }, []);
+
+  //props are expecting array format
+  return h(LibraryCardTable, {users, institutions, libraryCards});
+}
+	
