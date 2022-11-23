9625ef6b5a6817f16e57aa77171c4e0a527a8f88
Quazi <59845076+quazi-broad@users.noreply.github.com>
2022-11-23 12:58:20 -0500

    Update cypress/component/DataAccessRequest/researcher_info_new.spec.js
    Co-authored-by: codacy-production[bot] <61871480+codacy-production[bot]@users.noreply.github.com>

--- cypress/component/DataAccessRequest/researcher_info_new.spec.js
+++ cypress/component/DataAccessRequest/researcher_info_new.spec.js
@@ -2,7 +2,7 @@
 import {React} from 'react';
 import {mount} from 'cypress/react';
 import ResearcherInfo from '../../../src/pages/dar_application/ResearcherInfo_new.js';
-import {User} from "../../../src/libs/ajax";
+import {User} from '../../../src/libs/ajax';
 import {BrowserRouter} from 'react-router-dom';
 
 const props = {
