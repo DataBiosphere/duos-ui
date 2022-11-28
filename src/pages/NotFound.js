e1f69d3abdd33a15bf552bb600a37be0ffd19db5
Greg Rushton <grushton@broadinstitute.org>
2022-11-28 16:01:45 -0500

    minor update that shows the original message

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -7,9 +7,7 @@ export default function NotFound() {
   return <div className={'container container-wide'}>
     <div className={'row no-margin'}>
       <div className={'col-lg-12 col-md-12 col-sm-12 col-xs-12 no-padding'}>
-        <PageHeading id={'notFound'} color={'common'} title={'Not Found'}>
-          Sorry, the page you were looking for was not found.
-        </PageHeading>
+        <PageHeading id={'notFound'} color={'common'} title={'Sorry, the page you were looking for was not found.'}/>
       </div>
       <Link id={'btn_back'} className={'btn-primary btn-back f-left'} style={{'marginTop': '15px'}} to={'/home'}>
         Back to Home
