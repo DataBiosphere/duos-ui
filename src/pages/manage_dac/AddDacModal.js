8ad0685c2e9005a3db1f6cd3cfa6313b6725c33b
Kaite OFlaherty <koflaher@wm0a4-904.broadinstitute.org>
2023-01-19 13:38:59 -0500

    fixing outline format

--- src/pages/manage_dac/AddDacModal.js
+++ src/pages/manage_dac/AddDacModal.js
@@ -308,8 +308,9 @@ export const AddDacModal = hh(class AddDacModal extends Component {
               className: 'col-lg-3 col-md-3 col-sm-3 col-xs-4 control-label common-color'
             }, ['Email']),
             div({ className: 'col-lg-9 col-md-9 col-sm-9 col-xs-8' }, [
-              textarea({
+              input({
                 id: 'txt_dacEmail',
+                type: 'text',
                 defaultValue: this.state.dac.email,
                 onChange: this.handleChange,
                 name: 'email',
