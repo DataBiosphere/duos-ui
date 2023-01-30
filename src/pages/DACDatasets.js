bec4352c6ae9ccd713ce79f0e3e496973d3131bc
Greg Rushton <grushton@broadinstitute.org>
2023-01-30 14:50:07 -0500

    cleanup

--- src/pages/DACDatasets.js
+++ src/pages/DACDatasets.js
@@ -11,20 +11,12 @@ import '../components/dac_dataset_table/dac_dataset_table.css';
 import {Storage} from '../libs/storage';
 import {map, filter, isNil} from 'lodash/fp';
 
-const CHAIR = 'Chairperson';
-const ADMIN = 'Admin';
-
-export default function DACDatasets(props) {
+export default function DACDatasets() {
   const [datasets, setDatasets] = useState([]);
   const [filteredList, setFilteredList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  // new changes
-  // const [dacs, setDacs] = useState([]);
-  // const [dacIDs, setDacIDs] = useState([]);
-  // const [userRole, setUserRole] = useState();
   const searchRef = useRef('');
   const filterFn = getSearchFilterFunctions().datasets;
-  // const { history } = props;
 
   const handleSearchChange = useCallback((searchTerms) => searchOnFilteredList(
     searchTerms,
@@ -32,38 +24,6 @@ export default function DACDatasets(props) {
     filterFn,
     setFilteredList
   ), [datasets, filterFn]);
-
-  // const reloadUserRole = useCallback(async () => {
-  //   setIsLoading(true);
-  //   const currentUser = Storage.getCurrentUser();
-  //   const roles = currentUser.roles.map(r => r.name);
-  //   const role = contains(ADMIN)(roles) ? ADMIN : CHAIR;
-  //   let dacIDs = filter({name: CHAIR})(currentUser.roles);
-  //   dacIDs = map('dacId')(dacIDs);
-  //   if (role === CHAIR || ADMIN) {
-  //     setDacIDs(dacIDs);
-  //   }
-  //   setUserRole(role);
-  //   setIsLoading(false);
-  // }, []);
-
-  // const reloadDatasets = useCallback(async () => {
-  //   // take list of dacs, return list of all datasets
-  //   console.log(dacIDs);
-  //   const dacDatasets = dacIDs.map(async(dacID) => await DAC.datasets(dacID));
-  //   console.log(dacDatasets);
-  //   setFilteredList(dacDatasets);
-  // }, [dacIDs]);
-
-  // const getDatasets = useCallback(async () => {
-  //   // todo: /me/dac/datasets uses DatasetDTO objects (which are deprecated and don't include all fields. i think we should update this endpoint if we're going to still need it to return regular Dataset objects, then you shouldn't have to do this map operation
-  //   const userDatasets = await User.getUserRelevantDatasets();
-  //   userDatasets.map(async dataset => {
-  //     const datasetObject = await DataSet.getDataSetsByDatasetId(dataset.dataSetId);
-  //     dataset.dacApproval = datasetObject.dacApproval;
-  //   });
-  //   return userDatasets;
-  // }, []);
 
   useEffect(() => {
     const init = async () => {
@@ -84,27 +44,6 @@ export default function DACDatasets(props) {
     };
     init();
   }, []);
-
-  // useEffect(() => {
-  /*const init = async() => {
-    try {
-      setIsLoading(true);
-      const listOfDatasets = await User.getUserRelevantDatasets();
-      const listOfDatasets = await getDatasets();
-      setDatasets(listOfDatasets);
-      setFilteredList(listOfDatasets);
-      setIsLoading(false);
-    } catch (error) {
-      Notifications.showError({text: 'Error initializing datasets table'});
-    }
-  };
-  init();
-  */
-  // Promise.all([
-  //   reloadUserRole(),
-  //   reloadDatasets()
-  // ]);
-  // }, [reloadUserRole, reloadDatasets]);
 
   return div({style: Styles.PAGE}, [
     div({
