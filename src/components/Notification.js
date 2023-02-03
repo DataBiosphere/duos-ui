e74b60056692f9dc4613170d4182fe41b2127624
Greg Rushton <grushton@broadinstitute.org>
2023-02-03 15:18:02 -0500

    update styling to reduce whitespace over notification

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,5 +1,4 @@
-import {div} from 'react-hyperscript-helpers';
-import * as fp from 'lodash/fp';
+import {isEmpty} from 'lodash/fp';
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import WarningIcon from '@material-ui/icons/Warning';
@@ -9,9 +8,9 @@ import ReportIcon from '@material-ui/icons/Report';
 
 export const Notification = (props) => {
   const {notificationData} = props;
-  let notificationDiv = div({style: {display: 'none'}}, []);
+  let notificationDiv = <div style={{display: 'none'}}/>;
 
-  if (!fp.isEmpty(notificationData)) {
+  if (!isEmpty(notificationData)) {
     const iconStyle = {
       marginRight: '2rem',
       marginLeft: '1rem',
@@ -22,30 +21,28 @@ export const Notification = (props) => {
     let icon;
     switch (notificationData.level) {
       case 'success':
-        icon = <CheckCircleIcon fill={'#3c763d'} style={ iconStyle } />;
+        icon = <CheckCircleIcon fill={'#3c763d'} style={iconStyle}/>;
         break;
       case 'info':
-        icon = <InfoIcon fill={'#31708f'} style={ iconStyle } />;
+        icon = <InfoIcon fill={'#31708f'} style={iconStyle}/>;
         break;
       case 'warning':
-        icon = <WarningIcon fill={'#8a6d3b'} style={ iconStyle } />;
+        icon = <WarningIcon fill={'#8a6d3b'} style={iconStyle}/>;
         break;
       case 'danger':
-        icon = <ReportIcon fill={'#a94442'} style={ iconStyle } />;
+        icon = <ReportIcon fill={'#a94442'} style={iconStyle}/>;
         break;
       default:
-        icon = <InfoIcon fill={'#3c763d'} style={ iconStyle } />;
+        icon = <InfoIcon fill={'#3c763d'} style={iconStyle}/>;
         break;
     }
     // eslint-disable-next-line react/no-children-prop
     const content = <ReactMarkdown children={notificationData.message} linkTarget={'_blank'} className={'underlined'}/>;
-    notificationDiv = div({
-      style: {padding: '.5rem', paddingTop: '1.5rem'},
-      className: 'row no-margin alert alert-' + notificationData.level,
-    }, [
-      div({style: {float: 'left'}}, [icon]),
-      div({style: {marginTop: '0.5rem'}}, [content]),
-    ]);
+    notificationDiv = <div
+      className={'row no-margin alert alert-' + notificationData.level}>
+      <div style={{float: 'left'}}>{icon}</div>
+      <div>{content}</div>
+    </div>;
   }
 
   return notificationDiv;
