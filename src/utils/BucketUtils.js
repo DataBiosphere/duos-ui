bc2474369ca6e5637de273f8045a0862d5803766
Greg Rushton <grushton@broadinstitute.org>
2023-02-23 08:25:01 -0500

    add rus bucket

--- src/utils/BucketUtils.js
+++ src/utils/BucketUtils.js
@@ -1,11 +1,11 @@
-import {any, compact, findIndex, flow, forEach, get, getOr, includes, isEmpty, isEqual, join, map, toLower, uniq, values} from 'lodash/fp';
+import {any, compact, findIndex, flatMap, flow, forEach, get, getOr, includes, isEmpty, isEqual, join, map, pick, toLower, uniq, values} from 'lodash/fp';
 
 import {processVotesForBucket} from './DarCollectionUtils';
 import {Match} from '../libs/ajax';
 import {translateDataUseRestrictionsFromDataUseArray} from '../libs/dataUseTranslation';
 
 /**
- * TODO: Prepend a rus bucket, fix algorithm result
+ * TODO: fix algorithm result
  * @param collection The full Data Access Request Collection
  * @returns {Promise<*[Bucket]>}
  */
@@ -27,7 +27,7 @@ export const binCollectionToBuckets = async (collection) => {
   //      c: Set the bucket key/label from the dataUse
   // Step 2: Pull all elections for those datasets into the buckets
   // Step 3: Pull all votes up to a top level bucket field for easier iteration
-
+  // Step 4: Prepend an RP Vote bucket for the DAC to vote on the research purpose
 
   const datasets = get('datasets')(collection);
 
@@ -72,23 +72,20 @@ export const binCollectionToBuckets = async (collection) => {
       }
     }
 
-    // Step 1.a
-    // Find the match results for this bucket
+    // Step 1.a: Populate match results for this bucket
     forEach(m => {
       if (toLower(d.datasetIdentifier) === toLower(m.consent)) {
         bucket.matchResults.push(m);
       }
     })(matchData);
 
-    // Step 1.b
-    // Find the translation for this dataset's dataUse
+    // Step 1.b: Populate translated dataUses
     const index = findIndex(d.dataUse)(rawDataUses);
     if (index >= 0) {
       bucket.dataUses = flatTranslatedDataUses[index];
     }
 
-    // Step 1.c
-    // Generate the key used for the label
+    // Step 1.c: Generate the key used for the label
     if (!isEmpty(bucket.dataUses)) {
       bucket.key = flow(
         map((du) => du.alternateLabel || du.code),
@@ -100,7 +97,7 @@ export const binCollectionToBuckets = async (collection) => {
 
   })(datasets);
 
-  // Step 2
+  // Step 2: Populate elections
   const darMap = get('dars')(collection);
   forEach(dar => {
     const electionMap = get('elections')(dar);
@@ -115,10 +112,21 @@ export const binCollectionToBuckets = async (collection) => {
     })(values(electionMap));
   })(values(darMap));
 
-  // Step 3
+  // Step 3: Populate votes
   forEach(bucket => {
     bucket.votes.push(processVotesForBucket(bucket.elections));
   })(buckets);
+
+  // Step 4: Populate RUS Vote bucket with RP votes
+  const rpVotes = flow(
+    flatMap(b => b.votes),
+    map(pick('rp'))
+  )(buckets);
+  buckets.unshift({
+    isRp: true,
+    key: 'RUS Vote',
+    votes: rpVotes
+  });
 
   return buckets;
 };
