210fccafaa163b145e2f03a0d0e8e343edfdc010
Greg Rushton <grushton@broadinstitute.org>
2023-02-24 16:00:30 -0500

    gru test

--- cypress/component/utils/bucket_utils.spec.js
+++ cypress/component/utils/bucket_utils.spec.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 
 import {binCollectionToBuckets} from '../../../src/utils/BucketUtils';
-import {forEach} from 'lodash/fp';
+import {forEach, find} from 'lodash/fp';
 import {Match} from '../../../src/libs/ajax';
 
 const sample_collection = {
@@ -76,9 +76,73 @@ const sample_collection = {
               'displayName': 'User 1'
             }
           }
+        },
+        '3': {
+          'electionId': 3,
+          'electionType': 'DataAccess',
+          'referenceId': 'dar-reference-id-1',
+          'dataSetId': 2,
+          'votes': {
+            '11': {
+              'voteId': 11,
+              'dacUserId': 1,
+              'electionId': 3,
+              'rationale': '',
+              'type': 'Chairperson',
+              'displayName': 'User 1'
+            },
+            '22': {
+              'voteId': 22,
+              'dacUserId': 1,
+              'electionId': 3,
+              'rationale': '',
+              'type': 'DAC',
+              'displayName': 'User 1'
+            },
+            '33': {
+              'voteId': 33,
+              'dacUserId': 1,
+              'electionId': 3,
+              'rationale': '',
+              'type': 'Final',
+              'displayName': 'User 1'
+            }
+          }
+        },
+        '4': {
+          'electionId': 4,
+          'electionType': 'RP',
+          'referenceId': 'dar-reference-id-1',
+          'dataSetId': 2,
+          'votes': {
+            '44': {
+              'voteId': 44,
+              'dacUserId': 1,
+              'electionId': 4,
+              'rationale': '',
+              'type': 'Chairperson',
+              'displayName': 'User 1'
+            },
+            '55': {
+              'voteId': 55,
+              'dacUserId': 1,
+              'electionId': 4,
+              'rationale': '',
+              'type': 'DAC',
+              'displayName': 'User 1'
+            },
+            '66': {
+              'voteId': 66,
+              'dacUserId': 1,
+              'electionId': 4,
+              'rationale': '',
+              'type': 'Final',
+              'displayName': 'User 1'
+            }
+          }
         }
       },
-      'datasetIds': [1]
+      'datasetIds': [1,2]
     }
   }
   ,
@@ -88,22 +152,43 @@ const sample_collection = {
       'datasetName': 'ds 1',
       'datasetIdentifier': 'DUOS-000001',
       'dataUse': {'generalUse': true}
+    },
+    {
+      'dataSetId': 2,
+      'datasetName': 'ds 2',
+      'datasetIdentifier': 'DUOS-000002',
+      'dataUse': {'generalUse': true}
     }
   ],
 };
 
+const match_results = [
+  {
+    'id': 1,
+    'consent': 'DUOS-000001',
+    'purpose': 'dar-reference-id-1',
+    'match': true,
+    'failed': false,
+    'createDate': 'Jan 23, 2023',
+    'algorithmVersion': 'v2'
+  },
+  {
+    'id': 2,
+    'consent': 'DUOS-000002',
+    'purpose': 'dar-reference-id-1',
+    'match': true,
+    'failed': false,
+    'createDate': 'Jan 23, 2023',
+    'algorithmVersion': 'v2'
+  }
+];
+
+beforeEach(() => {
+  cy.stub(Match, 'findMatchBatch').returns(match_results);
+});
+
 describe('BucketUtils', () => {
   it('instantiates a collection into buckets', async () => {
-    cy.stub(Match, 'findMatchBatch').returns([{
-      'id': 1,
-      'consent': 'DUOS-000001',
-      'purpose': 'dar-reference-id-1',
-      'match': true,
-      'failed': false,
-      'createDate': 'Jan 23, 2023',
-      'algorithmVersion': 'v2'
-    }
-    ]);
     const buckets = await binCollectionToBuckets(sample_collection);
     expect(buckets).to.not.be.empty;
     forEach(b => {
@@ -120,4 +205,13 @@ describe('BucketUtils', () => {
       }
     })(buckets);
   });
+
+  it('there should be a bucket with two GRU datasets', async () => {
+    const buckets = await binCollectionToBuckets(sample_collection);
+    const gruBucket = find(b => b.label === 'GRU')(buckets);
+    expect(gruBucket).to.not.be.empty;
+    expect(gruBucket.datasets).to.not.be.empty;
+    expect(gruBucket.datasets.length).to.eq(2);
+  });
+
 });
