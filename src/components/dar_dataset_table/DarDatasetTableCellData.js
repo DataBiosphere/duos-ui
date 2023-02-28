06a3731217f345fa14fa824abe0ff6b85fe62d11
Greg Rushton <grushton@broadinstitute.org>
2023-02-28 14:48:12 -0500

    handle empty elections

--- src/components/dar_dataset_table/DarDatasetTableCellData.js
+++ src/components/dar_dataset_table/DarDatasetTableCellData.js
@@ -1,5 +1,5 @@
 import {styles} from './DarDatasetTable';
-import {filter, flatMap, flow, get, isUndefined, map, uniq} from 'lodash/fp';
+import {filter, flatMap, flow, get, isEmpty, isUndefined, map, uniq} from 'lodash/fp';
 
 export function dataUseGroupCellData({dataUseGroup, label= 'data-use'}) {
   return {
@@ -16,22 +16,25 @@ export function dataUseGroupCellData({dataUseGroup, label= 'data-use'}) {
 }
 
 export function votesCellData({elections, votes, dataUseGroup, label= 'votes'}) {
-  const datasetIds = flow(
-    filter(e => e.electionType === 'DataAccess'),
-    map(e => e.dataSetId),
-    uniq
-  )(elections);
-  const memberVotes = flow(
-    map(v => v.dataAccess),
-    flatMap(v => v.memberVotes)
-  )(votes);
-  const voteValues = flow(
-    filter(v => !isUndefined(v.vote)),
-    map(v => get('vote')(v))
-  )(memberVotes);
-  const numerator = voteValues.length / datasetIds.length;
-  const denominator = memberVotes.length / datasetIds.length;
-  const displayValue = numerator + '/' + denominator;
+  let displayValue = '-/-';
+  if (!isEmpty(elections)) {
+    const datasetIds = flow(
+      filter(e => e.electionType === 'DataAccess'),
+      map(e => e.dataSetId),
+      uniq
+    )(elections);
+    const memberVotes = flow(
+      map(v => v.dataAccess),
+      flatMap(v => v.memberVotes)
+    )(votes);
+    const voteValues = flow(
+      filter(v => !isUndefined(v.vote)),
+      map(v => get('vote')(v))
+    )(memberVotes);
+    const numerator = voteValues.length / datasetIds.length;
+    const denominator = memberVotes.length / datasetIds.length;
+    displayValue = numerator + '/' + denominator;
+  }
 
   return {
     data: displayValue,
