98768a30b5e2798b428bcc454b59ef6fa5e6f412
Greg Rushton <grushton@broadinstitute.org>
2023-02-28 16:02:58 -0500

    minor updates

--- src/components/dar_dataset_table/DarDatasetTableCellData.js
+++ src/components/dar_dataset_table/DarDatasetTableCellData.js
@@ -1,5 +1,5 @@
 import {styles} from './DarDatasetTable';
-import {filter, flatMap, flow, get, isEmpty, isUndefined, map, uniq} from 'lodash/fp';
+import {filter, flatMap, flow, get, isEmpty, isUndefined, map, uniq, values} from 'lodash/fp';
 
 export function dataUseGroupCellData({dataUseGroup, label= 'data-use'}) {
   return {
@@ -15,18 +15,19 @@ export function dataUseGroupCellData({dataUseGroup, label= 'data-use'}) {
   };
 }
 
-export function votesCellData({elections, votes, dataUseGroup, label= 'votes'}) {
+export function votesCellData({elections, dataUseGroup, label= 'votes'}) {
   let displayValue = '-/-';
   if (!isEmpty(elections)) {
+    const darElections = filter(e => e.electionType === 'DataAccess')(elections);
     const datasetIds = flow(
       filter(e => e.electionType === 'DataAccess'),
       map(e => e.dataSetId),
       uniq
-    )(elections);
+    )(darElections);
     const memberVotes = flow(
-      map(v => v.dataAccess),
-      flatMap(v => v.memberVotes)
-    )(votes);
+      flatMap(e => values(e.votes)),
+      filter(v => v.type === 'DAC')
+    )(darElections);
     const voteValues = flow(
       filter(v => !isUndefined(v.vote)),
       map(v => get('vote')(v))
