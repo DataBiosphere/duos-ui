16222136a127d997f38b9d0acdf5a5b95e0694e8
Connor Barker <connorlbark@gmail.com>
2023-04-07 07:19:07 -0400

    improve script

--- scripts/compile-jsonschema.sh
+++ scripts/compile-jsonschema.sh
@@ -1,4 +1,34 @@
 #!/usr/bin/env bash
+#
+# Compiles the json schemas needed for the frontend; output is at
+# ./src/assets/schemas.
+# You MUST have jq and ajv installed to be able to use this script.
+#
+# USAGE: ./compile-jsonschema.sh 
+#
+
+set -eu
+set -o pipefail
+
+check_jq_installed() {
+    if ! jq --version 1>/dev/null 2>&1; then
+        abort "jq v1.6 or above is required; install jq to continue"
+    fi
+}
+
+check_ajv_installed() {
+    if ! ajv help 1>/dev/null 2>&1; then
+        abort "ajv v5.0.0 or above is required; install ajv via 'npm install' to continue"
+    fi
+}
+
+prepend_text() {
+  echo "$1 $(cat $2)" > $2
+}
+
+check_jq_installed
+check_ajv_installed
+
 APIURL=https://consent.dsde-dev.broadinstitute.org/
 if test -f "./public/config.json"; then
   APIURL=$(jq '.apiUrl' ./public/config.json -r)
@@ -9,4 +39,4 @@ curl -s -S -X 'GET'  "$APIURL/schemas/dataset-registration/v1"  -H 'accept: appl
 
 ajv compile -s ./src/assets/schemas/DataRegistrationV1.json -o ./src/assets/schemas/DataRegistrationV1Validation.js --spec draft2019 -c ajv-formats --strict false --all-errors
 
-echo "/* eslint-disable */ $(cat ./src/assets/schemas/DataRegistrationV1Validation.js)" > ./src/assets/schemas/DataRegistrationV1Validation.js
+prepend_text "/* eslint-disable */" ./src/assets/schemas/DataRegistrationV1Validation.js
