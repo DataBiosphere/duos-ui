78007701c63de793fdef4bcc1622a8993172c35e
Greg Rushton <grushton@broadinstitute.org>
2023-06-08 14:24:13 -0400

    split out initializations into separate useEffects

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 import {Config} from './libs/config';
 import DuosFooter from './components/DuosFooter';
 import DuosHeader from './components/DuosHeader';
-import {useHistory} from 'react-router-dom';
+import {useHistory, useLocation} from 'react-router-dom';
 import loadingImage from './images/loading-indicator.svg';
 
 import {SpinnerComponent as Spinner} from './components/SpinnerComponent';
@@ -19,49 +19,53 @@ function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [env, setEnv] = useState('');
   let history = useHistory();
+  let location = useLocation();
 
   const trackPageView = (location) => {
     ReactGA.pageview(location.pathname + location.search);
   };
 
   useEffect(() => {
-    const setUserIsLogged = async () => {
-      const isLogged = await Storage.userIsLogged();
-      setIsLoggedIn(isLogged);
-    };
+    Modal.setAppElement(document.getElementById('modal-root'));
+  });
 
+  useEffect(() => {
+    const setEnvironment = async () => {
+      const environment = await Config.getEnv();
+      setEnv(environment);
+      await Storage.setEnv(environment);
+    };
+    setEnvironment();
+  });
+
+  useEffect(() => {
     const initializeReactGA = async (history) => {
       const gaId = await Config.getGAId();
       ReactGA.initialize(gaId, {
         titleCase: false
       });
       //call trackPageView to register initial page load
-      trackPageView(history.location);
+      trackPageView(location);
       //pass trackPageView as callback function for url change listener
       history.listen(trackPageView);
     };
+    initializeReactGA(history);
+  }, [history, location]);
 
+  useEffect(() => {
     const stackdriverStart = async () => {
       await StackdriverReporter.start();
     };
+    stackdriverStart();
+  });
 
-    const setEnvironment = async () => {
-      const environment = await Config.getEnv();
-      setEnv(environment);
-      Storage.setEnv(environment);
+  useEffect(() => {
+    const setUserIsLogged = async () => {
+      const isLogged = await Storage.userIsLogged();
+      setIsLoggedIn(isLogged);
     };
-
-    const initApp = async () => {
-      Modal.setAppElement(document.getElementById('modal-root'));
-      await initializeReactGA(history);
-      await setUserIsLogged();
-      await setEnvironment();
-      await stackdriverStart();
-    };
-
-    initApp();
-
-  }, [history]);
+    setUserIsLogged();
+  });
 
   const signOut = async () => {
     const clientId = await Config.getGoogleClientId();
@@ -77,14 +81,14 @@ function App() {
   };
 
   return (
-    div({ className: 'body' }, [
-      div({ className: 'wrap' }, [
-        div({ className: 'main' }, [
-          h(DuosHeader, { onSignOut: signOut }),
+    div({className: 'body'}, [
+      div({className: 'wrap'}, [
+        div({className: 'main'}, [
+          h(DuosHeader, {onSignOut: signOut}),
           h(Spinner, {
             name: 'mainSpinner', group: 'duos', loadingImage
           }),
-          h(Routes, { onSignOut: signOut, onSignIn: signIn, isLogged: isLoggedIn, env: env })
+          h(Routes, {onSignOut: signOut, onSignIn: signIn, isLogged: isLoggedIn, env: env})
         ])
       ]),
       DuosFooter()
