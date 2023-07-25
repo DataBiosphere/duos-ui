1d0d9d9248116b4d44ba414ca3d72e869f769038
Hamsini Malli Sivakumar <hmallisi@broadinstitute.org>
2023-07-25 15:48:15 -0400

    changed back button

--- src/pages/user_profile/RequestRole.js
+++ src/pages/user_profile/RequestRole.js
@@ -113,17 +113,6 @@ export default function RequestRole(props) {
       padding: '25px 270px 0px 270px'
     }
   }, [
-    button({
-      id: 'btn_submit',
-      onClick: goToPrevPage,
-      className: 'f-right',
-      style: {
-        marginTop: 7,
-        fontFamily: 'Montserrat',
-        fontSize: '18px',
-        padding: 5
-      },
-    }, ['< Go back to User Profile']),
     p({
       style: {
         color: '#01549F',
@@ -174,9 +163,16 @@ export default function RequestRole(props) {
         onChange: handleSupportRequestsChange,
       }),
     ]),
-
     button({
-      id: 'btn_submit',
+      id: 'btn_save',
+      onClick: goToPrevPage,
+      className: 'f-left btn-primary btn-back',
+      style: {
+        marginTop: '50px',
+      },
+    }, ['Back']),
+    button({
+      id: 'btn_save',
       onClick: submitForm,
       className: 'f-right btn-primary common-background',
       style: {
