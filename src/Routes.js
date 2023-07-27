3e11d74ea0005fc4db852030ffb10cddebe198e6
Florian Boulnois <fboulnoi@broadinstitute.org>
2023-07-27 13:57:24 -0400

    feat: add elastic search api and search page

--- src/Routes.js
+++ src/Routes.js
@@ -43,6 +43,7 @@ import CustomDatasetCatalog from './pages/dac_dataset_catalog/CustomDatasetCatal
 import {AnVILDMSPolicyInfo, NIHDMSPolicyInfo} from './pages/DMSPolicyInfo';
 import {checkEnv, envGroups} from './utils/EnvironmentUtils';
 import { DatasetUpdateForm } from './pages/DatasetUpdateForm';
+import DatasetSearch from './pages/DatasetSearch';
 
 const Routes = (props) => (
   <Switch>
@@ -94,6 +95,7 @@ const Routes = (props) => (
     <AuthenticatedRoute path="/admin_manage_dar_collections/" component={AdminManageDarCollections} props={props} rolesAllowed={[USER_ROLES.admin]} />
     {checkEnv(envGroups.NON_STAGING) && <AuthenticatedRoute path="/dataset_catalog/:variant" component={CustomDatasetCatalog} props={props} rolesAllowed={[USER_ROLES.researcher]}/>}
     <AuthenticatedRoute path="/dataset_catalog" component={DatasetCatalog} props={props} rolesAllowed={[USER_ROLES.admin, USER_ROLES.all]} />
+    <AuthenticatedRoute path="/dataset_search" component={DatasetSearch} props={props} rolesAllowed={[USER_ROLES.admin, USER_ROLES.all]} />
     <AuthenticatedRoute path="/dataset_statistics/:datasetId" component={DatasetStatistics} props={props}
       rolesAllowed={[USER_ROLES.all]} />
     <AuthenticatedRoute path="/dac_datasets" component={DACDatasets} props={props} rolesAllowed={[USER_ROLES.chairperson]} />
