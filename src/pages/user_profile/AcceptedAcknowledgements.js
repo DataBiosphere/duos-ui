9312ab15c463c285977fd5a2fbf12cfb2bff321b
Hamsini Malli Sivakumar <hmallisi@broadinstitute.org>
2023-07-31 16:13:44 -0400

    completed accepted terms

---
+++ src/pages/user_profile/AcceptedAcknowledgements.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from 'react';
+import { User } from '../../libs/ajax';
+import { Notifications } from '../../libs/utils';
+
+const Acknowledgment = ({ value }) =>
+  <div style={{
+    display: 'flex',
+    flexDirection: 'row',
+    fontFamily: 'Montserrat',
+    fontSize: '16px',
+    fontStyle: 'normal',
+    fontWeight: '400',
+    width: '675px'
+
+  }}>
+    <p>{value.name}</p>
+    <div style={{ flex: '1' }} />
+    <p>attested on: {value.attestedTime}</p>
+  </div>;
+
+export default function AcceptedAcknowledgements() {
+
+  const [acceptedAcknowledgements, setAcceptedAcknowledgements] = useState([]);
+
+  useEffect(() => {
+    const init = async () => {
+      const allAcknowledgements = [];
+      try {
+        const acknowledgements = await User.getAcknowledgements();
+        for (const key in acknowledgements) {
+          const currAcknowledgement = acknowledgements[key];
+          const date = new Date(currAcknowledgement.lastAcknowledged);
+          const month = String(date.getMonth() + 1).padStart(2, '0');
+          const day = String(date.getDate()).padStart(2, '0');
+          const year = date.getFullYear();
+          const newAcknowledgment = {
+            name: currAcknowledgement.ackKey,
+            attestedTime: `${month}/${day}/${year}`
+          };
+          allAcknowledgements.push(newAcknowledgment);
+        }
+        setAcceptedAcknowledgements(allAcknowledgements);
+      } catch (error) {
+        Notifications.showError({ text: 'Error: Unable to retrieve user data from server' });
+      }
+    };
+    init();
+  }, []);
+
+  return <div>
+    <h1
+      style={{
+        color: '#01549F',
+        fontSize: '20px',
+        fontWeight: '600',
+      }}
+    >
+      Accepted Terms & Policies
+    </h1>
+    <div style={{ marginTop: '20px' }} />
+    {acceptedAcknowledgements.map((value, index) => (
+      <Acknowledgment key={index} value={value} />
+    ))}
+  </div>;
+}
--- src/pages/user_profile/UserProfile.js
+++ src/pages/user_profile/UserProfile.js
@@ -4,11 +4,12 @@ import { FormField, FormFieldTypes } from '../../components/forms/forms';
 import { PageHeading } from '../../components/PageHeading';
 import { Notification } from '../../components/Notification';
 import AffiliationAndRoles from './AffiliationAndRoles';
-import ResearcherStatus from './ResearcherStatus';
 import { Institution } from '../../libs/ajax';
 import { Storage } from '../../libs/storage';
 import { NotificationService } from '../../libs/notificationService';
 import { Notifications, getPropertyValuesFromUser } from '../../libs/utils';
+import ResearcherStatus from './ResearcherStatus';
+import AcceptedAcknowledgements from './AcceptedAcknowledgements';
 
 export default function UserProfile(props) {
 
@@ -106,13 +107,15 @@ export default function UserProfile(props) {
         marginBottom: '50px'
       },
     }, ['Request a New Role']),
-
     div({ className: '', style: { 'marginTop': '115px' } }, []),
     ResearcherStatus({
       user: user,
       pageProps: props,
       profile: profile
-    })
+    }),
+    div({ className: '', style: { 'marginTop': '115px' } }, []),
+    AcceptedAcknowledgements(),
+    div({ className: '', style: { 'marginTop': '50px' } }, []),
   ]);
 
 }
