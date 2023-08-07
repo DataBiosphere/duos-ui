b63bd441ad8880f112d3dd8248c8deb65cad3511
koflaher <koflaher@broadinstitute.org>
2023-08-07 11:07:25 -0400

    cypress fix

--- src/components/data_submission/NihAnvilUse.js
+++ src/components/data_submission/NihAnvilUse.js
@@ -41,7 +41,7 @@ export default function NihAnvilUse(props) {
       onChange: (config) => {
         const value = nihAnvilUseLabels[config.value];
         onChange({key: config.key, value: value, isValid: config.isValid});
-        formData.properties.nihAnvilUse = config.value;
+        studyEditMode ? formData.properties.nihAnvilUse = config.value : undefined;
       },
       validation: validation.nihAnvilUse,
       onValidationChange,
