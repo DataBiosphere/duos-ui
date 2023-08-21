36cc48bfd7eda7787a77b5feb77d3a61298266bc
Hamsini Malli Sivakumar <hmallisi@broadinstitute.org>
2023-08-21 11:24:57 -0400

    WIP

---
+++ src/components/SortableTable.js
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import TableSortLabel from "@mui/material/TableSortLabel";
+
+export default function SortableTable(props) {
+
+  const {
+    rowNames,
+    rows
+  } = props
+
+  const [rowData, setRowData] = useState(rows);
+  const [rowHeaders, setRowHeaders] = useState(rowNames);
+
+  // function createRowHeaders(rowNames) {
+  //   const rowHeaders = []
+  //   rowNames.forEach((rowName) => (
+  //     rowHeaders.push(
+  //       {
+  //         rowHeader: rowName.ja,
+  //         orderDirection: "asc"
+  //       }
+  //     )
+  //   ))
+  //   return rowHeaders
+  // }
+
+  function descendingComparator(a, b, orderBy) {
+    if (b[orderBy] < a[orderBy]) {
+      return -1
+    }
+    if (b[orderBy] > a[orderBy]) {
+      return 1
+    }
+    return 0
+  }
+    
+  function getComparator(order, orderBy) {
+    return order === 'desc'
+      ? (a, b) => descendingComparator(a, b, orderBy)
+      : (a, b) => -descendingComparator(a, b, orderBy)
+  }
+
+  return (
+    <TableContainer component={Paper}>
+      <Table aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            {rowHeaders.map((item, i) => (
+              // item.sortable ? (
+              //   <TableCell key={i} align="center" onClick={() => handleSortRequest(item.rowHeader)}>
+              //     <TableSortLabel active={orderBy === item.rowHeader} direction={orderDirection}>
+              //       {item.name}
+              //     </TableSortLabel>
+              //   </TableCell>
+              // ) : (
+                <TableCell key={i} align="right">{item.name}</TableCell>
+              // )
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row, i) => (
+            <TableRow key={i}>
+            {Object.values(row).map((value, j) => (
+              <TableCell key={j}>{value}</TableCell>
+            ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
---
+++ src/pages/user_profile/ControlledAccessGrants.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import SortableTable from '../../components/SortableTable'
+
+export default function ControlledAccessGrants() {
+
+  const example = [
+    {
+      alias: 1,
+      darCode: "darCode1",
+      datasetName: "datasetName1",
+      dacName: "dacName1",
+      approvalDate: "approvalDate1",
+      datasetIdentifier: "datasetIdentifier1"
+    },
+    {
+      alias: 2,
+      darCode: "darCode2",
+      datasetName: "datasetName2",
+      dacName: "dacName2",
+      approvalDate: "approvalDate2",
+      datasetIdentifier: "datasetIdentifier2"
+    }
+  ];
+
+  const rows = [
+    example.map((exampleRow, i) => (
+      createData(exampleRow.darCode, exampleRow.approvalDate, exampleRow.datasetIdentifier, exampleRow.datasetName, exampleRow.dacName)
+    ))
+  ]
+
+  const columns = ["DAR ID", "Approval Date", "Dataset ID", "Dataset Name", "Data Access Committee"];
+
+  function createData(darCode, approvalDate, datasetIdentifier, datasetName, dacName) {
+    return {darCode, approvalDate, datasetIdentifier, datasetName, dacName};
+  }
+
+  const rowNames = [{name: "DAR ID", sortable: false}, {name: "Approval Date", sortable: false}, {name: "Dataset ID", sortable: false}, {name: "Dataset Name", sortable: false}, {name: "Data Access Committee", sortable: false}]
+  
+
+  return <div>
+    <h1
+      style={{
+        color: '#01549F',
+        fontSize: '20px',
+        fontWeight: '600',
+      }}
+    >
+      Controlled Access Grants
+    </h1>
+    <p
+      style={{
+        color: '#000',
+        fontSize: '16px',
+        fontWeight: '400',
+      }}
+    >
+      Your current dataset approvals
+    </p>
+    <div style={{ marginTop: '20px' }} />
+    <SortableTable rows={example} rowNames={rowNames} />
+  </div>;
+}
