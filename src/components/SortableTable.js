4955d886cf595e2da314cc0e99b073aafdfdd1a5
Hamsini Malli <hmallisi@broadinstitute.org>
2023-08-21 13:18:04 -0400

    Update src/components/SortableTable.js
    Co-authored-by: codacy-production[bot] <61871480+codacy-production[bot]@users.noreply.github.com>

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
+import TableCell from '@mui/material/TableCell';
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
