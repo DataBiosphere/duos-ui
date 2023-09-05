1b820b7668001a333096fbee0b7259f4474baa89
Hamsini Malli <hmallisi@broadinstitute.org>
2023-09-05 13:23:06 -0400

    [DUOS-2612] Controlled Access Grants (#2327)
    * WIP
    
    * sorting added + uses endpoint for data
    
    * extracted data from table component
    
    * finished styling table
    
    * repositioned component
    
    * created package for sortable table + fixed styling
    
    ---------
    
    Co-authored-by: codacy-production[bot] <61871480+codacy-production[bot]@users.noreply.github.com>

---
+++ src/components/sortable_table/SortableTable.js
@@ -0,0 +1,180 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { ThemeProvider } from '@mui/material/styles';
+import EnhancedTableHead from './EnhancedTableHead';
+import { theme } from './Themes';
+
+function descendingComparator(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function getComparator(order, orderBy) {
+  return order === 'desc'
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+function stableSort(array, comparator) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) {
+      return order;
+    }
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map((el) => el[0]);
+}
+
+export default function SortableTable(props) {
+
+  const {
+    rows,
+    headCells
+  } = props;
+
+  const [order, setOrder] = React.useState('asc');
+  const [orderBy, setOrderBy] = React.useState('darCode');
+  const [selected, setSelected] = React.useState([]);
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
+
+
+  const handleClick = (event, name) => {
+    const selectedIndex = selected.indexOf(name);
+    let newSelected = [];
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, name);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1)
+      );
+    }
+
+    setSelected(newSelected);
+  };
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+
+  const isSelected = (name) => selected.indexOf(name) !== -1;
+
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+  const visibleRows = React.useMemo(
+    () =>
+      stableSort(rows, getComparator(order, orderBy)).slice(
+        page * rowsPerPage,
+        page * rowsPerPage + rowsPerPage,
+      ),
+    [order, orderBy, page, rows, rowsPerPage],
+  );
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Box
+        sx={{
+          width: '100%',
+          fill: '#FFF',
+          strokeWidth: '1px',
+          stroke: '#ABABAB',
+          filter: 'drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))'
+        }} >
+        <Paper
+          sx={{
+            width: '100%',
+            mb: 2,
+            fontFamily: 'Montserrat',
+            fontSize: '100'
+          }} >
+          <TableContainer>
+            <Table>
+              <EnhancedTableHead
+                order={order}
+                orderBy={orderBy}
+                onRequestSort={handleRequestSort}
+                headCells={headCells}
+                sx={{ marginBottom: '15px' }} />
+              <TableBody>
+                {visibleRows.map((row, index) => {
+                  const isItemSelected = isSelected(row.name);
+                  const labelId = `enhanced-table-checkbox-${index}`;
+
+                  return (
+                    <TableRow
+                      hover
+                      onClick={(event) => handleClick(event, row.name)}
+                      tabIndex={-1}
+                      key={row.datasetIdentifier}
+                      selected={isItemSelected} >
+                      <TableCell
+                        component='th'
+                        id={labelId}
+                        scope='row'
+                        padding='none' >
+                        {row.darCode}
+                      </TableCell>
+                      <TableCell align='right'>{row.approvalDate}</TableCell>
+                      <TableCell align='right'>{row.datasetIdentifier}</TableCell>
+                      <TableCell align='right'>{row.datasetName}</TableCell>
+                      <TableCell align='right'>{row.dacName}</TableCell>
+                    </TableRow>
+                  );
+                })}
+                {emptyRows > 0 && (
+                  <TableRow
+                    style={{
+                      height: 53 * emptyRows,
+                    }} >
+                    <TableCell colSpan={6} />
+                  </TableRow>
+                )}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <TablePagination
+            rowsPerPageOptions={[]}
+            component='div'
+            count={rows.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage} />
+        </Paper>
+      </Box >
+    </ThemeProvider>
+  );
+}
---
+++ src/pages/user_profile/ControlledAccessGrants.js
@@ -0,0 +1,97 @@
+import React, { useState, useEffect } from 'react';
+import SortableTable from '../../components/sortable_table/SortableTable';
+import { User } from '../../libs/ajax';
+import { Notifications } from '../../libs/utils';
+
+const headCells = [
+  {
+    id: 'darCode',
+    numeric: false,
+    disablePadding: false,
+    label: 'DAR Code',
+  },
+  {
+    id: 'approvalDate',
+    numeric: false,
+    disablePadding: false,
+    label: 'Approval Date',
+  },
+  {
+    id: 'datasetIdentifier',
+    numeric: false,
+    disablePadding: false,
+    label: 'Dataset Identifier',
+  },
+  {
+    id: 'datasetName',
+    numeric: false,
+    disablePadding: false,
+    label: 'Dataset Name',
+  },
+  {
+    id: 'dacName',
+    numeric: false,
+    disablePadding: false,
+    label: 'DAC Name',
+  },
+];
+
+function createData(darCode, approvalDate, datasetIdentifier, datasetName, dacName) {
+  return {
+    darCode,
+    approvalDate,
+    datasetIdentifier,
+    datasetName,
+    dacName
+  };
+}
+
+function createRows(userRows) {
+  return userRows.map((exampleRow) => createData(
+    exampleRow.darCode,
+    exampleRow.approvalDate,
+    exampleRow.datasetIdentifier,
+    exampleRow.datasetName,
+    exampleRow.dacName
+  ));
+}
+
+export default function ControlledAccessGrants() {
+
+  const [rows, setRows] = useState([]);
+
+  useEffect(() => {
+    const init = async () => {
+      try {
+        const userRows = await User.getApprovedDatasets();
+        setRows(createRows(userRows));
+      } catch (error) {
+        Notifications.showError({ text: 'Error: Unable to retrieve user data from server' });
+      }
+    };
+    init();
+  }, []);
+
+  return <div>
+    <h1
+      style={{
+        color: '#01549F',
+        fontSize: '20px',
+        fontWeight: '600',
+      }}
+    >
+      Controlled Access Grants
+    </h1>
+    <p
+      style={{
+        color: '#000',
+        fontSize: '16px',
+        fontWeight: '400',
+      }}
+    >
+      Your current dataset approvals
+    </p>
+    <div style={{ marginTop: '20px' }} />
+    <SortableTable rows={rows} headCells={headCells} />
+  </div>;
+}
