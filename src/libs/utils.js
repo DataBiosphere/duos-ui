57bd68d0e94a82d600e30d765ac2ef22cba0f32e
Gregory Rushton <rushtong@users.noreply.github.com>
2023-09-19 14:56:08 -0400

    [DUOS-2641][risk=no] Submitted Datasets Subtab (#2322)
    * data submitter sub nav in researcher console

--- src/libs/utils.js
+++ src/libs/utils.js
@@ -1,11 +1,11 @@
 import Noty from 'noty';
 import 'noty/lib/noty.css';
 import 'noty/lib/themes/bootstrap-v3.css';
-import {map as lodashMap, forEach as lodashForEach, isArray} from 'lodash';
-import { DAR } from './ajax';
-import {Theme } from './theme';
-import { each, flatten, flow, forEach as lodashFPForEach, get, getOr, uniq, find, first, map, isEmpty, filter, cloneDeep, isNil, toLower, includes, every, capitalize } from 'lodash/fp';
-import { headerTabsConfig } from '../components/DuosHeader';
+import {forEach as lodashForEach, isArray, map as lodashMap} from 'lodash';
+import {DAR} from './ajax';
+import {Theme} from './theme';
+import {capitalize, cloneDeep, concat, each, every, filter, find, first, flatten, flow, forEach as lodashFPForEach, get, getOr, includes, isEmpty, isNil, join, map, toLower, uniq} from 'lodash/fp';
+import {headerTabsConfig} from '../components/DuosHeader';
 
 export const UserProperties = {
   SUGGESTED_SIGNING_OFFICIAL: 'suggestedSigningOfficial',
@@ -16,7 +16,7 @@ export const UserProperties = {
 
 ///////DAR Collection Utils///////////////////////////////////////////////////////////////////////////////////
 export const isCollectionCanceled = (collection) => {
-  const { dars } = collection;
+  const {dars} = collection;
   return every((dar) => toLower(dar.data.status) === 'canceled')(dars);
 };
 
@@ -28,15 +28,15 @@ export const darCollectionUtils = {
   determineCollectionStatus: (collection, relevantDatasets) => {
     const electionStatusCount = {};
     let output;
-    if(!isEmpty(collection.dars)) {
+    if (!isEmpty(collection.dars)) {
       const targetElections = flow([
         map((dar) => {
-          const { elections } = dar;
+          const {elections} = dar;
           //election is empty => no elections made for dar
           //need to figure out if dar is relevant, can obtain datasetId from dar.data
           //see if its relevant, if it is, add 1 to submitted on hash
           //return empty array at the end
-          if(isEmpty(elections)) {
+          if (isEmpty(elections)) {
             // Dataset IDs should be on the DAR, but if not, pull from the dar.data
             const datasetIds = isNil(dar.datasetIds) ? dar.data.datasetIds : dar.datasetIds;
             lodashFPForEach((datasetId) => {
@@ -53,7 +53,7 @@ export const darCollectionUtils = {
             //only Data Access elections impact the status of the collection
             //NOTE: Admin does not have relevantIds, DAC roles do
             const electionArr = filter(election => toLower(election.electionType) === 'dataaccess')(Object.values(elections));
-            if(isNil(relevantDatasets)) {
+            if (isNil(relevantDatasets)) {
               return electionArr;
             } else {
               const relevantIds = map(dataset => dataset.dataSetId)(relevantDatasets);
@@ -64,10 +64,10 @@ export const darCollectionUtils = {
         flatten
       ])(collection.dars);
 
-      if(isNil(relevantDatasets)) {
+      if (isNil(relevantDatasets)) {
         each(election => {
           const {status} = election;
-          if(isNil(electionStatusCount[status])) {
+          if (isNil(electionStatusCount[status])) {
             electionStatusCount[status] = 0;
           }
           electionStatusCount[status]++;
@@ -85,7 +85,7 @@ export const darCollectionUtils = {
 ///////DAR Collection Utils END/////////////////////////////////////////////////////////////////////////////////
 
 export const goToPage = (value, pageCount, setCurrentPage) => {
-  if(value >= 1 && value <= pageCount) {
+  if (value >= 1 && value <= pageCount) {
     setCurrentPage(value);
   }
 };
@@ -93,7 +93,7 @@ export const goToPage = (value, pageCount, setCurrentPage) => {
 export const findPropertyValue = (propName, researcher) => {
   const prop = isNil(researcher.researcherProperties) ?
     null
-    : find({ propertyKey: propName })(researcher.researcherProperties);
+    : find({propertyKey: propName})(researcher.researcherProperties);
   return isNil(prop) ? '' : prop.propertyValue;
 };
 
@@ -118,7 +118,7 @@ export const applyHoverEffects = (e, style) => {
 //currently, dars contain a list of datasets (any length) and a list of length 1 of a datasetId
 //go through the list of datasets and get the name of the dataset whose id is in the datasetId list
 export const getNameOfDatasetForThisDAR = (datasets, datasetId) => {
-  const data = !isNil(datasetId) && !isEmpty(datasetId) ? find({'value' : first(datasetId).toString()})(datasets) : null;
+  const data = !isNil(datasetId) && !isEmpty(datasetId) ? find({'value': first(datasetId).toString()})(datasets) : null;
   return isNil(data) ? '- -' : getDatasetNames([data]);
 };
 
@@ -127,7 +127,9 @@ export const formatDate = (dateval) => {
     return '---';
   }
 
-  if(toLower(dateval) === 'unsubmitted') {return dateval;}
+  if (toLower(dateval) === 'unsubmitted') {
+    return dateval;
+  }
 
   let dateFormat = new Date(dateval);
   let year = dateFormat.getFullYear();
@@ -161,16 +163,18 @@ export const USER_ROLES = {
 };
 
 export const getDatasetNames = (datasets) => {
-  if(!datasets){return '';}
+  if (!datasets) {
+    return '';
+  }
   const datasetNames = datasets.map((dataset) => {
-    return ((dataset.label) ?  dataset.label : dataset.name);
+    return ((dataset.label) ? dataset.label : dataset.name);
   });
   return datasetNames.join('\n');
 };
 
 //helper function to generate keys for rendered elements; splits on commas and whitespace
 export const convertLabelToKey = (label = '') => {
-  return label.split(/[\s,]+/) .join('-');
+  return label.split(/[\s,]+/).join('-');
 };
 
 export const setUserRoleStatuses = (user, Storage) => {
@@ -212,7 +216,7 @@ export const Navigation = {
 export const download = (fileName, text) => {
   const break_line = '\r\n \r\n';
   text = break_line + text;
-  let blob = new Blob([text], { type: 'text/plain' });
+  let blob = new Blob([text], {type: 'text/plain'});
   const url = window.URL.createObjectURL(blob);
   let a = document.createElement('a');
   a.href = url;
@@ -279,9 +283,9 @@ export const Notifications = {
  */
 export const PromiseSerial = funcs =>
   funcs.reduce((promise, func) =>
-    promise.then(result =>
-      func().then(Array.prototype.concat.bind(result))),
-  Promise.resolve([]));
+      promise.then(result =>
+        func().then(Array.prototype.concat.bind(result))),
+    Promise.resolve([]));
 
 //////////////////////////////////
 //DAR CONSOLES UTILITY FUNCTIONS//
@@ -300,7 +304,7 @@ export const outputCommaSeperatedElectionStatuses = (elections) => {
 
 export const getElectionDate = (election) => {
   let formattedString = '- -';
-  if(election) {
+  if (election) {
     //NOTE: some elections have a createDate attribute but not a lastUpdate attributes
     const targetDate = election.lastUpdate || election.createDate;
     formattedString = formatDate(targetDate);
@@ -308,7 +312,7 @@ export const getElectionDate = (election) => {
   return formattedString;
 };
 
-export const wasVoteSubmitted =(vote) => {
+export const wasVoteSubmitted = (vote) => {
   //NOTE: as mentioned elsewhere, legacy code has resulted in multiple sources for timestamps
   //current code will always provide lastUpdate
   const targetDate = vote.lastUpdate || vote.createDate || vote.updateDate || vote.lastUpdateDate;
@@ -323,20 +327,20 @@ export const wasFinalVoteTrue = (voteData) => {
 
 export const processElectionStatus = (election, votes, showVotes) => {
   let output;
-  const electionStatus = !isNil(get('status')(election))  ? toLower(election.status) : null;
+  const electionStatus = !isNil(get('status')(election)) ? toLower(election.status) : null;
   if (isNil(electionStatus)) {
     output = 'Unreviewed';
-  } else if(electionStatus === 'open') {
+  } else if (electionStatus === 'open') {
     //Null check since react doesn't necessarily perform prop updates immediately
-    if(!isEmpty(votes) && !isNil(election)) {
+    if (!isEmpty(votes) && !isNil(election)) {
       const dacVotes = filter((vote) => toLower(vote.type) === 'dac' && vote.electionId === election.electionId)(votes);
       const completedVotes = (filter(wasVoteSubmitted)(dacVotes)).length;
       const outputSuffix = `(${completedVotes} / ${dacVotes.length} votes)`;
       output = `Open${showVotes ? outputSuffix : ''}`;
     }
-  //some elections have electionStatus === Final, others have electionStatus === Closed
-  //both are, in this step of the process, technically referring to a closed election
-  //therefore both values must be checked for
+    //some elections have electionStatus === Final, others have electionStatus === Closed
+    //both are, in this step of the process, technically referring to a closed election
+    //therefore both values must be checked for
   } else if (electionStatus === 'final' || electionStatus === 'closed') {
     const finalVote = find(wasFinalVoteTrue)(votes);
     output = finalVote ? 'Approved' : 'Denied';
@@ -364,7 +368,7 @@ export const calcVisibleWindow = (currentPage, tableSize, filteredList) => {
 export const getSearchFilterFunctions = () => {
   return {
     dar: (term, targetList) => filter(electionData => {
-      const { election, dac, votes} = electionData;
+      const {election, dac, votes} = electionData;
       const dar = electionData.dar ? electionData.dar.data : undefined;
       const targetDarAttrs = !isNil(dar) ? JSON.stringify([toLower(dar.projectTitle), toLower(dar.darCode), toLower(getNameOfDatasetForThisDAR(dar.datasets, dar.datasetIds))]) : [];
       const targetDacAttrs = !isNil(dac) ? JSON.stringify([toLower(dac.name)]) : [];
@@ -372,7 +376,7 @@ export const getSearchFilterFunctions = () => {
       return includes(term, targetDarAttrs) || includes(term, targetDacAttrs) || includes(term, targetElectionAttrs);
     }, targetList),
     libraryCard: (term, targetList) => filter(libraryCard => {
-      const { userName, institution, createDate, updateDate, eraCommonsId, userEmail} = libraryCard;
+      const {userName, institution, createDate, updateDate, eraCommonsId, userEmail} = libraryCard;
       const institutionName = institution.name;
       return includes(term, toLower(userName)) ||
         includes(term, toLower(institutionName)) ||
@@ -382,7 +386,7 @@ export const getSearchFilterFunctions = () => {
         includes(term, toLower(eraCommonsId));
     }, targetList),
     signingOfficialResearchers: (term, targetList) => filter(researcher => {
-      const { displayName, eraCommonsId, email } = researcher;
+      const {displayName, eraCommonsId, email} = researcher;
       const roles = researcher.roles || [];
       const baseAttributes = [displayName, eraCommonsId, email];
       const includesRoles = roles.reduce((memo, current) => {
@@ -443,6 +447,7 @@ export const getSearchFilterFunctions = () => {
        * pre-populated with data use codes and translations
        */
       const loweredTerm = toLower(term);
+      const name = dataset.name;
       const alias = dataset.alias;
       const identifier = dataset.datasetIdentifier;
       const allPropValues = dataset.properties.map((p) => p.propertyValue).join('');
@@ -453,10 +458,44 @@ export const getSearchFilterFunctions = () => {
           : 'rejected'
         : 'yes no';
       return includes(loweredTerm, toLower(alias)) ||
-          includes(loweredTerm, toLower(identifier)) ||
-          includes(loweredTerm, toLower(allPropValues)) ||
-          includes(loweredTerm, toLower(dataset.codeList)) ||
-          includes(loweredTerm, toLower(status));
+        includes(loweredTerm, toLower(name)) ||
+        includes(loweredTerm, toLower(identifier)) ||
+        includes(loweredTerm, toLower(allPropValues)) ||
+        includes(loweredTerm, toLower(dataset.codeList)) ||
+        includes(loweredTerm, toLower(status));
+    }, targetList),
+    datasetTerms: (term, targetList) => filter(datasetTerm => {
+      /**
+       * This filter function is intended for Dataset Index Terms
+       */
+      const loweredTerm = toLower(term);
+      // Approval status
+      const status = !isNil(datasetTerm.dacApproval)
+        ? datasetTerm.dacApproval
+          ? 'accepted'
+          : 'rejected'
+        : 'pending';
+      const primaryCodes = datasetTerm.dataUse?.primary.map(du => du.code);
+      const secondaryCodes = datasetTerm.dataUse?.secondary.map(du => du.code);
+      const codes = join(', ')(concat(primaryCodes)(secondaryCodes));
+      const dataTypes = join(', ')(datasetTerm.study?.dataTypes);
+      const custodians = join(', ')(datasetTerm.study?.dataCustodianEmail);
+      return includes(loweredTerm, toLower(datasetTerm.datasetName)) ||
+        includes(loweredTerm, toLower(datasetTerm.datasetIdentifier)) ||
+        includes(loweredTerm, toLower(datasetTerm.dacName)) ||
+        includes(loweredTerm, toLower(datasetTerm.dataLocation)) ||
+        includes(loweredTerm, toLower(codes)) ||
+        includes(loweredTerm, toLower(datasetTerm.createUserDisplayName)) ||
+        includes(loweredTerm, toLower(datasetTerm.url)) ||
+        includes(loweredTerm, toLower(datasetTerm.study?.description)) ||
+        includes(loweredTerm, toLower(datasetTerm.study?.dataSubmitterEmail)) ||
+        includes(loweredTerm, toLower(dataTypes)) ||
+        includes(loweredTerm, toLower(custodians)) ||
+        includes(loweredTerm, toLower(datasetTerm.study?.phenotype)) ||
+        includes(loweredTerm, toLower(datasetTerm.study?.piName)) ||
+        includes(loweredTerm, toLower(datasetTerm.study?.species)) ||
+        includes(loweredTerm, toLower(datasetTerm.study?.studyName)) ||
+        includes(loweredTerm, toLower(status));
     }, targetList),
   };
 };
@@ -466,13 +505,13 @@ export const tableSearchHandler = (list, setFilteredList, setCurrentPage, modelN
   return (searchTerms) => {
     const rawSearchTerms = getOr(searchTerms, 'current.value', searchTerms);
     const searchTermValues = toLower(rawSearchTerms).split(/\s|,/);
-    if(isEmpty(searchTermValues)) {
+    if (isEmpty(searchTermValues)) {
       setFilteredList(list);
     } else {
       let newFilteredList = cloneDeep(list);
       lodashFPForEach((splitTerm) => {
         const term = splitTerm.trim();
-        if(!isEmpty(term)) {
+        if (!isEmpty(term)) {
           const filterFn = filterFnMap[modelName];
           newFilteredList = filterFn(term, newFilteredList);
         }
@@ -487,7 +526,7 @@ export const searchOntologies = (query, callback) => {
   let options = [];
   DAR.getAutoCompleteOT(query).then(
     items => {
-      options = items.map(function(item) {
+      options = items.map(function (item) {
         return {
           key: item.id,
           value: item.id,
@@ -500,7 +539,7 @@ export const searchOntologies = (query, callback) => {
 };
 
 export const setStyle = (disabled, baseStyle, targetColorAttribute) => {
-  let appliedStyle = disabled ? {[targetColorAttribute] : Theme.palette.disabled} : {};
+  let appliedStyle = disabled ? {[targetColorAttribute]: Theme.palette.disabled} : {};
   try {
     return Object.assign(baseStyle, appliedStyle);
   } catch (e) {
@@ -510,12 +549,12 @@ export const setStyle = (disabled, baseStyle, targetColorAttribute) => {
 
 export const setDivAttributes = (disabled, onClick, style, dataTip, onMouseEnter, onMouseLeave, key) => {
   let attributes;
-  if(!disabled) {
+  if (!disabled) {
     attributes = {onClick, onMouseEnter, onMouseLeave, style, 'data-tip': dataTip, key, id: key};
   } else {
     attributes = {style, disabled, 'data-tip': dataTip, key};
   }
-  if(!isEmpty(dataTip)) {
+  if (!isEmpty(dataTip)) {
     attributes['data-tip'] = dataTip;
   }
   return attributes;
@@ -524,12 +563,11 @@ export const setDivAttributes = (disabled, onClick, style, dataTip, onMouseEnter
 //each item in the list is an array of metadata representing a single table row
 //the metadata for each cell needs a data (exactly what is displayed in the table)
 //or value (string or number alternative) property which determines sorting
-export const sortVisibleTable = ({ list = [], sort }) => {
+export const sortVisibleTable = ({list = [], sort}) => {
   // Sort: { dir, colIndex }
   if (!sort || sort.colIndex === undefined) {
     return list;
-  }
-  else {
+  } else {
     return list.sort((a, b) => {
       const aVal = a[sort.colIndex].value || a[sort.colIndex].data;
       const bVal = b[sort.colIndex].value || b[sort.colIndex].data;
@@ -539,7 +577,7 @@ export const sortVisibleTable = ({ list = [], sort }) => {
         if (aVal === null || bVal === null) {
           return (aVal > bVal ? -1 : 1) * sort.dir;
         } else {
-          return (aVal.localeCompare(bVal, 'en', { sensitivity: 'base', numeric: true }) * sort.dir);
+          return (aVal.localeCompare(bVal, 'en', {sensitivity: 'base', numeric: true}) * sort.dir);
         }
       }
     });
@@ -553,7 +591,7 @@ export const recalculateVisibleTable = async ({
   try {
     // Sort data before applying paging
     if (sort) {
-      filteredList = sortVisibleTable({ list: filteredList, sort });
+      filteredList = sortVisibleTable({list: filteredList, sort});
     }
 
     // Set paging variables and truncate the list
@@ -568,13 +606,13 @@ export const recalculateVisibleTable = async ({
     );
     setVisibleList(visibleList);
   } catch (error) {
-    Notifications.showError({ text: 'Error updating table' });
+    Notifications.showError({text: 'Error updating table'});
   }
 };
 
 export const searchOnFilteredList = (searchTerms, originalList, filterFn, setFilteredList) => {
   let searchList = (!isNil(originalList) ? [...originalList] : []);
-  if(!isEmpty(searchTerms)) {
+  if (!isEmpty(searchTerms)) {
     const terms = searchTerms.split(' ');
     lodashFPForEach((term => searchList = filterFn(term, searchList)))(terms);
   }
@@ -595,6 +633,6 @@ export const getBooleanFromEventHtmlDataValue = (e) => {
 
 export const hasDataSubmitterRole = (user) => {
   const roles = get('roles')(user);
-  const dsRole = find({'roleId':8})(roles);
+  const dsRole = find({'roleId': 8})(roles);
   return !isNil(dsRole);
 };
