11f46e5e404ce12d53de7d10c5954cec3cafae74
Hamsini Malli Sivakumar <hmallisi@broadinstitute.org>
2023-09-21 10:11:05 -0400

    added separate component

--- src/components/SigningOfficialDaaAgreementWrapper.js
+++ src/components/SigningOfficialDaaAgreementWrapper.js
@@ -8,6 +8,7 @@ import { useEffect, useState } from 'react';
 import { spinnerService } from '../libs/spinner-service';
 import { isNil, isNull } from 'lodash';
 import { Styles } from '../libs/theme';
+import UsgOmbText from './UsgOmbText';
 
 export const SigningOfficialDaaAgreementWrapper = (props) => {
   const {
@@ -104,11 +105,7 @@ export const ensureSoHasDaaAcknowledgement = (component, isLibraryCardIssueTable
       h(SigningOfficialDaaAgreementWrapper, { isDataSubmitterTab }, [
         h(component, props),
       ]),
-      isLibraryCardIssueTable && div({ style: { marginTop: '50px', background: '#eee', padding: '20px' } }, [
-        p({}, ['OMB No.: 0925-7775']),
-        p({}, ['Expiration Date: 06/30/2025']),
-        p({}, ['Public reporting burden for this collection of information is estimated to average 45 minutes per response, including the time for reviewing instructions, searching existing data sources, gathering and maintaining the data needed, and completing and reviewing the collection of information. An agency may not conduct or sponsor, and a person is not required to respond to, a collection of information unless it displays a currently valid OMB control number. Send comments regarding this burden estimate or any other aspect of this collection of information, including suggestions for reducing this burden to: NIH, Project Clearance Branch, 6705 Rockledge Drive, MSC 7974, Bethesda, MD 20892-7974, ATTN: PRA (0925-7775). Do not return the completed form to this address.']),
-      ])
+      isLibraryCardIssueTable && UsgOmbText()
     ]));
 };
 
--- src/pages/DataSubmissionForm.js
+++ src/pages/DataSubmissionForm.js
@@ -18,6 +18,7 @@ import NihAnvilUse from '../components/data_submission/NihAnvilUse';
 // schama changes, then run `npm run genschemas` to regenerate this code
 import validateSchema from '../assets/schemas/DataRegistrationV1Validation';
 import { set } from 'lodash';
+import UsgOmbText from '../components/UsgOmbText';
 
 export const DataSubmissionForm = (props) => {
   const {
@@ -184,11 +185,7 @@ export const DataSubmissionForm = (props) => {
         </div>
       </form>
     </div>}
-    <div style={{ marginTop: '20px', background: '#eee', padding: '20px' }}>
-      <p>OMB No.: 0925-7775</p>
-      <p>Expiration Date: 06/30/2025</p>
-      <p>Public reporting burden for this collection of information is estimated to average 45 minutes per response, including the time for reviewing instructions, searching existing data sources, gathering and maintaining the data needed, and completing and reviewing the collection of information. An agency may not conduct or sponsor, and a person is not required to respond to, a collection of information unless it displays a currently valid OMB control number. Send comments regarding this burden estimate or any other aspect of this collection of information, including suggestions for reducing this burden to: NIH, Project Clearance Branch, 6705 Rockledge Drive, MSC 7974, Bethesda, MD 20892-7974, ATTN: PRA (0925-7775). Do not return the completed form to this address.</p>
-    </div>
+    <UsgOmbText />
   </div>;
 };
 
