16567f55a749abf5a57981495f81fb7eeabd9c3d
Greg Rushton <grushton@broadinstitute.org>
2023-10-25 08:18:49 -0400

    rm unused

--- src/libs/ajax.js
+++ src/libs/ajax.js
@@ -289,12 +289,6 @@ export const DataSet = {
     return await res.data;
   },
 
-  getRegistrationSchema: async () => {
-    const url = `${await getApiUrl()}/schemas/dataset-registration/v1`;
-    const res = await axios.get(url, Config.authOpts());
-    return await res.data;
-  },
-
   postDatasetForm: async (form) => {
     const url = `${await getApiUrl()}/api/dataset/v2`;
     const res = await fetchOk(url, fp.mergeAll([Config.authOpts(), Config.jsonBody(form), { method: 'POST' }]));
