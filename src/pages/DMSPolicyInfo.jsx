540e8c6748864e83f4f4bbe392787b7c48d329fd
Hamsini Malli <hmallisi@broadinstitute.org>
2023-10-26 13:10:26 -0400

    [DUOS-2723] Update Policy Pages (#2366)

--- src/pages/DMSPolicyInfo.jsx
+++ src/pages/DMSPolicyInfo.jsx
@@ -96,8 +96,8 @@ function DMSPolicyInfo(props) {
       subtitleImage = subtitleImageAnvil;
       subtitleText = (
         <p>
-          NHGRI&apos;s <b>AnVIL ecosystem</b> can be branded and customized to help your institution easily meet DMS requirements while maintaining
-          ownership and gaining better visibility of your institution&apos;s scientific data.
+          NHGRI&apos;s <b>AnVIL ecosystem</b> can help your institution easily meet DMS requirements while maintaining ownership and gaining better visibility of
+          your institution&apos;s scientific data.
         </p>
       );
       storeDataTitle = 'Store Data';
@@ -125,9 +125,12 @@ function DMSPolicyInfo(props) {
         <div className="section-text">
           <div className="centered" style={Styles.TITLE}>
             Meet NIH&apos;s 2023 Data Management & Sharing (DMS) Policy requirements for your institution
+            {variant === 'NIH' ? '' : ' with AnViL'}
           </div>
           <div className="centered" style={Styles.SUB_HEADER}>
-            An easy solution for grant compliance and research teams
+            An easy
+            {variant === 'NIH' ? '' : ' data sharing'}
+            &nbsp;solution for grant compliance and research teams
           </div>
         </div>
       </div>
@@ -172,8 +175,8 @@ function DMSPolicyInfo(props) {
               <div>
                 <p>
                   <b>DUOS</b> enables investigators to catalog data for search and publish in a publicly accessible catalog, including adding
-                  data use limitations and assigning to a <b>DAC - Data Access Committee</b>. When finished, investigators&apos; datasets will a have
-                  unique ID for their dataset which can be findable in the catalog by secondary researchers, satisfying DMS requirements for public sharing.
+                  data use limitations and assigning to a <b>DAC - Data Access Committee</b> if needed. When finished, investigators&apos; datasets will a have
+                  unique ID for their dataset, which can be findable in the catalog by secondary researchers, satisfying DMS requirements for public sharing.
                 </p>
               </div>
             </Grid>
@@ -189,15 +192,23 @@ function DMSPolicyInfo(props) {
                 Manage Access
               </div>
               <div>
-                <p>
-                  In <b>DUOS</b>, institutions are able to set up their own central DAC, allow investigators to create and administer DACs themselves, or
-                  leverage DUOS&apos; internal DAC for a fee.
-                </p>
+
+                {
+                  variant === 'NIH' ?
+                    <p>
+                      In <b>DUOS</b>, institutions are able to set up their own central DAC, allow investigators to create and administer DACs themselves, or
+                      leverage DUOS&apos; internal DAC for a fee.
+                    </p> :
+                    <p><b>DUOS</b> can register open-access and controlled-access datasets, regardless of the storage location, or if the DAC received
+                      requests via DUOS or another mechanism.</p>
+                }
+
 
                 <p>
                   DUOS offers a consistent, simple user interfaces for DACs to manage and respond to request, in addition to maintain auditable records,
                   providing institution level stats use of your research data and datasets shared publicly to ensure DMS Policy compliance.
                 </p>
+
               </div>
             </Grid>
             <Grid item xs={12} sm={6} className="vertical-centered">
