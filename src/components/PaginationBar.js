3ffdb8e9b88bae1f9a477818d5eeca759ed363af
Florian Boulnois <fboulnoi@broadinstitute.org>
2023-11-13 14:31:36 -0500

    refactor: switch PaginationBar to JSX

--- src/components/PaginationBar.js
+++ src/components/PaginationBar.js
@@ -1,4 +1,4 @@
-import { div, input, span } from 'react-hyperscript-helpers';
+import React from 'react';
 import { useRef, useEffect } from 'react';
 import { toNumber } from 'lodash';
 import {Styles, Theme} from '../libs/theme';
@@ -25,49 +25,41 @@ export default function PaginationBar(props) {
   }, [props.currentPage, props.tableSize]);
 
   return (
-    div({style: Styles.TABLE.FOOTER}, [
-      div({style: Styles.TABLE.PAGINATION_SECTION_OFFSET}),
-      div({style: Styles.TABLE.PAGINATION_BUTTON_SECTION}, [
-        div({
-          style: Styles.TABLE.PAGINATION_BUTTON
-        }, [
-          span({
-            onClick: () => goToPage(toNumber(currentPage.current.value) - 1),
-            onMouseEnter: applyTextHover,
-            onMouseLeave: (e) => removeTextHover(e, Theme.palette.primary),
-          },['Prev']),
-        ]),
-        div({style: Styles.TABLE.PAGINATION_CURRENT_PAGE}, [
-          span({},['Page ']),
-          input({
-            onChange: () => goToPage(toNumber(currentPage.current.value)),
-            type: 'text',
-            ref: currentPage,
-            defaultValue: props.currentPage,
-            style: Styles.TABLE.PAGINATION_INPUT
-          }),
-          span({}, [` of ${pageCount}`])
-        ]),
-        div({
-          style: Styles.TABLE.PAGINATION_BUTTON
-        }, [
-          span({
-            onClick: () => goToPage(toNumber(currentPage.current.value) + 1),
-            onMouseEnter: applyTextHover,
-            onMouseLeave: (e) => removeTextHover(e, '#1f3b50')
-          }, ['Next'])
-        ]),
-      ]),
-      div({style: Styles.TABLE.PAGINATION_TABLE_SIZE_SECTION}, [
-        span({style: {marginRight: '2%'}}, ['Rows per page: ']),
-        input({
-          onChange: () => changeTableSize(tableSize.current.value),
-          type: 'text',
-          ref: tableSize,
-          defaultValue: props.tableSize,
-          style: Styles.TABLE.PAGINATION_INPUT
-        })
-      ])
-    ])
+    <div style={Styles.TABLE.FOOTER}>
+      <div style={Styles.TABLE.PAGINATION_SECTION_OFFSET}></div>
+      <div style={Styles.TABLE.PAGINATION_BUTTON_SECTION}>
+        <div style={Styles.TABLE.PAGINATION_BUTTON}>
+          <span onClick={() => goToPage(toNumber(currentPage.current.value) - 1)}
+                onMouseEnter={applyTextHover}
+                onMouseLeave={(e) => removeTextHover(e, Theme.palette.primary)}>
+            Prev
+          </span>
+        </div>
+        <div style={Styles.TABLE.PAGINATION_CURRENT_PAGE}>
+          <span>Page </span>
+          <input onChange={() => goToPage(toNumber(currentPage.current.value))}
+                 type="text"
+                 ref={currentPage}
+                 defaultValue={props.currentPage}
+                 style={Styles.TABLE.PAGINATION_INPUT} />
+          <span> of {pageCount}</span>
+        </div>
+        <div style={Styles.TABLE.PAGINATION_BUTTON}>
+          <span onClick={() => goToPage(toNumber(currentPage.current.value) + 1)}
+                onMouseEnter={applyTextHover}
+                onMouseLeave={(e) => removeTextHover(e, '#1f3b50')}>
+            Next
+          </span>
+        </div>
+      </div>
+      <div style={Styles.TABLE.PAGINATION_TABLE_SIZE_SECTION}>
+        <span style={{marginRight: '2%'}}>Rows per page: </span>
+        <input onChange={() => changeTableSize(tableSize.current.value)}
+               type="text"
+               ref={tableSize}
+               defaultValue={props.tableSize}
+               style={Styles.TABLE.PAGINATION_INPUT} />
+      </div>
+    </div>
   );
 }
