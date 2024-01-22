182c859f9f0bb271a1e339a666b404021b61dc28
Florian Boulnois <fboulnoi@broadinstitute.org>
2024-01-22 17:50:15 -0500

    refactor: switch TableSkeletonLoader to JSX

--- src/components/TableSkeletonLoader.js
+++ src/components/TableSkeletonLoader.js
@@ -1,8 +1,7 @@
-import { div } from 'react-hyperscript-helpers';
+import React from 'react';
 import { Styles } from '../libs/theme';
 
 export default function TableSkeletonLoader(props) {
-
   const { tableHeaderTemplate, tableRowLoadingTemplate } = props;
 
   const blockStyleOverwrite = {
@@ -10,29 +9,31 @@ export default function TableSkeletonLoader(props) {
     height: '48px'
   };
 
-  const tableRowStyle = Object.assign({}, Styles.TABLE.RECORD_ROW, blockStyleOverwrite);
-  const modifiedTableRowStyle = {...tableRowStyle, borderTop: '1px solid rgba(109, 110, 112, 0.2)'};
+  const tableRowStyle = { ...Styles.TABLE.RECORD_ROW, ...blockStyleOverwrite };
+  const modifiedTableRowStyle = { ...tableRowStyle, borderTop: '1px solid rgba(109, 110, 112, 0.2)' };
 
   const generateLoaderTemplate = () => {
     let start = 1;
     const end = 10;
 
-    const template = [div({
-      style: Styles.TABLE.HEADER_ROW,
-    }, tableHeaderTemplate)];
+    const template = [
+      <div key={`row-loader-0`} style={Styles.TABLE.HEADER_ROW}>{tableHeaderTemplate}</div>
+    ];
 
-    while(start <= end) {
+    while (start <= end) {
       const targetStyle = start > 1 ? modifiedTableRowStyle : tableRowStyle;
-      const rowTemplate = div({style: targetStyle, key: `row-loader-${start}`}, tableRowLoadingTemplate);
+      const rowTemplate = (
+        <div style={targetStyle} key={`row-loader-${start}`}>
+          {tableRowLoadingTemplate}
+        </div>
+      );
       template.push(rowTemplate);
       start++;
     }
-    template.push(div({style: Styles.TABLE.FOOTER}));
+    template.push(<div style={Styles.TABLE.FOOTER} />);
 
     return template;
   };
 
-  return (
-    div({style: Styles.TABLE.CONTAINER}, generateLoaderTemplate())
-  );
+  return <div style={Styles.TABLE.CONTAINER}>{generateLoaderTemplate()}</div>;
 }
