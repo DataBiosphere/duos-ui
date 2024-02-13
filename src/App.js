149d5ec1f76a50b569aefb9d3748145bfc9e41d6
Gregory Rushton <rushtong@users.noreply.github.com>
2024-02-13 11:50:20 -0500

    [DUOS-2900][risk=no] Update to GA4 (#2459)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import ReactGA from 'react-ga';
+import ReactGA from 'react-ga4';
 import Modal from 'react-modal';
 import './App.css';
 import {Config} from './libs/config';
