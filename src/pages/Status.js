e73536765498c96c370bae940306337fca14ad79
Florian Boulnois <fboulnoi@broadinstitute.org>
2024-02-29 17:18:18 -0500

    refactor: switch Status to JSX

--- src/pages/Status.js
+++ src/pages/Status.js
@@ -1,12 +1,12 @@
+import React from 'react';
 import { getOr, isNil, map, uniq } from 'lodash/fp';
-import React, { Component } from 'react';
-import { a, div, h2, hh, hr, li, pre, ul } from 'react-hyperscript-helpers';
+import { Component } from 'react';
 import CheckboxMarkedCircleOutline from 'react-material-icon-svg/dist/CheckboxMarkedCircleOutline';
 import DiameterVariant from 'react-material-icon-svg/dist/DiameterVariant';
 import { getApiUrl, getOntologyUrl } from '../libs/ajax';
 
 
-export const Status = hh(class Status extends Component {
+class Status extends Component {
 
   constructor(props) {
     super(props);
@@ -50,17 +50,20 @@ export const Status = hh(class Status extends Component {
     const ontologyHealthy = this.isOntologyHealthy(this.state.ontologyStatus) ? healthyState : unhealthyState;
 
     return (
-      div({ style: { margin: '2rem' } }, [
-        ul({ style: { marginTop: '2rem', listStyle: 'none', fontSize: 'x-large' } }, [
-          li({}, [a({ href: '#consent' }, 'Consent'), ' ', consentHealthy]),
-          li({}, [a({ href: '#ontology' }, 'Ontology'), ' ', ontologyHealthy])
-        ]),
-        hr(),
-        h2({}, [a({ id: 'consent' }, ['Consent Status'])]),
-        pre({}, [JSON.stringify(this.state.consentStatus, null, 4)]),
-        h2({}, [a({ id: 'ontology' }, ['Ontology Status'])]),
-        pre({}, [JSON.stringify(this.state.ontologyStatus, null, 4)])
-      ]));
+      <div style={{ margin: '2rem' }}>
+        <ul style={{ marginTop: '2rem', listStyle: 'none', fontSize: 'x-large' }}>
+          <li><a href="#consent">Consent</a> {consentHealthy}</li>
+          <li><a href="#ontology">Ontology</a> {ontologyHealthy}</li>
+        </ul>
+        <hr />
+        <h2><a id="consent">Consent Status</a></h2>
+        <pre>{JSON.stringify(this.state.consentStatus, null, 4)}</pre>
+        <h2><a id="ontology">Ontology Status</a></h2>
+        <pre>{JSON.stringify(this.state.ontologyStatus, null, 4)}</pre>
+      </div>
+    );
   }
 
-});
+};
+
+export default Status;
