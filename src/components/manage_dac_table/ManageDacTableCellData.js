3d6a86b67c84c77f9bc8207e367722d648968a44
Florian Boulnois <fboulnoi@broadinstitute.org>
2024-03-01 10:06:09 -0500

    refactor: switch ManageDacTableCellData to JSX

--- src/components/manage_dac_table/ManageDacTableCellData.js
+++ src/components/manage_dac_table/ManageDacTableCellData.js
@@ -1,6 +1,6 @@
+import React from 'react';
 import {isNil, isEmpty} from 'lodash/fp';
 import {styles} from './ManageDacTable';
-import {a, label, h, div} from 'react-hyperscript-helpers';
 import TableIconButton from '../../components/TableIconButton';
 import {Styles} from '../../libs/theme';
 import {Delete, Edit} from '@mui/icons-material';
@@ -33,14 +33,17 @@ export function descriptionCellData({description = '- -', dacId, label= 'dac-des
 }
 
 
-export function datasetsCellData({dac, viewDatasets}) {
+export function datasetsCellData({dac, viewDatasets, label='dac-datasets'}) {
   return {
-    data: a({
-      id: dac.dacId + '_dacDatasets',
-      name: 'dacDatasets',
-      className: '',
-      onClick: () => viewDatasets(dac)
-    }, [`View Datasets`]),
+    data: (
+      <a
+        id={dac.dacId + '_dacDatasets'}
+        name='dacDatasets'
+        onClick={() => viewDatasets(dac)}
+      >
+        View Datasets
+      </a>
+    ),
     id: dac.dacId,
     style : {
       color: '#354052',
@@ -55,26 +58,27 @@ export function actionsCellData({dac, editDac, deleteDac, userRole}) {
   const isAdmin = (userRole === 'Admin');
   const deleteDisabled = (!isNil(dac.datasets) && !isEmpty(dac.datasets));
 
-  const actions = [
-    h(TableIconButton, {
-      key: 'edit-dac-icon',
-      dataTip: 'Edit DAC',
-      onClick: () => editDac(dac),
-      icon: Edit,
-      style: Object.assign({}, Styles.TABLE.TABLE_ICON_BUTTON),
-      hoverStyle: Object.assign({}, Styles.TABLE.TABLE_BUTTON_ICON_HOVER)
-    }),
-    h(TableIconButton, {
-      key: 'delete-dac-icon',
-      dataTip: (deleteDisabled?'All datasets assigned to this DAC must be reassigned before this can be deleted' :'Delete DAC'),
-      disabled: deleteDisabled,
-      isRendered: isAdmin,
-      onClick: () => deleteDac(dac),
-      icon: Delete,
-      style: Object.assign({}, Styles.TABLE.TABLE_ICON_BUTTON),
-      hoverStyle: Object.assign({}, Styles.TABLE.TABLE_BUTTON_ICON_HOVER)
-    })
-  ];
+  const actions = (
+    <>
+      <TableIconButton
+        key='edit-dac-icon'
+        dataTip='Edit DAC'
+        onClick={() => editDac(dac)}
+        icon={Edit}
+        style={Object.assign({}, Styles.TABLE.TABLE_ICON_BUTTON)}
+        hoverStyle={Object.assign({}, Styles.TABLE.TABLE_BUTTON_ICON_HOVER)}
+      />
+      {isAdmin && <TableIconButton
+        key='delete-dac-icon'
+        dataTip={(deleteDisabled?'All datasets assigned to this DAC must be reassigned before this can be deleted' :'Delete DAC')}
+        disabled={deleteDisabled}
+        onClick={() => deleteDac(dac)}
+        icon={Delete}
+        style={Object.assign({}, Styles.TABLE.TABLE_ICON_BUTTON)}
+        hoverStyle={Object.assign({}, Styles.TABLE.TABLE_BUTTON_ICON_HOVER)}
+      />}
+    </>
+  );
 
   return {
     isComponent: true,
@@ -84,11 +88,11 @@ export function actionsCellData({dac, editDac, deleteDac, userRole}) {
       fontSize: styles.fontSize.actions
     },
     label: 'table-actions',
-    data: div({
-      style: {
-        display: 'flex',
-      }
-    }, actions)
+    data: (
+      <div style={{ display: 'flex' }}>
+        {actions}
+      </div>
+    )
   };
 }
 
