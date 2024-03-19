6c716ceb45f06f1d958090b530174e18fefc93a7
Florian Boulnois <fboulnoi@broadinstitute.org>
2024-03-19 10:44:47 -0400

    refactor: switch DataAccessRequestApplication ext to JSX

--- src/pages/dar_application/DataAccessRequestApplication.jsx
+++ src/pages/dar_application/DataAccessRequestApplication.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import ResearcherInfo from './ResearcherInfo';
 import DataUseAgreements from './DataUseAgreements';
 import DataAccessRequest from './DataAccessRequest';
