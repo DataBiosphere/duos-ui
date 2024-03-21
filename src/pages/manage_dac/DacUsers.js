f3eeb2cde3bacd1c835a4176548657381612959b
PintoGideon <gideonpinto123@gmail.com>
2024-03-20 22:40:25 -0400

    refactor: Refactor the DacUsers component

--- src/pages/manage_dac/DacUsers.js
+++ src/pages/manage_dac/DacUsers.js
@@ -1,82 +1,74 @@
 import * as ld from 'lodash';
-import { Component } from 'react';
-import { a, div, hh } from 'react-hyperscript-helpers';
+import React, { useState } from 'react';
 import { CHAIR, MEMBER } from './AddDacModal';
-
 
 const buttonPadding = { paddingTop: 6 };
 const headerStyle = { fontWeight: 500, color: '#00609f' };
 
-export const DacUsers = hh(class DacUsers extends Component {
+export const DacUsers = (props) => {
+  const [state, setState] = useState({
+    dac: props.dac,
+    removeButton: props.removeButton,
+    removeHandler: props.removeButton ? props.removeHandler : () => { },
+    removedIds: []
+  });
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      dac: props.dac,
-      removeButton: props.removeButton,
-      removeHandler: props.removeButton ? props.removeHandler : () => {},
-      removedIds: []
-    };
-  }
-
-  onRemove = (dacId, userId, role) => {
-    if (this.state.removedIds.includes(userId)) {
-      this.setState(prev => {
-        prev.removedIds = ld.difference(prev.removedIds, [userId]);
-        return prev;
-      });
+  const onRemove = (dacId, userId, role) => {
+    if (state.removedIds.includes(userId)) {
+      setState(prev => ({
+        ...prev,
+        removeIds: ld.difference(prev.removeIds,[userId])
+      }));
     } else {
-      this.setState(prev => {
-        prev.removedIds = ld.union(prev.removedIds, [userId]);
-        return prev;
-      });
+      setState(prev => ({
+        ...prev,
+        removeIds: ld.union(prev.removeIds, [userId])
+      }));
     }
-    this.state.removeHandler(dacId, userId, role);
+    state.removeHandler(dacId, userId, role);
   };
 
-  columnClass = () => {
-    return this.state.removeButton ? 'col-md-4' : 'col-md-6';
+  const columnClass = () => {
+    return state.removeButton ? 'col-md-4' : 'col-md-6';
   };
 
-  makeRow = (u, role) => {
+  const makeRow = (u, role) => {
     const roleTitle = (role === CHAIR) ? 'Chairperson' : 'Member';
-    const isRemoved = this.state.removedIds.includes(u.userId);
+    const isRemoved = state.removedIds.includes(u.userId);
     const rowStyle = isRemoved ?
       { borderBottom: '1px solid white', padding: '.75rem 0 .75rem 0', backgroundColor: 'lightgray', opacity: .5, borderRadius: 5 } :
-      { borderBottom: '1px solid lightgray', padding: '.75rem 0 .75rem 0' }
-    ;
+      { borderBottom: '1px solid lightgray', padding: '.75rem 0 .75rem 0' };
     const buttonMessage = isRemoved ? 'Pending Removal' : 'Remove';
-    return div({ key: 'chair_' + u.userId, className: 'row', style: rowStyle }, [
-      div({ className: this.columnClass() }, [u.displayName + ' ' + u.email]),
-      div({ className: this.columnClass() }, [roleTitle]),
-      div({
-        isRendered: this.state.removeButton,
-        style: buttonPadding,
-        className: this.columnClass()
-      }, [
-        a({
-          style: { display: 'inline' },
-          role: 'button',
-          onClick: () => this.onRemove(this.state.dac.dacId, u.userId, role),
-          className: 'btn cell-button cancel-color'
-        }, [buttonMessage])
-      ])
-    ]);
+    return (
+      <div key={'chair_' + u.userId} className="row" style={rowStyle}>
+        <div className={columnClass()}>{u.displayName + ' ' + u.email}</div>
+        <div className={columnClass()}>{roleTitle}</div>
+        {state.removeButton &&
+          <div style={buttonPadding} className={columnClass()}>
+            <button
+              style={{ display: 'inline' }}
+              onClick={() => onRemove(state.dac.dacId, u.userId, role)}
+              className="btn cell-button cancel-color"
+            >
+              {buttonMessage}
+            </button>
+          </div>
+        }
+      </div>
+    );
   };
 
-  render() {
-    return div({ style: {}, className: 'container-fluid' }, [
-      div({ className: 'row' }, [
-        div({ style: headerStyle, className: this.columnClass() }, 'User'),
-        div({ style: headerStyle, className: this.columnClass() }, 'Role'),
-        div({
-          isRendered: this.state.removeButton,
-          style: { ...headerStyle, ...buttonPadding },
-          className: this.columnClass()
-        }, '')
-      ]),
-      ld.flatMap(this.state.dac.chairpersons, (u) => { return this.makeRow(u, CHAIR); }),
-      ld.flatMap(this.state.dac.members, (u) => { return this.makeRow(u, MEMBER); })
-    ]);
-  }
-});
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div style={headerStyle} className={columnClass()}>User</div>
+        <div style={headerStyle} className={columnClass()}>Role</div>
+        {state.removeButton &&
+          <div style={{ ...headerStyle, ...buttonPadding }} className={columnClass()}></div>
+        }
+      </div>
+      {ld.flatMap(state.dac.chairpersons, (u) => makeRow(u, CHAIR))}
+      {ld.flatMap(state.dac.members, (u) => makeRow(u, MEMBER))}
+    </div>
+  );
+}
