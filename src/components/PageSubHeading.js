8d4582433bbed4c39d225f44cefb9e5eca46475f
PintoGideon <gideonpinto123@gmail.com>
2024-03-23 15:54:15 -0400

    refactor: Refactor PageSubHeading

--- src/components/PageSubHeading.js
+++ src/components/PageSubHeading.js
@@ -1,18 +1,14 @@
-import {Component} from 'react';
-import {div, hh, img, h2, span} from 'react-hyperscript-helpers';
+import React from 'react';
 
-export const PageSubHeading = hh(class PageSubHeading extends Component {
-
-  margins(iconSize) {
-
+export const PageSubHeading = (props) => {
+  const { id, imgSrc, title, description, color, iconSize } = props;
+  const margins = (iconSize) => {
     const MEDIUM = {
       marginLeft: '45px'
     };
-
     const LARGE = {
       marginLeft: '55px'
     };
-
     const NONE = {
       marginLeft: '0'
     };
@@ -25,57 +21,50 @@ export const PageSubHeading = hh(class PageSubHeading extends Component {
     } else {
       return MEDIUM;
     }
-  }
+  };
 
-  render() {
+  const HEADING = {
+    width: '100%',
+    margin: '20px 0 10px 0',
+    position: 'relative'
+  };
 
-    const HEADING = {
-      width: '100%',
-      margin: '20px 0 10px 0',
-      position: 'relative'
-    };
+  const DESCRIPTION = {
+    color: '#000000',
+    fontSize: '16px',
+    fontWeight: '400'
+  };
 
-    const DESCRIPTION = {
-      color: '#000000',
-      fontSize: '16px',
-      fontWeight: '400'
-    };
+  const ICON = {
+    position: 'absolute',
+    top: '0',
+    left: '0',
+    height: '40px'
+  };
 
-    const ICON = {
-      position: 'absolute',
-      top: '0',
-      left: '0',
-      height: '40px'
-    };
+  const TITLE = {
+    margin: '10px 0 5px 0',
+    lineBreak: 'auto',
+    padding: '5px 10px 0 0',
+    fontSize: '22px',
+    fontWeight: '500',
+  };
 
-    const TITLE = {
-      margin: '10px 0 5px 0',
-      lineBreak: 'auto',
-      padding: '5px 10px 0 0',
-      fontSize: '22px',
-      fontWeight: '500',
-    };
+  const MARGINS = margins(iconSize);
 
-    const MARGINS = this.margins(this.props.iconSize);
-
-    return div({style: HEADING}, [
-      img({
-        id: this.props.id + '_icon',
-        isRendered: this.props.imgSrc !== undefined,
-        src: this.props.imgSrc,
-        alt: this.props.title,
-        style: ICON
-      }),
-      div({style: MARGINS}, [
-        h2({
-          id: this.props.id + '_title',
-          className: this.props.color + '-color',
-          style: TITLE
-        }, [this.props.title]),
-        span({id: this.props.id + '_description', style: DESCRIPTION}, [this.props.description]),
-      ])
-    ]);
-  }
-
-})
-;
+  return (
+    <div style={HEADING}>
+      {
+        imgSrc && <img id={id + '_icon'} src={imgSrc} alt={title} style={ICON} />
+      }
+      <div style={MARGINS}>
+        <h2 id={id + '_title'} className={color + '-color'} style={TITLE} >
+          {title}
+        </h2>
+        <span id={id + '_description'} style={DESCRIPTION}>
+          {description}
+        </span>
+      </div>
+    </div>
+  );
+};
