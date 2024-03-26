b33bf24a31b3ec9c4ff264e4750dc8f4985f8369
Sebastian Kobori <sjkobori@gmail.com>
2024-03-26 10:21:18 -0400

    Added oidcBroker and populated configs REBASE

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,30 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 // jquery is needed for bootstrap
-import 'jquery/src/jquery';
-import 'bootstrap/dist/js/bootstrap.min';
+import "jquery/src/jquery";
+import "bootstrap/dist/js/bootstrap.min";
 
-import React from 'react';
-import {createRoot} from 'react-dom/client';
-import './index.css';
-import App from './App';
-import {unregister} from './registerServiceWorker';
-import {BrowserRouter} from 'react-router-dom';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { unregister } from "./registerServiceWorker";
+import { BrowserRouter } from "react-router-dom";
+import { OidcBroker } from "./libs/oidcBroker";
+import { AuthProvider } from "react-oidc-context";
 
 const load = async () => {
-    unregister();
-    const container = document.getElementById('root');
-    const root = createRoot(container!);
-    root.render(<BrowserRouter><App/></BrowserRouter>);
+  unregister();
+  OidcBroker.initializeAuth();
+  const container = document.getElementById("root");
+  const root = createRoot(container!);
+  root.render(
+    <BrowserRouter>
+      <AuthProvider {...OidcBroker.getOidcUserManagerSettings()}>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  );
 };
 
 await load();
