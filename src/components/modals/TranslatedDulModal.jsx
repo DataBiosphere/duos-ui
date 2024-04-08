8b841ed90c1a4f8d65dc58f6e894b61986f5c219
PintoGideon <gideonpinto123@gmail.com>
2024-04-08 18:24:05 -0400

    feat: Updated the TranslatedDulModal component to use Dialog from material-ui

--- src/components/modals/TranslatedDulModal.jsx
+++ src/components/modals/TranslatedDulModal.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { UpdatedBaseModal } from '../UpdatedBaseModal';
+import {
+  Dialog, DialogTitle, DialogContent, DialogActions, Button, IconButton
+} from '@mui/material';
+import { Close as CloseIcon } from '@mui/icons-material';
 import { useState, useEffect } from 'react';
 import isEmpty from 'lodash/fp/isEmpty';
 
-
-const MODAL_ID = 'translatedDulModal';
-
 const listStyle = {
-  listStyle: 'none'
+  listStyle: 'none',
+  padding: '0',
+  fontSize:'1rem'
 };
 
 //NOTE: li partial can be used in components that only need the list
@@ -48,19 +50,36 @@ export default function TranslatedDulModal(props) {
   }, [dataUse]);
 
   return (
-    <UpdatedBaseModal
-      id={MODAL_ID}
-      showModal={showModal}
-      onRequestClose={closeHandler}
-      color='dataset'
-      type='informative'
-      iconSize='none'
-      title='Data Use Terms'
-      action={{ label: 'Close', handler: closeHandler }}
+    <Dialog
+      open={showModal}
+      onClose={closeHandler}
+      aria-labelledby='Data Use Terms'
+      aria-describedby='Dialog Description'
+      sx={{ transform: 'scale(1.5)' }}
     >
-      <ul key='dulUnorderedList' style={listStyle} id="txt_translatedRestrictions" className="row no-margin translated-restriction">
-        {translatedDulList}
-      </ul>
-    </UpdatedBaseModal>
+      <DialogTitle id='dialog-title'>
+        <span style={{ color: '#1976d2' }}>Data Use Terms</span>
+        <IconButton
+          aria-label="close"
+          onClick={closeHandler}
+          sx={{
+            position: 'absolute',
+            right: 8,
+            top: 8,
+            color: (theme) => theme.palette.grey[500],
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
+      </DialogTitle>
+      <DialogContent id='dialog-content'>
+        <ul key='dulUnorderedList' style={listStyle} id="txt_translatedRestrictions" className="row no-margin translated-restriction">
+          {translatedDulList}
+        </ul>
+      </DialogContent>
+      <DialogActions id='dialog-footer'>
+        <Button onClick={closeHandler} variant='outlined'>Close</Button>
+      </DialogActions>
+    </Dialog>
   );
 }
