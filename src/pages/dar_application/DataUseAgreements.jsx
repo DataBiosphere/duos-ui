753751fc6263e7ccdcaebbdb468aed2633d37f8e
Aarohi Nadkarni <anadkarn@broadinstitute.org>
2024-05-08 09:54:09 -0400

    feat: change duos code of conduct text

--- src/pages/dar_application/DataUseAgreements.jsx
+++ src/pages/dar_application/DataUseAgreements.jsx
@@ -79,16 +79,16 @@ export default function DataUseAgreements(props) {
 
         <ol className="data-use-list">
           <li>Use datasets solely in connection with the research project described in the approved Data Access Request for each dataset;</li>
-          <li>Make no attempt to identify or contact individual participants or groups from whom data were collected, or generate information that could allow participants’ identities to be readily ascertained, without appropriate approvals from the submitting institutions;</li>
+          <li>Make no attempt to identify or contact individual participants or groups from whom data were collected, or generate information that could allow participants’ identities to be readily ascertained, without appropriate approvals from the submitting institution;</li>
           <li>Maintain the confidentiality of the data and not distribute them to any entity or individual beyond those specified in the approved Data Access Request;</li>
-          <li>Adhere to the NIH Security Best Practices for Controlled-Access Data Subject to the NIH Genomic Data Sharing Policy and ensure that only approved users can gain access to data files;</li>
-          <li>Acknowledge the Intellectual Property terms as specified in the Library Card Agreement;</li>
+          <li>Adhere to the security best practices for controlled-access data subject to the genomic data sharing policy/policies listed below and ensure that only approved users can gain access to data files;</li>
+          <li>Acknowledge the Intellectual property terms as specified in the Data Access Agreements and data use certification;</li>
           <li>Provide appropriate acknowledgement in any dissemination of research findings including the investigator(s) who generated the data, the funding source, accession numbers of the dataset, and the data repository from which the data were accessed; and,</li>
-          <li>Report any inadvertent data release, breach of data security, or other data management incidents in accordance with the terms specified in the <a target="_blank" rel="noreferrer" href={BroadLibraryCardAgreementLink}>Library Card Agreement</a> and <a target="_blank" rel="noreferrer" href={ModelDucLink}>NIH Data Use Certification.</a></li>
+          <li>Report any inadvertent data release, break of data security, or other data management incidents in accordance with the terms specified in the Data Use Agreements below and data use certification.</li>
         </ol>
       </div>
 
-      <h3>By submitting this data access request, you agree to all terms in the agreement(s) listed below.</h3>
+      <h3>By submitting this data access request and in accordance with your Institution’s issuance of Library Cards to you for the agreement(s) below,</h3>
       <RequiredDAAs/>
 
       <div className="flex flex-row" style={{ justifyContent: 'around', paddingTop: '4rem' }}>
