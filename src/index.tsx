9204e57e838a7a0c5bb20de3608bd5d1a6d9843e
rushtong <grushton@broadinstitute.org>
2024-05-09 07:44:09 -0400

    feat: convert index.js to tx

---
+++ src/index.tsx
@@ -0,0 +1,21 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// jquery is needed for bootstrap
+import 'jquery/src/jquery';
+import 'bootstrap/dist/js/bootstrap.min';
+
+import React from 'react';
+import {createRoot} from 'react-dom/client';
+import './index.css';
+import App from './App';
+import {unregister} from './registerServiceWorker';
+import {BrowserRouter} from 'react-router-dom';
+
+const load = async () => {
+    unregister();
+    const container = document.getElementById('root');
+    const root = createRoot(container!);
+    root.render(<BrowserRouter><App/></BrowserRouter>);
+};
+
+load();
