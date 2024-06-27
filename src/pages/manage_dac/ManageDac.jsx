5affc19dd7760931692edc1e826242ca93780de3
Aarohi Nadkarni <anadkarn@broadinstitute.org>
2024-06-27 14:11:34 -0400

    feat: more taking out delete logic

--- src/pages/manage_dac/ManageDac.jsx
+++ src/pages/manage_dac/ManageDac.jsx
@@ -4,7 +4,6 @@ import {useState, useEffect, useCallback} from 'react';
 import { Styles } from '../../libs/theme';
 import lockIcon from '../../images/lock-icon.png';
 import { DAC } from '../../libs/ajax/DAC';
-import { DAA } from '../../libs/ajax/DAA';
 import {contains, filter, map} from 'lodash/fp';
 import {Storage} from '../../libs/storage';
 import {Notifications} from '../../libs/utils';
@@ -23,7 +22,6 @@ export const ManageDac = function ManageDac() {
 
   const [isLoading, setIsLoading] = useState(false);
   const [dacs, setDacs] = useState([]);
-  const [daas, setDaas] = useState([]);
   const [dacIDs, setDacIDs] = useState([]);
   const [userRole, setUserRole] = useState();
 
