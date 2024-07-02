ef832ef5d06fedf0c62be15fd40bff0514b342ff
rushtong <grushton@broadinstitute.org>
2024-07-02 12:39:13 -0400

    fix: safely navigate daa dacs

--- src/pages/signing_official_console/ManageResearcherDAAsTable.jsx
+++ src/pages/signing_official_console/ManageResearcherDAAsTable.jsx
@@ -94,7 +94,7 @@ export default function ManageResearcherDAAsTable(props) {
     columnHeaderFormat = {
       ...columnHeaderFormat,
       ...dacs.reduce((acc, dac) => {
-        const daa = daas.find(daa => daa.dacs.some(d => d.dacId === dac.dacId));
+        const daa = daas.find(daa => daa.dacs?.some(d => d.dacId === dac.dacId));
         const id = daa.daaId;
         const fileName = daa.file.fileName;
         acc[dac.name] = { label: dac.name, cellStyle: { width: `${dacColumnWidth}%` }, data: <ManageDaasDropdown actionsTitle={`${dac.name} Actions`} download={{id: id, fileName: fileName}} moreData={{id: id, name: dac.name}} researchers={props.researchers} refreshResearchers={refreshResearchers} setResearchers={setResearchers}/>};
