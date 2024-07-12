80b1f40681b239998fdd62422960d8b203b2e132
Aarohi Nadkarni <anadkarn@broadinstitute.org>
2024-07-12 10:09:09 -0400

    feat: fix styling of daa link

--- src/pages/user_profile/DAAs.jsx
+++ src/pages/user_profile/DAAs.jsx
@@ -13,7 +13,7 @@ export default function DAAs(props) {
     return (
       <div className="flex flex-row" style={{ justifyContent: 'flex-start', marginBottom: '30px'}}>
         <div>
-          <a target="_blank" rel="noreferrer" onClick={() => DAA.getDaaFileById(id, fileName)} className="button button-white" style={{ marginRight: '2rem'}}>
+          <a target="_blank" rel="noreferrer" onClick={() => DAA.getDaaFileById(id, fileName)} className="button button-white" style={{ marginRight: '2rem', display: 'flex', alignItems: 'center'}}>
             <span className="glyphicon glyphicon-download"></span>
             {' '}
             {fileName}
