10fbf4c346cd8067f73c6ef704916bcf07fe28d1
rushtong <grushton@broadinstitute.org>
2024-08-07 12:04:26 -0400

    feat: refactor dropdown hide/show for daa users

---
+++ src/pages/signing_official_console/DisplayNameCell.jsx
@@ -0,0 +1,22 @@
+import React, {useState} from 'react';
+import ManageUsersDropdown from './ManageUsersDropdown';
+
+export default function DisplayNameCell(props) {
+  const {displayName, email, id, daas, setResearchers, refreshResearchers} = props;
+  const [visible, setVisible] = useState(false);
+
+  return <>
+    <li className="dropdown" style={{listStyleType: 'none'}}>
+      <div role="button" data-toggle="dropdown" onClick={() => setVisible(!visible)} >
+        <div id="dacUser" style={{color: 'black'}}>
+          {displayName || 'Invite sent, pending registration'}
+          <span className="caret caret-margin" style={{color: '#337ab7', float: 'right', marginTop: '15px'}}></span>
+          <small><a href={`mailto:${email}`}>{email || '- -'}</a></small>
+        </div>
+      </div>
+      <div style={{display: (visible ? 'block' : 'none')}}>
+        <ManageUsersDropdown daas={daas} refreshResearchers={refreshResearchers} setResearchers={setResearchers} moreData={{id: id, name: displayName}}/>
+      </div>
+    </li>
+  </>;
+}
