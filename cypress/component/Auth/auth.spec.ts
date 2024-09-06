832f5b3e41b4e7b43998e73db42a5630b430400a
rushtong <grushton@broadinstitute.org>
2024-09-06 06:45:43 -0400

    PR feedback: boost storage coverage

--- cypress/component/Auth/auth.spec.ts
+++ cypress/component/Auth/auth.spec.ts
@@ -5,6 +5,7 @@ import {Config} from '../../../src/libs/config';
 import {GoogleIS} from '../../../src/libs/googleIS';
 import {OAuth2} from '../../../src/libs/ajax/OAuth2';
 import {Storage} from '../../../src/libs/storage';
+import { v4 as uuid } from 'uuid';
 
 describe('Auth', function () {
   // Intercept configuration calls
@@ -24,8 +25,17 @@ describe('Auth', function () {
     cy.stub(Config, 'getGoogleClientId').returns('12345');
     cy.stub(GoogleIS, 'revokeAccessToken');
     Storage.setUserIsLogged(true);
+    Storage.setAnonymousId(uuid());
+    Storage.setData('key', 'val');
+    Storage.setEnv('test');
     expect(Storage.userIsLogged()).to.be.true;
+    expect(Storage.getAnonymousId()).to.not.be.empty;
+    expect(Storage.getData('key')).to.not.be.empty;
+    expect(Storage.getEnv()).to.not.be.empty;
     await Auth.signOut();
     expect(Storage.userIsLogged()).to.be.false;
+    expect(Storage.getAnonymousId()).to.be.null;
+    expect(Storage.getData('key')).to.be.null;
+    expect(Storage.getEnv()).to.be.null;
   });
 });
