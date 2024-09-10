a3eaeba25ee4b44d79fca67ebb7556fb1c2809f2
rushtong <grushton@broadinstitute.org>
2024-09-10 10:18:43 -0400

    feat: sign-in test fix

--- cypress/component/SignIn/sign_in_button.spec.js
+++ cypress/component/SignIn/sign_in_button.spec.js
@@ -4,13 +4,13 @@ import React from 'react';
 import { mount } from 'cypress/react';
 import SignInButton from '../../../src/components/SignInButton';
 
-const signInText = 'Sign in';
+const signInText = 'Sign In';
 
 // Note that we do not want to click the signin button
 // in tests as that would trigger an auth-flow we cannot
 // replicate in a test environment.
 describe('Sign In Component', function() {
-  it('Sign In Button Loads when client id is valid', function () {
+  it('Sign In Button Loads', function () {
     cy.viewport(600, 300);
     mount(<SignInButton history={undefined} onSignIn={() => {}}/>);
     cy.contains(signInText).should('exist');
