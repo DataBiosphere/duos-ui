54f025be4e5e0b3b1a3b6de3190d9ace8abc1899
rushtong <grushton@broadinstitute.org>
2024-09-19 11:12:57 -0400

    feat: additional documentation

--- src/index.tsx
+++ src/index.tsx
@@ -11,6 +11,15 @@ import {BrowserRouter} from 'react-router-dom';
 const load = async () => {
   unregister();
   await Auth.initialize();
+  // The following handles the OIDC post-authentication flow.
+  // 1. User clicks Sign In button
+  // 2. OidcBroker instantiates a popup,
+  // 3. User completes the auth dance and is redirected to this path
+  // 4. Application (this component) detects that path and instructs the OidcBroker to close the popup
+  // 5. After popup close, user is directed to the original url. This works for the cases we support:
+  //   5a. Logging in from the home page
+  //   5b. Logging in from a link, i.e. `<origin>/dataLibrary`
+  //   5c. Logging in from a link with a `redirectTo` query param, i.e. `<origin>?redirectTo=/dataLibrary`
   if (window.location.pathname.startsWith('/redirect-from-oauth')) {
     await OidcBroker.getUserManager().signinPopupCallback(window.location.href);
   }
