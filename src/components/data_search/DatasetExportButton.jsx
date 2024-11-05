b7747a80b1bf97b65c62af2b8619b96e9b8ca1dc
Phil Shapiro <pshapiro@broadinstitute.org>
2024-11-05 14:05:45 -0500

    Add horizontal space between Export text

--- src/components/data_search/DatasetExportButton.jsx
+++ src/components/data_search/DatasetExportButton.jsx
@@ -16,7 +16,7 @@ export const DatasetExportButton = (props) => {
 
   const link = `${terraUrl}/#import-data?snapshotId=${snapshot.id}&format=tdrexport&tdrSyncPermissions=false`;
 
-  return <Link href={link} target="_blank" rel="noopener noreferrer" title={title} aria-label={title}>Export</Link>;
+  return <Link style={{marginRight: '5px'}} href={link} target="_blank" rel="noopener noreferrer" title={title} aria-label={title}>Export</Link>;
 };
 
 export default DatasetExportButton;
