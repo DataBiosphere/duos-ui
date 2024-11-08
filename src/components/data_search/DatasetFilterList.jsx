b5fe58f8108f50723c06e524de4807c801ec8140
Florian Boulnois <fboulnoi@broadinstitute.org>
2024-11-08 11:09:14 -0500

    refactor: convert filter item header and list into components

--- src/components/data_search/DatasetFilterList.jsx
+++ src/components/data_search/DatasetFilterList.jsx
@@ -10,6 +10,38 @@ import { Typography } from '@mui/material';
 import { Checkbox } from '@mui/material';
 import { flatten, uniq, compact, capitalize, orderBy } from 'lodash';
 
+export const FilterItemHeader = (props) => {
+  const { title, headerStyle = { fontFamily: 'Montserrat', fontWeight: '600', marginTop: '1em' } } = props;
+  return (
+    <Typography variant="h6" gutterBottom component="div" sx={headerStyle}>
+      {title}
+    </Typography>
+  );
+}
+
+export const FilterItemList = (props) => {
+  const { category, datasets, filter, filterHandler, isFiltered, filterNameFn } = props;
+  return (
+    <List sx={{ margin: '-0.5em -0.5em' }}>
+      {
+        filter.map((filter) => {
+          const filterName = filterNameFn(filter);
+          return (
+            <ListItem disablePadding key={filter}>
+              <ListItemButton sx={{ padding: '0' }} onClick={(event) => filterHandler(event, datasets, category, filter)}>
+                <ListItemIcon>
+                  <Checkbox checked={isFiltered(filter, category)} />
+                </ListItemIcon>
+                <ListItemText primary={filterName} sx={{ fontFamily: 'Montserrat', transform: 'scale(1.2)' }} />
+              </ListItemButton>
+            </ListItem>
+          );
+        })
+      }
+    </List>
+  );
+};
+
 export const DatasetFilterList = (props) => {
   const { datasets, filters, filterHandler, isFiltered } = props;
 
@@ -23,69 +55,35 @@ export const DatasetFilterList = (props) => {
         Filters
       </Typography>
       <Divider />
-      <Typography variant="h6" gutterBottom component="div" sx={{ fontFamily: 'Montserrat', fontWeight: '600' }} marginTop="1em">
-        Access Type
-      </Typography>
-      <List sx={{ margin: '-0.5em -0.5em'}}>
-        {
-          accessManagementFilters.map((filter) => {
-            const filterName = capitalize(filter);
-            const category = 'accessManagement';
-            return (
-              <ListItem disablePadding key={filter}>
-                <ListItemButton sx={{ padding: '0' }} onClick={(event) => filterHandler(event, datasets, category, filter)}>
-                  <ListItemIcon>
-                    <Checkbox checked={isFiltered(filter, category)} />
-                  </ListItemIcon>
-                  <ListItemText primary={filterName} sx={{ fontFamily: 'Montserrat', transform: 'scale(1.2)' }} />
-                </ListItemButton>
-              </ListItem>
-            );
-          })
-        }
-      </List>
-      <Typography variant="h6" gutterBottom component="div" sx={{ fontFamily: 'Montserrat', fontWeight: '600' }} marginTop="1em">
-        Primary Data Use
-      </Typography>
-      <List sx={{ margin: '-0.5em -0.5em'}}>
-        {
-          dataUseFilters.map((filter) => {
-            const filterName = filter.toUpperCase();
-            const category = 'dataUse';
-            return (
-              <ListItem disablePadding key={filter}>
-                <ListItemButton sx={{ padding: '0' }} onClick={(event) => filterHandler(event, datasets, category, filter)}>
-                  <ListItemIcon>
-                    <Checkbox checked={isFiltered(filter, category)} />
-                  </ListItemIcon>
-                  <ListItemText primary={filterName} sx={{ fontFamily: 'Montserrat', transform: 'scale(1.2)' }} />
-                </ListItemButton>
-              </ListItem>
-            );
-          })
-        }
-      </List>
-      <Typography variant="h6" gutterBottom component="div" sx={{ fontFamily: 'Montserrat' }} marginTop="1em">
-        <span style={{ fontWeight: '600' }}>Data Access Committee</span> <span>(DACs)</span>
-      </Typography>
-      <List sx={{ margin: '-0.5em -0.5em'}}>
-        {
-          dacFilters.map((filter) => {
-            const filterName = filter;
-            const category = 'dac';
-            return (
-              <ListItem disablePadding key={filter}>
-                <ListItemButton sx={{ padding: '0' }} onClick={(event) => filterHandler(event, datasets, category, filter)}>
-                  <ListItemIcon>
-                    <Checkbox checked={isFiltered(filter, category)} />
-                  </ListItemIcon>
-                  <ListItemText primary={filterName} sx={{ fontFamily: 'Montserrat', transform: 'scale(1.2)' }} />
-                </ListItemButton>
-              </ListItem>
-            );
-          })
-        }
-      </List>
+      <FilterItemHeader title="Access Type" />
+      <FilterItemList
+        category="accessManagement"
+        datasets={datasets}
+        filter={accessManagementFilters}
+        filterHandler={filterHandler}
+        isFiltered={isFiltered}
+        filterNameFn={capitalize} />
+      <FilterItemHeader title="Data Use" />
+      <FilterItemList
+        category="dataUse"
+        datasets={datasets}
+        filter={dataUseFilters}
+        filterHandler={filterHandler}
+        isFiltered={isFiltered}
+        filterNameFn={(filter) => filter.toUpperCase()}
+      />
+      <FilterItemHeader
+        title={<><span style={{ fontWeight: '600' }}>Data Access Committee</span> <span>(DACs)</span></>}
+        headerStyle={{ fontFamily: 'Montserrat', marginTop: '1em' }}
+      />
+      <FilterItemList
+        category="dac"
+        datasets={datasets}
+        filter={dacFilters}
+        filterHandler={filterHandler}
+        isFiltered={isFiltered}
+        filterNameFn={(filter) => filter}
+      />
     </Box>
   );
 };
