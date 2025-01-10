c23831a9014afbb8385b41adf3e1ea6109d58f13
fboulnois <fboulnois@users.noreply.github.com>
2025-01-10 10:11:08 -0500

    [DT-688] Disable DUOS sign-in when Sam is down (#2763)

--- src/pages/Status.jsx
+++ src/pages/Status.jsx
@@ -1,69 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { getOr, isNil, map, uniq } from 'lodash/fp';
-import { Component } from 'react';
 import CheckboxMarkedCircleOutline from 'react-material-icon-svg/dist/CheckboxMarkedCircleOutline';
 import DiameterVariant from 'react-material-icon-svg/dist/DiameterVariant';
-import { getApiUrl, getOntologyUrl } from '../libs/ajax';
+import { ServiceStatus } from '../libs/ajax/ServiceStatus';
 
+const Status = () => {
+  const [consentStatus, setConsentStatus] = useState({});
+  const [ontologyStatus, setOntologyStatus] = useState({});
+  const [samStatus, setSamStatus] = useState({});
 
-class Status extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      consentStatus: {},
-      ontologyStatus: {}
-    };
-  }
-
-  isConsentHealthy = (elements) => {
+  const isConsentHealthy = (elements) => {
     return getOr(false)('ok')(elements);
   };
 
-  isOntologyHealthy = (elements) => {
+  const isOntologyHealthy = (elements) => {
     const ok = getOr(undefined)('ok')(elements);
     if (!isNil(ok)) {
-      // return the OK status from ontology if it exists
       return ok;
     } else {
-      // find all values of "healthy" and ensure that they are all true
       const bools = uniq(map('healthy')(elements));
       return bools.length === 1 && bools[0];
     }
   };
 
-  async componentDidMount() {
-    const consentStatusUrl = `${ await getApiUrl() }/status`;
-    const ontologyStatusUrl = `${ await getOntologyUrl() }/status`;
-    fetch(consentStatusUrl, { method: 'GET' })
-      .then(response => response.json())
-      .then(data => this.setState({ consentStatus: data }));
-    fetch(ontologyStatusUrl, { method: 'GET' })
-      .then(response => response.json())
-      .then(data => this.setState({ ontologyStatus: data }));
-  }
+  useEffect(() => {
+    const fetchStatus = async () => {
+      const consentData = await ServiceStatus.getConsentStatus();
+      setConsentStatus(consentData);
+      setSamStatus(consentData.systems.sam.details);
 
-  render() {
-    const healthyState = <CheckboxMarkedCircleOutline fill={ 'green' } style={ { marginLeft: '2rem', verticalAlign: 'middle', height: '24px' } }/>;
-    const unhealthyState = <DiameterVariant fill={ 'red' } style={ { marginLeft: '2rem', verticalAlign: 'middle', height: '24px' } }/>;
-    const consentHealthy = this.isConsentHealthy(this.state.consentStatus) ? healthyState : unhealthyState;
-    const ontologyHealthy = this.isOntologyHealthy(this.state.ontologyStatus) ? healthyState : unhealthyState;
+      const ontologyData = await ServiceStatus.getOntologyStatus();
+      setOntologyStatus(ontologyData);
+    };
+    fetchStatus();
+  }, []);
 
-    return (
-      <div style={{ margin: '2rem' }}>
-        <ul style={{ marginTop: '2rem', listStyle: 'none', fontSize: 'x-large' }}>
-          <li><a href="#consent">Consent</a> {consentHealthy}</li>
-          <li><a href="#ontology">Ontology</a> {ontologyHealthy}</li>
-        </ul>
-        <hr />
-        <h2><a id="consent">Consent Status</a></h2>
-        <pre>{JSON.stringify(this.state.consentStatus, null, 4)}</pre>
-        <h2><a id="ontology">Ontology Status</a></h2>
-        <pre>{JSON.stringify(this.state.ontologyStatus, null, 4)}</pre>
-      </div>
-    );
-  }
+  const healthyState = <CheckboxMarkedCircleOutline fill={'green'} style={{ marginLeft: '2rem', verticalAlign: 'middle', height: '24px' }} />;
+  const unhealthyState = <DiameterVariant fill={'red'} style={{ marginLeft: '2rem', verticalAlign: 'middle', height: '24px' }} />;
 
+  const consentHealthy = isConsentHealthy(consentStatus) ? healthyState : unhealthyState;
+  const ontologyHealthy = isOntologyHealthy(ontologyStatus) ? healthyState : unhealthyState;
+  const samHealthy = isConsentHealthy(samStatus) ? healthyState : unhealthyState;
+
+  return (
+    <div style={{ margin: '2rem' }}>
+      <ul style={{ marginTop: '2rem', listStyle: 'none', fontSize: 'x-large' }}>
+        <li><a href="#consent">Consent</a> {consentHealthy}</li>
+        <li><a href="#ontology">Ontology</a> {ontologyHealthy}</li>
+        <li><a href="#sam">Sam</a> {samHealthy}</li>
+      </ul>
+      <hr />
+      <h2><a id="consent">Consent Status</a></h2>
+      <pre>{JSON.stringify(consentStatus, null, 4)}</pre>
+      <h2><a id="ontology">Ontology Status</a></h2>
+      <pre>{JSON.stringify(ontologyStatus, null, 4)}</pre>
+    </div>
+  );
 };
 
 export default Status;
