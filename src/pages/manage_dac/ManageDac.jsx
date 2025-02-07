87643937caaad0c50cc5cf7639b0a08f9cfe31ad
rushtong <grushton@broadinstitute.org>
2025-02-07 14:55:33 -0500

    fix: check for DAA functionality instead of environment.

--- src/pages/manage_dac/ManageDac.jsx
+++ src/pages/manage_dac/ManageDac.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import ManageDacTable from '../../components/manage_dac_table/ManageDacTable';
 import {useState, useEffect} from 'react';
-import { Styles } from '../../libs/theme';
+import {Styles} from '../../libs/theme';
 import lockIcon from '../../images/lock-icon.png';
-import { DAC } from '../../libs/ajax/DAC';
+import {DAC} from '../../libs/ajax/DAC';
 import {Storage} from '../../libs/storage';
 import {Notifications} from '../../libs/utils';
 import DacDatasetsModal from '../../components/modals/DacDatasetsModal';
 import {DacMembersModal} from './DacMembersModal';
 import ConfirmationModal from '../../components/modals/ConfirmationModal';
 import ManageEditDac from './ManageEditDac';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import EditDac from './EditDac';
-import { checkEnv, envGroups } from '../../utils/EnvironmentUtils';
+import {checkEnv, envGroups} from '../../utils/EnvironmentUtils';
+import {DAAUtils} from 'src/utils/DAAUtils';
 
 const CHAIR = 'Chairperson';
 const ADMIN = 'Admin';
@@ -93,29 +94,30 @@ export const ManageDac = function ManageDac() {
 
   return (
     <div style={Styles.PAGE}>
-      <div style={{ display: 'flex', justifyContent: 'space-between', width: '112%', marginLeft: '-6%', padding: '0 2.5%' }}>
-        <div className="left-header-section" style={Styles.LEFT_HEADER_SECTION}>
+      <div
+        style={{display: 'flex', justifyContent: 'space-between', width: '112%', marginLeft: '-6%', padding: '0 2.5%'}}>
+        <div className='left-header-section' style={Styles.LEFT_HEADER_SECTION}>
           <div style={Styles.ICON_CONTAINER}>
-            <img id="lock-icon" src={lockIcon} style={Styles.HEADER_IMG} />
+            <img id='lock-icon' src={lockIcon} style={Styles.HEADER_IMG} alt={'Manage Data Access Committee'}/>
           </div>
           <div style={Styles.HEADER_CONTAINER}>
-            <div style={{ fontFamily: 'Montserrat', fontWeight: 600, fontSize: '2.8rem' }}>
+            <div style={{fontFamily: 'Montserrat', fontWeight: 600, fontSize: '2.8rem'}}>
               Manage Data Access Committee
             </div>
-            <div style={{ fontFamily: 'Montserrat', fontSize: '1.6rem' }}>
+            <div style={{fontFamily: 'Montserrat', fontSize: '1.6rem'}}>
               Create and manage Data Access Committee
             </div>
           </div>
         </div>
-        <div className="right-header-section">
+        <div className='right-header-section'>
           <Link
-            id="btn_addDAC"
-            className="btn-primary btn-add common-background"
-            style={{ marginTop: '30%', display: 'flex' }}
+            id='btn_addDAC'
+            className='btn-primary btn-add common-background'
+            style={{marginTop: '30%', display: 'flex'}}
             onClick={addDac}
             to={{
-              pathname: checkEnv(envGroups.DEV) ? `/manage_add_dac_daa` : `/manage_add_dac`,
-              state: { userRole: userRole}
+              pathname: DAAUtils.isEnabled() ? `/manage_add_dac_daa` : `/manage_add_dac`,
+              state: {userRole: userRole}
             }}
           >
             <span>Add DAC</span>
@@ -136,8 +138,8 @@ export const ManageDac = function ManageDac() {
       <ConfirmationModal
         showConfirmation={showConfirmationModal}
         closeConfirmation={closeConfirmation}
-        title="Delete DAC?"
-        message="Are you sure you want to delete this Data Access Committee?"
+        title='Delete DAC?'
+        message='Are you sure you want to delete this Data Access Committee?'
         header={selectedDac.name}
         onConfirm={handleDeleteDac}
       />
