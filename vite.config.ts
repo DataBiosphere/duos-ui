892ccdda35aadfe2f3e41a8e7e3889808992c344
fboulnois <fboulnois@users.noreply.github.com>
2025-02-27 13:32:46 -0500

    [DT-1283] Replace source map explorer (#2794)

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react({ include: /\.(mdx|js|jsx|ts|tsx)$/ }),
+    (process.env.VISUALIZER_PLUGIN === 'true' && visualizer({ open: true, filename: 'build/stats.html' })),
   ],
   assetsInclude: ['**/*.md'],
   build: {
