85823d5314c1528976c05d3b85550d00518d5797
rushtong <grushton@broadinstitute.org>
2025-02-27 14:41:44 -0500

    feat: update for lint warnings

--- src/components/dar_dataset_table/DarDatasetTable.jsx
+++ src/components/dar_dataset_table/DarDatasetTable.jsx
@@ -163,8 +163,7 @@ export const DarDatasetTable = (props) => {
       );
       setBuckets(dataAccessBuckets);
       setTableSize(dataAccessBuckets.length);
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    } catch (error) {
+    } catch (_error) {
       Notifications.showError({
         text: 'Error initializing DAR Collection Dataset summary.',
       });
@@ -175,8 +174,7 @@ export const DarDatasetTable = (props) => {
   useEffect(() => {
     try {
       init();
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    } catch (error) {
+    } catch (_error) {
       Notifications.showError({ text: 'Failed to initialize collection' });
     }
   }, [init]);
