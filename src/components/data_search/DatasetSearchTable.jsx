d9b8bebcf5a43d5a33169a074ce84214fca1df5e
Elliot Otchet <otchet@broadinstitute.org>
2025-04-15 17:45:00 -0400

    Fix spacing.

--- src/components/data_search/DatasetSearchTable.jsx
+++ src/components/data_search/DatasetSearchTable.jsx
@@ -36,7 +36,7 @@ export const applyForAccess = async (selected, history) => {
     const draftResponse = await DAR.postDarDraft({ datasetId: selected });
     if (draftResponse.referenceId) {
       history.push(`/dar_application/${draftResponse.referenceId}`);
-    } else if (draftResponse.code && draftResponse.code===422) {
+    } else if (draftResponse.code && draftResponse.code === 422) {
       Notifications.showError(
           {
             text: "Please register in DUOS with your institution's email and obtain approval from your Signing Official in order to submit a data access request.",
