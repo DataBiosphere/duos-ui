be7d6674bed8db94c432d4129f3cebbc8113548b
rushtong <grushton@broadinstitute.org>
2025-05-21 09:43:43 -0400

    feat: revert - it is used for highlighting error state

--- src/pages/progress_reports/SummarySection.tsx
+++ src/pages/progress_reports/SummarySection.tsx
@@ -49,6 +49,8 @@ export default function SummarySection(props: SummarySectionProps): React.JSX.El
                         onNihStatusUpdate={() => {
                         }}
                         location={location}
+                        validationError={() => {
+                        }}
                         readOnly={readOnly}
                         header={true}
                         required={true}
