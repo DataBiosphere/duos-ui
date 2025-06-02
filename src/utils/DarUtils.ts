82e16979b839537c10174eec75468b553da135f0
Florian Boulnois <fboulnoi@broadinstitute.org>
2025-06-02 13:38:56 -0400

    fix: remove logging and rename variable

--- src/utils/DarUtils.ts
+++ src/utils/DarUtils.ts
@@ -1,15 +1,12 @@
 import { Election } from "src/types/model";
 
 export function getApprovedElectionDatasetIds(elections: Array<Election>) : Array<number> {
-
-    console.log('elections', elections);
-
     const approvedDatasetIds = [];
     for (const election of elections) {
         if (election.electionType === 'DataAccess') {
             const votes = Object.values(election.votes);
-            const anyApprovedVotes = votes.some(vote => vote.type === 'FINAL' && vote.vote);
-            if (anyApprovedVotes) {
+            const anyApprovedFinalVotes = votes.some(vote => vote.type === 'FINAL' && vote.vote);
+            if (anyApprovedFinalVotes) {
                 approvedDatasetIds.push(election.datasetId);
             }
         }
