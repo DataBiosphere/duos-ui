630516137b24662fdf4860124786b2c61ea03674
Elliot Otchet <otchet@broadinstitute.org>
2025-06-05 14:26:20 -0400

    Leave our future selves a note about re-enabling the test when we turn ras back on.

--- src/utils/ERACommonsUtils.js
+++ src/utils/ERACommonsUtils.js
@@ -2,6 +2,8 @@ import {Buffer} from 'buffer';
 //import EnvironmentUtils, {envGroups} from '../utils/EnvironmentUtils.js';
 
 export const rasEnabled = () => {
+  // when re-enabling this, don't forget to turn back on the test in era_commons_spec.jsx.
+  // search DT-1800 to find it.
   //return EnvironmentUtils.checkEnv(envGroups.DEV);
   return false;
 }
