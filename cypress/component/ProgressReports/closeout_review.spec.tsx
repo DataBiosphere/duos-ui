b0050973b01291d1076b17e748d1aab53828b4e6
rushtong <grushton@broadinstitute.org>
2025-06-30 09:27:16 -0400

    feat: fix comment again

--- cypress/component/ProgressReports/closeout_review.spec.tsx
+++ cypress/component/ProgressReports/closeout_review.spec.tsx
@@ -89,7 +89,7 @@ describe('CloseoutReview - Component Tests', () => {
   it('displays closeout approve when no acknowledgement exists', () => {
     mountComponent();
 
-    // Approved button should be visible
+    // Approve button should be visible
     cy.get('[data-cy="closeout-review"]').should('exist');
     cy.get('[data-cy="closeout-review-approve-button"]').should('exist');
   });
