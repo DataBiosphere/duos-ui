b4ab415db6ffbe55a6e74acc193dad7b3803888d
rushtong <grushton@broadinstitute.org>
2025-07-15 11:53:44 -0400

    feat: cleanup

--- src/components/institution_table/InstitutionTableUtils.tsx
+++ src/components/institution_table/InstitutionTableUtils.tsx
@@ -84,15 +84,7 @@ export const columnConfig: ColumnConfig = {
     cellStyle: {width: columnWidths.name},
     cellDataFn: (row: Institution) => {
       return <Link
-          to={{
-            pathname: `/admin_manage_institutions/${row.id}`
-            // If we have the institution list already loaded, pass it along to the Edit Institution
-            // page so we can check for duplicate domains before hitting the backend or fetching the
-            // entire institution list again. If this value is not present or not yet loaded, the Edit
-            // Institution page will rely on backend validation.
-            // TODO: Fix this!
-            // state: { institutionList }
-          }}
+          to={{pathname: `/admin_manage_institutions/${row.id}`}}
           style={{
             overflow: 'hidden',
             whiteSpace: 'nowrap',
