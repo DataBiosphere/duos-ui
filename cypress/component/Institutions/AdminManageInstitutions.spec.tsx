0712a802e49f758c527043ded519e94368388190
rushtong <grushton@broadinstitute.org>
2025-07-17 09:13:39 -0400

    feat: test stubs

---
+++ cypress/component/Institutions/AdminManageInstitutions.spec.tsx
@@ -0,0 +1,21 @@
+describe('AdminManageInstitutions', () => {
+  it('renders', () => {
+    // TODO: mount the component and check for basic render
+  });
+
+  it('displays the institution list', () => {
+    // TODO: mock institution data and verify table renders
+  });
+
+  it('filters institutions on search', () => {
+    // TODO: simulate search input and check filtered results
+  });
+
+  it('link Add Institution page', () => {
+    // TODO: click add button and verify modal appears
+  });
+
+  it('handles loading state', () => {
+    // TODO: mock loading and check skeleton loader
+  });
+});
---
+++ cypress/component/Institutions/InstitutionTable.spec.tsx
@@ -0,0 +1,25 @@
+describe('InstitutionTable', () => {
+  it('renders', () => {
+    // TODO: mount the component and check for basic render
+  });
+
+  it('displays paginated institution rows', () => {
+    // TODO: provide mock data and verify rows are rendered
+  });
+
+  it('handles loading state', () => {
+    // TODO: set isLoading prop and check for loading indicator
+  });
+
+  it('sorts columns when header is clicked', () => {
+    // TODO: simulate sort interaction and verify order
+  });
+
+  it('changes page when pagination bar is used', () => {
+    // TODO: interact with pagination and check page change
+  });
+
+  it('links to the update institution page', () => {
+    // TODO: trigger modal open/close and verify visibility
+  });
+});
