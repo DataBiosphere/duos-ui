4fd7e36bd7aa9fbb238e9e15157099e096a7d9ac
rushtong <grushton@broadinstitute.org>
2025-07-17 11:49:59 -0400

    feat: flesh out tests

--- cypress/component/Institutions/AdminManageInstitutions.spec.tsx
+++ cypress/component/Institutions/AdminManageInstitutions.spec.tsx
@@ -1,21 +1,163 @@
+import React from 'react';
+import {mount} from 'cypress/react';
+import AdminManageInstitutions from 'src/pages/AdminManageInstitutions';
+import {BrowserRouter} from 'react-router-dom';
+import {Institution as InstitutionAPI} from 'src/libs/ajax/Institution';
+import {DuosUser, Institution} from 'src/types/model';
+
+
+const createUser: DuosUser = {
+  createDate: new Date(),
+  displayName: 'Create User',
+  email: 'create@test.com',
+  emailPreference: true,
+  eraCommonsId: 'admin-user',
+  isAdmin: true,
+  isAlumni: false,
+  isChairPerson: false,
+  isDataSubmitter: false,
+  isMember: false,
+  isResearcher: true,
+  isSigningOfficial: false,
+  roles: [{
+    roleId: 4,
+    name: 'Admin',
+    userId: 1,
+    userRoleId: 1,
+  }],
+  userId: 1
+};
+
+const updateUser: DuosUser = {
+  createDate: new Date(),
+  displayName: 'Update User',
+  email: 'update@test.com',
+  emailPreference: true,
+  eraCommonsId: 'update-user',
+  isAdmin: true,
+  isAlumni: false,
+  isChairPerson: false,
+  isDataSubmitter: false,
+  isMember: false,
+  isResearcher: true,
+  isSigningOfficial: false,
+  roles: [{
+    roleId: 4,
+    name: 'Admin',
+    userId: 2,
+    userRoleId: 2,
+  }],
+  userId: 2
+};
+
+const mockInstitutions = [
+  {
+    id: 1,
+    name: 'Test Institution 1',
+    domains: ['test1.edu'],
+    signingOfficials: [{userId: '1', displayName: 'User 1', email: 'email1'}],
+    createDate: 'Feb 1, 2023',
+    createUser: createUser,
+    createUserId: createUser.userId,
+  } as unknown as Institution,
+  {
+    id: 2,
+    name: 'Test Institution 2',
+    domains: ['test2.edu'],
+    signingOfficials: [{userId: '2', displayName: 'User 2', email: 'email2'}],
+    createDate: 'Jul 1, 2025',
+    createUser: createUser,
+    createUserId: createUser.userId,
+    updateDate: 'Jul 2, 2025',
+    updateUser: updateUser,
+    updateUserId: updateUser.userId,
+  } as unknown as Institution,
+];
+
 describe('AdminManageInstitutions', () => {
+  beforeEach(() => {
+    cy.viewport(1400, 600);
+    cy.initApplicationConfig();
+  });
+
   it('renders', () => {
-    // TODO: mount the component and check for basic render
+    cy.stub(InstitutionAPI, 'list').returns(Promise.resolve(mockInstitutions));
+    mount(<BrowserRouter><AdminManageInstitutions/></BrowserRouter>);
+    cy.get('[data-cy="admin-manage-institutions"]').should('exist');
+    cy.get('[data-cy="search-bar"]').should('exist');
   });
 
-  it('displays the institution list', () => {
-    // TODO: mock institution data and verify table renders
+  it('displays all institutions in list', () => {
+    cy.stub(InstitutionAPI, 'list').returns(Promise.resolve(mockInstitutions));
+    mount(<BrowserRouter><AdminManageInstitutions/></BrowserRouter>);
+    mockInstitutions.forEach((institution) => {
+      cy.contains(institution.name).should('exist');
+      cy.contains(institution.id).should('exist');
+      if (institution.updateDate) {
+        cy.contains(institution.updateDate).should('exist');
+      } else {
+        cy.contains(institution.createDate).should('exist');
+      }
+      if (institution.updateUser) {
+        cy.contains(institution.updateUser.displayName).should('exist');
+      } else {
+        cy.contains(institution.createUser.displayName).should('exist');
+      }
+      if (institution.domains) {
+        institution.domains.forEach((domain) => {
+          cy.contains(domain).should('exist');
+        });
+      }
+    });
   });
 
+  /*
+   * Note: SearchBar functionality via cypress looks finicky. It works normally in the browser,
+   * but to see the onChange effect, we need to add an extra space after the search term. That component
+   * is outside the scope of this test, so we will leave it as is.
+   */
   it('filters institutions on search', () => {
-    // TODO: simulate search input and check filtered results
+    cy.stub(InstitutionAPI, 'list').returns(Promise.resolve(mockInstitutions));
+    mount(<BrowserRouter><AdminManageInstitutions/></BrowserRouter>);
+    const institution1 = mockInstitutions[0];
+    const institution2 = mockInstitutions[1];
+    cy.get('[data-cy="search-bar"]').clear();
+    cy.get('[data-cy="search-bar"]').click();
+    cy.get('[data-cy="search-bar"]').type(institution1.name + ' ');
+    cy.get('[data-cy="search-bar"]').blur();
+    cy.get('[data-cy="admin-manage-institutions"]').should('not.contain', institution2.name);
+    cy.get('[data-cy="search-bar"]').clear();
+    cy.get('[data-cy="search-bar"]').click();
+    cy.get('[data-cy="search-bar"]').type(institution2.name + ' ');
+    cy.get('[data-cy="search-bar"]').blur();
+    cy.get('[data-cy="admin-manage-institutions"]').should('not.contain', institution1.name);
+    cy.get('[data-cy="search-bar"]').clear();
+    cy.get('[data-cy="search-bar"]').click();
+    cy.get('[data-cy="search-bar"]').type(' ');
+    cy.get('[data-cy="search-bar"]').blur();
+    cy.get('[data-cy="admin-manage-institutions"]').should('contain', institution1.name);
+    cy.get('[data-cy="admin-manage-institutions"]').should('contain', institution2.name);
   });
 
   it('link Add Institution page', () => {
-    // TODO: click add button and verify modal appears
+    cy.stub(InstitutionAPI, 'list').returns(Promise.resolve(mockInstitutions));
+    mount(<BrowserRouter><AdminManageInstitutions/></BrowserRouter>);
+    cy.get('[data-cy="admin-manage-institutions"]').should('contain', 'Add Institution');
+    cy.get('[id="btn_addInstitution"]').should('exist');
   });
 
   it('handles loading state', () => {
-    // TODO: mock loading and check skeleton loader
+    // cy.stub(InstitutionAPI, 'list').returns(Promise.resolve(mockInstitutions));
+    cy.intercept('GET', '/api/institutions', (req) => {
+      req.reply({
+        delay: 1000, // Simulate a delay to show loading state
+        body: mockInstitutions
+      })
+    });
+    mount(<BrowserRouter><AdminManageInstitutions/></BrowserRouter>);
+    cy.get('[data-cy="table-skeleton-loader"]').should('exist');
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(1500); // Wait for the loading state to finish
+    cy.get('[data-cy="table-skeleton-loader"]').should('not.exist');
   });
 });
