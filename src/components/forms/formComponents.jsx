2171c22b10e5ee7a1f5456f81c608573dd337190
Eric Weitz <eweitz@broadinstitute.org>
2025-07-25 10:20:55 -0400

    Disable 'Upload file' link in read-only mode

--- src/components/forms/formComponents.jsx
+++ src/components/forms/formComponents.jsx
@@ -499,12 +499,13 @@ export const FormInputFile = (config) => {
     placeholder = 'Filename.txt',
     accept = '',
     validation,
+    disabled = false,
   } = config
 
   return (
     <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
       {hideInput === false && (
-        <div className="form-file-upload">
+        <div className={`form-file-upload ${disabled ? 'disabled' : ''}`}>
           <input
             id={id}
             name={name || id}
@@ -521,6 +522,7 @@ export const FormInputFile = (config) => {
                 onFormInputChange(config, e.target.files[0])
               }
             }}
+            disabled={disabled}
           />
           <label htmlFor={`${id}`} className={`form-file-label ${!isValid(validation) ? 'errored' : ''}`}>
             <PublishIcon />
