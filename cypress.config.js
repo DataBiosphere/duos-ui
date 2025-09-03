10a58acfd6ed722caf141f800e10b4efdbccdf20
Florian Boulnois <fboulnoi@broadinstitute.org>
2025-09-03 16:39:08 -0400

    fix: update e2e tests to address layout changes

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'cypress'
+import vitePreprocessor from 'cypress-vite'
 
 export default defineConfig({
   chromeWebSecurity: false,
@@ -6,9 +7,10 @@ export default defineConfig({
     baseUrl: 'http://localhost:3000/',
   },
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
     baseUrl: 'http://localhost:3000/',
+    setupNodeEvents(on) {
+      on('file:preprocessor', vitePreprocessor())
+    },
   },
   component: {
     specPattern: ['**/*.spec.js', '**/*.spec.jsx', '**/*.spec.ts', '**/*.spec.tsx'],
