3c158c5d6de41340da406e9d8b921e094e85464b
Kevin Marete <kevinmmarete@gmail.com>
2025-09-22 14:29:26 -0400

    Typescript lint fixes

--- src/utils/FileDownload.ts
+++ src/utils/FileDownload.ts
@@ -1,4 +1,6 @@
 export const fileDownload = (data: string | ArrayBuffer | ArrayBufferView | Blob, filename: string, mime?: string) => {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-expect-error
   const blob = new Blob([data], { type: mime || 'application/octet-stream' })
   const blobUrl = URL.createObjectURL(blob)
 
