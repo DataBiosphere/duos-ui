1275d63f5bc7f90d5e7c8aa879e9f1274cdb80b4
Kevin Marete <kevinmmarete@gmail.com>
2025-09-24 14:35:00 -0400

    Add tests

--- src/components/SearchSelect.tsx
+++ src/components/SearchSelect.tsx
@@ -3,7 +3,7 @@ import './SearchSelect.css'
 import { FormField, FormFieldTypes } from './forms/forms'
 
 type Option = {
-  key: string
+  key: string | number
   displayText: string
 }
 
@@ -18,27 +18,21 @@ type SearchSelectProps = {
 
 export const SearchSelect: React.FC<SearchSelectProps> = (props: SearchSelectProps) => {
   const { onSelection, placeholder, options, value, isClearable, disabled = false } = props
-  const [selectedInstitution, setSelectedInstitution] = useState<Option | undefined>()
+  const [selectedOption, setSelectedOption] = useState<Option | undefined>()
 
   useEffect(() => {
-    const selected = options.find(o => o.key === value)
-    setSelectedInstitution(selected)
+    const selected = options.find(option => option.key === value)
+    setSelectedOption(selected)
   }, [value, options])
 
   return (
     <FormField
       id={'form_field_selection_id_' + value}
       type={FormFieldTypes.SELECT}
-      selectOptions={options.map(i => ({
-        institutionId: i?.key,
-        displayText: i?.displayText,
-      }))}
+      selectOptions={options}
       placeholder={placeholder}
       isCreatable={false}
-      defaultValue={{
-        institutionId: selectedInstitution?.key,
-        displayText: selectedInstitution?.displayText,
-      }}
+      defaultValue={selectedOption}
       isClearable={isClearable}
       onChange={onSelection}
       disabled={disabled}
