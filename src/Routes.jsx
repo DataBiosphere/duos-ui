2bdff5e0df98e70f2a101dc52c4945d3a96428b6
rushtong <grushton@broadinstitute.org>
2025-09-26 08:46:46 -0400

    add redirects for study identifier, dataset identifier; tests.

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -133,6 +133,7 @@ const Routes = props => (
     <AuthenticatedRoute path="/datalibrary" component={DatasetSearch} props={props} rolesAllowed={[USER_ROLES.admin, USER_ROLES.all]} />
     <AuthenticatedRoute path="/studies/:studyId" component={StudyDetails} props={props} rolesAllowed={[USER_ROLES.admin, USER_ROLES.all]} />
     <AuthenticatedRoute path="/dataset/:datasetIdentifier" component={DatasetStatistics} props={props} rolesAllowed={[USER_ROLES.all]} />
+    <Redirect from="/DUOS-S:studyId" to="/studies/:studyId" props={props} rolesAllowed={[USER_ROLES.all]} />
     <Redirect from="/DUOS-:duosId" to="/dataset/DUOS-:duosId" props={props} rolesAllowed={[USER_ROLES.all]} />
     <AuthenticatedRoute path="/dac_datasets" component={DACDatasets} props={props} rolesAllowed={[USER_ROLES.chairperson]} />
     <AuthenticatedRoute path="/tos_acceptance" component={TermsOfServiceAcceptance} props={props} rolesAllowed={[USER_ROLES.all]} />
