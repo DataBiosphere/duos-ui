ff5c9047dac13774610b1ee4d4606cd97393a8c7
otchet-broad <111771148+otchet-broad@users.noreply.github.com>
2025-09-29 21:07:15 -0400

    [DT-2286] Adjust links per ticket. (#3072)

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -8,21 +8,21 @@ describe('Home', function () {
     cy.contains('Institutional Oversight')
     cy.contains('Looking for data')
     cy.contains('Data Libraries in DUOS')
-    cy.get('#terra-support-dac-link').should(
+    cy.get('#blog-support-dac-link').should(
       'have.attr',
       'href',
-      'https://support.terra.bio/hc/en-us/articles/28513346337179-Overview-DUOS-for-Data-Access-Committees-DACs',
+      'https://duos.blog/help/dacguide/',
     )
     cy.get('#terra-support-so-link').should(
       'have.attr',
       'href',
-      'https://support.terra.bio/hc/en-us/articles/28512587249051-How-to-Pre-Authorize-Researchers-to-Submit-Data-Access-Requests-in-DUOS',
+      'https://duos.blog/help/preauthorize_researchers_librarycards/',
     )
 
-    cy.get('#terra-support-researcher-link').should(
+    cy.get('#blog-support-researcher-link').should(
       'have.attr',
       'href',
-      'https://support.terra.bio/hc/en-us/articles/28510385779099-Overview-DUOS-for-Researchers',
+      'https://duos.blog/help/researcherguide/',
     )
   })
 })
