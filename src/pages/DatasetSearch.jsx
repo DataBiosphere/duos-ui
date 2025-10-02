6cc3c1b960a0808c26f33fe9408ef53f51d2d871
Austin Embrack <austinembrack@Austins-MBP.lan>
2025-10-02 15:33:22 -0400

    Improve data library performance: reduce over-fetching and optimize table rendering

--- src/pages/DatasetSearch.jsx
+++ src/pages/DatasetSearch.jsx
@@ -83,7 +83,7 @@ const assembleFullQuery = (isSigningOfficial, isInstitutionQuery, subQuery) => {
 
   return {
     from: 0,
-    size: 10000,
+    size: 50,
     query: {
       bool: {
         must: queryChunks,
