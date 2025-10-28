c924d245f98f59c505f608b729d934a2cf3af967
rushtong <grushton@broadinstitute.org>
2025-10-28 13:37:46 -0400

    fix redirectTo from unauthenticated state

--- src/routing/Authenticated.tsx
+++ src/routing/Authenticated.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import { Notifications } from 'src/libs/utils'
 import { Storage } from 'src/libs/storage'
-import { Navigate, Outlet } from 'react-router-dom'
+import { Navigate, Outlet, useLocation } from 'react-router-dom'
 
 const Authenticated = () => {
+  const location = useLocation()
   const [authenticated, setAuthenticated] = useState<boolean | null>(null)
   const [isLoading, setIsLoading] = useState(true)
 
@@ -32,7 +33,7 @@ const Authenticated = () => {
 
   return authenticated
     ? <Outlet />
-    : <Navigate to="/" />
+    : <Navigate to={location.pathname ? `/?redirectTo=${location.pathname}` : '/'} />
 }
 
 export default Authenticated
