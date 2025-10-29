bf7c3face1cdaa8b23492ce7f9106e37c2902576
Eric Weitz <eweitz@broadinstitute.org>
2025-10-29 13:03:05 -0400

    [DT-2298, DT-2337] Enrich data libraries on home page (#3128)

--- cypress/component/Home/home.spec.jsx
+++ cypress/component/Home/home.spec.jsx
@@ -24,13 +24,14 @@ describe('Home Page - Tests', function () {
     })
 
     it('displays tooltips with login required message for data libraries', function () {
-      cy.get('[data-for="anvil"]').find('span[title="Please login to access AnVIL Data Library"]').should('exist')
-      cy.get('[data-for="broad"]').find('span[title="Please login to access Broad Institute Data Library"]').should('exist')
-      cy.get('[data-for="hca"]').find('span[title="Please login to access Human Cell Atlas Data Library"]').should('exist')
+      // Just check that the first few featured libraries have tooltips
+      cy.get('[data-for="/datalibrary"]').find('span[title="Please login to access DUOS Data Library"]').should('exist')
+      cy.get('[data-for="broad"]').find('span[title="Please login to access Broad Data Library"]').should('exist')
+      cy.get('[data-for="elwazi"]').find('span[title="Please login to access eLwazi Data Library"]').should('exist')
     })
 
     it('interacts with library card links when not logged in', function () {
-      cy.get('.logo-card').should('have.length', 3)
+      cy.get('.logo-card').should('have.length', 20)
       cy.get('.logo-card').each(($card) => {
         cy.wrap($card).find('a').should('exist')
       })
@@ -56,7 +57,7 @@ describe('Home Page - Tests', function () {
       cy.get('@scrollTo').should('be.called')
 
       // URL should contain the redirectTo parameter
-      cy.location('search').should('include', 'redirectTo=%2Fdatalibrary%2Fanvil')
+      cy.location('search').should('include', 'redirectTo=%2Fdatalibrary%2F%2Fdatalibrary')
     })
   })
 
@@ -80,35 +81,104 @@ describe('Home Page - Tests', function () {
     })
 
     it('displays tooltips with correct text for data libraries', function () {
-      cy.get('[data-for="anvil"]').find('span[title="AnVIL"]').should('exist')
-      cy.get('[data-for="broad"]').find('span[title="Broad Institute"]').should('exist')
-      cy.get('[data-for="hca"]').find('span[title="Human Cell Atlas"]').should('exist')
+      // Check first few featured libraries
+      cy.get('[data-for="/datalibrary"]').find('span[title="DUOS"]').should('exist')
+      cy.get('[data-for="broad"]').find('span[title="Broad"]').should('exist')
+      cy.get('[data-for="elwazi"]').find('span[title="eLwazi"]').should('exist')
     })
 
     it('has direct navigation links when logged in', function () {
-      cy.get('a[href="/datalibrary/anvil"]').should('exist')
+      // Check first few featured libraries
+      cy.get('a[href="/datalibrary//datalibrary"]').should('exist')
       cy.get('a[href="/datalibrary/broad"]').should('exist')
-      cy.get('a[href="/datalibrary/HCA"]').should('exist')
+      cy.get('a[href="/datalibrary/elwazi"]').should('exist')
     })
 
     it('navigates directly without calling handleSignIn when logged in', function () {
       cy.window().then((win) => {
         cy.spy(win.history, 'replaceState').as('replaceState')
       })
-      cy.get('a[href="/datalibrary/anvil"]').click({ force: true })
+      cy.get('a[href="/datalibrary/broad"]').click({ force: true })
       cy.get('@replaceState').should('not.be.called')
     })
 
     it('displays logos horizontally on desktop', function () {
       cy.viewport(1200, 800)
       cy.get('.logo-grid').should('have.css', 'flex-direction', 'row')
-      cy.get('.logo-card').should('have.length', 3)
+      cy.get('.logo-card').should('have.length', 20)
     })
 
-    it('displays logos vertically on mobile', function () {
+    it('displays logos responsively on mobile', function () {
       cy.viewport(600, 800)
-      cy.get('.logo-grid').should('have.css', 'flex-direction', 'column')
-      cy.get('.logo-card').should('have.length', 3)
+      cy.get('.logo-grid').should('have.css', 'display', 'flex')
+      cy.get('.logo-grid').should('have.css', 'flex-wrap', 'wrap')
+      cy.get('.logo-card').should('have.length', 20)
+    })
+  })
+
+  describe('Featured libraries functionality', function () {
+    it('only displays libraries marked as featured', function () {
+      mount(
+        <MemoryRouter>
+          <Home isLogged={true} />
+        </MemoryRouter>,
+      )
+
+      // Should show 20 featured libraries
+      cy.get('.logo-card').should('have.length', 20)
+
+      // Should not display terra or mgb (featured: false)
+      cy.get('[data-for="terra"]').should('not.exist')
+      cy.get('[data-for="mgb"]').should('not.exist')
+    })
+
+    it('displays featured libraries in order', function () {
+      mount(
+        <MemoryRouter>
+          <Home isLogged={true} />
+        </MemoryRouter>,
+      )
+
+      cy.get('.logo-card').should('have.length', 20)
+
+      // Verify first three libraries are in correct order
+      cy.get('.logo-card').eq(0).find('img').should('have.attr', 'alt', 'DUOS')
+      cy.get('.logo-card').eq(1).find('img').should('have.attr', 'alt', 'Broad')
+      cy.get('.logo-card').eq(2).find('img').should('have.attr', 'alt', 'eLwazi')
+    })
+
+    it('uses special styling for Broad Institute logo', function () {
+      mount(
+        <MemoryRouter>
+          <Home isLogged={true} />
+        </MemoryRouter>,
+      )
+
+      // Find the Broad card by looking for the Broad image (alt="Broad", not "Broad Data Library")
+      cy.get('.logo-card').filter(':has(img[alt="Broad"])')
+        .should('have.css', 'background', 'rgb(31, 59, 80) none repeat scroll 0% 0% / auto padding-box border-box')
+        .and('have.css', 'padding', '15px')
+    })
+
+    it('handles responsive layout correctly', function () {
+      mount(
+        <MemoryRouter>
+          <Home isLogged={true} />
+        </MemoryRouter>,
+      )
+
+      // Desktop
+      cy.viewport(1200, 800)
+      cy.get('.logo-grid').should('have.css', 'display', 'flex')
+      cy.get('.logo-grid').should('have.css', 'justify-content', 'center')
+
+      // Tablet
+      cy.viewport(768, 1024)
+      cy.get('.logo-card').should('have.length', 20)
+
+      // Mobile
+      cy.viewport(480, 800)
+      cy.get('.logo-card').should('have.length', 20)
     })
   })
 })
--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -3,7 +3,7 @@ describe('Home', function () {
     cy.visit(Cypress.env('baseUrl'))
     cy.contains('DUOS')
     cy.contains('Sign In')
-    cy.contains('What is DUOS and how does it work?')
+    cy.contains('How does DUOS expedite compliant data sharing?')
     cy.contains('DUOS for DACs')
     cy.contains('Institutional Oversight')
     cy.contains('Looking for data')
@@ -13,16 +13,16 @@ describe('Home', function () {
       'href',
       'https://duos.blog/help/dacguide/',
     )
-    cy.get('#terra-support-so-link').should(
+    cy.get('#blog-support-so-link').should(
       'have.attr',
       'href',
       'https://duos.blog/help/preauthorize_researchers_librarycards/',
     )
 
-    cy.get('#blog-support-researcher-link').should(
+    cy.get('#data-library-link').should(
       'have.attr',
       'href',
-      'https://duos.blog/help/researcherguide/',
+      'https://duos.org/datalibrary',
     )
   })
 })
