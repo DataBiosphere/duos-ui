d9b9acfc4f185429263be3ac79a2f5d3f29c3d23
rushtong <grushton@broadinstitute.org>
2025-10-29 11:57:32 -0400

    simplify and tests for role route

--- src/routing/RoleBAC.tsx
+++ src/routing/RoleBAC.tsx
@@ -1,54 +1,24 @@
-import React, { useEffect, useState } from 'react'
-import * as Utils from 'src/libs/utils'
-import { Notifications } from 'src/libs/utils'
+import React from 'react'
+import { USER_ROLES } from 'src/libs/utils'
 import { Storage } from 'src/libs/storage'
 import { UserRole } from 'src/types/model'
 import { Navigate, Outlet } from 'react-router-dom'
-import { extractError } from 'src/utils/ErrorUtils'
 
 interface RoleBACProps {
   readonly rolesAllowed: string[]
 }
 
 const RoleBAC = ({ rolesAllowed }: RoleBACProps) => {
-  const [roleAllowed, setRoleAllowed] = useState<boolean | null>(null)
-  const [isLoading, setIsLoading] = useState(true)
-
   const checkRoles = (allowedRoles: Array<string>) => {
     const user = Storage.getCurrentUser()
     const userRoles: Array<UserRole> = user.roles ? user.roles : []
     const currentUserRoleNames = new Set(userRoles.map(role => role.name as string))
     return allowedRoles.some(
-      allowedRole => (currentUserRoleNames.has(allowedRole) || allowedRole === Utils.USER_ROLES.all),
+      allowedRole => (currentUserRoleNames.has(allowedRole) || allowedRole === USER_ROLES.all),
     )
   }
 
-  useEffect(() => {
-    const init = async () => {
-      try {
-        setIsLoading(true)
-        // Check if SO has accepted the required DAA acknowledgments
-        const accepted = checkRoles(rolesAllowed)
-        setRoleAllowed(accepted)
-        setIsLoading(false)
-      }
-      catch (error) {
-        const message = extractError(error)
-        Notifications.showError({
-          text: 'Error: Unable to verify user roles: ' + message,
-        })
-        setRoleAllowed(false)
-        setIsLoading(false)
-      }
-    }
-    init()
-  }, [rolesAllowed])
-
-  if (isLoading) {
-    return <div>Loading...</div>
-  }
-
-  return roleAllowed
+  return checkRoles(rolesAllowed)
     ? <Outlet />
     : <Navigate to="/" />
 }
