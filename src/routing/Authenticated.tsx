c30169939ecdb5a0f6deb748086cf6e35d08822a
rushtong <grushton@broadinstitute.org>
2025-10-29 12:35:56 -0400

    tests for authenticated route

--- src/routing/Authenticated.tsx
+++ src/routing/Authenticated.tsx
@@ -1,37 +1,11 @@
-import React, { useEffect, useState } from 'react'
-import { Notifications } from 'src/libs/utils'
+import React from 'react'
 import { Storage } from 'src/libs/storage'
 import { Navigate, Outlet, useLocation } from 'react-router-dom'
 
 const Authenticated = () => {
   const location = useLocation()
-  const [authenticated, setAuthenticated] = useState<boolean | null>(null)
-  const [isLoading, setIsLoading] = useState(true)
 
-  useEffect(() => {
-    const init = () => {
-      try {
-        setIsLoading(true)
-        setAuthenticated(Storage.userIsLogged())
-        setIsLoading(false)
-      }
-      catch (error) {
-        console.error('Error checking authentication:', error)
-        Notifications.showError({
-          text: 'Error: Unable to verify authentication',
-        })
-        setAuthenticated(false)
-        setIsLoading(false)
-      }
-    }
-    init()
-  }, [])
-
-  if (isLoading) {
-    return <div>Loading...</div>
-  }
-
-  return authenticated
+  return Storage.userIsLogged()
     ? <Outlet />
     : <Navigate to={location.pathname ? `/?redirectTo=${location.pathname}` : '/'} />
 }
