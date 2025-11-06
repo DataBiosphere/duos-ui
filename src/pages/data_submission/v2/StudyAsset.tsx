4d0c6f385985500d7eddc16a8101f5f2b23d0978
Kevin Marete <kevinmmarete@gmail.com>
2025-11-06 18:57:06 -0500

    Use button and children props

--- src/pages/data_submission/v2/StudyAsset.tsx
+++ src/pages/data_submission/v2/StudyAsset.tsx
@@ -4,7 +4,8 @@ export interface StudyAssetConfig {
   icon: string
   title: string
   description: string
-  component: React.ReactNode
+  children?: React.ReactNode
+  button?: React.ReactNode
 }
 
 export interface StudyAssetProps {
@@ -12,29 +13,34 @@ export interface StudyAssetProps {
 }
 
 export const StudyAsset: React.FC<StudyAssetProps> = ({ config }) => {
-  const { icon, title, description, component } = config
+  const { icon, title, description, children, button } = config
 
   return (
-    <div style={{ marginTop: '2rem' }}>
+    <div style={{ marginTop: '2rem', width: '100%' }}>
       <div style={{
         display: 'flex',
-        alignItems: 'center',
-        gap: '1rem',
+        alignItems: 'flex-start',
+        gap: '2rem',
         marginBottom: '1rem',
       }}
       >
-        <span
-          className={icon}
-          style={{ fontSize: '32px', flexShrink: 0 }}
-          aria-hidden="true"
-        />
-        <div style={{ flex: 1 }}>
-          <h3 style={{ margin: 0 }}>{title}</h3>
-          <p style={{ margin: '0.25rem 0 0 0', color: '#666' }}>{description}</p>
+        <div style={{
+          display: 'flex',
+          alignItems: 'flex-start',
+          gap: '1rem',
+          flex: 1,
+        }}
+        >
+          <span className={icon} style={{ fontSize: '32px', flexShrink: 0 }} aria-hidden="true" />
+          <div>
+            <h3 style={{ margin: 0 }}>{title}</h3>
+            <p style={{ margin: '0.25rem 0 0 0', color: '#666' }}>{description}</p>
+          </div>
         </div>
-        <div style={{ flexShrink: 0 }}>
-          {component}
-        </div>
+        {button && <div style={{ flexShrink: 0 }}>{button}</div>}
+      </div>
+      <div style={{ width: '100%' }}>
+        {children}
       </div>
     </div>
   )
--- src/pages/data_submission/v2/StudyAssetManagement.tsx
+++ src/pages/data_submission/v2/StudyAssetManagement.tsx
@@ -1,5 +1,5 @@
 import { Study } from 'src/pages/data_submission/v2/v2-models'
-import React, { useState } from 'react'
+import React, { ReactNode, useState } from 'react'
 import PublicationList from 'src/components/publications_list/PublicationList'
 import PresentationList from 'src/components/presentations_list/PresentationList'
 import IntellectualPropertyList from 'src/components/intellectual_property_list/IntellectualPropertyList'
@@ -12,7 +12,7 @@ import {
   Publication,
   Workspace,
 } from 'src/types/model'
-import { StudyAsset, StudyAssetConfig } from 'src/pages/data_submission/v2/StudyAsset'
+import { StudyAsset } from 'src/pages/data_submission/v2/StudyAsset'
 import AiModelList from 'src/components/ai_models_list/AiModelList'
 import WorkspaceList from 'src/components/workspaces_list/WorkspaceList'
 import ClinicalTrialList from 'src/components/clinical_trial_list/ClinicalTrialList'
@@ -36,140 +36,183 @@ export const StudyAssetManagement = (props: StudyAssetManagementProps) => {
 
   const onModelChange = (models: AiModel[]) => {
     setModels(models)
-    setStudy(prev => ({ ...prev, models }))
+    setStudy(prev => ({
+      ...prev,
+      assets: { ...prev.assets, models },
+    }))
   }
 
   const onWorkspaceChange = (workspaces: Workspace[]) => {
     setWorkspaces(workspaces)
-    setStudy(prev => ({ ...prev, workspaces }))
+    setStudy(prev => ({
+      ...prev,
+      assets: { ...prev.assets, workspaces },
+    }))
   }
 
   const onPublicationChange = (publications: Publication[]) => {
     setPublications(publications)
-    setStudy(prev => ({ ...prev, publications }))
+    setStudy(prev => ({
+      ...prev,
+      assets: { ...prev.assets, publications },
+    }))
   }
 
   const onPresentationChange = (presentations: Presentation[]) => {
     setPresentations(presentations)
-    setStudy(prev => ({ ...prev, presentations }))
+    setStudy(prev => ({
+      ...prev,
+      assets: { ...prev.assets, presentations },
+    }))
   }
 
   const onClinicalTrialChange = (clinicalTrials: ClinicalTrial[]) => {
     setClinicalTrials(clinicalTrials)
-    setStudy(prev => ({ ...prev, clinicalTrials }))
+    setStudy(prev => ({
+      ...prev,
+      assets: { ...prev.assets, clinicalTrials },
+    }))
   }
 
   const onIntellectualPropertyChange = (intellectualProperties: IntellectualProperty[]) => {
     setIntellectualProperties(intellectualProperties)
-    setStudy(prev => ({ ...prev, intellectualProperties }))
+    setStudy(prev => ({
+      ...prev,
+      assets: { ...prev.assets, intellectualProperty: intellectualProperties },
+    }))
   }
 
   const onFundingResourceChange = (fundingResources: FundingResource[]) => {
     setFundingResources(fundingResources)
-    setStudy(prev => ({ ...prev, fundingResources }))
+    setStudy(prev => ({
+      ...prev,
+      assets: { ...prev.assets, funding: fundingResources },
+    }))
   }
-
-  const assetConfigs: StudyAssetConfig[] = [
-    {
-      icon: 'glyphicon glyphicon-hdd',
-      title: 'Datasets',
-      description: 'Add datasets associated with this study',
-      component: <button type="button">Add Dataset</button>,
-    },
-    {
-      icon: 'glyphicon glyphicon-cog',
-      title: 'Models',
-      description: 'Add computational models or algorithms derived from this study',
-      component: (
-        <AiModelList
-          aiModels={models}
-          onAiModelsChange={onModelChange}
-          disabled={false}
-        />
-      ),
-    },
-    {
-      icon: 'glyphicon glyphicon-briefcase',
-      title: 'Featured Workspaces',
-      description: 'Add computational workspaces for data analysis',
-      component: (
-        <WorkspaceList
-          workspaces={workspaces}
-          onWorkspaceChange={onWorkspaceChange}
-          disabled={false}
-        />
-      ),
-    },
-    {
-      icon: 'glyphicon glyphicon-file',
-      title: 'Publications',
-      description: 'Add published research papers related to this study',
-      component: (
-        <PublicationList
-          publications={publications}
-          onPublicationChange={onPublicationChange}
-          disabled={false}
-        />
-      ),
-    },
-    {
-      icon: 'glyphicon glyphicon-bullhorn',
-      title: 'Presentations',
-      description: 'Add conference presentations or talks about this study',
-      component: (
-        <PresentationList
-          presentations={presentations}
-          onPresentationChange={onPresentationChange}
-          disabled={false}
-        />
-      ),
-    },
-    {
-      icon: 'glyphicon glyphicon-stats',
-      title: 'Clinical Trials',
-      description: 'Add clinical trials associated with this study',
-      component: (
-        <ClinicalTrialList
-          clinicalTrials={clinicalTrials}
-          onClinicalTrialChange={onClinicalTrialChange}
-          disabled={false}
-        />
-      ),
-    },
-    {
-      icon: 'glyphicon glyphicon-copyright-mark',
-      title: 'Intellectual Property',
-      description: 'Add patents or other IP related to this study',
-      component: (
-        <IntellectualPropertyList
-          intellectualProperties={intellectualProperties}
-          onIntellectualPropertyChange={onIntellectualPropertyChange}
-          disabled={false}
-        />
-      ),
-    },
-    {
-      icon: 'glyphicon glyphicon-usd',
-      title: 'Funding Resources',
-      description: 'Add grants and funding sources for this study',
-      component: (
-        <FundingResourceList
-          fundingResources={fundingResources}
-          onFundingResourceChange={onFundingResourceChange}
-          disabled={false}
-        />
-      ),
-    },
-  ]
 
   return (
     <div className="data-submitter-section">
       <h2>Study Assets</h2>
       <p>Add datasets, models, workspaces, and other resources associated with this study</p>
 
-      {assetConfigs.map((config, index) => (
-        <StudyAsset key={index} config={config} />
-      ))}
+      <AiModelList
+        aiModels={models}
+        onAiModelsChange={onModelChange}
+        disabled={false}
+        studyAssetWrapper={(content: ReactNode, button: ReactNode) => (
+          <StudyAsset
+            config={{
+              icon: 'glyphicon glyphicon-cog',
+              title: 'Models',
+              description: 'Add computational models or algorithms derived from this study',
+              children: content,
+              button: button,
+            }}
+          />
+        )}
+      />
+
+      <WorkspaceList
+        workspaces={workspaces}
+        onWorkspaceChange={onWorkspaceChange}
+        disabled={false}
+        studyAssetWrapper={(content: ReactNode, button: ReactNode) => (
+          <StudyAsset
+            config={{
+              icon: 'glyphicon glyphicon-briefcase',
+              title: 'Featured Workspaces',
+              description: 'Add computational workspaces for data analysis',
+              children: content,
+              button: button,
+            }}
+          />
+        )}
+      />
+
+      <PublicationList
+        publications={publications}
+        onPublicationChange={onPublicationChange}
+        disabled={false}
+        studyAssetWrapper={(content: ReactNode, button: ReactNode) => (
+          <StudyAsset
+            config={{
+              icon: 'glyphicon glyphicon-file',
+              title: 'Publications',
+              description: 'Add published research papers related to this study',
+              children: content,
+              button: button,
+            }}
+          />
+        )}
+      />
+
+      <PresentationList
+        presentations={presentations}
+        onPresentationChange={onPresentationChange}
+        disabled={false}
+        studyAssetWrapper={(content: ReactNode, button: ReactNode) => (
+          <StudyAsset
+            config={{
+              icon: 'glyphicon glyphicon-bullhorn',
+              title: 'Presentations',
+              description: 'Add conference presentations or talks about this study',
+              children: content,
+              button: button,
+            }}
+          />
+        )}
+      />
+
+      <ClinicalTrialList
+        clinicalTrials={clinicalTrials}
+        onClinicalTrialChange={onClinicalTrialChange}
+        disabled={false}
+        studyAssetWrapper={(content: ReactNode, button: ReactNode) => (
+          <StudyAsset
+            config={{
+              icon: 'glyphicon glyphicon-stats',
+              title: 'Clinical Trials',
+              description: 'Add clinical trials associated with this study',
+              children: content,
+              button: button,
+            }}
+          />
+        )}
+      />
+
+      <IntellectualPropertyList
+        intellectualProperties={intellectualProperties}
+        onIntellectualPropertyChange={onIntellectualPropertyChange}
+        disabled={false}
+        studyAssetWrapper={(content: ReactNode, button: ReactNode) => (
+          <StudyAsset
+            config={{
+              icon: 'glyphicon glyphicon-copyright-mark',
+              title: 'Intellectual Property',
+              description: 'Add patents or other IP related to this study',
+              children: content,
+              button: button,
+            }}
+          />
+        )}
+      />
+
+      <FundingResourceList
+        fundingResources={fundingResources}
+        onFundingResourceChange={onFundingResourceChange}
+        disabled={false}
+        studyAssetWrapper={(content: ReactNode, button: ReactNode) => (
+          <StudyAsset
+            config={{
+              icon: 'glyphicon glyphicon-usd',
+              title: 'Funding Resources',
+              description: 'Add grants and funding sources for this study',
+              children: content,
+              button: button,
+            }}
+          />
+        )}
+      />
     </div>
   )
 }
