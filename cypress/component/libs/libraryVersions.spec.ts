7d766f954f7847fe3552de1127c5fdf2b7ab92af
Kevin Marete <kevinmmarete@gmail.com>
2025-11-07 18:51:22 -0500

    Fix tests

--- cypress/component/libs/libraryVersions.spec.ts
+++ cypress/component/libs/libraryVersions.spec.ts
@@ -39,7 +39,7 @@ describe('Library Versions - Tests', function () {
       expect(featuredLibraries).to.include('scp')
 
       // Verify count
-      expect(featuredLibraries.length).to.equal(21)
+      expect(featuredLibraries.length).to.equal(20)
     })
 
     it('marks non-featured libraries correctly', function () {
@@ -138,8 +138,8 @@ describe('Library Versions - Tests', function () {
         .map(([key, library]) => ({ key, ...library }))
         .sort((a, b) => a.order - b.order || a.key.localeCompare(b.key))
 
-      // Should have 21 featured libraries
-      expect(featuredLibraries.length).to.equal(21)
+      // Should have 20 featured libraries
+      expect(featuredLibraries.length).to.equal(20)
 
       // Should be sorted by order parameter
       expect(featuredLibraries[0].key).to.equal('/datalibrary') // order: 1
