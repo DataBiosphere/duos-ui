7a675e4c9baec40c1dd6783e641de3b7294f4934
Kevin Marete <kevinmmarete@gmail.com>
2025-11-07 19:03:14 -0500

    Declare jpg image sources

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -10,4 +10,18 @@ declare module '*.pdf' {
   const src: string
   export default src
 }
-declare module '*.png'
+
+declare module '*.png' {
+  const src: string
+  export default src
+}
+
+declare module '*.jpg' {
+  const src: string
+  export default src
+}
+
+declare module '*.jpeg' {
+  const src: string
+  export default src
+}
