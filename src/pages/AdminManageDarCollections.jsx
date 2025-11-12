7aef1e4b3926a5ba8480b6e292e292aab399c03a
Eric Weitz <eweitz@broadinstitute.org>
2025-11-12 11:02:32 -0500

    Incidentally refactor to absolute imports in other modules, too

--- src/pages/AdminManageDarCollections.jsx
+++ src/pages/AdminManageDarCollections.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import { useState, useEffect, useRef, useCallback } from 'react'
-import SearchBar from '../components/SearchBar'
-import { Collections } from '../libs/ajax/Collections'
-import { Notifications, searchOnFilteredList, getSearchFilterFunctions, USER_ROLES } from '../libs/utils'
-import { Styles } from '../libs/theme'
-import lockIcon from '../images/lock-icon.png'
-import { DarCollectionTable } from '../components/dar_collection_table/DarCollectionTable'
+import SearchBar from 'src/components/SearchBar'
+import { Collections } from 'src/libs/ajax/Collections'
+import { Notifications, searchOnFilteredList, getSearchFilterFunctions, USER_ROLES } from 'src/libs/utils'
+import { Styles } from 'src/libs/theme'
+import lockIcon from 'src/images/lock-icon.png'
+import { DarCollectionTable } from 'src/components/dar_collection_table/DarCollectionTable'
 import {
   cancelCollectionFn,
   consoleTypes,
   openCollectionFn,
   updateCollectionFn,
-} from '../utils/DarCollectionUtils'
-import { useResponsiveDarCollectionColumns } from '../hooks/useResponsiveDarCollectionColumns'
-import { usePageTitle } from '../hooks/usePageTitle'
+} from 'src/utils/DarCollectionUtils'
+import { useResponsiveDarCollectionColumns } from 'src/hooks/useResponsiveDarCollectionColumns'
+import { usePageTitle } from 'src/hooks/usePageTitle'
 
 export default function AdminManageDarCollections() {
   usePageTitle('DAR Requests')
