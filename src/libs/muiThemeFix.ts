7d7d60feaad202058d0311d82594824cebbbb8d9
Florian Boulnois <fboulnoi@broadinstitute.org>
2025-11-12 22:48:41 +0000

    fix: ensure mui styles are correctly sized

---
+++ src/libs/muiThemeFix.ts
@@ -0,0 +1,18 @@
+import { createTheme } from '@mui/material/styles'
+
+/**
+ * Global Material UI Theme Configuration
+ *
+ * This theme configures MUI to work correctly despite the root HTML font-size
+ * being 10px due to bootstrap_replacement.css. Since rem units are calculated
+ * relative to root font-size, all MUI default sizes need to be adjusted
+ * proportionally.
+ *
+ * This file should be removed once bootstrap_replacement.css is removed.
+ */
+export const muiThemeFix = createTheme({
+  typography: {
+    htmlFontSize: 10,
+    fontFamily: 'Montserrat, Helvetica Neue, Helvetica, Arial, sans-serif',
+  },
+})
