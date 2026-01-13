902095f101328a37e4a3c20a2b31eb4f8a94baf6
Gregory Rushton <rushtong@users.noreply.github.com>
2026-01-13 11:29:04 -0500

    DT-2548: Migrate App to Typescript (#3257)

--- cypress/component/App.spec.tsx
+++ cypress/component/App.spec.tsx
@@ -154,7 +154,7 @@ describe('Main App Functions', () => {
     cy.stub(AuthenticateNIH, 'getECMProviderLinkInfo').returns(linkInfo)
     cy.stub(AuthenticateNIH, 'getSyncedUser').returns(user)
     const pageVisitStub = cy.stub()
-    const LocationSpy = ({ onLocationChange }) => {
+    const LocationSpy = ({ onLocationChange }: { onLocationChange: (pathname: string) => void }) => {
       const location = useLocation()
       React.useEffect(() => {
         onLocationChange(location.pathname)
