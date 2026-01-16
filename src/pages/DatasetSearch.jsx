4d5a0fc548370d85da3d98b80cb34ab7d6e2267d
Eric Weitz <eweitz@broadinstitute.org>
2026-01-16 16:55:27 -0500

    Fix lint

--- src/pages/DatasetSearch.jsx
+++ src/pages/DatasetSearch.jsx
@@ -144,7 +144,7 @@ export const DatasetSearch = (props) => {
       }
     }
     init()
-  }, [loading, isInstitutionSet, fullQuery, navigate, hasChangedPage])
+  }, [loading, isInstitutionSet, fullQuery, navigate, hasChangedPage, query])
 
   return (
     loading
@@ -153,15 +153,17 @@ export const DatasetSearch = (props) => {
             <CircularProgress />
           </Box>
         )
-      : <DatasetSearchTable
-          {...props}
-          datasets={datasets}
-          icon={version.icon}
-          title={version.title}
-          assembleFullQuery={assembleFullQuery}
-          isSigningOfficial={isSigningOfficial}
-          isInstitutionQuery={isInstitutionQuery}
-        />
+      : (
+          <DatasetSearchTable
+            {...props}
+            datasets={datasets}
+            icon={version.icon}
+            title={version.title}
+            assembleFullQuery={assembleFullQuery}
+            isSigningOfficial={isSigningOfficial}
+            isInstitutionQuery={isInstitutionQuery}
+          />
+        )
   )
 }
 
