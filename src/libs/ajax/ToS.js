fa67418a2cca7be78817f78d1bfd0059bce76e3b
Gregory Rushton <rushtong@users.noreply.github.com>
2026-02-25 08:21:14 -0500

    DT-2918: Get Terms of Service state from user instead of separate API call (#3337)

--- src/libs/ajax/ToS.js
+++ src/libs/ajax/ToS.js
@@ -7,24 +7,6 @@ export const ToS = {
     const res = await fetchGet(url, Config.textPlain())
     return res.data
   },
-  /**
-   * Returns a json structure of various statuses for an authenticated user.
-   * See https://consent.dsde-prod.broadinstitute.org/#/Sam/get_api_sam_register_self_diagnostics
-   * for more info.
-   * {
-   *   'adminEnabled': false,
-   *   'enabled': false,
-   *   'inAllUsersGroup': true,
-   *   'inGoogleProxyGroup': false,
-   *   'tosAccepted': true
-   * }
-   * @returns {Promise<any>}
-   */
-  getStatus: async () => {
-    const url = `${await Config.getApiUrl()}/api/sam/register/self/diagnostics`
-    const res = await fetchGet(url, Config.authOpts())
-    return res.data
-  },
   acceptToS: async () => {
     const url = `${await Config.getApiUrl()}/api/sam/register/self/tos`
     const res = await fetchPost(url, {}, Config.authOpts())
