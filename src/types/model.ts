3fbd96313fdfde00c6c7ac65c949e59b76b9d744
Kevin Marete <kevinmmarete@gmail.com>
2026-03-03 10:40:50 -0500

    Revert "Add assets to StudyTerm"
    This reverts commit ae8755a7dd39ea8a9a09a328babc0606dd8216f3.

--- src/types/model.ts
+++ src/types/model.ts
@@ -202,16 +202,6 @@ export interface StudyTerm {
   dataCustodianEmail: string[]
   publicVisibility: boolean
   dataTypes: string[]
-  assets?: {
-    models?: Array<AiModel>
-    workspaces?: Array<Workspace>
-    publications?: Array<Publication>
-    presentations?: Array<Presentation>
-    clinicalTrials?: Array<ClinicalTrial>
-    funding?: Array<FundingResource>
-    intellectualProperties?: Array<IntellectualProperty>
-    biospecimens?: Array<Biospecimen>
-  }
 }
 
 export interface DataUseTerm {
