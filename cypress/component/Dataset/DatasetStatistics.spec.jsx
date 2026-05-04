0bca4eada5e08c1586eeb9f53d3457f9e61bcda0
Greg <grushton@broadinstitute.org>
2026-05-03 20:43:52 -0400

    feat: locally (only) failing test fix

--- cypress/component/Dataset/DatasetStatistics.spec.jsx
+++ cypress/component/Dataset/DatasetStatistics.spec.jsx
@@ -3,7 +3,18 @@ import DatasetStatistics from 'src/pages/DatasetStatistics'
 import datasetTerm from './datasetTerm.json'
 import { DataSet } from 'src/libs/ajax/DataSet'
 import { DatasetMetrics } from 'src/libs/ajax/DatasetMetrics'
-import { BrowserRouter } from 'react-router-dom'
+import { MemoryRouter, Route, Routes } from 'react-router-dom'
+
+// Helper: mounts DatasetStatistics inside a real route so useParams() works correctly
+const mountDatasetStatistics = (datasetIdentifier) => {
+  cy.mount(
+    <MemoryRouter initialEntries={[`/dataset/${datasetIdentifier}`]}>
+      <Routes>
+        <Route path="/dataset/:datasetIdentifier" element={<DatasetStatistics />} />
+      </Routes>
+    </MemoryRouter>,
+  )
+}
 
 describe('Dataset Statistics Tests', () => {
   beforeEach(() => {
@@ -13,38 +24,14 @@ describe('Dataset Statistics Tests', () => {
   it('Renders the correct dataset from a DUOS-xxx identifier path paramter', () => {
     cy.stub(DataSet, 'searchDatasetIndex').returns(Promise.resolve([datasetTerm]))
     cy.stub(DatasetMetrics, 'getDatasetStats').returns(Promise.resolve([]))
-
-    const props = {
-      match: {
-        params: {
-          datasetIdentifier: datasetTerm.datasetIdentifier,
-        },
-      },
-      history: {
-        push() {
-        },
-      },
-    }
-    cy.mount(<BrowserRouter><DatasetStatistics {...props} /></BrowserRouter>)
+    mountDatasetStatistics(datasetTerm.datasetIdentifier)
     cy.contains(datasetTerm.datasetIdentifier).should('exist')
   })
 
   it('Renders the correct dataset from a DUOS-Dxxx identifier path parameter', () => {
     cy.stub(DataSet, 'searchDatasetIndex').returns(Promise.resolve([datasetTerm]))
     cy.stub(DatasetMetrics, 'getDatasetStats').returns(Promise.resolve([]))
-
-    const props = {
-      match: {
-        params: {
-          datasetIdentifier: 'DUOS-D' + datasetTerm.datasetId,
-        },
-      },
-      history: {
-        push() {
-        },
-      },
-    }
-    cy.mount(<BrowserRouter><DatasetStatistics {...props} /></BrowserRouter>)
+    mountDatasetStatistics('DUOS-D' + datasetTerm.datasetId)
     cy.contains(datasetTerm.datasetIdentifier).should('exist')
   })
 
@@ -52,19 +39,7 @@ describe('Dataset Statistics Tests', () => {
     const controlled = { ...datasetTerm, accessManagement: 'controlled' }
     cy.stub(DataSet, 'searchDatasetIndex').returns(Promise.resolve([controlled]))
     cy.stub(DatasetMetrics, 'getDatasetStats').returns(Promise.resolve([]))
-
-    const props = {
-      match: {
-        params: {
-          datasetIdentifier: datasetTerm.datasetIdentifier,
-        },
-      },
-      history: {
-        push() {
-        },
-      },
-    }
-    cy.mount(<BrowserRouter><DatasetStatistics {...props} /></BrowserRouter>)
+    mountDatasetStatistics(datasetTerm.datasetIdentifier)
     cy.contains(controlled.datasetIdentifier).should('exist')
     cy.contains(controlled.datasetName).should('exist')
     cy.contains('Apply for Access').should('exist')
@@ -74,19 +49,7 @@ describe('Dataset Statistics Tests', () => {
     const external = { ...datasetTerm, accessManagement: 'external', url: 'https://duos.org' }
     cy.stub(DataSet, 'searchDatasetIndex').returns(Promise.resolve([external]))
     cy.stub(DatasetMetrics, 'getDatasetStats').returns(Promise.resolve([]))
-
-    const props = {
-      match: {
-        params: {
-          datasetIdentifier: external.datasetIdentifier,
-        },
-      },
-      history: {
-        push() {
-        },
-      },
-    }
-    cy.mount(<BrowserRouter><DatasetStatistics {...props} /></BrowserRouter>)
+    mountDatasetStatistics(external.datasetIdentifier)
     cy.contains(external.datasetIdentifier).should('exist')
     cy.contains(external.datasetName).should('exist')
     cy.contains('This dataset is externally managed').should('exist')
@@ -97,19 +60,7 @@ describe('Dataset Statistics Tests', () => {
     const external = { ...datasetTerm, accessManagement: 'external' }
     cy.stub(DataSet, 'searchDatasetIndex').returns(Promise.resolve([external]))
     cy.stub(DatasetMetrics, 'getDatasetStats').returns(Promise.resolve([]))
-
-    const props = {
-      match: {
-        params: {
-          datasetIdentifier: external.datasetIdentifier,
-        },
-      },
-      history: {
-        push() {
-        },
-      },
-    }
-    cy.mount(<BrowserRouter><DatasetStatistics {...props} /></BrowserRouter>)
+    mountDatasetStatistics(external.datasetIdentifier)
     cy.contains(external.datasetIdentifier).should('exist')
     cy.contains(external.datasetName).should('exist')
     cy.contains('This dataset is externally managed').should('exist')
@@ -120,19 +71,7 @@ describe('Dataset Statistics Tests', () => {
     const open = { ...datasetTerm, accessManagement: 'open', url: 'https://duos.org' }
     cy.stub(DataSet, 'searchDatasetIndex').returns(Promise.resolve([open]))
     cy.stub(DatasetMetrics, 'getDatasetStats').returns(Promise.resolve([]))
-
-    const props = {
-      match: {
-        params: {
-          datasetIdentifier: open.datasetIdentifier,
-        },
-      },
-      history: {
-        push() {
-        },
-      },
-    }
-    cy.mount(<BrowserRouter><DatasetStatistics {...props} /></BrowserRouter>)
+    mountDatasetStatistics(open.datasetIdentifier)
     cy.contains(open.datasetIdentifier).should('exist')
     cy.contains(open.datasetName).should('exist')
     cy.contains('This dataset is open access, does not require an access request').should('exist')
@@ -143,19 +82,7 @@ describe('Dataset Statistics Tests', () => {
     const open = { ...datasetTerm, accessManagement: 'open' }
     cy.stub(DataSet, 'searchDatasetIndex').returns(Promise.resolve([open]))
     cy.stub(DatasetMetrics, 'getDatasetStats').returns(Promise.resolve([]))
-
-    const props = {
-      match: {
-        params: {
-          datasetIdentifier: open.datasetIdentifier,
-        },
-      },
-      history: {
-        push() {
-        },
-      },
-    }
-    cy.mount(<BrowserRouter><DatasetStatistics {...props} /></BrowserRouter>)
+    mountDatasetStatistics(open.datasetIdentifier)
     cy.contains(open.datasetIdentifier).should('exist')
     cy.contains(open.datasetName).should('exist')
     cy.contains('This dataset is open access, does not require an access request').should('exist')
@@ -165,19 +92,7 @@ describe('Dataset Statistics Tests', () => {
   it('Displays with no additional properties', () => {
     cy.stub(DataSet, 'searchDatasetIndex').returns(Promise.resolve([datasetTerm]))
     cy.stub(DatasetMetrics, 'getDatasetStats').returns(Promise.resolve([]))
-
-    const props = {
-      match: {
-        params: {
-          datasetIdentifier: datasetTerm.datasetIdentifier,
-        },
-      },
-      history: {
-        push() {
-        },
-      },
-    }
-    cy.mount(<BrowserRouter><DatasetStatistics {...props} /></BrowserRouter>)
+    mountDatasetStatistics(datasetTerm.datasetIdentifier)
     cy.contains(datasetTerm.datasetIdentifier).should('exist')
   })
 
@@ -190,22 +105,9 @@ describe('Dataset Statistics Tests', () => {
         dataCustodianEmail: dataCustodians,
       },
     }
-
     cy.stub(DataSet, 'searchDatasetIndex').returns(Promise.resolve([datasetWithCustodians]))
     cy.stub(DatasetMetrics, 'getDatasetStats').returns(Promise.resolve([]))
-
-    const props = {
-      match: {
-        params: {
-          datasetIdentifier: datasetTerm.datasetIdentifier,
-        },
-      },
-      history: {
-        push() {
-        },
-      },
-    }
-    cy.mount(<BrowserRouter><DatasetStatistics {...props} /></BrowserRouter>)
+    mountDatasetStatistics(datasetTerm.datasetIdentifier)
     cy.contains('Data Custodian').should('exist')
     dataCustodians.forEach((dataCustodian) => {
       cy.contains(dataCustodian).should('exist')
@@ -216,19 +118,7 @@ describe('Dataset Statistics Tests', () => {
     const open = { ...datasetTerm, accessManagement: 'open' }
     cy.stub(DataSet, 'searchDatasetIndex').returns(Promise.resolve([open]))
     cy.stub(DatasetMetrics, 'getDatasetStats').returns(Promise.resolve([]))
-
-    const props = {
-      match: {
-        params: {
-          datasetIdentifier: open.datasetIdentifier,
-        },
-      },
-      history: {
-        push() {
-        },
-      },
-    }
-    cy.mount(<BrowserRouter><DatasetStatistics {...props} /></BrowserRouter>)
+    mountDatasetStatistics(open.datasetIdentifier)
     cy.contains('Data Use').should('not.exist')
   })
 
@@ -236,19 +126,7 @@ describe('Dataset Statistics Tests', () => {
     const controlled = { ...datasetTerm, accessManagement: 'controlled' }
     cy.stub(DataSet, 'searchDatasetIndex').returns(Promise.resolve([controlled]))
     cy.stub(DatasetMetrics, 'getDatasetStats').returns(Promise.resolve([]))
-
-    const props = {
-      match: {
-        params: {
-          datasetIdentifier: datasetTerm.datasetIdentifier,
-        },
-      },
-      history: {
-        push() {
-        },
-      },
-    }
-    cy.mount(<BrowserRouter><DatasetStatistics {...props} /></BrowserRouter>)
+    mountDatasetStatistics(datasetTerm.datasetIdentifier)
     cy.contains('Data Use').should('exist')
     cy.contains(datasetTerm.dataUse.primary[0].code).should('exist')
   })
@@ -256,19 +134,7 @@ describe('Dataset Statistics Tests', () => {
   it('Displays the Principal Investigator field', () => {
     cy.stub(DataSet, 'searchDatasetIndex').returns(Promise.resolve([datasetTerm]))
     cy.stub(DatasetMetrics, 'getDatasetStats').returns(Promise.resolve([]))
-
-    const props = {
-      match: {
-        params: {
-          datasetIdentifier: datasetTerm.datasetIdentifier,
-        },
-      },
-      history: {
-        push() {
-        },
-      },
-    }
-    cy.mount(<BrowserRouter><DatasetStatistics {...props} /></BrowserRouter>)
+    mountDatasetStatistics(datasetTerm.datasetIdentifier)
     cy.contains('Principal Investigator').should('exist')
     cy.contains(datasetTerm.study.piName).should('exist')
   })
@@ -281,16 +147,9 @@ describe('Dataset Statistics Tests', () => {
       nonTechRus: 'Test summary',
       expired: false,
     }]
-
     cy.stub(DataSet, 'searchDatasetIndex').returns(Promise.resolve([datasetTerm]))
     cy.stub(DatasetMetrics, 'getDatasetStats').returns(Promise.resolve(darsData))
-
-    const props = {
-      match: { params: { datasetIdentifier: datasetTerm.datasetIdentifier } },
-      history: { push() {} },
-    }
-
-    cy.mount(<BrowserRouter><DatasetStatistics {...props} /></BrowserRouter>)
+    mountDatasetStatistics(datasetTerm.datasetIdentifier)
     cy.contains('Data Access Requests for this dataset').should('exist')
     cy.contains('DAR-123').should('exist')
     cy.contains('Test Project').should('exist')
@@ -299,22 +158,18 @@ describe('Dataset Statistics Tests', () => {
   it('Displays message when no DARs exist', () => {
     cy.stub(DataSet, 'searchDatasetIndex').returns(Promise.resolve([datasetTerm]))
     cy.stub(DatasetMetrics, 'getDatasetStats').returns(Promise.resolve([]))
-
-    const props = {
-      match: { params: { datasetIdentifier: datasetTerm.datasetIdentifier } },
-      history: { push() {} },
-    }
-
-    cy.mount(<BrowserRouter><DatasetStatistics {...props} /></BrowserRouter>)
+    mountDatasetStatistics(datasetTerm.datasetIdentifier)
     cy.contains('No Data Access Requests have been created for this dataset').should('exist')
   })
 
   it('Displays DAR section with expired data', () => {
-    const year = new Date().getFullYear()
     const expired = new Date()
-    expired.setFullYear(year - 2)
-    const dateString = expired.toISOString().split('T')[0]
+    expired.setFullYear(expired.getFullYear() - 2)
     const dateTime = expired.getTime()
+    // Build the expected date string using local-timezone methods — the same way
+    // formatDate() in the component works (getFullYear/getMonth/getDate, not toISOString).
+    const pad = n => ('0' + n).slice(-2)
+    const dateString = `${expired.getFullYear()}-${pad(expired.getMonth() + 1)}-${pad(expired.getDate())}`
 
     const darsData = [{
       darCode: 'DAR-123',
@@ -326,13 +181,7 @@ describe('Dataset Statistics Tests', () => {
 
     cy.stub(DataSet, 'searchDatasetIndex').returns(Promise.resolve([datasetTerm]))
     cy.stub(DatasetMetrics, 'getDatasetStats').returns(Promise.resolve(darsData))
-
-    const props = {
-      match: { params: { datasetIdentifier: datasetTerm.datasetIdentifier } },
-      history: { push() {} },
-    }
-
-    cy.mount(<BrowserRouter><DatasetStatistics {...props} /></BrowserRouter>)
+    mountDatasetStatistics(datasetTerm.datasetIdentifier)
     cy.contains('Data Access Requests for this dataset').should('exist')
     cy.contains('DAR-123').should('exist')
     cy.contains('Test Project').should('exist')
