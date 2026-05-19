6213ed9f7cb4599a5d84302a591359e72323516c
Florian Boulnois <fboulnoi@broadinstitute.org>
2026-05-19 17:31:59 +0000

    fix: address risk of catastrophic backtracking

--- src/utils/NodashUtil.ts
+++ src/utils/NodashUtil.ts
@@ -286,7 +286,7 @@ export const kebabCase = (str: string): string => {
   //  - between letters and numbers (Server2 -> Server 2)
   const withSpaces = s
     .replace(/([a-z0-9])([A-Z])/g, '$1 $2')
-    .replace(/([A-Z]+)([A-Z][a-z])/g, '$1 $2')
+    .replace(/([A-Z])(?=[A-Z][a-z])/g, '$1 ')
     .replace(/([a-zA-Z])([0-9])/g, '$1 $2')
     .replace(/([0-9])([a-zA-Z])/g, '$1 $2')
 
