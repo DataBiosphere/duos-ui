3796d9d71ed19083fd69b5842b61d503cb57cfee
Kevin Marete <kevinmmarete@gmail.com>
2026-06-05 17:54:16 -0400

    feat: add oracle to cloud providers

--- src/components/forms/CloudProviders.ts
+++ src/components/forms/CloudProviders.ts
@@ -4,9 +4,11 @@ export class CloudProviders {
   static GCP: SelectOptionWithKeyNameAndAbbreviation = { key: 'GCP', name: 'GCP' }
   static AWS: SelectOptionWithKeyNameAndAbbreviation = { key: 'AWS', name: 'AWS' }
   static AZURE: SelectOptionWithKeyNameAndAbbreviation = { key: 'Azure', name: 'Azure' }
+  static ORACLE: SelectOptionWithKeyNameAndAbbreviation = { key: 'Oracle', name: 'Oracle' }
   static VALUES: SelectOptionWithKeyNameAndAbbreviation[] = [
     CloudProviders.GCP,
     CloudProviders.AWS,
     CloudProviders.AZURE,
+    CloudProviders.ORACLE,
   ]
 }
