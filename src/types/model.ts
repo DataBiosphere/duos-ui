e1f00fd9800d2ba7b376b349a830b45abacc4b5b
Kevin Marete <kevinmmarete@gmail.com>
2026-06-05 17:55:26 -0400

    feat: add cloud field to models

--- src/types/model.ts
+++ src/types/model.ts
@@ -379,6 +379,7 @@ export interface AiModel {
   name: string
   description: string
   url: string
+  cloud?: string
   format: string
   license: string
   trainedOnDatasets: string[]
@@ -392,6 +393,7 @@ export interface Workspace {
   name: string
   platform: string
   url: string
+  cloud?: string
   description: string
   tools?: string[]
   access?: string
