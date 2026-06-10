b66ed707ad01740c291f348b6e9d4c371c3d4903
Kevin Marete <kevinmmarete@gmail.com>
2026-06-10 15:40:24 -0400

    feat: update tests

--- cypress/component/SignIn/SignInButton.spec.tsx
+++ cypress/component/SignIn/SignInButton.spec.tsx
@@ -61,6 +61,10 @@ describe('Sign In: Component Loads', function () {
     cy.mount(<BrowserRouter><SignInButton /></BrowserRouter>)
     cy.contains(signInText).should('exist')
     cy.get('button').should('exist').and('not.be.disabled')
+    cy.get('button')
+      .should('have.css', 'color', 'rgb(255, 255, 255)')
+      .and('have.css', 'background-color', 'rgba(0, 0, 0, 0)')
+      .and('have.css', 'border-color', 'rgb(255, 255, 255)')
   })
 
   it('Sign In: On Success', function () {
