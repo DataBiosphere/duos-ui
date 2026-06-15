be965aba4a53729cbb31d0b6929838e43d5ed825
Kevin Marete <49183352+kevinmarete@users.noreply.github.com>
2026-06-15 14:42:33 -0400

    [DT-3617] Convert `SigningOfficialLibraryCards.jsx` to Typescript (#3579)

--- src/pages/signing_official_console/SigningOfficialLibraryCards.tsx
+++ src/pages/signing_official_console/SigningOfficialLibraryCards.tsx
@@ -1,30 +1,31 @@
 import React, { useEffect, useState } from 'react'
 import { Notifications, USER_ROLES } from 'src/libs/utils'
 import { Styles } from 'src/libs/theme'
-import SigningOfficialTable from './SigningOfficialTable'
+import SigningOfficialTable from 'src/pages/signing_official_console/SigningOfficialTable'
 import { User } from 'src/libs/ajax/User'
 import { usePageTitle } from 'src/hooks/usePageTitle'
+import { DuosUser, DuosUserWithInstitutionId } from 'src/types/model'
 
-export default function SigningOfficialLibraryCards() {
+export default function SigningOfficialLibraryCards(): React.JSX.Element {
   usePageTitle('Library Cards')
-  const [signingOfficial, setSigningOfficial] = useState({})
-  const [researchers, setResearchers] = useState([])
+  const [signingOfficial, setSigningOfficial] = useState<DuosUserWithInstitutionId>()
+  const [researchers, setResearchers] = useState<DuosUser[]>([])
 
   // states to be added and used for manage researcher component
-  const [isLoading, setIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState<boolean>(true)
 
   useEffect(() => {
-    const init = async () => {
+    const init = async (): Promise<void> => {
       try {
         setIsLoading(true)
-        const soUser = await User.getMe()
+        const soUser = await User.getMe() as DuosUserWithInstitutionId
         const researcherList = await User.list(USER_ROLES.signingOfficial)
 
         setResearchers(researcherList)
         setSigningOfficial(soUser)
         setIsLoading(false)
       }
-      catch (_error) {
+      catch {
         Notifications.showError({ text: 'Error: Unable to retrieve current user from server' })
         setIsLoading(false)
       }
@@ -35,7 +36,13 @@ export default function SigningOfficialLibraryCards() {
   return (
     <div style={Styles.PAGE}>
       <div className="signing-official-tabs">
-        <SigningOfficialTable researchers={researchers} signingOfficial={signingOfficial} isLoading={isLoading} />
+        {signingOfficial && (
+          <SigningOfficialTable
+            researchers={researchers}
+            signingOfficial={signingOfficial}
+            isLoading={isLoading}
+          />
+        )}
       </div>
     </div>
   )
