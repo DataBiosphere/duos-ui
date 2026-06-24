f130288e72036b8e84bfbe668d3c1c260128d3a0
Kevin Marete <kevinmmarete@gmail.com>
2026-06-24 19:27:38 -0400

    feat: convert to typescript

--- src/components/common/VotesPieChart.tsx
+++ src/components/common/VotesPieChart.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { Chart } from 'react-google-charts'
 import { isNil, isEmpty, map } from 'src/utils/NodashUtil'
-import { useMemo } from 'react'
+import { Vote } from 'src/types/model'
 
 const pieSliceColors = {
   0: { color: '#1FA371' },
@@ -9,33 +9,46 @@ const pieSliceColors = {
   2: { color: '#979797' },
 }
 
-const processVotes = (votes) => {
+const getVoteLabel = (vote: boolean | undefined): string => {
+  if (vote) return 'Yes'
+  if (isNil(vote)) return 'Not Yet Voted'
+  return 'No'
+}
+
+const processVotes = (votes: Vote[]) => {
   const headerData = ['Vote', 'Total Votes']
-  const decisionMap = {
+  const decisionMap: Record<string, number> = {
     'Yes': 0,
     'No': 0,
     'Not Yet Voted': 0,
   }
 
   votes.forEach((v) => {
-    const value = v.vote ? 'Yes' : !isNil(v.vote) ? 'No' : 'Not Yet Voted'
-    decisionMap[value]++
+    decisionMap[getVoteLabel(v.vote)]++
   })
   const decisionDataArray = map(decisionMap, (count, key) => [key, count])
   return [headerData, ...decisionDataArray]
 }
 
-export default function VotesPieChart(props) {
-  const {
-    votes = [],
-    keyString,
-    pieHole = 0.3,
-    height = 'inherit',
-    width = '100%',
-    style = { width: '70%' },
-    styleOverride,
-  } = props
+interface VotesPieChartProps {
+  readonly votes?: Vote[]
+  readonly keyString: string
+  readonly pieHole?: number
+  readonly height?: string
+  readonly width?: string
+  readonly style?: React.CSSProperties
+  readonly styleOverride?: React.CSSProperties
+}
 
+export default function VotesPieChart({
+  votes = [],
+  keyString,
+  pieHole = 0.3,
+  height = 'inherit',
+  width = '100%',
+  style = { width: '70%' },
+  styleOverride,
+}: VotesPieChartProps) {
   const processedVotes = useMemo(() => processVotes(votes), [votes])
   const options = {
     pieHole,
