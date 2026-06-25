9fc54dcb9d3fc152c4dec59047c9fb5ef31e8d24
Kevin Marete <kevinmmarete@gmail.com>
2026-06-24 20:31:25 -0400

    feat: convert to typescript

--- test/components/collection_voting_slab/DataUseAlertBox.spec.tsx
+++ test/components/collection_voting_slab/DataUseAlertBox.spec.tsx
@@ -3,28 +3,33 @@ import '@testing-library/jest-dom/vitest'
 import { describe, it, expect } from 'vitest'
 import { render, screen } from '@testing-library/react'
 import DataUseAlertBox from 'src/components/collection_voting_slab/DataUseAlertBox'
+import { TranslationEntry } from 'src/libs/dataUseTranslation'
 
-const dataUseManualReviewTrue = {
+const dataUseManualReviewTrue: TranslationEntry = {
   code: 'ABC',
   description: 'data use 1',
   manualReview: true,
+  type: 'Permissions',
 }
 
-const dataUseManualReviewTrue2 = {
+const dataUseManualReviewTrue2: TranslationEntry = {
   code: 'DEF',
   description: 'data use 2',
   manualReview: true,
+  type: 'Permissions',
 }
 
-const dataUseManualReviewFalse = {
+const dataUseManualReviewFalse: TranslationEntry = {
   code: 'MNOP',
   description: 'data use 3',
   manualReview: false,
+  type: 'Permissions',
 }
 
-const dataUseNoManualReview = {
+const dataUseNoManualReview: TranslationEntry = {
   code: 'XYZ',
   description: 'data use 4',
+  type: 'Permissions',
 }
 
 describe('DataUseAlertBox - Tests', () => {
@@ -34,7 +39,7 @@ describe('DataUseAlertBox - Tests', () => {
         translatedDataUse={{ primary: [dataUseManualReviewTrue] }}
       />,
     )
-    expect(document.querySelector('[datacy="alert-box"]')).toBeInTheDocument()
+    expect(document.querySelector('[data-cy="alert-box"]')).toBeInTheDocument()
     expect(screen.getByText('!')).toBeInTheDocument()
   })
 
@@ -44,7 +49,7 @@ describe('DataUseAlertBox - Tests', () => {
         translatedDataUse={{ primary: [dataUseManualReviewFalse] }}
       />,
     )
-    expect(document.querySelector('[datacy="alert-box"]')).toBeNull()
+    expect(document.querySelector('[data-cy="alert-box"]')).toBeNull()
   })
 
   it('Does not render the description of a data use without a manuallyReviewed attribute', () => {
@@ -53,7 +58,7 @@ describe('DataUseAlertBox - Tests', () => {
         translatedDataUse={{ primary: [dataUseNoManualReview] }}
       />,
     )
-    expect(document.querySelector('[datacy="alert-box"]')).toBeNull()
+    expect(document.querySelector('[data-cy="alert-box"]')).toBeNull()
   })
 
   it('Renders the description of a primary use manually reviewed data use', () => {
@@ -62,7 +67,7 @@ describe('DataUseAlertBox - Tests', () => {
         translatedDataUse={{ primary: [dataUseManualReviewTrue], secondary: [dataUseManualReviewFalse] }}
       />,
     )
-    expect(document.querySelector('[datacy="alert-box"]')).toBeInTheDocument()
+    expect(document.querySelector('[data-cy="alert-box"]')).toBeInTheDocument()
     expect(screen.getByText('data use 1')).toBeInTheDocument()
     expect(screen.queryByText('data use 3')).toBeNull()
   })
@@ -73,7 +78,7 @@ describe('DataUseAlertBox - Tests', () => {
         translatedDataUse={{ primary: [dataUseManualReviewFalse], secondary: [dataUseManualReviewTrue] }}
       />,
     )
-    expect(document.querySelector('[datacy="alert-box"]')).toBeInTheDocument()
+    expect(document.querySelector('[data-cy="alert-box"]')).toBeInTheDocument()
     expect(screen.getByText('data use 1')).toBeInTheDocument()
     expect(screen.queryByText('data use 3')).toBeNull()
   })
@@ -84,7 +89,7 @@ describe('DataUseAlertBox - Tests', () => {
         translatedDataUse={{ Primary: [dataUseManualReviewTrue, dataUseManualReviewTrue2] }}
       />,
     )
-    expect(document.querySelector('[datacy="alert-box"]')).toBeInTheDocument()
+    expect(document.querySelector('[data-cy="alert-box"]')).toBeInTheDocument()
     expect(screen.getByText('data use 1')).toBeInTheDocument()
     expect(screen.getByText('data use 2')).toBeInTheDocument()
   })
@@ -95,7 +100,7 @@ describe('DataUseAlertBox - Tests', () => {
         translatedDataUse={{ primary: [dataUseManualReviewTrue2], secondary: [dataUseManualReviewTrue] }}
       />,
     )
-    expect(document.querySelector('[datacy="alert-box"]')).toBeInTheDocument()
+    expect(document.querySelector('[data-cy="alert-box"]')).toBeInTheDocument()
     expect(screen.getByText('data use 1')).toBeInTheDocument()
     expect(screen.getByText('data use 2')).toBeInTheDocument()
   })
