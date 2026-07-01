4de0beed9f2579ee3f92170e8920a4f4c2dec4a6
Greg <grushton@broadinstitute.org>
2026-07-01 12:53:17 -0400

    test: fix broken tests

--- test/pages/BackgroundSignIn.spec.tsx
+++ test/pages/BackgroundSignIn.spec.tsx
@@ -44,8 +44,8 @@ vi.mock('src/libs/utils', async (importActual) => {
 vi.mock('src/libs/auth/oidcBroker', () => ({}))
 
 vi.mock('src/components/SpinnerComponent', () => ({
-  SpinnerComponent: ({ show, name }: { show: boolean, name: string }) =>
-    React.createElement('div', { 'data-testid': 'spinner', 'data-show': String(show), 'data-name': name }),
+  SpinnerComponent: ({ loadingImage }: { loadingImage?: string }) =>
+    React.createElement('div', { 'data-testid': 'spinner', 'data-loading-image': loadingImage }),
 }))
 
 vi.mock('src/images/loading-indicator.svg', () => ({ default: 'loading.svg' }))
@@ -95,7 +95,7 @@ describe('BackgroundSignIn', () => {
     vi.mocked(User.getMe).mockReturnValue(new Promise(() => {}))
     renderComponent({ bearerToken: 'my-token' })
     expect(screen.getByTestId('spinner')).toBeInTheDocument()
-    expect(screen.getByTestId('spinner')).toHaveAttribute('data-show', 'true')
+    expect(screen.getByTestId('spinner')).toHaveAttribute('data-loading-image', 'loading.svg')
   })
 
   it('calls User.getMe and Storage.setOidcUser when bearerToken is provided', async () => {
