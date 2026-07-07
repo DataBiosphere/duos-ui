71a3fab32856efd49bf9ae4e43e2e3ff90a0fd19
Kevin Marete <kevinmmarete@gmail.com>
2026-07-07 14:01:05 -0400

    feat: add columnTestUtils

---
+++ test/components/data_library/columns/columnTestUtils.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
+import { GridColDef, GridRenderCellParams, GridValidRowModel } from '@mui/x-data-grid'
+
+export const makeMockParams = <T extends GridValidRowModel>(value: unknown, row: T): GridRenderCellParams<T> =>
+  ({ value, row, id: 1, field: '', formattedValue: value, colDef: {}, api: {} } as unknown as GridRenderCellParams<T>)
+
+export const makeRenderCellHelper = <T extends GridValidRowModel>(
+  makeColumns: () => GridColDef[],
+  makeRow: (overrides?: Partial<T>) => T,
+) =>
+  (field: string, value: unknown, rowOverrides: Partial<T> = {}) => {
+    const col = makeColumns().find(c => c.field === field)!
+    return render(
+      <MemoryRouter>
+        {col.renderCell!(makeMockParams(value, makeRow(rowOverrides))) as React.ReactElement}
+      </MemoryRouter>,
+    )
+  }
