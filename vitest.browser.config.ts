8bc9fadf4bf22f1df7e8611206efe1db8e5c512a
Kevin Marete <kevinmmarete@gmail.com>
2026-07-09 16:16:42 -0400

    feat: add vitest browser and playwright packages and config

--- vitest.browser.config.ts
+++ vitest.browser.config.ts
@@ -1,4 +1,5 @@
 import react from '@vitejs/plugin-react'
+import { playwright } from '@vitest/browser-playwright'
 import { defineConfig } from 'vitest/config'
 import { aliases_from_tsconfig } from './aliases'
 
@@ -11,14 +12,14 @@ export default defineConfig({
     alias: aliases_from_tsconfig(),
   },
   test: {
-    css: true,
+    name: 'browser',
     globals: true,
-    environment: 'jsdom',
-    include: ['test/**/*.{spec,test}.{js,jsx,ts,tsx}'],
-    exclude: ['test/browser/**', 'build/**', 'cypress/**', 'node_modules/**', 'server/**'],
-    coverage: {
-      provider: 'v8',
-      reporter: ['json-summary', 'json'],
+    include: ['test/browser/**/*.{spec,test}.{js,jsx,ts,tsx}'],
+    browser: {
+      enabled: true,
+      headless: true,
+      provider: playwright(),
+      instances: [{ browser: 'chromium' }],
     },
   },
 })
