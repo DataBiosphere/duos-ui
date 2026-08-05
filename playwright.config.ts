adbd91181755f2e900efe956486702287e5129a8
Gregory Rushton <rushtong@users.noreply.github.com>
2026-08-05 16:05:40 -0400

    [DT-3932] Improve unit and Playwright test stability (#3815)

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,14 +3,20 @@ import { BASE_URL } from './test/e2e/support/baseUrl'
 
 export default defineConfig({
   testDir: 'test/e2e',
+  timeout: 30_000,
+  expect: {
+    timeout: 10_000,
+  },
   use: {
     baseURL: BASE_URL,
     ignoreHTTPSErrors: true,
+    navigationTimeout: 30_000,
   },
   projects: [
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
+      retries: process.env.CI ? 1 : 0,
     },
   ],
   webServer: {
