2eb023cb890bdbf37803445b0c0ad2e60f05b427
Elliot Otchet <otchet@broadinstitute.org>
2026-08-21 17:36:30 +0000

    Update to MUI Alert/Banner.

--- src/pages/user_profile/SigningOfficialRequest.tsx
+++ src/pages/user_profile/SigningOfficialRequest.tsx
@@ -1,5 +1,5 @@
-import InfoIcon from '@mui/icons-material/Info'
 import React, { useState } from 'react'
+import Alert from '@mui/material/Alert'
 import { Support } from 'src/libs/ajax/Support'
 import { User } from 'src/libs/ajax/User'
 import { Notifications } from 'src/libs/utils'
@@ -63,15 +63,12 @@ export default function SigningOfficialRequest({ user }: SigningOfficialRequestP
   return (
     <section className="signing-official-request" aria-labelledby="signing-official-request-title">
       <h2 id="signing-official-request-title">Request Signing Official Status</h2>
-      <div className="signing-official-request-notice">
-        <InfoIcon fontSize="large" color="info" />
-        <div>
-          You cannot be both the requestor and the Signing Official on the same Data Access Request.
-          The Signing Official is typically a member of your institution&apos;s Contracts Office, Office
-          of Sponsored Programs, or Legal/General Counsel &mdash; not the researcher submitting the request.
-          Only request this status if that describes your role.
-        </div>
-      </div>
+      <Alert severity="info" sx={{ mb: 2, fontStyle: 'italic' }}>
+        You cannot be both the requestor and the Signing Official on the same Data Access Request.
+        The Signing Official is typically a member of your institution&apos;s Contracts Office, Office
+        of Sponsored Programs, or Legal/General Counsel &mdash; not the researcher submitting the request.
+        Only request this status if that describes your role.
+      </Alert>
       <p>
         I legally attest that I am a Signing Official for the above listed institution, and have the authority to engage my institution in contracts related to data access and submission.
       </p>
